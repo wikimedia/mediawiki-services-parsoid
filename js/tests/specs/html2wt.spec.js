c0877186b0423658cc4c2bcd4a390d2ce043d0c3
Subramanya Sastry <ssastry@wikimedia.org>
2012-08-01 16:14:37 -0500

    Additional html2wt tests for testing wikitext escaping.
    Change-Id: I3bc2997c92230f7779569d3b09dc5fbd552cc477

--- js/tests/specs/html2wt.spec.js
+++ js/tests/specs/html2wt.spec.js
@@ -15,32 +15,59 @@ function wikitext(dom) {
 	return out.join('');
 }
 
+function char_sequence(c, n) {
+	var buf = [];
+	for (var i = 0; i < n; i++) buf.push(c);
+	return buf.join('');
+}
+
 // Heading specs
 describe("Headings", function() {
 	// Regular non-empty specs
 	it("should be serialized properly when non-empty", function() {
-		wikitext(dom("<h1>foo</h1>")).should.equal("=foo=");
-		wikitext(dom("<h2>foo</h2>")).should.equal("==foo==");
-		wikitext(dom("<h3>foo</h3>")).should.equal("===foo===");
-		wikitext(dom("<h4>foo</h4>")).should.equal("====foo====");
-		wikitext(dom("<h5>foo</h5>")).should.equal("=====foo=====");
-		wikitext(dom("<h6>foo</h6>")).should.equal("======foo======");
+		// wikitext(dom("<h1>foo</h1>")).should.equal("=foo=");
+		// Repeat pattern from h1 .. h6
+		for (var i = 1; i <= 6; i++) {
+			var str  = char_sequence("=", i);
+			var tag  = "h" + i;
+			var html = "<" + tag + ">foo</" + tag + ">";
+			var res  = str + "foo" + str;
+			wikitext(dom(html)).should.equal(res);
+		}
 	});
 
 	// Empty heading specs
 	it("should be serialized properly when empty", function() {
-		wikitext(dom("<h1></h1>")).should.equal("=<nowiki></nowiki>=");
-		wikitext(dom("<h2></h2>")).should.equal("==<nowiki></nowiki>==");
-		wikitext(dom("<h3></h3>")).should.equal("===<nowiki></nowiki>===");
-		wikitext(dom("<h4></h4>")).should.equal("====<nowiki></nowiki>====");
-		wikitext(dom("<h5></h5>")).should.equal("=====<nowiki></nowiki>=====");
-		wikitext(dom("<h6></h6>")).should.equal("======<nowiki></nowiki>======");
+		// wikitext(dom("<h1></h1>")).should.equal("=<nowiki></nowiki>=");
+		// Repeat pattern from h1 .. h6
+		for (var i = 1; i <= 6; i++) {
+			var str  = char_sequence("=", i);
+			var tag  = "h" + i;
+			var html = "<" + tag + "></" + tag + ">";
+			var res  = str + "<nowiki></nowiki>" + str;
+			wikitext(dom(html)).should.equal(res);
+		}
 	});
 
 	// Escape specs
 	it("should escape wikitext properly", function() {
-		wikitext(dom("<h1>=foo=</h1>")).should.equal("=<nowiki>=</nowiki>foo<nowiki>=</nowiki>=");
-		wikitext(dom("<h3>=foo=</h3>")).should.equal("===<nowiki>=</nowiki>foo<nowiki>=</nowiki>===");
+		wikitext(dom("=foo=")).should.equal("<nowiki>=</nowiki>foo<nowiki>=</nowiki>");
+
+		// wikitext(dom("<h1>=foo=</h1>")).should.equal("=<nowiki>=</nowiki>foo<nowiki>=</nowiki>=");
+		// Repeat pattern from h1 .. h5
+		for (var i = 1; i <= 5; i++) {
+			var str  = char_sequence("=", i);
+			var tag  = "h" + i;
+			var html = "<" + tag + ">=foo=</" + tag + ">";
+			var res  = str + "<nowiki>=</nowiki>foo<nowiki>=</nowiki>" + str;
+			wikitext(dom(html)).should.equal(res);
+		}
+	});
+
+	// Are we supporting this HTML at this time?
+	it ("should escape wikitext after the closing tag if on the same line", function() {
+		wikitext(dom("<h1>foo</h1>*bar")).should.equal("=foo=\n<nowiki>*</nowiki>bar");
+		wikitext(dom("<h1>foo</h1>=bar=")).should.equal("=foo=\n<nowiki>=</nowiki>bar<nowiki>=</nowiki>");
 	});
 });
 
@@ -48,6 +75,36 @@ describe("Headings", function() {
 describe("Lists", function() {
 	// Escape specs
 	it("should escape wikitext properly", function() {
-		wikitext(dom("<ul><li>*foo</li></ul>")).should.equal("*<nowiki>*</nowiki>foo");
+		var liChars = ["*", "#", ":", ";"];
+		for (var i = 0; i < liChars.length; i++) {
+			var c    = liChars[i];
+			var html = "<ul><li>" + c + "foo</li></ul>";
+			var res  = "*<nowiki>" + c + "</nowiki>foo";
+			wikitext(dom(html)).should.equal(res);
+		}
+
+		for (var i = 0; i < liChars.length; i++) {
+			var c    = liChars[i];
+			var html = "<ol><li>" + c + "foo</li></ol>";
+			var res  = "#<nowiki>" + c + "</nowiki>foo";
+			wikitext(dom(html)).should.equal(res);
+		}
+	});
+});
+
+// HR specs
+describe("<hr>", function() {
+	// Escape specs
+	it("should escape wikitext properly", function() {
+		wikitext(dom("<hr/>\n----")).should.equal("----\n<nowiki>----</nowiki>");
+		wikitext(dom("<hr/>\n=foo=")).should.equal("----\n<nowiki>=</nowiki>foo<nowiki>=</nowiki>");
+		wikitext(dom("<hr/>\n*foo")).should.equal("----\n<nowiki>*</nowiki>foo");
+	});
+
+	// Are we supporting this HTML at this time?
+	it ("should escape wikitext after the closing tag if on the same line", function() {
+		wikitext(dom("<hr/>----")).should.equal("----\n<nowiki>----</nowiki>");
+		wikitext(dom("<hr/>=foo=")).should.equal("----\n<nowiki>=</nowiki>foo<nowiki>=</nowiki>");
+		wikitext(dom("<hr/>*foo")).should.equal("----\n<nowiki>*</nowiki>foo");
 	});
 });
