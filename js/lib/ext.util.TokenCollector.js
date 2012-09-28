c212c48c05006e609c2bb9367504b8e81baa6252
Subramanya Sastry <ssastry@wikimedia.org>
2012-09-28 01:11:22 -0500

    Added support for nested tags in token collector.
    * Template:refn exists solely to support nested <ref> tags (see doc)
      Parsing and RTing this properly required support for collecting
      nested <ref> tags in the token collector which has been added.
    
    * No change in parserTests failures.
    
    * With these fixes, we now have:
      - :en:Buddha RT-ing very cleanly (no template erros)
      - :en:Advaita_Vedanta doesn't crash anymore (but 90% of
         output is swallowed becuase of the bug below).
    
    * This fix exposed a tokenizer bug because of a bad extlink
      in a refn template use.  A simpified example of this is:
    
         {{refn|<ref>[http://en.wikipedia.org | foo]</ref>}}
    
      In this example, the "|" char throws off the tokenizer and
      it doesn't recognize the ext-link and ends up swallowing
      a </ref> tag which then leads the entire page to be collected
      as a ref.
    
    Change-Id: Ibcee40d0e8bdb6cfee2444570aa77bd74f84b854

--- js/lib/ext.util.TokenCollector.js
+++ js/lib/ext.util.TokenCollector.js
@@ -6,10 +6,9 @@
  * would wreak havoc with this kind of collector.
  *
  * @author Gabriel Wicke <gwicke@wikimedia.org>
+ * @author Subramanya Sastry <ssastry@wikimedia.org>
  *
  * Calls the passed-in callback with the collected tokens.
- *
- * XXX: optionally support nested delimiters using a stack?
  */
 
 /**
@@ -36,11 +35,10 @@ function TokenCollector ( manager, transformation, toEnd, rank, type, name ) {
 	this.type = type;
 	this.name = name;
 	this.toEnd = toEnd;
-	this.tokens = [];
-	this.isActive = false;
+	this.scopeStack = [];
 	manager.addTransform( this._onDelimiterToken.bind( this ), "TokenCollector:_onDelimiterToken", rank, type, name );
 }
-		
+
 /**
  * Register any collector with slightly lower priority than the start/end token type
  * XXX: This feels a bit hackish, a list-of-registrations per rank might be
@@ -54,58 +52,63 @@ TokenCollector.prototype._anyDelta = 0.00001;
  * XXX: Adjust to sync phase callback when that is modified!
  */
 TokenCollector.prototype._onDelimiterToken = function ( token, frame, cb ) {
-	if ( token.constructor === SelfclosingTagTk && !this.isActive ) {
-		this.manager.env.dp( 'skipping collection on ', token );
-		// just ignore it
-		return { tokens: [ token ] }; //this.transformation( [token, token] );
-	} else if ( this.isActive &&
-			( token.constructor === EndTagTk || token.constructor === EOFTk ) ) {
-		this.manager.env.dp( 'finishing collection on ', token );
-		var res;
-		// finish processing
-		this.tokens.push ( token );
-		this.isActive = false;
-		this.manager.removeTransform( this.rank + this._anyDelta, 'any' );
-		this.manager.removeTransform( this.rank, 'end' );
-		if ( token.constructor !== EOFTk || this.toEnd ) {
-			// end token
-			res = this.transformation ( this.tokens );
-			this.tokens = [];
-			// Transformation can be either sync or async, but receives all collected
-			// tokens instead of a single token.
-			return res;
-			// XXX sync version: return tokens
-		} else {
-			// just return collected tokens
-			res = this.tokens;
-			this.tokens = [];
-			return { tokens: res };
-		}
-	} else if ( token.constructor !== EOFTk ) {
-
-		if ( this.isActive ) {
-			this.manager.env.dp( 'already active: ', token );
-		} else {
-			// start collection
+	var haveOpenTag = this.scopeStack.length > 0;
+	var tc = token.constructor;
+	if (tc === TagTk) {
+		if (this.scopeStack.length === 0) {
+			// Set up transforms
 			this.manager.env.dp( 'starting collection on ', token );
 			this.manager.addTransform( this._onAnyToken.bind ( this ),"TokenCollector:_onAnyToken",
 					this.rank + this._anyDelta, 'any' );
 			this.manager.addTransform( this._onDelimiterToken.bind( this ),"TokenCollector:_onDelimiterToken:end",
 					this.rank, 'end' );
-			this.isActive = true;
 		}
-		this.tokens.push ( token );
-		// Did not encounter a matching end token before the end, and are not
-		// supposed to collect to the end. So just return the tokens verbatim.
+
+		// Push a new scope
+		var newScope = [];
+		this.scopeStack.push(newScope);
+		newScope.push(token);
+
 		return { };
+	} else if (tc === SelfclosingTagTk) {
+		// Nothing to do -- just pass it along.
+		this.manager.env.dp( 'skipping collection on ', token );
+		return { tokens: [ token ] }; //this.transformation( [token, token] );
+	} else if (haveOpenTag) {
+		// EOFTk or EndTagTk
+		this.manager.env.dp( 'finishing collection on ', token );
+
+		// Pop top scope and push token onto it
+		var activeTokens = this.scopeStack.pop();
+		activeTokens.push(token);
+
+		// clean up
+		if (this.scopeStack.length === 0 || token.constructor === EOFTk) {
+			this.manager.removeTransform( this.rank + this._anyDelta, 'any' );
+			this.manager.removeTransform( this.rank, 'end' );
+		}
+
+		if (tc === EndTagTk) {
+			// Transformation can be either sync or async, but receives all collected
+			// tokens instead of a single token.
+			return this.transformation (activeTokens);
+			// XXX sync version: return tokens
+		} else {
+			// EOF -- collapse stack!
+			var allToks = [];
+			for (var i = 0, n = this.scopeStack.length; i < n; i++) {
+				allToks = allToks.concat(this.scopeStack[i]);
+			}
+			allToks = allToks.concat(activeTokens);
+
+			return this.toEnd ? this.transformation(allToks) : { tokens: allToks };
+		}
 	} else {
-		// pass through end token
-		this.tokens = [];
-		this.isActive = false;
-		return { tokens: [ token ] };
+		// EOFTk or  EndTagTk
+		// EOFTk is okay, but EndTagTk is an error (ignoring error!)
+		return { tokens: [ token ] }; //this.transformation( [token, token] );
 	}
 };
-
 
 /**
  * Handle 'any' token in between delimiter tokens. Activated when
@@ -114,7 +117,7 @@ TokenCollector.prototype._onDelimiterToken = function ( token, frame, cb ) {
  */
 TokenCollector.prototype._onAnyToken = function ( token, frame, cb ) {
 	// Simply collect anything ordinary in between
-	this.tokens.push( token );
+	this.scopeStack[this.scopeStack.length-1].push( token );
 	return { };
 };
 
