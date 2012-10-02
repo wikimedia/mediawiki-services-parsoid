5599c0320da2e0ca0cd637f53d09c84eda9dd3b3
Gabriel Wicke <gwicke@wikimedia.org>
2012-10-02 16:43:34 -0700

    Don't inherit node env vars
    Change-Id: Ib0789ff0f27fbefb7aaba2d3981146fc7a79d6a0

--- js/api/testGerritChange.sh
+++ js/api/testGerritChange.sh
@@ -5,7 +5,7 @@ set -e
 
 cd testing-repos
 BASEPATH=`pwd`
-NODECMD=` ( nodejs --version > /dev/null 2>&1 && echo 'nodejs' ) || echo 'node' `
+NODECMD="env -i ` ( nodejs --version > /dev/null 2>&1 && echo 'nodejs' ) || echo 'node' `"
 OURPATH=`mktemp -d --tmpdir=$BASEPATH`
 
 git clone -q master $OURPATH
@@ -15,12 +15,8 @@ git checkout -q FETCH_HEAD
 cd js
 ln -s $BASEPATH/testing-repos/master/js/node_modules
 
-# Maybe don't need to run these. Maybe do.
-OLDNWI=$NODE_WORKER_ID
-NODE_WORKER_ID=""
 cd tests
 wget "https://gerrit.wikimedia.org/r/gitweb?p=mediawiki/core.git;a=blob_plain;hb=HEAD;f=tests/parser/parserTests.txt" -O parserTests.txt
 $NODECMD parserTests.js --wt2wt --wt2html --html2wt --html2html --xml
-NODE_WORKER_ID=$OLDNWI
 
 rm -rf $OURPATH
