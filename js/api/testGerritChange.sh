1ef022e94ed3af0639016a452d3ce1db1d872d61
Mark Holmquist <mtraceur@member.fsf.org>
2012-10-02 16:09:17 -0700

    Fixing some issues with the server-start script
    Change-Id: I2c981dbc1fa2dbc7597767dec9bcff1feb5430d8

--- js/api/testGerritChange.sh
+++ js/api/testGerritChange.sh
@@ -2,15 +2,14 @@
 # Wrapper for testing Gerrit changes. Will take some doing.
 
 set -e
-set -u
-
-BASEPATH=`pwd`
-NODECMD=` ( nodejs --version > /dev/null 2>&1 && echo 'nodejs' ) || echo 'node' `
 
 cd testing-repos
-mkdir -p $1
-git clone -q master $1
-cd $1
+BASEPATH=`pwd`
+NODECMD=` ( nodejs --version > /dev/null 2>&1 && echo 'nodejs' ) || echo 'node' `
+OURPATH=`mktemp -d --tmpdir=$BASEPATH`
+
+git clone -q master $OURPATH
+cd $OURPATH
 git fetch -q https://gerrit.wikimedia.org/r/mediawiki/extensions/Parsoid $1
 git checkout -q FETCH_HEAD
 cd js
@@ -20,8 +19,8 @@ ln -s $BASEPATH/testing-repos/master/js/node_modules
 OLDNWI=$NODE_WORKER_ID
 NODE_WORKER_ID=""
 cd tests
-$NODECMD fetch-parserTests.txt.js 2>&1 > /dev/null
+wget "https://gerrit.wikimedia.org/r/gitweb?p=mediawiki/core.git;a=blob_plain;hb=HEAD;f=tests/parser/parserTests.txt" -O parserTests.txt
 $NODECMD parserTests.js --wt2wt --wt2html --html2wt --html2html --xml
 NODE_WORKER_ID=$OLDNWI
 
-rm -rf $BASEPATH/testing-repos/$1
+rm -rf $OURPATH
