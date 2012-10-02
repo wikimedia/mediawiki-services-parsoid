e5e4882f5c123e1046d3d123446f89df36421427
Subramanya Sastry <ssastry@wikimedia.org>
2012-10-02 12:04:36 -0500

    Handle indent-pre with a token stream transformer.
    * Fixed bug in SyncTokenTransformManager which was ignoring
      the rank set on the returned chunk.
    * Removed <pre> ... </pre> tokens from the tokenizer.
    * Updated the pre-handler to use newlines and detect spaces
      in sol position to insert <pre> .. </pre> in the right
      places based on the expanded stream.
      - The new handler is based on a 4-state FSM which makes
        it more robust and maintainable.
    * More fixes to come:
      - fixes to serializer (comments & pre interaction broken).
      - fixes to peg tokenizer to expose white space
        and newlines in significant places.
      - possible fixes to pre-handler based on tokenizer fixes.
      - more parser tests.
    
    Results on parser tests:
    - Improved parser tests coverage (+21 on default run)
      +2 wt2html, +1 wt2wt, +18 html2html
    
    Results on :en:Anna_Hazare:
    - " {{cite|title=foo}}" now RTs correctly which was
      the reduced test case on the Anna_Hazare page that
      triggered this entire rewrite.
    - Anna_Hazare page RT's much more cleanly.
    
    Results on :en:Barack_Obama page:
    - No RT diffs.
    - ~25% smaller html output because a lot span-wrapping of
      whitespace is gone (2.6 mb --> 2.0 mb)
    - ~8% faster to parse (52 sec --> 48 sec)
    - Parser output diffs exist.
      - several references are pre-wrapped whereas they
        shouldn't be (I might be missing some check)
    
    Change-Id: I93596ed89122dbb142cfe702248f6bf94d00a920

--- js/lib/ext.core.PreHandler.js
+++ js/lib/ext.core.PreHandler.js
@@ -1,106 +1,212 @@
-/*
- * FIXME: As per Gabriel's suggestion, a better solution would be
- * to replace the pre_indent production in the tokenizer and
- * handle it completely in the token stream transformer when more
- * complete info is available.  This gets rid of incorrect handling
- * in the tokenizer and fixup later on.
- *
- * So, PreHandler would register for the newline tokens instead of
- * pre tokens, and go from there.
- */
-
 var Util = require('./mediawiki.Util.js').Util;
 
+// Constructor
 function PreHandler( manager, options ) {
 	this.manager = manager;
-	this.manager.addTransform(this.onPre.bind( this ), "PreHandler:onPre", this.rank, 'tag', 'pre');
-	this.collecting = false;
+	this.manager.addTransform(this.onNewline.bind(this),
+		"PreHandler:onNewline", this.nlRank, 'newline');
+	this.manager.addTransform(this.onEnd.bind(this),
+		"PreHandler:onEnd", this.endRank, 'end');
+	init(this, true);
 }
 
-PreHandler.prototype.rank = 3.00;
-PreHandler.prototype.anyRank = 3.01;
+// Handler ranks
+PreHandler.prototype.nlRank  = 2.01;
+PreHandler.prototype.anyRank = 2.02;
+PreHandler.prototype.endRank = 2.03;
 
-PreHandler.prototype.onPre = function ( token, manager, cb ) {
-	if (token.constructor === TagTk) {
-		if (!token.isHTMLTag()) {
-			this.tokens = [token];
-			this.collecting = true;
-			this.manager.addTransform(this.onAny.bind( this ), "PreHandler:onAny", this.anyRank, 'any');
-			return { tokens: null };
-		} else {
-			return { tokens: [token] };
-		}
-	} else if (this.collecting) {
-		this.collecting = false;
-		this.manager.removeTransform( this.anyRank, 'any' );
+// FSM states
+PreHandler.STATE_SOL     = 1;
+PreHandler.STATE_PRE     = 2;
+PreHandler.STATE_COLLECT = 3;
+PreHandler.STATE_IGNORE  = 4;
 
-		var strip = true;
-
-		// Check if we need to strip out the <pre> tags
-		// Skip the leading <pre> tag
-		for (var i = 1, l = this.tokens.length; i < l; i++) {
-			var t = this.tokens[i];
-			var tc = t.constructor;
-			if (tc === NlTk) {
-				break;
-			} else if (tc === String) {
-				// If we encounter a new line without text chars, we are done
-				if (t.match(/^\s?\n/)) {
-					break;
-				}
-				// If we have non-space chars, no stripping!
-				if (t.match(/[^\s]/)) {
-					strip = false;
-					break;
-				}
-			} else if (t.isHTMLTag() && Util.isBlockTag(t.name)) {
-				// Done -- a block tag starts a new line
-				break;
-			} else if (tc !== CommentTk && (tc !== SelfclosingTagTk || t.name !== 'meta')) {
-				// Non-meta tag.  No stripping
-				strip = false;
-				break;
-			}
-		}
-
-		if (strip) {
-			this.tokens.shift();
-			// SSS FIXME: Worth creating a white-space token so that
-			// the token can be discarded from certain contexts like
-			// tables (right now, these get fostered out of the table
-			// and become significant ws instead of non-significant ws.)
-			//
-			// <table>
-			//   <tr>
-			//     <td> foo </td>
-			//   </td>
-			// </table>
-			//
-			// This is probably the reason for ton of white-space in the
-			// beginning of parsed output of pages like :en:Barack_Obama
-			//
-			// Insert a placeholder span with a single space
-			// so we dont lose the leading space in RT-ing
-			this.tokens.unshift(new EndTagTk('span'));
-			this.tokens.unshift(' ');
-			this.tokens.unshift(new TagTk('span', [{k: 'typeof', v: 'mw:Placeholder'}], { src: ' '}));
-			return { tokens: this.tokens };
-		} else {
-			this.tokens.push(token);
-			return { tokens: this.tokens };
-		}
-	} else {
-		return { tokens: [token] };
+function init(handler, addAnyHandler) {
+	handler.lastNLTk = null;
+	handler.onlyWS = true;
+	handler.tokens = [];
+	handler.state  = PreHandler.STATE_SOL;
+	if (addAnyHandler) {
+		handler.manager.addTransform(handler.onAny.bind(handler),
+			"PreHandler:onAny", handler.anyRank, 'any');
 	}
+}
+
+function isSolTransparent(token) {
+	var tc = token.constructor;
+	if (tc === String) {
+		if (token.match(/[^\s]/)) {
+			return false;
+		}
+	} else if (tc !== CommentTk && (tc !== SelfclosingTagTk || token.name !== 'meta')) {
+		return false;
+	}
+
+	return true;
+}
+
+PreHandler.prototype.moveToIgnoreState = function() {
+	this.state = PreHandler.STATE_IGNORE;
+	this.manager.removeTransform(this.anyRank, 'any');
+};
+
+PreHandler.prototype.popLastNL = function(ret) {
+	if (this.lastNlTk) {
+		ret.push(this.lastNlTk);
+		this.lastNlTk = null;
+	}
+}
+
+PreHandler.prototype.getResultAndReset = function(token) {
+	this.popLastNL(this.tokens);
+
+	var ret = this.tokens;
+	ret.push(token);
+	this.tokens = [];
+
+	ret.rank = this.anyRank + 0.03; // prevent them from being processed again
+	return ret;
+};
+
+PreHandler.prototype.processPre = function(token) {
+	var ret;
+	if (this.onlyWS) {
+		ret = this.tokens.length > 0 ? [' '].concat(this.tokens) : this.tokens;
+	} else {
+		ret = [ new TagTk('pre') ].concat(this.tokens);
+		ret.push(new EndTagTk('pre'));
+	}
+
+	// push the last new line and the current token
+	this.popLastNL(ret);
+	ret.push(token);
+
+	// reset!
+	this.onlyWS = true;
+	this.tokens = [];
+
+	ret.rank = this.anyRank + 0.03; // prevent them from being processed again
+	return ret;
+};
+
+PreHandler.prototype.onNewline = function (token, manager, cb) {
+/*
+	console.warn("----------");
+	console.warn("ST: " + this.state + "; NL: " + JSON.stringify(token));
+*/
+
+	var ret = null;
+	switch (this.state) {
+		case PreHandler.STATE_PRE:
+			this.state = PreHandler.STATE_COLLECT;
+			this.lastNlTk = token;
+			break;
+
+		case PreHandler.STATE_IGNORE:
+			ret = this.getResultAndReset(token);
+			init(this, true); // Reset!
+			break;
+
+		case PreHandler.STATE_SOL:
+			ret = this.getResultAndReset(token);
+			break;
+
+		case PreHandler.STATE_COLLECT:
+			ret = this.processPre(token);
+			break;
+	}
+
+/*
+	console.warn("TOKS: " + JSON.stringify(this.tokens));
+	console.warn("RET:  " + JSON.stringify(ret));
+*/
+	return { tokens: ret };
+};
+
+PreHandler.prototype.onEnd = function (token, manager, cb) {
+	if (this.state !== PreHandler.STATE_IGNORE) {
+		console.error("!ERROR! Not IGNORE! Cannot get here: " + this.state + "; " + JSON.stringify(token));
+		init(this, false);
+		return {tokens: [token]};
+	}
+
+	init(this, true);
+	return {tokens: [token]};
 };
 
 PreHandler.prototype.onAny = function ( token, manager, cb ) {
-	if (this.collecting) {
-		this.tokens.push(token);
-		return { tokens: null };
-	} else {
-		return { tokens: [token] };
+/*
+	console.warn("----------");
+	console.warn("ST: " + this.state + "; T: " + JSON.stringify(token));
+*/
+
+	if (this.state === PreHandler.STATE_IGNORE) {
+		console.error("!ERROR! IGNORE! Cannot get here: " + JSON.stringify(token));
+		return {tokens: null};
 	}
+
+	var ret = null;
+	var tc = token.constructor;
+	if (tc === EOFTk) {
+		if (this.state === PreHandler.STATE_SOL) {
+			ret = this.getResultAndReset(token);
+		} else {
+			ret = this.processPre(token);
+		}
+
+		// reset for next use of this pipeline!
+		init(this, false);
+	} else {
+		switch (this.state) {
+			case PreHandler.STATE_SOL:
+				if ((tc === String) && token.match(/^\s/)) {
+					ret = this.tokens;
+					this.tokens = [];
+					this.state = PreHandler.STATE_PRE;
+				} else if (isSolTransparent(token)) { // continue watching
+					this.tokens.push(token);
+				} else {
+					ret = this.getResultAndReset(token);
+					this.moveToIgnoreState();
+				}
+				break;
+
+			case PreHandler.STATE_COLLECT:
+				if ((tc === String) && token.match(/^\s/)) {
+					this.popLastNL(this.tokens);
+					this.state = PreHandler.STATE_PRE;
+					// SSS FIXME: white-space token is lost and won't be RT-ed.
+					// Ex: " a\n<!--a-->\nc" VS " a\n <!--a-->\nc"
+				} else if (isSolTransparent(token)) { // continue watching
+					this.popLastNL(this.tokens);
+					this.tokens.push(token);
+				} else {
+					ret = this.processPre(token);
+					this.moveToIgnoreState();
+				}
+				break;
+
+			case PreHandler.STATE_PRE:
+				if (token.isHTMLTag() && Util.isBlockTag(token.name)) {
+					ret = this.processPre(token);
+					this.moveToIgnoreState();
+				} else {
+					// nothing to do .. keep collecting!
+					if (!isSolTransparent(token)) {
+						this.onlyWS = false;
+					}
+					this.tokens.push(token);
+				}
+				break;
+		}
+	}
+
+/*
+	console.warn("TOKS: " + JSON.stringify(this.tokens));
+	console.warn("RET:  " + JSON.stringify(ret));
+*/
+
+	return { tokens: ret };
 };
 
 if (typeof module === "object") {
