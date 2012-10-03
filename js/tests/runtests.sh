4a821c458464c1f59cc2ff988df75888ccd96c72
Antoine Musso <hashar@free.fr>
2012-10-03 17:48:29 +0200

    handle shell errors in runtests.sh
    Whenever node has an error, the stderr is intercepted to a result file
    and nothing is shown. That might confuse a newcomer. This change
    intercept the node exit code and will show the content of the result
    file for user to investigate.
    
    A typical use case is a node module depency missing.
    
    Change-Id: I946771aea4289e2a5389dbca8b928d83722b234a

--- js/tests/runtests.sh
+++ js/tests/runtests.sh
@@ -28,15 +28,30 @@ fi
 node=` ( nodejs --version > /dev/null 2>&1 && echo 'nodejs' ) || echo 'node' `
 
 if [ "$1" = "--wt2wt" ];then
+	OUTPUT="results/roundtrip.txt"
     time $node parserTests.js --cache --wt2wt \
-        > results/roundtrip.txt 2>&1 || exit 1
+        > $OUTPUT 2>&1
+	TEST_EXIT_CODE=$?
 elif [ "$1" = '--wt2html' ];then
+	OUTPUT="results/html.txt"
     time $node parserTests.js --wt2html --cache --printwhitelist \
-        > results/html.txt 2>&1 || exit 1
+        > $OUTPUT 2>&1
+	TEST_EXIT_CODE=$?
 else
+	OUTPUT="results/all.txt"
     time $node parserTests.js --wt2html --wt2wt --html2html --cache --printwhitelist \
-        > results/all.txt 2>&1 || exit 1
+        > $OUTPUT 2>&1
+	TEST_EXIT_CODE=$?
 fi
+
+# Handle any error that might have occured during the tests above
+# such as a missing npm module.
+if [ $TEST_EXIT_CODE -ne 0 ]; then
+	warn "\nSome error occured. Dumping recorded output:"
+	cat $OUTPUT 1>&2
+	warn "\n... exiting '$0'"
+	exit 1
+fi;
 
 cd results || exit 1
 if [ "$1" != '-c' -a "$2" != '-c' ];then
