c9c6588a34ef01af4d2fca1b9a45b00525dbd7af
Mark Holmquist <mtraceur@member.fsf.org>
2012-10-17 08:29:42 -0700

    Fix locking error and work on race condition
    The database lock function is working really well, but I was foolish
    and didn't use it to set the claimed status of a title. So now it
    should work better.
    
    Change-Id: Icc57d87a110559e8d5d5d24e7bc1c2dba7132cdf

--- js/tests/server/server.js
+++ js/tests/server/server.js
@@ -4,45 +4,31 @@ var http = require( 'http' ),
 	express = require( 'express' ),
 	sqlite = require( 'sqlite3' ),
 	dbStack = [], dbFlag = false,
-
-withDb = function ( cb ) {
-	var db;
-
-	dbStack.push( cb );
-
-	if ( !dbFlag ) {
-		dbFlag = true;
-		db = new sqlite.Database( 'pages.db' );
-		while ( dbStack.length > 0 ) {
-			dbStack.pop()( db );
-		}
-		db.close();
-		dbFlag = false;
-	}
-},
+	db = new sqlite.Database( 'pages.db' ),
 
 getTitle = function ( req, res ) {
 	console.log( 'Client asked for title.' );
 
 	res.setHeader( 'Content-Type', 'text/plain; charset=UTF-8' );
 
-	withDb( function ( db ) {
-	db.get( 'SELECT title FROM pages WHERE ( result IS NULL AND claimed IS NULL )', function ( err, row ) {
-		if ( err ) {
-			console.log( err );
-			res.send( 'Error! ' + err.toString(), 500 );
-		} else if ( row ) {
-			res.send( row.title, 200 );
-			db.run( 'UPDATE pages SET claimed = ? WHERE title = ?', [ Date.now(), row.title ], function ( err ) {
-				if ( err ) {
-					console.log( 'Could not set the claimed status of the title. This could cause problems.' );
-					console.log( err );
-				}
-			} );
-		} else {
-			res.send( 'no available titles that fit those constraints', 404 );
-		}
-	} );
+	db.serialize( function () {
+		db.get( 'SELECT title FROM pages WHERE result IS NULL AND claimed IS NULL LIMIT 1', function ( err, row ) {
+			if ( err ) {
+				console.log( err );
+				res.send( 'Error! ' + err.toString(), 500 );
+			} else if ( row ) {
+				db.run( 'UPDATE pages SET claimed = ? WHERE title = ?', [ Date.now(), row.title ], function ( err ) {
+					if ( err ) {
+						console.log( err );
+						res.send( 'Error! ' + err.toString(), 500 );
+					} else {
+						res.send( row.title );
+					}
+				} );
+			} else {
+				res.send( 'no available titles that fit those constraints', 404 );
+			}
+		} );
 	} );
 },
 
@@ -58,7 +44,6 @@ recieveResults = function ( req, res ) {
 	res.setHeader( 'Content-Type', 'text/plain; charset=UTF-8' );
 
 	console.log( 'Updating database' );
-	withDb( function ( db ) {
 	db.run( 'UPDATE pages SET result = ?, skips = ?, fails = ?, client = ? WHERE title = ?',
 		[ result, skipCount, failCount, clientName, title ], function ( err ) {
 		console.log( 'Updated.' );
@@ -69,13 +54,11 @@ recieveResults = function ( req, res ) {
 			res.send( '', 200 );
 		}
 	} );
-	} );
 },
 
 resultsWebInterface = function ( req, res ) {
 	var hasStarted = false;
 
-	withDb( function ( db ) {
 	db.all( 'SELECT result FROM pages WHERE result IS NOT NULL', function ( err, rows ) {
 		var i;
 		if ( err ) {
@@ -97,7 +80,6 @@ resultsWebInterface = function ( req, res ) {
 			}
 		}
 	} );
-	} );
 },
 
 
@@ -116,14 +98,14 @@ app.get( /^\/title$/, getTitle );
 // Recieve results from clients
 app.post( /^\/result\/([^\/]+)\/([^\/]+)/, recieveResults );
 
-withDb( function ( db ) {
-db.run( 'CREATE TABLE IF NOT EXISTS pages ( title TEXT DEFAULT "", result TEXT DEFAULT NULL, claimed INTEGER DEFAULT NULL, client TEXT DEFAULT NULL , fails INTEGER DEFAULT NULL, skips INTEGER DEFAULT NULL );', function ( err )  {
-	if ( err ) {
-		console.log( dberr || err );
-	} else {
-		app.listen( 8001 );
-	}
-} );
+db.serialize( function () {
+	db.run( 'CREATE TABLE IF NOT EXISTS pages ( title TEXT DEFAULT "", result TEXT DEFAULT NULL, claimed INTEGER DEFAULT NULL, client TEXT DEFAULT NULL , fails INTEGER DEFAULT NULL, skips INTEGER DEFAULT NULL );', function ( err )  {
+		if ( err ) {
+			console.log( dberr || err );
+		} else {
+			app.listen( 8001 );
+		}
+	} );
 } );
 
 }() );
