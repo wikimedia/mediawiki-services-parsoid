0a47a2554250b6927639ac01d7e9ce37ba1c394b
Gabriel Wicke <gwicke@wikimedia.org>
2012-10-22 11:14:11 -0700

    Limit the number of retries per article
    * Limit the number of retries to 3, mark articles as failed if no result came
      back (result = null). This should get rt tests going again.
    * Use the error count to track the number of retries
    * Break lines in code a bit to fit in normal editor windows and diff views. We
      are aiming for 80 chars, definitely below 100.
    
    Change-Id: I651433980e39b31d50062c5d7af19d7538853346

--- js/tests/server/server.js
+++ js/tests/server/server.js
@@ -5,6 +5,8 @@ var http = require( 'http' ),
 	sqlite = require( 'sqlite3' ),
 	dbStack = [], dbFlag = false,
 	db = new sqlite.Database( 'pages.db' ),
+	// The maximum number of tries per article
+	maxTries = 3,
 
 getTitle = function ( req, res ) {
 	res.setHeader( 'Content-Type', 'text/plain; charset=UTF-8' );
@@ -13,28 +15,22 @@ getTitle = function ( req, res ) {
 	var cutOffTimestamp = Date.now() - 3600,
 		randOffset = Math.floor(Math.random() * 100);
 	db.serialize( function () {
-		db.get( 'SELECT title FROM pages WHERE result IS NULL AND (claimed < ? or claimed is null) LIMIT 1 OFFSET ?', [cutOffTimestamp, randOffset], function ( err, row ) {
-			if ( err ) {
-				console.log( err );
-				res.send( 'Error! ' + err.toString(), 500 );
-			} else if ( row ) {
-				db.run( 'UPDATE pages SET claimed = ? WHERE title = ?', [ Date.now(), row.title ], function ( err ) {
-					if ( err ) {
-						console.log( err );
-						res.send( 'Error! ' + err.toString(), 500 );
-					} else {
-						console.log( 'Dispatching ', row.title );
-						res.send( row.title );
-					}
-				} );
-			} else {
-				// Fall back to slow order by random() method
-				db.get( 'SELECT title FROM pages WHERE result IS NULL AND (claimed < ? or claimed is null) ORDER BY RANDOM() LIMIT 1', [cutOffTimestamp], function ( err, row ) {
-					if ( err ) {
-						console.log( err );
-						res.send( 'Error! ' + err.toString(), 500 );
-					} else if ( row ) {
-						db.run( 'UPDATE pages SET claimed = ? WHERE title = ?', [ Date.now(), row.title ], function ( err ) {
+		db.get( 'SELECT title, errors FROM pages ' +
+			'WHERE result IS NULL AND ' +
+			'(claimed < ? or claimed is null) and errors < ? LIMIT 1 OFFSET ?',
+			[cutOffTimestamp, maxTries, randOffset],
+			// FIXME: move cb out!
+			function ( err, row ) {
+				if ( err ) {
+					console.log( err );
+					res.send( 'Error! ' + err.toString(), 500 );
+				} else if ( row ) {
+					db.run( 'UPDATE pages SET claimed = ?, errors = ? WHERE title = ?',
+						[ Date.now(),
+						(row.errors === null ? 0 : row.errors + 1),
+						row.title ],
+						function ( err ) {
+						// FIXME: move cb out!
 							if ( err ) {
 								console.log( err );
 								res.send( 'Error! ' + err.toString(), 500 );
@@ -42,19 +38,54 @@ getTitle = function ( req, res ) {
 								console.log( 'Dispatching ', row.title );
 								res.send( row.title );
 							}
-						} );
-					} else {
-						res.send( 'no available titles that fit those constraints', 404 );
-					}
-				} );
+						}
+						);
+				} else {
+					// Fall back to slow order by random() method
+					db.get( 'SELECT title, errors FROM pages WHERE ' +
+							'result IS NULL AND ' +
+							'(claimed < ? or claimed is null) '+
+							' and errors < ? ORDER BY RANDOM() LIMIT 1',
+							[cutOffTimestamp, maxTries],
+							// FIXME: move cb to separate function!
+							function ( err, row )
+					{
+						if ( err ) {
+							console.log( err );
+							res.send( 'Error! ' + err.toString(), 500 );
+						} else if ( row ) {
+							db.run( 'UPDATE pages SET claimed = ?, errors = ? WHERE title = ?',
+								[ Date.now(),
+								(row.errors === null ? 0 : row.errors + 1),
+								row.title ],
+								// FIXME: move cb out!
+								function ( err ) {
+									if ( err ) {
+										console.log( err );
+										res.send( 'Error! ' + err.toString(), 500 );
+									} else {
+										console.log( 'Dispatching ', row.title );
+										res.send( row.title );
+									}
+								}
+								);
+						} else {
+							res.send( 'no available titles that fit those constraints', 404 );
+						}
+					} );
+				}
 			}
-		} );
+		);
 	} );
 },
 
 recieveResults = function ( req, res ) {
-	var clientName = req.params[0], title = decodeURIComponent( req.params[1] ), result = req.body.results,
-		skipCount = result.match( /\<skipped/g ), failCount = result.match( /\<failure/g ), errorCount = result.match( /\<error/g );
+	var clientName = req.params[0],
+		title = decodeURIComponent( req.params[1] ),
+		result = req.body.results,
+		skipCount = result.match( /\<skipped/g ),
+		failCount = result.match( /\<failure/g ),
+		errorCount = result.match( /\<error/g );
 
 	skipCount = skipCount ? skipCount.length - 1 : 0;
 	failCount = failCount ? failCount.length - 1 : 0;
@@ -70,23 +101,28 @@ recieveResults = function ( req, res ) {
 		console.log( 'DoesNotExist error get, skipping update.' );
 		res.send( '', 200 );
 	} else {
-		db.run( 'UPDATE pages SET result = ?, skips = ?, fails = ?, errors = ?, client = ? WHERE title = ?',
-			[ result, skipCount, failCount, errorCount, clientName, title ], function ( err ) {
-			console.log( 'Updated.' );
-			if ( err ) {
-				res.send( err.toString(), 500 );
-			} else {
-				console.log( title, '-', skipCount, 'skips,', failCount, 'fails,', errorCount, 'errors.' );
-				res.send( '', 200 );
+		db.run( 'UPDATE pages SET ' +
+				'result = ?, skips = ?, fails = ?, errors = ?, client = ? ' +
+				' WHERE title = ?',
+			[ result, skipCount, failCount, errorCount, clientName, title ],
+			function ( err ) {
+				console.log( 'Updated.' );
+				if ( err ) {
+					res.send( err.toString(), 500 );
+				} else {
+					console.log( title, '-', skipCount, 'skips,',
+						failCount, 'fails,', errorCount, 'errors.' );
+					res.send( '', 200 );
+				}
 			}
-		} );
+		);
 	}
 },
 
 statsWebInterface = function ( req, res ) {
 	db.serialize( function () {
 		db.get( 'SELECT * FROM ((SELECT count(*) FROM pages WHERE result IS NOT NULL) AS total,'
-				+ '(SELECT count(*) FROM pages WHERE result IS NOT NULL AND errors = 0) AS noError,'
+				+ '(SELECT count(*) FROM pages WHERE errors = 0) AS noError,'
 				+ '(SELECT count(*) FROM pages WHERE result IS NOT NULL AND errors = 0 AND fails = 0) AS noFail,'
 				+ '(SELECT count(*) FROM pages WHERE result IS NOT NULL AND errors = 0 AND fails = 0 AND skips = 0) AS noSkip) AS temp', function ( err, row ) {
 			if ( err ) {
@@ -139,49 +175,55 @@ failsWebInterface = function ( req, res ) {
 		var page = ( req.params[0] || 0 ) - 0,
 			offset = page * 40;
 
-		db.all( 'SELECT title, skips, fails, errors FROM pages WHERE result IS NOT NULL ORDER BY errors DESC, fails DESC, skips DESC LIMIT 40 OFFSET ?', [ offset ], function ( err, rows ) {
-			var i, row, output, matches, total = {};
+		db.all( 'SELECT title, skips, fails, errors FROM pages ' +
+			'WHERE (result IS NOT NULL OR errors >= ?) ' +
+			'ORDER BY errors DESC, fails DESC, skips DESC LIMIT 40 OFFSET ?',
+			[ maxTries, offset ],
+			function ( err, rows )
+			{
+				var i, row, output, matches, total = {};
 
-			if ( err ) {
-				res.send( err.toString(), 500 );
-			} else if ( rows.length <= 0 ) {
-				res.send( 'No entries found', 404 );
-			} else {
-				res.setHeader( 'Content-Type', 'text/html' );
-				res.status( 200 );
-				res.write( '<html><body>' );
-
-				res.write( '<p>' );
-				if ( page > 0 ) {
-					res.write( '<a href="/topfails/' + ( page - 1 ) + '">Previous</a> | ' );
+				if ( err ) {
+					res.send( err.toString(), 500 );
+				} else if ( rows.length <= 0 ) {
+					res.send( 'No entries found', 404 );
 				} else {
-					res.write( 'Previous | ' );
-				}
-				res.write( '<a href="/topfails/' + ( page + 1 ) + '">Next</a>' );
-				res.write( '</p>' );
+					res.setHeader( 'Content-Type', 'text/html' );
+					res.status( 200 );
+					res.write( '<html><body>' );
 
-				res.write( '<table><tr><th>Title</th><th>Syntactic diffs</th><th>Semantic diffs</th><th>Errors</th></tr>' );
-
-				for ( i = 0; i < rows.length; i++ ) {
-					res.write( '<tr><td style="color: ' );
-					row = rows[i];
-
-					if ( row.skips === 0 && row.fails === 0 && row.errors === 0 ) {
-						res.write( 'green' );
-					} else if ( row.errors > 0 ) {
-						res.write( 'red' );
-					} else if ( row.fails === 0 ) {
-						res.write( 'orange' );
+					res.write( '<p>' );
+					if ( page > 0 ) {
+						res.write( '<a href="/topfails/' + ( page - 1 ) + '">Previous</a> | ' );
 					} else {
-						res.write( 'red' );
+						res.write( 'Previous | ' );
 					}
+					res.write( '<a href="/topfails/' + ( page + 1 ) + '">Next</a>' );
+					res.write( '</p>' );
 
-					res.write( '">' + row.title + '</td>' );
-					res.write( '<td>' + row.skips + '</td><td>' + row.fails + '</td><td>' + ( row.errors === null ? 0 : row.errors ) + '</td></tr>' );
+					res.write( '<table><tr><th>Title</th><th>Syntactic diffs</th><th>Semantic diffs</th><th>Errors</th></tr>' );
+
+					for ( i = 0; i < rows.length; i++ ) {
+						res.write( '<tr><td style="color: ' );
+						row = rows[i];
+
+						if ( row.skips === 0 && row.fails === 0 && row.errors === 0 ) {
+							res.write( 'green' );
+						} else if ( row.errors > 0 ) {
+							res.write( 'red' );
+						} else if ( row.fails === 0 ) {
+							res.write( 'orange' );
+						} else {
+							res.write( 'red' );
+						}
+
+						res.write( '">' + row.title + '</td>' );
+						res.write( '<td>' + row.skips + '</td><td>' + row.fails + '</td><td>' + ( row.errors === null ? 0 : row.errors ) + '</td></tr>' );
+					}
+					res.end( '</table></body></html>' );
 				}
-				res.end( '</table></body></html>' );
 			}
-		} );
+		);
 	} );
 },
 
