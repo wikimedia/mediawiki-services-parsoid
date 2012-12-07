wikimedia/mediawiki-services-parsoid

commit c9a74d674419924d68b3798a21d36181788763f3

Minor async cleanup and EOFTk sanity checking
* replace oddball switchToAsync result with plain { async: true }
* check for EOFTk in the middle of returned token chunks

No changes in parser tests or async expansion behavior (tested on Obama).

Change-Id: Iaa70c000e06455ece0ff03cddf0d91019b1f0e5d

js/lib/ext.core.ParserFunctions.js
@@ -87,18 +87,10 @@ ParserFunctions.prototype._switchLookupFallback = function ( frame, kvs, key, di
 	this.manager.env.tp('swl');
 	this.manager.env.dp('_switchLookupFallback', kvs.length, key, v );
 	var _cbTrim = function( res ) {
-		if (res.switchToAsync) {
-			cb(res);
-		} else {
-			cb( { tokens: Util.tokenTrim(res) } );
-		}
+		cb( { tokens: Util.tokenTrim(res), async: res.async } );
 	};
 	var _cbNoTrim = function( res ) {
-		if (res.switchToAsync) {
-			cb(res);
-		} else {
-			cb( { tokens: res } );
-		}
+		cb( { tokens: res, async: res.async } );
 	};
 
 	// 'v' need not be a string in cases where it is the last fall-through case

js/lib/ext.core.TemplateHandler.js
@@ -90,16 +90,12 @@ TemplateHandler.prototype.onTemplate = function ( token, frame, cb ) {
  * Parser functions also need template wrapping
  */
 TemplateHandler.prototype._parserFunctionsWrapper = function(state, cb, ret) {
-	if (ret.switchToAsync) {
-		cb(ret);
-	} else {
-		if (ret.tokens) {
-			this._onChunk(state, cb, ret.tokens);
-		}
-		if (!ret.async) {
-			// Now, ready to finish up
-			this._onEnd(state, cb);
-		}
+	if (ret.tokens) {
+		this._onChunk(state, cb, ret.tokens);
+	}
+	if (!ret.async) {
+		// Now, ready to finish up
+		this._onEnd(state, cb);
 	}
 };
 

js/lib/mediawiki.TokenTransformManager.js
@@ -269,11 +269,19 @@ AsyncTokenTransformManager.prototype.emitChunk = function( ret ) {
 		if ( ! ret.async ) {
 			// Check if an EOFTk went missing
 			if ( this.frame.depth === 0 &&
-					ret.tokens && ret.tokens.length &&
-					ret.tokens.last() && ret.tokens.last().constructor !== EOFTk )
-			{
-				console.error("ERROR: EOFTk went missing in AsyncTokenTransformManager");
-				ret.tokens.push(new EOFTk());
+					ret.tokens && ret.tokens.length ) {
+				if ( ret.tokens.last() && ret.tokens.last().constructor !== EOFTk )
+				{
+					console.error("ERROR: EOFTk went missing in AsyncTokenTransformManager");
+					ret.tokens.push(new EOFTk());
+				}
+				for ( var i = 0, l = ret.tokens.lengh; i < l - 1; i++ ) {
+					if ( ret.tokens[i] && ret.tokens[1].constructor === EOFTk ) {
+						console.error("ERROR: EOFTk in the middle of chunk");
+						console.trace();
+					}
+				}
+
 			}
 
 			this.emit( 'chunk', ret.tokens );
@@ -284,6 +292,16 @@ AsyncTokenTransformManager.prototype.emitChunk = function( ret ) {
 			// and not confuse anyone wondering if there is a missing return here.
 			return;
 		} else {
+			if ( this.frame.depth === 0 &&
+					ret.tokens && ret.tokens.length ) {
+				for ( var i = 0, l = ret.tokens.lengh; i < l; i++ ) {
+					if ( ret.tokens[i] && ret.tokens[1].constructor === EOFTk ) {
+						console.error("ERROR: EOFTk in the middle of chunk");
+						console.trace();
+					}
+				}
+
+			}
 			this.emit( 'chunk', ret.tokens );
 			// allow accumulators to go direct
 			return this.emitChunk.bind( this );
@@ -577,14 +595,6 @@ AsyncTokenTransformManager.prototype.transformTokens = function ( tokens, parent
  * the provided asyncCB (TokenAccumulator._returnTokens normally).
  */
 AsyncTokenTransformManager.prototype.maybeSyncReturn = function ( s, cbs, ret ) {
-	if (ret.switchToAsync) {
-		// simple async signalling
-		// SSS FIXME: Is this check required at all?
-		if (s.transforming) {
-			s.res.async = true;
-		}
-		return;
-	}
 
 	if ( s.transforming ) {
 		// transformTokens is still ongoing, handle as sync return by
@@ -603,7 +613,6 @@ AsyncTokenTransformManager.prototype.maybeSyncReturn = function ( s, cbs, ret ) 
 					// is safe. We might want to revisit that later.
 					Math.min( oldRank, ret.tokens.rank );
 				}
-				s.res.async = ret.async;
 			} else {
 				s.res = ret;
 			}
@@ -662,7 +671,14 @@ AsyncTokenTransformManager.prototype.maybeSyncReturn = function ( s, cbs, ret ) 
 					}
 				}
 			}
+		} else if ( ret.async === true ) {
+			// No tokens, was supposed to indicate async processing but came
+			// too late.
+			// TODO: Track down sources for these (unnecessary) calls and try
+			// to avoid them if possible.
+			return;
 		}
+
 
 		asyncCB( ret );
 
@@ -1266,7 +1282,7 @@ Frame.prototype.expand = function ( chunk, options ) {
 	if ( outType === 'tokens/x-mediawiki/expanded' ) {
 		if ( options.asyncCB ) {
 			// Signal (potentially) asynchronous expansion to parent.
-			options.asyncCB({ switchToAsync: true });
+			options.asyncCB({ async: true });
 		}
 
 		// Downstream template uses should be tracked and wrapped only if:

js/lib/mediawiki.Util.js
@@ -343,7 +343,7 @@ var Util = {
 			// * Accumulate async call results in an array
 			//   till we get the signal that we are all done
 			// * Once we are done, pass everything to the target cb.
-			if (!res.switchToAsync) {
+			if (res.async !== true) {
 				// There are 3 kinds of callbacks:
 				// 1. cb({tokens: .. })
 				// 2. cb({}) ==> toks can be undefined

commit da960d3251e9ca7ca384919e1bbb84286dffc908

Fix async regression introduced in c9a74d674419924d68b3798a21d36181788763f3
This fixes a bunch of crashers caused by
c9a74d674419924d68b3798a21d36181788763f3. No changes in parser tests, but all
pages with crashes like
http://parsoid.wmflabs.org:8001/result/291a3cac60ff296b42fc1f388401ccd2001b883a/Sir%C3%A8nes
are now working fine.

Change-Id: I9e5552beb1f0fce537a3b93bc1839f46c5148338

js/lib/ext.core.ParserFunctions.js
@@ -87,10 +87,26 @@ ParserFunctions.prototype._switchLookupFallback = function ( frame, kvs, key, di
 	this.manager.env.tp('swl');
 	this.manager.env.dp('_switchLookupFallback', kvs.length, key, v );
 	var _cbTrim = function( res ) {
-		cb( { tokens: Util.tokenTrim(res), async: res.async } );
+		if ( res.constructor === String ) {
+			cb( { tokens: [ res.trim() ], async: res.async } );
+		} else if ( res.constructor === Array ) {
+			cb( { tokens: Util.tokenTrim(res), async: res.async } );
+		} else {
+			cb( res );
+		}
 	};
 	var _cbNoTrim = function( res ) {
-		cb( { tokens: res, async: res.async } );
+		if ( res.constructor === String ) {
+			cb( { tokens: [ res ], async: res.async } );
+		} else if ( res.constructor === Array ) {
+			cb( { tokens: res, async: res.async } );
+		} else if ( res.async ) {
+			cb( res );
+		} else {
+			console.log( res );
+			console.trace();
+		}
+
 	};
 
 	// 'v' need not be a string in cases where it is the last fall-through case

js/lib/mediawiki.HTML5TreeBuilder.node.js
@@ -186,6 +186,7 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 			console.warn("-------- Unhandled token ---------");
 			console.warn("TYPE: " + token.constructor.name);
 			console.warn("VAL : " + JSON.stringify(token));
+			console.trace();
 			break;
 	}
 	this.lastToken = token;

js/lib/mediawiki.WikitextSerializer.js
@@ -1729,6 +1729,7 @@ WSP._serializeToken = function ( state, token ) {
 			default:
 				res = '';
 				console.warn( 'Unhandled token type ' + JSON.stringify( token ) );
+				console.trace();
 				break;
 		}
 	}