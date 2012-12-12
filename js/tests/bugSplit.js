8266f030ce991b1ef3561c8b5bbbbc5d535a48ec
Gabriel Wicke <gwicke@wikimedia.org>
2012-12-12 10:40:41 -0800

    10th time lucky
    Change-Id: Ia1f9c1023b376780903535c7e475a2dfed518f48

--- js/tests/bugSplit.js
+++ js/tests/bugSplit.js
@@ -8,7 +8,7 @@ function writeFiles ( data ) {
 	var keys = Object.keys(data);
 	for ( var i = 0; i < keys.length; i++ ) {
 		var key = keys[i],
-			fileName = encodeURIComponent(data[key]);
+			fileName = encodeURIComponent(key);
 		console.log( 'Creating file ' + fileName );
 		fs.writeFileSync(fileName, data[key]);
 	}
