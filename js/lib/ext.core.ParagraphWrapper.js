wikimedia/mediawiki-services-parsoid

commit 3c4c33f323854688426e507c7d645561ac58741d

Dont emit NLs for readability in p-wrapper -- screws up DSR.
* P-wrapper was emitting newlines for readability which was
  messing up DSR computation in certain scenarios.

  Test case: "a\n\n\n" is now fixed and the p-tag no longer
  has a negative offset.

* No change in parser tests or DSR warnings.

Change-Id: I19074e21eec33e7bbe347a1fac97557ead8cafbd

js/lib/ext.core.ParagraphWrapper.js
@@ -151,14 +151,7 @@ ParagraphWrapper.prototype.processPendingNLs = function (isBlockToken) {
 				nlTk2 = this.discardOneNlTk();
 				this.discardOneNlTk();
 
-				if (hadOpenTag) {
-					// We strictly dont need this for correctness,
-					// but useful for readable html output
-					resToks.push(nlTk);
-				} else {
-					resToks.push(nlTk);
-					resToks.push(nlTk2);
-				}
+				resToks.push(nlTk);
 
 				// 3. Insert <p><br></p> sections
 				// FIXME: Mark this as a placeholder for now until the

commit 928e09aa4968705ecf185109f0afd9f048703917

Handle serialization of p-br-p html correctly.
* Fixed p-wrapper to move newline into p-br-p rather than before
  p-br-p when handling a set of 3 newlines.  Cleaner.

* Fixed serializer to correctly serialize br when used in a p-br-p
  context.  This also makes it less sensitive to excess newlines
  before/inside the p-br-p html.  All the following 5 HTML snippets
  serialize to 3-newlines now.  Note that the parser itself never
  generates newlines outside p-br-p blocks anymore so the newlines
  in this scenario are not semantic anymore.

  \n\n<p><br/><p>
  \n<p><br/><p>
  <p><br/><p>
  <p>\n<br/><p>
  <p>\n\n<br/><p>

* Additional 4 html2html tests passing + 1 selser test passing for
  certain change files.

* Fixes regression of en:Residential Segregation introduced by
  patch 3c4c33f.

Change-Id: I4ab921f0ae6c017332d273b6af7ec89850662270

js/lib/ext.core.ParagraphWrapper.js
@@ -151,12 +151,11 @@ ParagraphWrapper.prototype.processPendingNLs = function (isBlockToken) {
 				nlTk2 = this.discardOneNlTk();
 				this.discardOneNlTk();
 
-				resToks.push(nlTk);
-
 				// 3. Insert <p><br></p> sections
 				// FIXME: Mark this as a placeholder for now until the
 				// editor handles this properly
 				resToks.push(new TagTk( 'p', [new KV('typeof', 'mw:Placeholder')] ));
+				resToks.push(nlTk);
 				resToks.push(new SelfclosingTagTk('br'));
 				if (newLineCount > 3) {
 					resToks.push(new EndTagTk('p'));

js/lib/mediawiki.WikitextSerializer.js
@@ -1419,7 +1419,20 @@ WSP.tagHandlers = {
 		start: {
 			startsNewline: true,
 			endsLine: true,
-			handle: id("\n")
+			handle: function(state, token) {
+				var prevTagToken = state.prevTagToken;
+				if (prevTagToken &&
+					prevTagToken.constructor === TagTk &&
+					prevTagToken.name === 'p')
+				{
+					if (state.availableNewlineCount < 3) {
+						state.availableNewlineCount = 3;
+					}
+					return '';
+				} else {
+					return '\n';
+				}
+			}
 		}
 	},
 	b:  {
@@ -1794,6 +1807,8 @@ WSP._serializeToken = function ( state, token ) {
 				}
 				state.selser.generatedOutput = true;
 				state.chunkCB( res, state.selser.serializeInfo );
+				res = '';
+				state.availableNewlineCount = 0;
 				break;
 			default:
 				res = '';