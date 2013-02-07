wikimedia/mediawiki-services-parsoid

commit 1ad1a8123bda48c77cf0d29db853874873e36dcb

Code cleanup + refactoring in Template Handler.
* Params.dict and Params.named were computing the dictionary
  each time they were called which is not necessary.  Added
  memoized properties that compute these dicts once per tpl.

* Did some code refactoring and cleanup in template handler code
  in preparation for setting up template params object to be
  passed to the VE.  The template target was not being resolved
  when the PHP preprocessor was called to expand template
  transclusions.  Updated code to resolve template target always
  so that the template params object can be set up as per
  http://www.mediawiki.org/wiki/Parsoid/RDFa_vocabulary#Non-semantic_parameter_editing

* No change in parser tests (as it should be for refactoring).

Change-Id: I6d5b16413521a1c3359499a2883647ec6b07eeda

js/lib/ext.core.TemplateHandler.js
@@ -58,6 +58,7 @@ TemplateHandler.prototype.onTemplate = function ( token, frame, cb ) {
 	var state = { token: token };
 	if (this.options.wrapTemplates) {
 		state.wrapperType = 'mw:Object/Template';
+		// state.recordArgDict = true;
 		state.wrappedObjectId = this.manager.env.newObjectId();
 		state.emittedFirstChunk = false;
 	}
@@ -67,8 +68,9 @@ TemplateHandler.prototype.onTemplate = function ( token, frame, cb ) {
 		if ( this.options.wrapTemplates ) {
 			// Use MediaWiki's action=expandtemplates preprocessor
 			var text = token.getWTSource( this.manager.env ),
+				templateName = this.resolveTemplateTarget(token.attribs[0].k || '').target || "",
 				srcHandler = this._processTemplateAndTitle.bind( this, state, frame,
-						cb, text, [] );
+						cb, templateName, [] );
 			//console.log( text );
 			cb( { async: true } );
 			this.fetchExpandedTplOrExtension( this.manager.env.page.name || '',
@@ -105,30 +107,70 @@ TemplateHandler.prototype._parserFunctionsWrapper = function(state, cb, ret) {
 	}
 };
 
-TemplateHandler.prototype.targetToString = function ( tokens ) {
-	var maybeTarget = Util.tokensToString( tokens, true );
-	if ( maybeTarget.constructor === Array ) {
-		for ( var i = 0, l = maybeTarget[1].length; i < l; i++ ) {
-			var ntt = maybeTarget[1][0];
-			var nonTextTokenCons = ntt.constructor;
-			if ( nonTextTokenCons === TagTk ||
-					nonTextTokenCons === SelfclosingTagTk ||
-					nonTextTokenCons === EndTagTk )
-			{
-				if (ntt.name !== 'meta' ||
-						!ntt.getAttribute("typeof") ||
-						!ntt.getAttribute("typeof").match(/mw:/))
+TemplateHandler.prototype.resolveTemplateTarget = function ( targetToks ) {
+
+	function isConvertibleToString( tokens ) {
+		var maybeTarget = Util.tokensToString( tokens, true );
+		if ( maybeTarget.constructor === Array ) {
+			for ( var i = 0, l = maybeTarget[1].length; i < l; i++ ) {
+				var ntt = maybeTarget[1][0];
+				var nonTextTokenCons = ntt.constructor;
+				if ( nonTextTokenCons === TagTk ||
+						nonTextTokenCons === SelfclosingTagTk ||
+						nonTextTokenCons === EndTagTk )
 				{
-					return null;
+					if (ntt.name !== 'meta' ||
+							!ntt.getAttribute("typeof") ||
+							!ntt.getAttribute("typeof").match(/mw:/))
+					{
+						return false;
+					}
 				}
 			}
+
+			return true;
+		} else {
+			return true;
 		}
-		// No tag tokens, strip comments and newlines
-		return Util.tokensToString(tokens).trim();
-	} else {
-		// Only string
-		return maybeTarget.trim();
 	}
+
+	var env = this.manager.env;
+
+	// Convert the target to a string while stripping all non-text tokens
+	var target = Util.tokensToString(targetToks).trim();
+
+	// strip subst for now.
+	target = target.replace( /^(safe)?subst:/, '' );
+
+	// Check if we have a parser function
+	var prefix = target.split(':', 1)[0].trim();
+	var lowerPrefix = prefix.toLowerCase();
+	var translatedPrefix = env.conf.wiki.magicWords[prefix] || env.conf.wiki.magicWords[lowerPrefix] || lowerPrefix;
+	if ( translatedPrefix && 'pf_' + translatedPrefix in this.parserFunctions ) {
+		return {
+			isPF: true,
+			prefix: prefix,
+			target: 'pf_' + translatedPrefix,
+			pfArg: target.substr( prefix.length + 1 )
+		};
+	}
+
+	// We are dealing with a real template, not a parser function.
+	// Apply more stringent standards for template targets.
+	if (isConvertibleToString(targetToks)) {
+		// We can use the stringified target tokens
+
+		// Normalize the target before template processing
+		// preserve the leading colon in the target
+		target = env.normalizeTitle( target, false, true );
+
+		// Resolve a possibly relative link
+		target = env.resolveTitle(target, 'Template');
+	} else {
+		target = null;
+	}
+
+	return { isPF: false, target: target };
 };
 
 
@@ -138,65 +180,10 @@ TemplateHandler.prototype.targetToString = function ( tokens ) {
  */
 TemplateHandler.prototype._expandTemplate = function ( state, frame, cb, attribs ) {
 
-	//console.warn('TemplateHandler.expandTemplate: ' +
-	//		JSON.stringify( tplExpandData, null, 2 ) );
-	var env = this.manager.env,
-		target = attribs[0].k,
-		self = this;
-
-	if ( ! target ) {
-		env.ap( 'No target! ', attribs );
-		console.trace();
-	}
-
-	// TODO:
-	// check for 'subst:'
-	// check for variable magic names
-	// check for msg, msgnw, raw magics
-	// check for parser functions
-
-
-	//var args = Util.KVtoHash( tplExpandData.expandedArgs );
-
-	// Convert the target to a string while stripping all non-text tokens
-	target = Util.tokensToString(target);
-
-	// strip subst for now.
-	target = target.replace( /^(safe)?subst:/, '' );
-
-	// XXX: wrap attribs in object with .dict() and .named() methods,
-	// and each member (key/value) into object with .tokens(), .dom() and
-	// .wikitext() methods (subclass of Array)
-
-	var prefix = target.split(':', 1)[0].trim();
-	var lowerPrefix = prefix.toLowerCase();
-	var translatedPrefix = env.conf.wiki.magicWords[prefix] || env.conf.wiki.magicWords[lowerPrefix] || lowerPrefix;
-	if ( translatedPrefix && 'pf_' + translatedPrefix in this.parserFunctions ) {
-		var pfAttribs = new Params( env, attribs );
-		pfAttribs[0] = new KV( target.substr( prefix.length + 1 ), [] );
-		//env.dp( 'func prefix/args: ', prefix,
-		//		tplExpandData.expandedArgs,
-		//		'unnamedArgs', tplExpandData.origToken.attribs,
-		//		'funcArg:', funcArg
-		//		);
-		env.dp( 'entering prefix', target, state.token  );
-		var newCB;
-		if (this.options.wrapTemplates) {
-			newCB = this._parserFunctionsWrapper.bind(this, state, cb);
-		} else {
-			newCB = cb;
-		}
-		this.parserFunctions['pf_' + translatedPrefix](state.token, this.manager.frame, newCB, pfAttribs);
-		return;
-	}
-
-	// We are dealing with a real template, not a parser function.
-	// Apply more stringent standards for template targets.
-	target = this.targetToString(attribs[0].k);
-	if ( target === null ) {
-		// Target contains tags, convert template braces and pipes back into text
-		// Re-join attribute tokens with '=' and '|'
+	function convertAttribsToString(attribs, cb) {
 		cb( { async: true } );
+
+		// Re-join attribute tokens with '=' and '|'
 		Util.expandParserValueValues (
 				attribs,
 				function ( expandedAttrs ) {
@@ -230,27 +217,59 @@ TemplateHandler.prototype._expandTemplate = function ( state, frame, cb, attribs
 					cb( { tokens: tokens } );
 				}
 		);
+	}
+
+	var env = this.manager.env,
+		target = attribs[0].k,
+		self = this;
+
+	if ( ! target ) {
+		env.ap( 'No target! ', attribs );
+		console.trace();
+	}
+
+	var resolvedTgt = this.resolveTemplateTarget(target);
+	target = resolvedTgt.target;
+
+	// TODO:
+	// check for 'subst:'
+	// check for variable magic names
+	// check for msg, msgnw, raw magics
+	// check for parser functions
+
+	// XXX: wrap attribs in object with .dict() and .named() methods,
+	// and each member (key/value) into object with .tokens(), .dom() and
+	// .wikitext() methods (subclass of Array)
+
+	if ( resolvedTgt.isPF ) {
+		var pfAttribs = new Params( env, attribs );
+		pfAttribs[0] = new KV( resolvedTgt.pfArg, [] );
+		env.dp( 'entering prefix', target, state.token  );
+		var newCB;
+		if (this.options.wrapTemplates) {
+			newCB = this._parserFunctionsWrapper.bind(this, state, cb);
+		} else {
+			newCB = cb;
+		}
+		state.tokenTarget = target;
+		this.parserFunctions[target](state.token, this.manager.frame, newCB, pfAttribs);
 		return;
 	}
-	// strip subst for now.
-	target = target.replace( /^(safe)?subst:/, '' );
-	env.tp( 'template target: ' + target );
 
-	// now normalize the target before template processing
-	target = env.normalizeTitle( target, false,
-			// preserve the leading colon in the target
-			true );
+	if ( target === null ) {
+		// Target contains tags, convert template braces and pipes back into text
+		// Re-join attribute tokens with '=' and '|'
+		convertAttribsToString(attribs, cb);
+		return;
+	}
 
-	// Resolve a possibly relative link
-	var templateName = env.resolveTitle(target, 'Template');
-
-	var checkRes = this.manager.frame.loopAndDepthCheck( templateName, env.conf.parsoid.maxDepth );
+	var checkRes = this.manager.frame.loopAndDepthCheck( target, env.conf.parsoid.maxDepth );
 	if( checkRes ) {
 		// Loop detected or depth limit exceeded, abort!
 		var res = [
 				checkRes,
 				new TagTk( 'a', [{k: 'href', v: target}] ),
-				templateName,
+				target,
 				new EndTagTk( 'a' )
 			];
 		res.rank = this.manager.phaseEndRank;
@@ -269,9 +288,9 @@ TemplateHandler.prototype._expandTemplate = function ( state, frame, cb, attribs
 	// For now, just fetch the template and pass the callback for further
 	// processing along.
 	this._fetchTemplateAndTitle(
-			templateName,
+			target,
 			cb,
-			this._processTemplateAndTitle.bind( this, state, frame, cb, templateName, attribs )
+			this._processTemplateAndTitle.bind( this, state, frame, cb, target, attribs )
 		);
 };
 
@@ -308,6 +327,7 @@ TemplateHandler.prototype._processTemplateAndTitle = function( state, frame, cb,
 			);
 
 	pipeline.setFrame( this.manager.frame, name, attribs );
+	state.tokenTarget = name;
 
 	// Hook up the inputPipeline output events to our handlers
 	pipeline.addListener( 'chunk', this._onChunk.bind ( this, state, cb ) );
@@ -341,9 +361,27 @@ TemplateHandler.prototype.addEncapsulationInfo = function ( state, chunk ) {
 	// * ref all tables to this (just add about)
 	// * ref end token to this, add property="mw:Object/Template/End"
 
-	var tsr = state.token.dataAttribs.tsr;
-	var src = state.token.getWTSource(this.manager.env);
-	var done = false;
+	var done = false,
+		attrs = [
+			new KV('typeof', state.wrapperType),
+			new KV('about', '#' + state.wrappedObjectId),
+			new KV('id', state.wrappedObjectId)
+		],
+		dataParsoid = {
+			tsr: Util.clone(state.token.dataAttribs.tsr),
+			src: state.token.getWTSource(this.manager.env)
+		};
+
+	/*
+	if (state.recordArgDict) {
+		dataParsoid.argInfo = {
+			id: state.wrapperObjectId,
+			target: state.tokenTarget,
+			params: state.token.attribs.slice(1)
+		};
+	}
+	*/
+
 	if ( chunk.length ) {
 		var firstToken = chunk[0];
 		if ( firstToken.constructor === String ) {
@@ -353,17 +391,7 @@ TemplateHandler.prototype.addEncapsulationInfo = function ( state, chunk ) {
 				stringTokens.push( chunk.shift() );
 			}
 			// Wrap in span with info
-			var span = new TagTk( 'span',
-						[
-							new KV('typeof', state.wrapperType),
-							new KV('about', '#' + state.wrappedObjectId),
-							new KV('id', state.wrappedObjectId)
-						],
-						{
-							tsr: Util.clone(tsr),
-							src: src
-						}
-					);
+			var span = new TagTk( 'span', attrs, dataParsoid );
 			chunk = [span].concat(stringTokens, [ new EndTagTk( 'span' ) ], chunk);
 			done = true;
 		}
@@ -371,15 +399,7 @@ TemplateHandler.prototype.addEncapsulationInfo = function ( state, chunk ) {
 
 	if (!done) {
 		// add meta tag
-		var mtag = new SelfclosingTagTk( 'meta', [
-					new KV( 'about', '#' + state.wrappedObjectId ),
-					new KV( 'typeof', state.wrapperType ),
-					new KV('id', state.wrappedObjectId)
-				], {
-					tsr: Util.clone(tsr),
-					src: src
-				});
-		chunk = [mtag].concat(chunk);
+		chunk = [new SelfclosingTagTk( 'meta', attrs, dataParsoid )].concat(chunk);
 	}
 
 	// add about ref to all tables
@@ -570,7 +590,7 @@ TemplateHandler.prototype.onTemplateArg = function (token, frame, cb) {
 			toks = tplHandler.addEncapsulationInfo(state, toks);
 			toks.push(tplHandler.getEncapsulationInfoEndTag(state));
 			cb( {tokens: toks});
-		}
+		};
 	} else {
 		newCB = cb;
 	}

js/lib/mediawiki.parser.defines.js
@@ -555,6 +555,8 @@ function Params ( env, params ) {
 	for (var i = 0; i < params.length; i++) {
 		this.push( params[i] );
 	}
+	this.argDict = null;
+	this.namedArgsDict = null;
 }
 
 Params.prototype = [];
@@ -566,40 +568,48 @@ Params.prototype.toString = function () {
 };
 
 Params.prototype.dict = function () {
-	var res = {};
-	for ( var i = 0, l = this.length; i < l; i++ ) {
-		var kv = this[i],
-			key = Util.tokensToString( kv.k ).trim();
-		res[key] = kv.v;
+	if (this.argDict === null) {
+		var res = {};
+		for ( var i = 0, l = this.length; i < l; i++ ) {
+			var kv = this[i],
+				key = Util.tokensToString( kv.k ).trim();
+			res[key] = kv.v;
+		}
+		this.argDict = res;
 	}
-	return res;
+
+	return this.argDict;
 };
 
 Params.prototype.named = function () {
-	var n = 1,
-		out = {},
-		namedArgs = {};
+	if (this.namedArgsDict === null) {
+		var n = 1,
+			out = {},
+			namedArgs = {};
 
-	for ( var i = 0, l = this.length; i < l; i++ ) {
-		// FIXME: Also check for whitespace-only named args!
-		var k = this[i].k;
-		var v = this[i].v;
-		if ( k.constructor === String ) {
-			k = k.trim();
+		for ( var i = 0, l = this.length; i < l; i++ ) {
+			// FIXME: Also check for whitespace-only named args!
+			var k = this[i].k;
+			var v = this[i].v;
+			if ( k.constructor === String ) {
+				k = k.trim();
+			}
+			if ( ! k.length ) {
+				out[n.toString()] = v;
+				n++;
+			} else if ( k.constructor === String ) {
+				namedArgs[k] = true;
+				out[k] = v;
+			} else {
+				k = Util.tokensToString( k ).trim();
+				namedArgs[k] = true;
+				out[k] = v;
+			}
 		}
-		if ( ! k.length ) {
-			out[n.toString()] = v;
-			n++;
-		} else if ( k.constructor === String ) {
-			namedArgs[k] = true;
-			out[k] = v;
-		} else {
-			k = Util.tokensToString( k ).trim();
-			namedArgs[k] = true;
-			out[k] = v;
-		}
+		this.namedArgsDict = { namedArgs: namedArgs, dict: out };
 	}
-	return { namedArgs: namedArgs, dict: out };
+
+	return this.namedArgsDict;
 };
 
 /**

commit 3135316c4c8dc86bcd8bf507fc9a9f5dd7010d71

Fix token caching that was broken by commit 1ad1a81
* processTitleAndTemplate was implicitly using the template name
  as a cache key.  This is okay when we use our own pipeline to
  expand templates since the tokens of a fetched template won't
  change across uses since the tpl tokens dont have parameters
  substituted in them yet.  However, when we use the PHP preprocessor
  to expand the tpls. for us, the wikitext we get has parameters
  substituted in them already.  So, the template name is not the
  right cache key -- name + args (or source wikitext sent to the
  preprocessor) is the correct key.

* Updated interface of processTitleAndTemplate to explicitly
  accept a cache-key that can be passed into the expansion pipeline
  and passed name/wikitext in the two use cases above.

* This fixes incorrect parse output found on en:Barack Obama and
  possibly several others.

Change-Id: Ia2c12353399cca9d50e3020f6ddcf0bd69bc887d

js/lib/ext.core.TemplateHandler.js
@@ -67,10 +67,19 @@ TemplateHandler.prototype.onTemplate = function ( token, frame, cb ) {
 			this.manager.env.conf.wiki.apiURI !== null ) {
 		if ( this.options.wrapTemplates ) {
 			// Use MediaWiki's action=expandtemplates preprocessor
-			var text = token.getWTSource( this.manager.env ),
+			// We'll never get to frame depth beyond 1 in this scenario
+			// which means cached content in this frame will not be used
+			// by any child frames since there won't be any children.
+			// So, it is sufficient to pass in '[]' in place of attribs
+			// since the cache key for Frame doesn't matter.
+			//
+			// However, tokenizer needs to use 'text' as the cache key
+			// for caching expanded tokens from the expanded transclusion text
+			// that we get from the preprocessor.
+			var text = token.getWTSource( this.manager.env );
 				templateName = this.resolveTemplateTarget(token.attribs[0].k || '').target || "",
 				srcHandler = this._processTemplateAndTitle.bind( this, state, frame,
-						cb, templateName, [] );
+					cb, templateName, [], text );
 			//console.log( text );
 			cb( { async: true } );
 			this.fetchExpandedTplOrExtension( this.manager.env.page.name || '',
@@ -290,14 +299,14 @@ TemplateHandler.prototype._expandTemplate = function ( state, frame, cb, attribs
 	this._fetchTemplateAndTitle(
 			target,
 			cb,
-			this._processTemplateAndTitle.bind( this, state, frame, cb, target, attribs )
+			this._processTemplateAndTitle.bind( this, state, frame, cb, target, attribs, target )
 		);
 };
 
 /**
  * Process a fetched template source
  */
-TemplateHandler.prototype._processTemplateAndTitle = function( state, frame, cb, name, attribs, err, src, type ) {
+TemplateHandler.prototype._processTemplateAndTitle = function( state, frame, cb, name, attribs, cacheKey, err, src, type ) {
 
 	// We have a choice between aborting or keeping going and reporting the
 	// error inline.
@@ -334,7 +343,7 @@ TemplateHandler.prototype._processTemplateAndTitle = function( state, frame, cb,
 	pipeline.addListener( 'end', this._onEnd.bind ( this, state, cb ) );
 	// Feed the pipeline. XXX: Support different formats.
 	this.manager.env.dp( 'TemplateHandler._processTemplateAndTitle', name, attribs );
-	pipeline.process ( src, name );
+	pipeline.process ( src, cacheKey );
 };
 
 TemplateHandler.prototype.addAboutToTableElements = function ( state, tokens ) {