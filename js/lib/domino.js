af8ac6f6750bf6968bf3ac99e5fe4686d4d733f9
C. Scott Ananian <cscott@cscott.net>
2013-04-12 13:38:49 -0400

    We don't need the domino version check any more.
    All existing contributors have updated their domino, and the
    package.json should take care of any new contributors.
    
    Change-Id: I96370e7259f3e17d3d9322f2a452906d4029d4dc

--- js/lib/domino.js
+++ js/lib/domino.js
@@ -1,10 +1,5 @@
 "use strict";
 var domino = require('domino');
-// double-check that domino is up-to-date
-var pjson = require('domino/package.json');
-if (/1.0.[0-8]/.test(pjson.version)) {
-    throw new Error("Your domino library is out-of-date.  Please update it.");
-}
 
 /** If we need to hot-patch domino to fix upstream bugs, this is the place
  * to do it.   All Parsoid code depends on this module, not directly on
