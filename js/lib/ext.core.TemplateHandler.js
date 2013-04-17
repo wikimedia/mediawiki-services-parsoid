wikimedia/mediawiki-services-parsoid

commit 6d704298f8953950beb3c0aee81e3e9803e36149

Collect page meta info and serialize it in the head (bug 45206).
Right now we just add the data to this.env.page.meta.  A later patch will
serialize some of this in the <head> for users of the external API.

Change-Id: Ib7b866b899cfbcf47d8a06190a411d1a3d46dd50

js/lib/ext.core.TemplateHandler.js
@@ -557,7 +557,9 @@ TemplateHandler.prototype._fetchTemplateAndTitle = function ( title, parentCB, c
 		//} else {
 
 		// append request, process in document order
-		env.requestQueue[title].listeners( 'src' ).push( cb );
+		env.requestQueue[title].listeners( 'src' ).push( function(page) {
+			cb(page.revision['*']);
+		});
 
 		//}
 		parentCB ( { async: true } );

js/lib/mediawiki.SelectiveSerializer.js
@@ -600,7 +600,8 @@ SSP.doSerializeDOM = function ( err, doc, cb, finalcb ) {
  * @param {string} cb.res The result of the chunk serialization.
  * @param {Function} finalcb The callback for after we've serialized the entire document.
  * @param {Error} err
- * @param {string} src The wikitext source of the document.
+ * @param {string} src The wikitext source of the document (optionally
+ *                     including page metadata)
  */
 SSP.parseOriginalSource = function ( doc, cb, finalcb, err, src ) {
 	var self = this,
@@ -608,7 +609,7 @@ SSP.parseOriginalSource = function ( doc, cb, finalcb, err, src ) {
 		parserPipeline = parserPipelineFactory.makePipeline( 'text/x-mediawiki/full' );
 
 	// Makes sure that the src is available even when just fetched.
-	this.env.page.src = src;
+	this.env.setPageSrcInfo( src );
 
 	// Parse the wikitext src to the original DOM, and pass that on to
 	// doSerializeDOM
@@ -620,7 +621,7 @@ SSP.parseOriginalSource = function ( doc, cb, finalcb, err, src ) {
 		console.log(body.outerHTML);
 		self.doSerializeDOM(null, doc, cb, finalcb);
 	} );
-	parserPipeline.process(src);
+	parserPipeline.process(env.page.src);
 };
 
 

js/lib/mediawiki.Util.js
@@ -1268,7 +1268,7 @@ Util.parse = function ( env, cb, err, src ) {
 Util.getPageSrc = function ( env, title, cb, oldid ) {
 	title = env.resolveTitle( title, '' );
 	var pageRequest = new TemplateRequest( env, title, oldid );
-	pageRequest.once( 'src', cb );
+	pageRequest.once( 'src', function(page) { cb(page.revision['*']); });
 };
 
 /**

js/api/ParserService.js
@@ -272,7 +272,7 @@ var roundTripDiff = function ( req, res, env, document ) {
 				}, finalCB );
 };
 
-var parse = function ( env, req, res, cb, err, src ) {
+var parse = function ( env, req, res, cb, err, src_and_metadata ) {
 	var newCb = function ( src, err, doc ) {
 		if ( err !== null ) {
 			if ( !err.code ) {
@@ -289,9 +289,9 @@ var parse = function ( env, req, res, cb, err, src ) {
 	};
 
 	// Set the source
-	env.page.src = src;
+	env.setPageSrcInfo( src_and_metadata );
 
-	Util.parse( env, newCb, err, src );
+	Util.parse( env, newCb, err, env.page.src );
 };
 
 /* -------------------- web app access points below --------------------- */
@@ -450,7 +450,7 @@ app.post(/\/_wikitext\/(.*)/, function ( req, res ) {
 		try {
 			console.log('starting parsing of ' + req.params[0]);
 			// FIXME: This does not handle includes or templates correctly
-			env.page.src = src;
+			env.setPageSrcInfo( src );
 			parser.process( src );
 		} catch (e) {
 			res.setHeader('Content-Type', 'text/plain; charset=UTF-8');
@@ -532,7 +532,9 @@ app.post(/\/_rtform\/(.*)/, function ( req, res ) {
 	var cb = function ( env ) {
 		res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 		// we don't care about \r, and normalize everything to \n
-		parse( env, req, res, roundTripDiff, null, req.body.content.replace(/\r/g, ''));
+		parse( env, req, res, roundTripDiff, null, {
+			revision: { '*': req.body.content.replace(/\r/g, '') }
+		});
 	};
 
 	getParserServiceEnv( res, parsoidConfig.defaultWiki, req.params[0], cb );

js/lib/mediawiki.ApiRequest.js
@@ -6,6 +6,10 @@ var request = require('request'),
 	events = require('events'),
 	util = require('util');
 
+// all revision properties which parsoid is interested in.
+var PARSOID_RVPROP = ('content|ids|timestamp|user|userid|size|sha1|'+
+					  'contentmodel|comment');
+
 /**
  * @class
  * @extends Error
@@ -174,7 +178,7 @@ function TemplateRequest ( env, title, oldid ) {
 		format: 'json',
 		action: 'query',
 		prop: 'revisions',
-		rvprop: 'content',
+		rvprop: PARSOID_RVPROP,
 		titles: title
 	};
 	if ( oldid ) {
@@ -212,12 +216,17 @@ util.inherits(TemplateRequest, ApiRequest);
 TemplateRequest.prototype._handleJSON = function ( error, data ) {
 	var regex, title, err, location, iwstr, interwiki, src = '',
 		self = this;
+	var metadata = { title: self.title };
 
 	if ( error ) {
 		this._processListeners( error, '' );
 		return;
 	}
 
+	if ( data.query.normalized && data.query.normalized.length ) {
+		// update title (ie, "foo_Bar" -> "Foo Bar")
+		metadata.title = data.query.normalized[0].to;
+	}
 	if ( !data.query.pages ) {
 		if ( data.query.interwiki ) {
 			// Essentially redirect, but don't actually redirect.
@@ -244,7 +253,10 @@ TemplateRequest.prototype._handleJSON = function ( error, data ) {
 		try {
 			$.each( data.query.pages, function(i, page) {
 				if (page.revisions && page.revisions.length) {
-					src = page.revisions[0]['*'];
+					metadata.id = page.pageid;
+					metadata.ns = page.ns;
+					metadata.revision = page.revisions[0];
+					src = metadata.revision['*']; // for redirect handling & cache
 				} else {
 					throw new DoesNotExistError( 'Did not find page revisions for ' + self.title );
 				}
@@ -267,24 +279,26 @@ TemplateRequest.prototype._handleJSON = function ( error, data ) {
 			url = this.env.conf.parsoid.apiURI + '?' +
 				qs.stringify( {
 					format: 'json',
-				action: 'query',
-				prop: 'revisions',
-				rvprop: 'content',
-				titles: title
+					action: 'query',
+					prop: 'revisions',
+					rvprop: PARSOID_RVPROP,
+					titles: title
 				} );
 		//'?format=json&action=query&prop=revisions&rvprop=content&titles=' + title;
 		this.requestOptions.url = url;
+		this.title = title;
 		request( this.requestOptions, this._requestCB.bind(this) );
 		return;
 	}
 
-	//console.warn( 'Page ' + title + ': got ' + src );
-	this.env.tp( 'Retrieved ' + this.title, src );
+	//console.warn( 'Page ' + this.title + ': got ' + JSON.stringify(metadata) );
+	this.env.tp( 'Retrieved ' + this.title, metadata );
 
 	// Add the source to the cache
-	this.env.pageCache[this.title] = src;
+	// (both original title as well as possible redirected title)
+	this.env.pageCache[this.queueKey] = this.env.pageCache[this.title] = src;
 
-	this._processListeners( error, src );
+	this._processListeners( error, metadata );
 };
 
 /**

js/lib/mediawiki.parser.environment.js
@@ -118,6 +118,71 @@ var MWParserEnvironment = function ( parsoidConfig, wikiConfig ) {
  * @property {Number/null} page.id
  * The revision ID we want to use for the page.
  */
+/**
+ * @method
+ *
+ * Set the src and optionally meta information for the page we're parsing.
+ *
+ * If the argument is a simple string, will clear metadata and just
+ * set this.page.src.  Otherwise, the provided metadata object should
+ * have fields corresponding to the JSON output given by
+ * action=query&prop=revisions on the MW API.  That is:
+ * <pre>
+ *  metadata = {
+ *    title: // normalized title (ie, spaces not underscores)
+ *    ns:    // namespace
+ *    id:    // page id
+ *    revision: {
+ *      revid:    // revision id
+ *      parentid: // revision parent
+ *      timestamp:
+ *      user:     // contributor username
+ *      userid:   // contributor user id
+ *      sha1:
+ *      size:     // in bytes
+ *      comment:
+ *      contentmodel:
+ *      contentformat:
+ *      "*":     // actual source text --> copied to this.page.src
+ *    }
+ *  }
+ * </pre>
+ *
+ * @param {String or Object} page source or metadata
+ */
+MWParserEnvironment.prototype.setPageSrcInfo = function ( src_or_metadata ) {
+    if (typeof(src_or_metadata)==='string' || src_or_metadata===null) {
+	this.page.meta = { revision: {} };
+	this.page.src = src_or_metadata;
+	return;
+    }
+    // I'm chosing to initialize this.page.meta "the hard way" (rather than
+    // simply cloning the provided object) in part to document/enforce the
+    // expected structure and fields.
+    var metadata = src_or_metadata;
+    var m = this.page.meta;
+    if (!m) { m = this.page.meta = {}; }
+    m.title = metadata.title;
+    m.ns = metadata.ns;
+    m.id = metadata.id;
+    var r = m.revision;
+    if (!r) { r = m.revision = {}; }
+    if (metadata.revision) {
+	r.revid = metadata.revision.revid;
+	r.parentid = metadata.revision.parentid;
+	r.timestamp = metadata.revision.timestamp;
+	r.user = metadata.revision.user;
+	r.userid = metadata.revision.userid;
+	r.sha1 = metadata.revision.sha1;
+	r.size = metadata.revision.size;
+	r.comment = metadata.revision.comment;
+	r.contentmodel = metadata.revision.contentmodel;
+	r.contentformat = metadata.revision.contentformat;
+	if ('*' in metadata.revision) {
+	    this.page.src = metadata.revision['*'];
+	}
+    }
+};
 
 /**
  * @property {Object} conf

js/tests/parse.js
@@ -229,7 +229,7 @@ function dumpFlags() {
 			if ( argv.oldtextfile ) {
 				argv.oldtext = fs.readFileSync(argv.oldtextfile, 'utf8');
 			}
-			env.page.src = argv.oldtext || null;
+			env.setPageSrcInfo( argv.oldtext || null );
 			if ( argv.selser ) {
 				serializer = new SelectiveSerializer( { env: env, oldid: null } );
 			} else {
@@ -251,7 +251,7 @@ function dumpFlags() {
                 serializer.serializeDOM( doc.body, function ( chunk ) {
                     wt += chunk;
                 }, function () {
-                    env.page.src = wt;
+                    env.setPageSrcInfo( wt );
                     if (argv.html2wt) {
 // add a trailing newline for shell user's benefit
                         stdout.write(wt);
@@ -283,8 +283,8 @@ function dumpFlags() {
                 });
 
 // Kick off the pipeline by feeding the input into the parser pipeline
-                env.page.src = input;
-                parserPipeline.process( input );
+                env.setPageSrcInfo( input );
+                parserPipeline.process( env.page.src );
             }
         };
 

js/tests/parserTests.js
@@ -47,8 +47,7 @@ var mp = '../lib/',
 	MWParserEnvironment = require(mp + 'mediawiki.parser.environment.js').MWParserEnvironment,
 	WikitextSerializer = require(mp + 'mediawiki.WikitextSerializer.js').WikitextSerializer,
 	SelectiveSerializer = require( mp + 'mediawiki.SelectiveSerializer.js' ).SelectiveSerializer,
-	ParsoidConfig = require( mp + 'mediawiki.ParsoidConfig' ).ParsoidConfig,
-	TemplateRequest = require(mp + 'mediawiki.ApiRequest.js').TemplateRequest;
+	ParsoidConfig = require( mp + 'mediawiki.ParsoidConfig' ).ParsoidConfig;
 
 // For now most modules only need this for $.extend and $.each :)
 var $ = require(mp + 'fakejquery');
@@ -424,7 +423,7 @@ ParserTests.prototype.convertHtml2Wt = function( options, mode, item, doc, proce
 	try {
 		this.env.page.dom = item.cachedHTML || null;
 		if ( mode === 'selser' ) {
-			this.env.page.src = item.input;
+			this.env.setPageSrcInfo( item.input );
 			this.env.page.name = '';
 			if ( options.changesin && item.changes === undefined ) {
 				// A changesin option was passed, so set the changes to 0,
@@ -432,21 +431,21 @@ ParserTests.prototype.convertHtml2Wt = function( options, mode, item, doc, proce
 				item.changes = 0;
 			}
 		} else if (booleanOption(options.use_source) && startsAtWikitext ) {
-			this.env.page.src = item.input;
+			this.env.setPageSrcInfo( item.input );
 		} else {
-			this.env.page.src = null;
+			this.env.setPageSrcInfo( null );
 		}
 		serializer.serializeDOM( content, function ( res ) {
 			wt += res;
 		}, function () {
 			processWikitextCB( null, wt );
-			self.env.page.src = null;
+			self.env.setPageSrcInfo( null );
 			self.env.page.dom = null;
 		} );
 	} catch ( e ) {
 		console.error(e.stack);
 		processWikitextCB( e, null );
-		this.env.page.src = null;
+		this.env.setPageSrcInfo( null );
 		this.env.page.dom = null;
 	}
 };
@@ -653,7 +652,7 @@ ParserTests.prototype.convertWt2Html = function( mode, prefix, variant, wikitext
 	} catch ( e ) {
 		processHtmlCB( e );
 	}
-	this.env.page.src = wikitext;
+	this.env.setPageSrcInfo( wikitext );
 	this.env.switchToConfig( prefix, function( err ) {
 		if ( err ) {
 			processHtmlCB( err );

commit 2cfa305529879634d554b2a82a3a900d36fc5b34

Bugfix to SelectiveSerializer.
Commit 6d704298f8953950beb3c0aee81e3e9803e36149 introduced a few bugs
because I didn't realize that the callbacks took two parameters; the
first was supposed to be an 'error' object. (I also missed a 'this.'
in one instance.)  Squash the bugs.

Change-Id: I45f9874d2000e8d8bc5c64cec08e9fbd90e77668

js/lib/ext.core.TemplateHandler.js
@@ -557,8 +557,8 @@ TemplateHandler.prototype._fetchTemplateAndTitle = function ( title, parentCB, c
 		//} else {
 
 		// append request, process in document order
-		env.requestQueue[title].listeners( 'src' ).push( function(page) {
-			cb(page.revision['*']);
+		env.requestQueue[title].listeners( 'src' ).push( function(err, page) {
+			cb(err, page ? page.revision['*'] : null);
 		});
 
 		//}

js/lib/mediawiki.SelectiveSerializer.js
@@ -155,7 +155,7 @@ SSP.parseOriginalSource = function ( doc, cb, finalcb, err, src ) {
 		//console.log(body.outerHTML);
 		self.doSerializeDOM(null, doc, cb, finalcb);
 	} );
-	parserPipeline.process(env.page.src);
+	parserPipeline.process(this.env.page.src);
 };
 
 

js/lib/mediawiki.Util.js
@@ -1268,7 +1268,9 @@ Util.parse = function ( env, cb, err, src ) {
 Util.getPageSrc = function ( env, title, cb, oldid ) {
 	title = env.resolveTitle( title, '' );
 	var pageRequest = new TemplateRequest( env, title, oldid );
-	pageRequest.once( 'src', function(page) { cb(page.revision['*']); });
+	pageRequest.once( 'src', function(err, page) {
+		cb(err, page ? page.revision['*'] : null);
+	});
 };
 
 /**