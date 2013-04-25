6fdb0b75938c26b6d58305240efc5a8c00f0c046
Gabriel Wicke <gwicke@wikimedia.org>
2013-04-25 15:27:51 -0700

    Cache invalidation infrastructure for Parsoid varnishes, first cut
    This patch develops a hook-based cache invalidation infrastructure for the
    Parsoid varnishes, according to
    http://www.mediawiki.org/wiki/User:GWicke/Minimal_performance_strategy_for_July_release.
    
    The deferred job is based on HTMLCacheUpdateJob so that the recursive batch
    splitting logic can be shared. A good amount of edit-related hooks are
    registered, mostly following the example of the FlaggedRevision extension. It
    looks like we'll get away without a need for any core modifications.
    
    Asynchronous refresh and PURGE functionality is implemented using curl_multi.
    Additional information about the predecessor revision id and the reason for
    the update (title triggering it and edit vs. template vs. image updates) are
    passed in an X-Parsoid header.
    
    TODO:
    - Test and refine with actual Varnish servers
    
    Change-Id: I47331bf638dc93cf444ceeecb89ef9f4c3f40ae3

---
+++ php/Parsoid.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Basic cache invalidation for Parsoid
+ */
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "Parsoid extension\n";
+	exit( 1 );
+}
+
+
+/**
+ * Class containing basic setup functions.
+ */
+class ParsoidSetup {
+	/**
+	 * Register hook handlers.
+	 * This function must NOT depend on any config vars.
+	 *
+	 * @return void
+	 */
+	public static function setUnconditionalHooks() {
+		global $wgHooks, $wgAutoloadClasses, $wgJobClasses;
+
+		$dir = dirname( __FILE__ );
+
+		# Set up class autoloading
+		$wgAutoloadClasses['ParsoidHooks'] = "$dir/Parsoid.hooks.php";
+		$wgAutoloadClasses['ParsoidCacheUpdateJob'] = "$dir/ParsoidCacheUpdateJob.php";
+		$wgAutoloadClasses['CurlMultiClient'] = "$dir/CurlMultiClient.php";
+
+		# Add the ParsoidCacheUpdateJob to the job classes so it can be de-serialized
+		$wgJobClasses['ParsoidCacheUpdateJob'] = 'ParsoidCacheUpdateJob';
+
+		# Article edit/create
+		$wgHooks['ArticleEditUpdates'][] = 'ParsoidHooks::onArticleEditUpdates';
+		# Article delete/restore
+		$wgHooks['ArticleDeleteComplete'][] = 'ParsoidHooks::onArticleDelete';
+		$wgHooks['ArticleUndelete'][] = 'ParsoidHooks::onArticleUndelete';
+		# Revision delete/restore
+		$wgHooks['ArticleRevisionVisibilitySet'][] = 'ParsoidHooks::onRevisionDelete';
+		# Article move
+		$wgHooks['TitleMoveComplete'][] = 'ParsoidHooks::onTitleMoveComplete';
+		# File upload
+		$wgHooks['FileUpload'][] = 'ParsoidHooks::onFileUpload';
+	}
+}
+
+
+# Load hooks that are always set
+ParsoidSetup::setUnconditionalHooks();
