wikimedia/mediawiki-services-parsoid

commit 7da439e6c1bcdbb94b3796b36bbc7d9a85c698d9

(Bug 46470) More robust handling of default sort
* Default sort key can be:
  - a plain-old string (foo)
  - a string with entities ("foo"bar)
  - partially/fully-templated ({{foo}}bar)

* Thus far, default-sort handling was restricted to the plain-old
  string scenario.

* This patch expands that to handle entities as well as templated
  pieces of the sort key.

* No change in parser test results (TODO: add more)

* Tested on the following cases and verified wt2html and wt2wt:
  - {{DEFAULTSORT:foo}}
  - {{DEFAULTSORT:'foo'bar}}
  - {{DEFAULTSORT:{{foo}}bar}}

Change-Id: I45bbb5c01b00cffb07ac69e237aca63a32730f17

js/lib/ext.core.TemplateHandler.js
@@ -85,12 +85,9 @@ TemplateHandler.prototype.onTemplate = function ( token, frame, cb ) {
 	//		' args: ' + JSON.stringify( this.manager.args ));
 
 	// magic word variables can be mistaken for templates
-	var translatedMagicWordKVs = this.checkForMagicWordVariable(token.attribs[0].k);
-	if (translatedMagicWordKVs) {
-		var metaToken = new defines.SelfclosingTagTk('meta',
-											 translatedMagicWordKVs,
-											 Util.clone(token.dataAttribs));
-		cb({ tokens: [metaToken] });
+	var magicWord = this.checkForMagicWordVariable(token);
+	if (magicWord) {
+		cb({ tokens: [magicWord] });
 		return;
 	}
 
@@ -181,23 +178,87 @@ TemplateHandler.prototype._parserFunctionsWrapper = function(state, cb, ret) {
  * Check if token is a magic word masquerading as a template
  * - currently only DEFAULTSORT is considered
  */
-TemplateHandler.prototype.checkForMagicWordVariable = function (magicWord) {
-	if (magicWord.constructor !== String) {
-		// maybe we should try to convert to string, but for now we only care for DEFAULTSORT
-		return null;
+TemplateHandler.prototype.checkForMagicWordVariable = function(tplToken) {
+	// Deal with the following scenarios:
+	//
+	// 1. Normal string:        {{DEFAULTSORT:foo}}
+	// 2. String with entities: {{DEFAULTSORT:"foo"bar}}
+	// 3. Templated key:        {{DEFAULTSORT:{{foo}}bar}}
+
+	var property, key, propAndKey, keyToks,
+		magicWord = tplToken.attribs[0].k;
+
+	if (magicWord.constructor === String) {
+		// Scenario 1. above -- common case
+		propAndKey = magicWord.match(/^([^:]+:)(.*)$/);
+		if (propAndKey) {
+			property = propAndKey[1];
+			key = propAndKey[2];
+		}
+	} else if (magicWord.constructor === Array) {
+		// Scenario 2. or 3. above -- uncommon case
+
+		property = magicWord[0];
+		if (!property || property.constructor !== String) {
+			// FIXME: We don't know if this is a magic word at this point.
+			// Ex: {{ {{echo|DEFAULTSORT}}:foo }}
+			//     {{ {{echo|lc}}:foo }}
+			// This requires more info from the preprocessor than
+			// we have currently. This will be handled at a later point.
+			return null;
+		}
+
+		propAndKey = property.match(/^([^:]+:)(.*)$/);
+		if (propAndKey) {
+			property = propAndKey[1];
+			key = propAndKey[2];
+		}
+
+		keyToks = [key].concat(magicWord.slice(1));
 	}
 
-	var propAndKey = magicWord.match(/^([^:]+:)(.*)$/);
-	if (propAndKey) {
-		var prop = propAndKey[1].trim(),
-			key = propAndKey[2].trim();
-
-		if (this.manager.env.conf.wiki.magicWords[prop] === 'defaultsort' && key.length > 0) {
-			return [
-				new KV('property', 'mw:PageProp/categorydefaultsort'),
-				new KV('content', key)
-		   ];
+	// TODO gwicke: factor out generic magic word (and parser function) round-tripping logic!
+	if (property && this.manager.env.conf.wiki.magicWords[property.trim()] === 'defaultsort') {
+		var templatedKey = false;
+		if (keyToks) {
+			// Check if any part of the key is templated
+			for (var i = 0, n = keyToks.length; i < n; i++) {
+				if (Util.isTemplateToken(keyToks[i])) {
+					templatedKey = true;
+					break;
+				}
+			}
+			key = Util.tokensToString(keyToks);
 		}
+
+		var metaToken = new defines.SelfclosingTagTk(
+				'meta',
+				[new KV('property', 'mw:PageProp/categorydefaultsort')],
+				Util.clone(tplToken.dataAttribs)
+			);
+
+		if (templatedKey) {
+			// No shadowing if templated
+			//
+			// SSS FIXME: post-tpl-expansion, WS won't be trimmed. How do we handle this?
+			metaToken.addAttribute("content", keyToks);
+		} else {
+			// Leading/trailing WS should be stripped
+			key = key.trim();
+
+			var src = (tplToken.dataAttribs || {}).src;
+			if (src) {
+				// If the token has original wikitext, shadow the sort-key
+				var origKey = src.replace(/[^:]+:/, '');
+				metaToken.addNormalizedAttribute("content", key, origKey);
+			} else {
+				// If not, this token came from an extension/template
+				// in which case, dont bother with shadowing since the token
+				// will never be edited directly.
+				metaToken.addAttribute("content", key);
+			}
+		}
+		return metaToken;
 	}
 
 	return null;

js/lib/mediawiki.Util.js
@@ -245,6 +245,10 @@ var Util = {
 		}
 	},
 
+	isTemplateToken: function(token) {
+		return token && token.constructor === pd.SelfclosingTagTk && token.name === 'template';
+	},
+
 	isTableTag: function(token) {
 		var tc = token.constructor;
 		return (tc === pd.TagTk || tc === pd.EndTagTk) &&

js/lib/mediawiki.WikitextSerializer.js
@@ -2105,7 +2105,27 @@ WSP.tagHandlers = {
 				content = node.getAttribute('content'),
 				property = node.getAttribute('property');
 
-			if ( type ) {
+			// Check for property before type so that page properties with templated attrs
+			// roundtrip properly.  Ex: {{DEFAULTSORT:{{echo|foo}} }}
+			if ( property ) {
+				var switchType = property.match( /^mw\:PageProp\/(.*)$/ );
+				if ( switchType ) {
+					var out = switchType[1];
+					if (out === 'categorydefaultsort') {
+						if (node.data.parsoid.src) {
+							// Use content so that VE modifications are preserved
+							var contentInfo = DU.getAttributeShadowInfo(node, "content", state.tplAttrs);
+							out = node.data.parsoid.src.replace(/^([^:]+:)(.*)$/, "$1" + contentInfo.value + "}}");
+						} else {
+							console.warn('defaultsort is missing source. Rendering as DEFAULTSORT magicword');
+							out = "{{DEFAULTSORT:" + content + "}}";
+						}
+					} else if ( node.data.parsoid.magicSrc ) {
+						out = node.data.parsoid.magicSrc;
+					}
+					cb(out, node);
+				}
+			} else if ( type ) {
 				switch ( type ) {
 					case 'mw:tag':
 							 // we use this currently for nowiki and co
@@ -2144,23 +2164,6 @@ WSP.tagHandlers = {
 					default:
 							 state.serializer._htmlElementHandler(node, state, cb);
 							 break;
-				}
-			} else if ( property ) {
-				var switchType = property.match( /^mw\:PageProp\/(.*)$/ );
-				if ( switchType ) {
-					var out = switchType[1];
-					if (out === 'categorydefaultsort') {
-						if (node.data.parsoid.src) {
-							// Use content so that VE modifications are preserved
-							out = node.data.parsoid.src.replace(/^([^:]+:)(.*)$/, "$1" + content + "}}");
-						} else {
-							console.warn('defaultsort is missing source. Rendering as DEFAULTSORT magicword');
-							out = "{{DEFAULTSORT:" + content + "}}";
-						}
-					} else if ( node.data.parsoid.magicSrc ) {
-						out = node.data.parsoid.magicSrc;
-					}
-					cb(out, node);
 				}
 			} else {
 				state.serializer._htmlElementHandler(node, state, cb);

commit fdffc626057c5fea2cf6b835160b362a86417a2f

Fix bug in shadowing of default sort key
* Bug introduced by 7da439e6 -- looks like I didn't test the
  examples in the commit summary after later amends of the patch.

  {{DEFAULTSORT:foo}} shadowed the 'foo' key with 'foo}}' because
  of a bad regexp that extracted the key from the source.

Change-Id: Ife44147de2a1f2ab32904b46c377b77ec600ed14

js/lib/ext.core.TemplateHandler.js
@@ -242,7 +242,7 @@ TemplateHandler.prototype.checkForMagicWordVariable = function(tplToken) {
 			var src = (tplToken.dataAttribs || {}).src;
 			if (src) {
 				// If the token has original wikitext, shadow the sort-key
-				var origKey = src.replace(/[^:]+:/, '');
+				var origKey = src.replace(/[^:]+:/, '').replace(/}}$/, '');
 				metaToken.addNormalizedAttribute("content", key, origKey);
 			} else {
 				// If not, this token came from an extension/template