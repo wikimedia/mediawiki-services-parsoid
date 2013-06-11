wikimedia/mediawiki-services-parsoid

commit 6e40256821408ca3b89dc114d68a982ea68c6ea4

(Bug 49405) Dont rely on dp.src while serializing tpls/exts/params
* Even though the serializer had the ability to serialize new
  transclusions, extension uses, or params, the serializer handler
  fetching code was still checking for the presence of original
  source for these uses which prevented new <ref> tags from being
  serialized properly.

* No change in parser test results.

Change-Id: I797b2bd87da10cd44c5eecd7b5c814777ea1a05e

js/lib/mediawiki.WikitextSerializer.js
@@ -2640,43 +2640,52 @@ WSP._getDOMHandler = function(node, state, cb) {
 		typeOf = node.getAttribute( 'typeof' ) || '';
 
 	// XXX: Convert into separate handlers?
-	if ( dp.src !== undefined ) {
-		//console.log(node.parentNode.outerHTML);
-		if (/\bmw:(?:Transclusion\b|Param\b|Extension\/[^\s]+)/.test(typeOf)) {
-			// Source-based template/extension round-tripping for now
-			return {
-				handle: function () {
-					// In RT-testing mode, there will not be any edits to tpls/extensions.
-					// So, use original source to eliminate spurious diffs showing up
-					// in RT testing results.
-					var src, dataMW;
-					if (state.rtTesting) {
-						src = dp.src;
-					} else if (/mw:Extension\//.test(typeOf)) {
-						dataMW = JSON.parse(node.getAttribute("data-mw"));
-						src = !dataMW ? dp.src : state.serializer._buildExtensionWT(state, node, dataMW);
+	if (/\bmw:(?:Transclusion\b|Param\b|Extension\/[^\s]+)/.test(typeOf)) {
+		return {
+			handle: function () {
+				// In RT-testing mode, there will not be any edits to tpls/extensions.
+				// So, use original source to eliminate spurious diffs showing up
+				// in RT testing results.
+				var src, dataMW;
+				if (state.rtTesting && dp.src !== undefined) {
+					src = dp.src;
+				} else if (/mw:Extension\//.test(typeOf)) {
+					dataMW = JSON.parse(node.getAttribute("data-mw"));
+					src = !dataMW ? dp.src : state.serializer._buildExtensionWT(state, node, dataMW);
+				} else {
+					dataMW = JSON.parse(node.getAttribute("data-mw"));
+					if (dataMW) {
+						src = state.serializer._buildTemplateWT(state, dataMW.parts || [{ template: dataMW }]);
 					} else {
-						dataMW = JSON.parse(node.getAttribute("data-mw"));
-						if (dataMW) {
-							src = state.serializer._buildTemplateWT(state, dataMW.parts || [{ template: dataMW }]);
-						} else {
-							console.error("ERROR: No data-mw for: " + node.outerHTML);
-							src = dp.src;
-						}
+						console.error("ERROR: No data-mw for: " + node.outerHTML);
+						src = dp.src;
 					}
+				}
+
+				if (src) {
 					self.emitWikitext(src, state, cb, node);
 					return self.skipOverEncapsulatedContent(node);
-				},
-				sepnls: {
-					// XXX: This is questionable, as the template can expand
-					// to newlines too. Which default should we pick for new
-					// content? We don't really want to make separator
-					// newlines in HTML significant for the semantics of the
-					// template content.
-					before: id({min:0, max:2})
+				} else {
+					console.error("ERROR: No handler for: " + node.outerHTML);
+					console.error("Serializing as HTML.");
+					return self._htmlElementHandler(node, state);
 				}
-			};
-		} else if (typeOf === "mw:Placeholder" ||
+			},
+			sepnls: {
+				// XXX: This is questionable, as the template can expand
+				// to newlines too. Which default should we pick for new
+				// content? We don't really want to make separator
+				// newlines in HTML significant for the semantics of the
+				// template content.
+				before: id({min:0, max:2})
+			}
+		};
+	}
+
+	if ( dp.src !== undefined ) {
+		// Source-based template/extension round-tripping for now
+		//console.log(node.parentNode.outerHTML);
+		if (typeOf === "mw:Placeholder" ||
 				(typeOf === "mw:Nowiki" && node.textContent === dp.src )) {
 			// implement generic src round-tripping:
 			// return src, and drop the generated content

commit 2ec7497bbbdf444d9eec9ae9c4ae29c4a4b391bc

Fix html2html crashers introduced by 6e402568
* I had fixed this but must have accidentally reverted the fix
  after I finished my testing while working on 6e402568.

Change-Id: I01257fefee0ce3348c3d2f333c4c8bdfbdc46621

js/lib/mediawiki.WikitextSerializer.js
@@ -2668,7 +2668,7 @@ WSP._getDOMHandler = function(node, state, cb) {
 				} else {
 					console.error("ERROR: No handler for: " + node.outerHTML);
 					console.error("Serializing as HTML.");
-					return self._htmlElementHandler(node, state);
+					return self._htmlElementHandler(node, state, cb);
 				}
 			},
 			sepnls: {