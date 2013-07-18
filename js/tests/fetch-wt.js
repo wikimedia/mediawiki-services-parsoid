06ae961bf06f7d8aa1d98149ad8d9c5be9fd7bb1
C. Scott Ananian <cscott@cscott.net>
2013-07-18 11:55:30 -0400

    Update fetch-wt.js to fetch by title, use optimist, etc.
    Change-Id: I0283867b88ef6e2dfa2c5832374250db6aaa2a68

--- js/tests/fetch-wt.js
+++ js/tests/fetch-wt.js
@@ -1,32 +1,116 @@
 #!/usr/bin/env node
+"use strict";
 
-/*
- * This could have been done via wget with this url:
+/** Fetch the wikitext for a page, given title or revision id.
  *
- * http://<PREFIX>.wikipedia.org/w/api.php?format=txt&action=query&prop=revisions&rvprop=content&revids=<OLDID>
- *
- * and then use sed to extract the wikitext out of the result
- *
- * But I had to do it the hard way mostly because this has independent utility
+ *  This is very useful for extracting test cases which can then be passed
+ *  to tests/parse.js
  */
 
-var api = require('../lib/mediawiki.ApiRequest.js'),
-	ParsoidConfig = require( '../lib/mediawiki.ParsoidConfig.js' ).ParsoidConfig,
-	ParserEnv = require('../lib/mediawiki.parser.environment.js').MWParserEnvironment;
+var fs = require( 'fs' ),
+	optimist = require( 'optimist' ),
 
-var argv = process.argv.slice(process.argv[0] === 'node' ? 2 : 1);
-if (argv.length === 0) {
-	console.warn("Usage: [node] fetch-wt.js <rev-id> [<opt-wiki-prefix>]");
+	TemplateRequest = require( '../lib/mediawiki.ApiRequest.js' ).TemplateRequest,
+	ParsoidConfig = require( '../lib/mediawiki.ParsoidConfig' ).ParsoidConfig,
+	MWParserEnvironment = require( '../lib/mediawiki.parser.environment.js' ).MWParserEnvironment;
+
+
+var fetch = function ( page, revid, cb, options ) {
+	cb = typeof cb === 'function' ? cb : function () {};
+
+	var envCb = function ( err, env ) {
+		env.errCB = function ( error ) {
+			cb( error, env, [] );
+		};
+		if ( err !== null ) {
+			env.errCB( err );
+			return;
+		}
+
+		var target = page ? env.resolveTitle( env.normalizeTitle( env.page.name ), '' ) : null;
+		var tpr = new TemplateRequest( env, target, revid );
+
+		tpr.once( 'src', function ( err, src_and_metadata ) {
+			if ( err ) {
+				cb( err, env, [] );
+			} else {
+				env.setPageSrcInfo( src_and_metadata );
+
+				// ok, got it
+				if ( options.output ) {
+					fs.writeFileSync( options.output, env.page.src, 'utf8');
+				} else {
+					console.log( env.page.src );
+				}
+			}
+		} );
+	};
+
+	var prefix = options.prefix || null;
+
+	if ( options.apiURL ) {
+		prefix = 'customwiki';
+	}
+
+	var parsoidConfig = new ParsoidConfig( options, { defaultWiki: prefix } );
+
+	if ( options.apiURL ) {
+		parsoidConfig.setInterwiki( 'customwiki', options.apiURL );
+	}
+
+	MWParserEnvironment.getParserEnv( parsoidConfig, null, prefix, page, envCb );
+};
+
+var usage = 'Usage: $0 [options] <page-title or rev-id>\n' +
+	'If first argument is numeric, it is used as a rev id; otherwise it is\n' +
+	'used as a title.  Use the --title option for a numeric title.';
+var opts = optimist.usage( usage, {
+	'output': {
+		description: "Write page to given file"
+	},
+	'prefix': {
+		description: 'Which wiki prefix to use; e.g. "en" for English wikipedia, "es" for Spanish, "mw" for mediawiki.org',
+		'boolean': false,
+		'default': 'en'
+	},
+	'title': {
+		description: 'Page title to fetch',
+		'boolean': false
+	},
+	'rev': {
+		description: 'Page revision to fetch',
+		'boolean': false
+	},
+	'help': {
+		description: 'Show this message',
+		'boolean': true,
+		'default': false
+	}
+});
+
+var argv = opts.argv;
+var title = null, revid = null;
+var error;
+if (argv.title && argv.revid) {
+	error = "Can't specify title and revid at the same time.";
+} else if (argv.title) {
+	title = '' + argv.title; // convert, in case it's numeric.
+} else if (argv.revid) {
+	revid = +argv.revid;
+} else if (typeof(argv._[0]) === 'number') {
+	revid = argv._[0];
+} else if (argv._[0]) {
+	title = argv._[0];
+} else {
+	error = "Must specify a title or revision id.";
+}
+
+if (argv.help || error) {
+	optimist.showHelp();
+	if (error) {
+		console.error('ERROR:', error);
+	}
 	return;
 }
 
-var revid = argv[0];
-var prefix = argv.length > 0 ? argv[1] : "en";
-var parsoidConfig = new ParsoidConfig( null, { defaultWiki: prefix } );
-
-ParserEnv.getParserEnv( parsoidConfig, null, prefix, null, function ( err, env ) {
-	var req = new api.TemplateRequest(env, null, revid);
-	req.listeners('src').push(function(err, page) {
-		console.log(page.revision['*']);
-	});
-});
+fetch(title, revid, null, argv);
