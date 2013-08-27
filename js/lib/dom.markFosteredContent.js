wikimedia/mediawiki-services-parsoid

commit 7bd2df748b013d17971427e953b23a53356a982c

Generalize foster parented content detection
Fostered content is boxed with an inserted meta tag.

Bug: 53110
Change-Id: I04f3751660caf37964fb4a1d79e8d16fc9464055

js/lib/dom.markFosteredContent.js
@@ -2,85 +2,48 @@
 
 var DU = require('./mediawiki.DOMUtils.js').DOMUtils;
 
-/* ------------------------------------------------------------------------------------
- * Non-IEW (inter-element-whitespace) can only be found in <td> <th> and <caption> tags
- * in a table.  If found elsewhere within a table, such content will be moved out of
- * the table and be "adopted" by the table's sibling ("foster parent").  The content
- * that gets adopted is "fostered content".
+/* ------------------------------------------------------------------------
+ * Non-IEW (inter-element-whitespace) can only be found in <td> <th> and
+ * <caption> tags in a table.  If found elsewhere within a table, such
+ * content will be moved out of the table and be "adopted" by the table's
+ * sibling ("foster parent"). The content that gets adopted is "fostered
+ * content".
  *
- * See http://dev.w3.org/html5/spec-LC/tree-construction.html#foster-parenting
- * ------------------------------------------------------------------------------------ */
-function markFosteredContent(node, env) {
-	function findFosteredContent(table) {
-		var tableTagId = table.data.parsoid.tagId,
-			n = table.previousSibling,
-			initPos = table.data.parsoid.tsr ? table.data.parsoid.tsr[0] : null,
-			fosteredText = "",
-			nodeBuf = [],
-			tsrGap = 0;
+ * http://dev.w3.org/html5/spec-LC/tree-construction.html#foster-parenting
+ * ------------------------------------------------------------------------ */
 
-		while (n) {
-			if (DU.isElt(n)) {
-				if (typeof(n.data.parsoid.tagId) !== 'number' || n.data.parsoid.tagId < tableTagId) {
-					if (initPos && n.data.parsoid.tsr && DU.tsrSpansTagDOM(n, n.data.parsoid)) {
-						var expectedGap = initPos - n.data.parsoid.tsr[1];
-						if (tsrGap !== expectedGap) {
-							/*
-							console.log("Fostered text/comments: " +
-								JSON.stringify(fosteredText.substring(expectedGap)));
-							*/
-							while (nodeBuf.length > 0) {
-								// Wrap each node in a span wrapper
-								var x = nodeBuf.pop();
-								var span = table.ownerDocument.createElement('span');
-								span.data = { parsoid: { fostered: true } };
-								x.parentNode.insertBefore(span, x);
-								span.appendChild(x);
-							}
-						}
-					} else {
-						/* jshint noempty: false */
+function markFosteredContent( node, env ) {
+	var span, sibling, c = node.firstChild;
 
-						// No clue if the text in fosteredText is really fostered content.
-						// If we ran this pass post-dsr-computation, we might be able to
-						// detect this in more scenarios. Something to consider.
+	while ( c ) {
+		sibling = c.nextSibling;
 
-						/*
-						console.warn("initPos: " + initPos);
-						console.warn("have tsr: " + n.data.parsoid.tsr);
-						console.warn("spans tsr: " + (n.data.parsoid.tsr && DU.tsrSpansTagDOM(n, n.data.parsoid)));
-						*/
-					}
-					// All good at this point
-					break;
+		if ( DU.isMarkerMeta( c, "mw:FosterBox" ) ) {
+			while ( sibling && (
+				!DU.isElt( sibling ) || !DU.hasNodeName( sibling, "table" )
+			) ) {
+				if ( DU.isElt( sibling ) ) {
+					sibling.data.parsoid.fostered = true;
 				} else {
-					n.data.parsoid.fostered = true;
+					span = sibling.ownerDocument.createElement( "span" );
+					span.data = { parsoid: { fostered: true } };
+					sibling.parentNode.insertBefore( span, sibling );
+					span.appendChild( sibling );
 				}
-			} else {
-				var str = DU.isText(n) ? n.nodeValue : "<!--" + n.nodeValue + "-->";
-				tsrGap += str.length;
-				fosteredText = str + fosteredText;
-				nodeBuf.push(n);
+				sibling = sibling.nextSibling;
 			}
-			n = n.previousSibling;
-		}
-	}
-
-	var c = node.firstChild;
-	while (c) {
-		var sibling = c.nextSibling;
-
-		if (DU.isElt(c) && c.nodeName === 'TABLE') {
-			findFosteredContent(c);
+			DU.deleteNode( c );
 		}
 
-		if (c.childNodes.length > 0) {
-			markFosteredContent(c, env);
+		if ( c.childNodes.length > 0 ) {
+			markFosteredContent( c, env );
 		}
+
 		c = sibling;
 	}
+
 }
 
-if (typeof module === "object") {
+if ( typeof module === "object" ) {
 	module.exports.markFosteredContent = markFosteredContent;
 }

js/lib/mediawiki.DOMPostProcessor.js
@@ -110,8 +110,8 @@ function DOMPostProcessor(env, options) {
 	// Common post processing
 	this.processors = [
 		dataParsoidLoader.traverse.bind( dataParsoidLoader ),
-		handleUnbalancedTables,
 		markFosteredContent,
+		handleUnbalancedTables,
 		migrateStartMetas,
 		markTreeBuilderFixups,
 		handlePres,

js/lib/mediawiki.HTML5TreeBuilder.node.js
@@ -173,6 +173,16 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 			break;
 		case TagTk:
 			tName = token.name;
+			if ( tName === "table" ) {
+				if ( this.trace ) {
+					console.warn('inserting foster box meta');
+				}
+				this.emit('token', {
+					type: 'StartTag',
+					name: 'meta',
+					data: [ { nodeName: "typeof", nodeValue: "mw:FosterBox" } ]
+				});
+			}
 			this.emit('token', {type: 'StartTag', name: tName, data: this._att(attribs)});
 			attrs = [];
 			if ( this.trace ) { console.warn('inserting shadow meta for ' + tName); }

commit 15d2394402d35b93d6907c268cdb9ae4a66bb091

Fix bug in foster parented content detection
Introduced in 7bd2df748b013d17971427e953b23a53356a982c.

nextSibling was null for text nodes after wrapping in span, ending the
walk. Adds a test case to ensure subsequent nodes are wrapped.

Change-Id: Ie61ddc5f78e9b62af9441a965e8e66e4b19789d4

js/lib/dom.markFosteredContent.js
@@ -29,6 +29,7 @@ function markFosteredContent( node, env ) {
 					span.data = { parsoid: { fostered: true } };
 					sibling.parentNode.insertBefore( span, sibling );
 					span.appendChild( sibling );
+					sibling = span;
 				}
 				sibling = sibling.nextSibling;
 			}