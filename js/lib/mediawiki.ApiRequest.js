c700b2f7dd0ffe96a1ba59eceb1ed3ed03daaca9
Gabriel Wicke <gwicke@wikimedia.org>
2013-09-17 16:23:22 -0700

    Use just the 'Parsoid/0.1' user agent so that we don't distort stats
    We don't want to distort the browser stats at
    http://stats.wikimedia.org/wikimedia/squids/SquidReportClients.htm, so don't
    masquerade as Firefox.
    
    Change-Id: I26349e7751368f943c1b5e07f98100f5715463a7

--- js/lib/mediawiki.ApiRequest.js
+++ js/lib/mediawiki.ApiRequest.js
@@ -10,8 +10,9 @@ var request = require('request'),
 var PARSOID_RVPROP = ('content|ids|timestamp|user|userid|size|sha1|'+
 					  'contentmodel|comment');
 
-var userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) ' +
-							'Gecko/20100101 Firefox/9.0.1 Parsoid/0.1';
+//var userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) ' +
+//							'Gecko/20100101 Firefox/9.0.1 Parsoid/0.1';
+var userAgent = 'Parsoid/0.1';
 
 /**
  * @class
