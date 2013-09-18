wikimedia/mediawiki-services-parsoid

commit 1dba828d7eddf9f00aea6b47242ecfc4aedcdd07

Encapsulate foster-parented transclusion content.
Transclusion content is shadowed before the tree is built to detect
fostering. If fostering of transclusion content occurs, new metas are
inserted to wrap the table and foster box as transclusion marked.

Bug: 52945
Change-Id: I7bb99e3cc354effc9342004e0e7533f39de9907f

js/lib/dom.markFosteredContent.js
@@ -12,31 +12,121 @@ var DU = require('./mediawiki.DOMUtils.js').DOMUtils;
  * http://dev.w3.org/html5/spec-LC/tree-construction.html#foster-parenting
  * ------------------------------------------------------------------------ */
 
+// cleans up transclusion shadows, keeping track of fostered transclusions
+function removeTransclusionShadows( node ) {
+	var sibling, fosteredTransclusions = false;
+	if ( DU.isElt( node ) ) {
+		if ( DU.isMarkerMeta( node, "mw:TransclusionShadow" ) ) {
+			DU.deleteNode( node );
+			return true;
+		} else if ( node.data.parsoid.inTransclusion ) {
+			fosteredTransclusions = true;
+			delete node.data.parsoid.inTransclusion;
+		}
+		node = node.firstChild;
+		while ( node ) {
+			sibling = node.nextSibling;
+			if ( removeTransclusionShadows( node ) ) {
+				fosteredTransclusions = true;
+			}
+			node = sibling;
+		}
+	}
+	return fosteredTransclusions;
+}
+
+// inserts metas around the fosterbox and table
+function insertTransclusionMetas( env, fosterBox, table ) {
+
+	// find tsr[1] and end-boundary
+	var tsr1, sibling = table.nextSibling;
+	while ( sibling ) {
+		if ( sibling.data.parsoid.tsr ) {
+			tsr1 = sibling.data.parsoid.tsr[ 1 ];
+			break;
+		}
+		sibling = sibling.nextSibling;
+	}
+	if ( typeof tsr1 !== "number" ) {
+		tsr1 = table.parentNode.data.parsoid.tsr[ 1 ];
+	}
+
+	// get a new about id
+	var aboutId = env.newAboutId();
+
+	//  create start-meta and insert
+	var s = DU.createNodeWithAttributes( fosterBox.ownerDocument, "meta", {
+		"about": aboutId,
+		"id": aboutId.substring( 1 ),
+		"typeof": "mw:Transclusion",
+		"data-parsoid": JSON.stringify({
+			"tsr": [ table.data.parsoid.tsr[ 0 ], tsr1 ]
+		})
+	});
+	fosterBox.parentNode.insertBefore( s, fosterBox );
+
+	// create end-meta, find insertion-point, and insert
+	var e = DU.createNodeWithAttributes( table.ownerDocument, "meta", {
+		"about": aboutId,
+		"typeof": "mw:Transclusion/End"
+	});
+
+	// skip table end mw:shadow
+	if ( sibling && DU.isMarkerMeta( sibling, "mw:EndTag" ) ) {
+		sibling = sibling.nextSibling;
+	}
+
+	// special case where the table end and inner transclusion coincide
+	if ( sibling && DU.isMarkerMeta( sibling, "mw:Transclusion/End" ) ) {
+		sibling = sibling.nextSibling;
+	}
+
+	table.parentNode.insertBefore( e, sibling );
+
+}
+
+// Searches for FosterBoxes and does two things when it hits one:
+// * Marks all nextSiblings as fostered until the accompanying table.
+// * Wraps the whole thing (table + fosterbox) with transclusion metas if
+//   there is any fostered transclusion content.
 function markFosteredContent( node, env ) {
-	var span, sibling, c = node.firstChild;
+	var span, sibling, next, fosteredTransclusions, c = node.firstChild;
 
 	while ( c ) {
 		sibling = c.nextSibling;
+		fosteredTransclusions = false;
 
 		if ( DU.isMarkerMeta( c, "mw:FosterBox" ) ) {
-			while ( sibling && (
-				!DU.isElt( sibling ) || !DU.hasNodeName( sibling, "table" )
-			) ) {
+
+			// mark as fostered until we hit the table
+			while ( !DU.isElt( sibling ) || !DU.hasNodeName( sibling, "table" ) ) {
+				next = sibling.nextSibling;
 				if ( DU.isElt( sibling ) ) {
 					sibling.data.parsoid.fostered = true;
+					if ( removeTransclusionShadows( sibling ) ) {
+						fosteredTransclusions = true;
+					}
 				} else {
 					span = sibling.ownerDocument.createElement( "span" );
 					span.data = { parsoid: { fostered: true } };
 					sibling.parentNode.insertBefore( span, sibling );
 					span.appendChild( sibling );
-					sibling = span;
 				}
-				sibling = sibling.nextSibling;
+				sibling = next;
 			}
-			DU.deleteNode( c );
-		}
 
-		if ( c.childNodes.length > 0 ) {
+			// we have fostered transclusions
+			// wrap the whole thing in a transclusion
+			if ( fosteredTransclusions ) {
+				insertTransclusionMetas( env, c, sibling );
+			}
+
+			// remove the foster box
+			DU.deleteNode( c );
+
+		} else if ( DU.isMarkerMeta( c, "mw:TransclusionShadow" ) ) {
+			DU.deleteNode( c );
+		} else if ( c.childNodes.length > 0 ) {
 			markFosteredContent( c, env );
 		}
 

js/lib/dom.wrapTemplates.js
@@ -12,8 +12,17 @@ function getDOMRange( env, doc, startElem, endMeta, endElem ) {
 
 	// Detect empty content
 	if (startElemIsMeta && startElem.nextSibling === endElem) {
-		var emptySpan = doc.createElement('span');
-		startElem.parentNode.insertBefore(emptySpan, endElem);
+		// Adding spans in a fosterable position will result
+		// in breaking skipOverEncapsulatedContent in rt.
+		// If the metas are in a fosterable position and
+		// content is empty, it's usually an indication that
+		// the content was fostered out anyways and spans aren't
+		// necessary. A {{blank}} template in a fosterable
+		// position probably won't be handled properly though.
+		if ( !DU.isFosterablePosition( endElem ) ) {
+			var emptySpan = doc.createElement('span');
+			startElem.parentNode.insertBefore(emptySpan, endElem);
+		}
 	}
 
 	var startAncestors = DU.pathToRoot(startElem);

js/lib/mediawiki.DOMUtils.js
@@ -894,6 +894,15 @@ var DOMUtils = {
 	},
 
 	/**
+	 * Create a new DOM node with attributes
+	 */
+	createNodeWithAttributes: function ( document, type, attrs ) {
+		var node = document.createElement( type );
+		this.addAttributes(node, attrs);
+		return node;
+	},
+
+	/**
 	 * Return `true` iff the element has a `class` attribute containing
 	 * `someClass` (among other space-separated classes).
 	 * @param {Element} ele

js/lib/mediawiki.HTML5TreeBuilder.node.js
@@ -68,6 +68,7 @@ FauxHTML5.TreeBuilder.prototype.resetState = function () {
 	this.processToken(new TagTk( 'body' ));
 
 	this.needsReset = false;
+	this.inTransclusion = false;
 };
 
 FauxHTML5.TreeBuilder.prototype.onChunk = function ( tokens ) {
@@ -132,6 +133,13 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 		dataAttribs = {};
 	}
 
+	if ( this.inTransclusion ) {
+		if ( Object.isFrozen( dataAttribs ) ) {
+			dataAttribs = Util.clone( dataAttribs );
+		}
+		dataAttribs.inTransclusion = true;
+	}
+
 	// Assign tagid to open/self-closing tags
 	if ((token.constructor === TagTk || token.constructor === SelfclosingTagTk) &&
 		token.name !== 'body')
@@ -174,6 +182,18 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 				// Emit the newline as Characters token to prevent it from
 				// being eaten by the treebuilder when preceded by a pre.
 				this.emit('token', {type: 'Characters', data: token});
+
+				if ( this.inTransclusion ) {
+					if ( this.trace ) {
+						console.warn('inserting shadow transclusion meta');
+					}
+					this.emit('token', {
+						type: 'StartTag',
+						name: 'meta',
+						data: [ { name: "typeof", value: "mw:TransclusionShadow" } ]
+					});
+				}
+
 			}
 			break;
 		case NlTk:
@@ -219,6 +239,12 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 			// Convert mw metas to comments to avoid fostering.
 			tTypeOf = token.getAttribute( "typeof" );
 			if ( tName === "meta" && tTypeOf && tTypeOf.match( /^mw:/ ) ) {
+
+				// transclusions state
+				if ( tTypeOf.match( /^mw:Transclusion/ ) ) {
+					this.inTransclusion = /^mw:Transclusion$/.test( tTypeOf );
+				}
+
 				this.emit( "token", { type: "Comment", data: JSON.stringify({
 					"@type": tTypeOf,
 					attrs: this._att( attribs )

js/tests/parserTests-blacklist.js
@@ -610,7 +610,6 @@ add("wt2wt", "2. includeonly in html attr value");
 add("wt2wt", "Templates: HTML Tag: 2. Generation of HTML attr. value");
 add("wt2wt", "Templates: HTML Tag: 3. Generation of HTML attr key and value");
 add("wt2wt", "Templates: HTML Tables: 5. Proper fostering of categories from inside");
-add("wt2wt", "Templates: Wiki Tables: 1b. Fostering of entire template content");
 add("wt2wt", "pre-save transform: nonexistent template");
 add("wt2wt", "pre-save transform: mixed tag case");
 add("wt2wt", "pre-save transform: Signature expansion");

commit cbfbd6dbea7d7f2309f30abb2fe7ee851fdee4b0

Use table's tsr info for encapsulating transclusion meta.
The earlier implementation searched the table's siblings for tsr[0] to
avoid the case where the table's tsr[1] wasn't present.

However, since d05894f170ba8e9a712099d3e6e6cd48ff53808d, encapsulation
doesn't happen when the foster box, and thus the table, are in the
transclusion. So, tsr[1] should always be available.

Fixes regression introduced in 1dba828d7eddf9f00aea6b47242ecfc4aedcdd07.

Change-Id: I2da552b102c666f4942f07b1ac56163552d2a401

js/lib/dom.markFosteredContent.js
@@ -38,38 +38,26 @@ function removeTransclusionShadows( node ) {
 // inserts metas around the fosterbox and table
 function insertTransclusionMetas( env, fosterBox, table ) {
 
-	// find tsr[1] and end-boundary
-	var tsr1, sibling = table.nextSibling;
-	while ( sibling ) {
-		if ( sibling.data.parsoid.tsr ) {
-			tsr1 = sibling.data.parsoid.tsr[ 1 ];
-			break;
-		}
-		sibling = sibling.nextSibling;
-	}
-	if ( typeof tsr1 !== "number" ) {
-		tsr1 = table.parentNode.data.parsoid.tsr[ 1 ];
-	}
-
-	// get a new about id
 	var aboutId = env.newAboutId();
 
-	//  create start-meta and insert
+    // You might be asking yourself, why is table.data.parsoid.tsr[1] always
+	// present? The earlier implementation searched the table's siblings for
+	// their tsr[0]. However, encapsulation doesn't happen when the foster box,
+	// and thus the table, are in the transclusion.
 	var s = DU.createNodeWithAttributes( fosterBox.ownerDocument, "meta", {
 		"about": aboutId,
 		"id": aboutId.substring( 1 ),
 		"typeof": "mw:Transclusion",
-		"data-parsoid": JSON.stringify({
-			"tsr": [ table.data.parsoid.tsr[ 0 ], tsr1 ]
-		})
+		"data-parsoid": JSON.stringify({ "tsr": table.data.parsoid.tsr })
 	});
 	fosterBox.parentNode.insertBefore( s, fosterBox );
 
-	// create end-meta, find insertion-point, and insert
 	var e = DU.createNodeWithAttributes( table.ownerDocument, "meta", {
 		"about": aboutId,
 		"typeof": "mw:Transclusion/End"
 	});
+
+	var sibling = table.nextSibling;
 
 	// skip table end mw:shadow
 	if ( sibling && DU.isMarkerMeta( sibling, "mw:EndTag" ) ) {