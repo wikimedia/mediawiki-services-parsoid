d3eec0aba850515daeab6c58432573984f5540e7
C. Scott Ananian <cscott@cscott.net>
2013-09-20 14:24:44 -0400

    Fix parserTests.js --filter
    In node 0.10, we get a bunch of "recursive invocation of process.nextTick, use
    setImmediate" errors on console when we use --filter.  Switch to setImmediate
    to make node happy, and add a compatibility thunk in core-upgrade.js so that
    we still work fine on node 0.8 as well.
    
    Change-Id: I55679b2144bf71e6093bd77507e426ce4250989a

--- js/lib/core-upgrade.js
+++ js/lib/core-upgrade.js
@@ -3,3 +3,8 @@ if (!Array.prototype.last) {
 		value: function() { return this[this.length - 1]; }
 	});
 }
+(function(global) {
+	if (!global.setImmediate) {
+		global.setImmediate = process.nextTick.bind(process);
+	}
+})(typeof global === 'object' && global ? global : this );
