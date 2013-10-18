wikimedia/mediawiki-services-parsoid

commit 396070ebcea438d774d1dacf01575ab0b1c16d00

Only copy data-parsoid attribute from element to shadow meta
Others aren't necessary and if they aren't sanitized, result in crashes.

Does a bit of es5 cleanup.

Bug: 55820
Change-Id: Ib82891a374bf1e8b5e87309d62b3a53e4e12b7eb

js/lib/mediawiki.HTML5TreeBuilder.node.js
@@ -7,7 +7,6 @@
 
 var events = require('events'),
 	util = require('util'),
-	$ = require('./fakejquery'),
 	HTML5 = require('html5'),
 	domino = require('./domino'),
 	defines = require('./mediawiki.parser.defines.js'),
@@ -113,14 +112,12 @@ FauxHTML5.TreeBuilder.prototype.onEnd = function ( ) {
 };
 
 FauxHTML5.TreeBuilder.prototype._att = function (maybeAttribs) {
-	var atts = [];
-	if ( maybeAttribs && $.isArray( maybeAttribs ) ) {
-		for(var i = 0, length = maybeAttribs.length; i < length; i++) {
-			var att = maybeAttribs[i];
-			atts.push({name: att.k, value: att.v});
-		}
+	if ( Array.isArray( maybeAttribs ) ) {
+		return maybeAttribs.map(function ( attr ) {
+			return { name: attr.k, value: attr.v };
+		});
 	}
-	return atts;
+	return [];
 };
 
 // Adapt the token format to internal HTML tree builder format, call the actual
@@ -153,9 +150,10 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 		dataAttribs.tagId = this.tagId++;
 	}
 
+	var dpString = JSON.stringify(dataAttribs);
 	attribs = attribs.concat([ {
 		k: 'data-parsoid',
-		v: JSON.stringify(dataAttribs)
+		v: dpString
 	} ]);
 
 	if (this.trace) {
@@ -273,9 +271,11 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 			this.emit('token', {type: 'EndTag', name: tName});
 
 			if ( this.trace ) { console.warn('inserting shadow meta for ' + tName); }
-			attrs = this._att(attribs);
-			attrs.push({name: "typeof", value: "mw:EndTag"});
-			attrs.push({name: "data-etag", value: tName});
+			attrs = [
+				{ name: "data-parsoid", value: dpString },
+				{ name: "typeof", value: "mw:EndTag" },
+				{ name: "data-etag", value: tName }
+			];
 			this.emit('token', {type: 'Comment', data: JSON.stringify({
 				"@type": "mw:shadow",
 				attrs: attrs

js/lib/mediawiki.DOMPostProcessor.js
@@ -109,7 +109,7 @@ function prepareDOM( node ) {
 				try {
 					meta.setAttribute( attr.name, attr.value );
 				} catch(e) {
-					console.error('WARNING: prepareDOM: Dropped invalid attribute ' + attr);
+					console.error( 'WARNING: prepareDOM: Dropped invalid attribute ' + attr.name );
 				}
 			});
 			node.parentNode.insertBefore( meta, node );

js/lib/mediawiki.Util.js
@@ -319,7 +319,7 @@ var Util = {
 	tokensToString: function ( tokens, strict ) {
 		var out = [];
 		// XXX: quick hack, track down non-array sources later!
-		if ( ! $.isArray( tokens ) ) {
+		if ( !Array.isArray( tokens ) ) {
 			tokens = [ tokens ];
 		}
 		for ( var i = 0, l = tokens.length; i < l; i++ ) {

commit 028bb94cbc8b11fc4ab07af499d840c4c26f7d34

Remove attribute dropping to avoid masking issues with sanitizer
Introduced in 396070ebcea438d774d1dacf01575ab0b1c16d00,
but unnecessary since e7dc56c94dcfa64f4cab3240fc8f68cc35097856.

Change-Id: I70db9fd4c98b89e2fb40d83b19848ed006132660

js/lib/mediawiki.HTML5TreeBuilder.node.js
@@ -150,10 +150,9 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 		dataAttribs.tagId = this.tagId++;
 	}
 
-	var dpString = JSON.stringify(dataAttribs);
 	attribs = attribs.concat([ {
 		k: 'data-parsoid',
-		v: dpString
+		v: JSON.stringify(dataAttribs)
 	} ]);
 
 	if (this.trace) {
@@ -269,13 +268,10 @@ FauxHTML5.TreeBuilder.prototype.processToken = function (token) {
 		case EndTagTk:
 			tName = token.name;
 			this.emit('token', {type: 'EndTag', name: tName});
-
 			if ( this.trace ) { console.warn('inserting shadow meta for ' + tName); }
-			attrs = [
-				{ name: "data-parsoid", value: dpString },
-				{ name: "typeof", value: "mw:EndTag" },
-				{ name: "data-etag", value: tName }
-			];
+			attrs = this._att( attribs );
+			attrs.push({ name: "typeof", value: "mw:EndTag" });
+			attrs.push({ name: "data-etag", value: tName });
 			this.emit('token', {type: 'Comment', data: JSON.stringify({
 				"@type": "mw:shadow",
 				attrs: attrs