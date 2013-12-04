wikimedia/mediawiki-services-parsoid

commit 283bfd554bf1040f7067954ae5ca75b4c5a3dbe4

Updates to handling of templated attributes
* This patch updates parser output for templated attributes to
  conform to the new spec @
  https://www.mediawiki.org/wiki/Parsoid/MediaWiki_DOM_spec#Transclusion-affected_attributes

* The serializer has been fixed up accordingly as well. But,
  the serializer continues to support the old spec for backwards
  compatibility till the time we purge the parser cache and get
  rid of old HTML completely.

* There are currently 2 failing wt2wt tests that correspond to
  entire "k=v" string being processed via *include* directives.
  There is a hack in attribute-expander that could be expanded
  to support them, but unclear if we have to continue supporting
  them and they are valid use cases. The selser failures are
  likely either a result of selser being more accurate than wt2wt
  but being tagged failing because of mismatch with wt2wt output.

* The other 10 failing selser tests correspond to these two tests:

2. includeonly in html attr value
Sanitizer: Validating that <meta> and <link> work, but only for Microdata

  These are cases where selser is more accurate than wt2wt. With
  change in parse output, for both these tests, there has been
  a reshuffling of failing and passing selser tests without any
  net change.

* Now, 20 additional wt2html tests are passing because of the
  absence of meta-tags from the earlier spec and all templated
  attr info being recorded in data-mw. data-mw is stripped out
  while comparing HTML output with PHP output and all these tests
  now pass.

Change-Id: I4d132e611547996e99b8376e089bc6fa5b9b63bf

js/lib/ext.core.AttributeExpander.js
@@ -3,26 +3,28 @@
  */
 "use strict";
 
-var Util = require('./mediawiki.Util.js').Util,
+var async = require('async'),
+	Util = require('./mediawiki.Util.js').Util,
 	AttributeTransformManager = require('./mediawiki.TokenTransformManager.js').
 	                            AttributeTransformManager,
 	defines = require('./mediawiki.parser.defines.js');
 // define some constructor shortcuts
 var KV = defines.KV,
+    EOFTk = defines.EOFTk,
     TagTk = defines.TagTk,
     SelfclosingTagTk = defines.SelfclosingTagTk;
 
 /* ----------------------------------------------------------
  * This helper method does two different things:
  *
- * 1. Strips all meta tags
+ * 1. Strips all meta tags (introduced by transclusions, etc)
  * 2. In wrap-template mode, it identifies the meta-object type
  *    and returns it.
  * ---------------------------------------------------------- */
 function stripMetaTags( tokens, wrapTemplates ) {
 	var isPushed, buf = [],
 		wikitext = [],
-		metaObjTypes = [],
+		hasGeneratedContent = false,
 		inTpl = false,
 		inInclude = false;
 
@@ -40,17 +42,14 @@ function stripMetaTags( tokens, wrapTemplates ) {
 				var t = token.getAttribute("typeof");
 				var typeMatch = t && t.match(/(mw:(Transclusion|Param|Extension|Includes\/)(.*)?$)/);
 				if (typeMatch) {
-					if (typeMatch[1].match(/\/End$/)) {
-						inTpl = false;
-						inInclude = false;
-					} else {
+					if (!typeMatch[1].match(/\/End$/)) {
 						inTpl = typeMatch[1].match(/Transclusion|Param|Extension/);
 						inInclude = !inTpl;
-					}
-
-					if (inTpl || inInclude) {
-						metaObjTypes.push(typeMatch[1]);
+						hasGeneratedContent = true;
 						wikitext.push(token.dataAttribs.src);
+					} else {
+						inTpl = false;
+						inInclude = false;
 					}
 				} else {
 					isPushed = true;
@@ -74,34 +73,10 @@ function stripMetaTags( tokens, wrapTemplates ) {
 	}
 
 	return {
-		// SSS FIXME: Assumes that either the attr. has only 1 expansion
-		// OR all expansions are of the same type.
-		// Consider the attr composed of pieces: s1, s2, s3
-		// - s1 can be generated by a template
-		// - s2 can be plain text
-		// - s3 can be generated by an extension.
-		// While that might be considered utter madness, there is nothing in
-		// the spec right now that prevents this.  In any case, not sure
-		// we do require all expandable types to be tracked.
-		metaObjType: metaObjTypes[0],
+		hasGeneratedContent: hasGeneratedContent,
 		wikitext: Util.tokensToString(wikitext),
 		value: buf
 	};
-}
-
-function makeTplAffectedMeta( contentType, key, val ) {
-	// SSS FIXME: Assumes that all expanded attrs. have the same expandable type
-	// - attr1 can be expanded by a template
-	// - attr2 can be expanded by an extension
-	// While that might be considered madness, there is nothing in the spec right
-	// not that prevents this.  In any case, not sure we do require all
-	// expandable types to be tracked.
-
-	// <meta about="#mwt1" property="mw:objectAttr#href" data-parsoid="...">
-	// about will be filled out in the end
-	return new SelfclosingTagTk( 'meta',
-		[new KV( "property", "mw:" + contentType + "#" + key )],
-		{ src: val.wikitext });
 }
 
 /**
@@ -142,15 +117,14 @@ AttributeExpander.prototype.onToken = function ( token, frame, cb ) {
 		// Do not process dom-fragment tokens: a separate handler deals with them.
 		token.name !== 'mw:dom-fragment-token' &&
 		token.attribs && token.attribs.length ) {
-		var atm = new AttributeTransformManager(
-					this.manager,
-					{ wrapTemplates: this.options.wrapTemplates },
-					this._returnAttributes.bind( this, token, cb )
-				);
 		cb( { async: true } );
-		atm.process(token.attribs);
+		(new AttributeTransformManager(
+			this.manager,
+			{ wrapTemplates: this.options.wrapTemplates },
+			this._returnAttributes.bind( this, token, cb )
+		)).process(token.attribs);
 	} else {
-		cb ( { tokens: [token] } );
+		cb( { tokens: [token] } );
 	}
 };
 
@@ -163,41 +137,30 @@ AttributeExpander.prototype._returnAttributes = function ( token, cb, newAttrs )
 {
 	this.manager.env.dp( 'AttributeExpander._returnAttributes: ', newAttrs );
 
-	var modified = false;
-	var metaTokens = [];
-	var oldAttrs   = token.attribs;
-	var a, newA, newK, i, l, metaObjType, producerObjType, kv, updatedK, updatedV;
+	var modified = false,
+		metaTokens = [],
+		tmpDataMW = new Map(),
+		oldAttrs = token.attribs,
+		a, newA, newK, i, l, updatedK, updatedV;
 
 	// Identify attributes that were generated in full or in part using templates
-	// and add appropriate meta tags for them.
 	for (i = 0, l = oldAttrs.length; i < l; i++) {
 		a    = oldAttrs[i];
 		newA = newAttrs[i];
 		newK = newA.k;
 
-		// Preserve the key and value source, if available
-		// But, if 'a' wasn't cloned, newA will be the same as a.
+		// Preserve the key and value source, if available.
+		// But, if 'a' wasn't cloned, newA will be the same as 'a'.
 		// Dont try to update it and crash since a is frozen.
 		if (a !== newA) {
-			if (a.ksrc) {
-				newA.ksrc = a.ksrc;
-			}
-			if (a.vsrc) {
-				newA.vsrc = a.vsrc;
-			}
-			if (a.srcOffsets) {
-				newA.srcOffsets = a.srcOffsets;
-			}
+			newA.ksrc = a.ksrc;
+			newA.vsrc = a.vsrc;
+			newA.srcOffsets = a.srcOffsets;
 		}
 
 		if (newK) {
-			var contentType = "objectAttrKey"; // default
 			if (a.k.constructor === Array) {
-				if ( newK.constructor === String && newK.match( /mw\:maybeContent/ ) ) {
-					updatedK = stripMetaTags( 'mw:keyAffected', this.options.wrapTemplates );
-					newAttrs.push( new KV( 'mw:keyAffected', newA.v ) );
-					newK = updatedK.value;
-				} else {
+				if ( newK.constructor !== String || !newK.match( /mw\:maybeContent/ ) ) {
 					updatedK = stripMetaTags(newK, this.options.wrapTemplates);
 					newK = updatedK.value;
 					if (newA.v === '') {
@@ -211,11 +174,15 @@ AttributeExpander.prototype._returnAttributes = function ( token, cb, newAttrs )
 						// If we got a string of the form "k=v" and our orig-v
 						// was empty, then, we split the template content around
 						// the '=' and update the 'k' and 'v' to the split values.
-						var kArr = Util.tokensToString(newK, true);
-						var kStr = (kArr.constructor === String) ? kArr : kArr[0];
-						var m    = kStr.match(/([^=]+)=['"]?([^'"]*)['"]?$/);
+						var kArr = Util.tokensToString(newK, true),
+							kStr = (kArr.constructor === String) ? kArr : kArr[0],
+							m    = kStr.match(/([^=]+)=['"]?([^'"]*)['"]?$/);
+
+						// SSS FIXME: Do we continue to support this?
+						// If yes, how do we represent this in data-mw format?
+						// If no, we need to figure to if there are templates
+						// with this use-case, and how common that is.
 						if (m) {
-							contentType = "objectAttr"; // both key and value
 							newK = m[1];
 							if (kArr.constructor === String) {
 								newA.v = m[2];
@@ -225,64 +192,42 @@ AttributeExpander.prototype._returnAttributes = function ( token, cb, newAttrs )
 							}
 						}
 					}
-					newA.k = newK;
-				}
 
-				if ( updatedK ) {
-					modified = true;
-					metaObjType = updatedK.metaObjType;
-					if (metaObjType) {
-						producerObjType = metaObjType;
+					if (updatedK.hasGeneratedContent) {
 						// newK can be an array
-						var key = newK.constructor === String ? newK : Util.tokensToString(newK);
-						metaTokens.push( makeTplAffectedMeta(contentType, key, updatedK) );
+						if (newK.constructor !== String) {
+							var key = Util.tokensToString(newK);
+							tmpDataMW.set(key, {
+								k: { "txt": key, "html": newA.k },
+								v: { "html": newA.v }
+							});
+						}
 					}
+
+					modified = true;
+					newA.k = newK;
 				}
 			} else if (newK !== a.k) {
 				modified = true;
 			}
 
-
 			// We have a string key and potentially expanded value.
 			// Check if the value came from a template/extension expansion.
 			if (newK.constructor === String && a.v.constructor === Array) {
 				modified = true;
-				if (newK.match( /mw\:maybeContent/ ) ) {
-					// For mw:maybeContent attributes, at this point, we do not really know
-					// what this attribute represents.
-					//
-					// - For regular links and images [[Foo|bar]], this attr (bar) represents
-					//   link text which transforms to a DOM sub-tree. If 'bar' comes from
-					//   a template, we can let template meta tags stay in the DOM sub-tree.
-					//
-					// - For categories [[Category:Foo|bar]], this attr (bar) is just a sort
-					//   key that will be part of the href attr and will not be a DOM subtree.
-					//   If 'bar' comes from a template, we have to strip all meta tags from
-					//   the token stream of 'bar' and add new meta tags outside the category
-					//   token recording the fact that the sort key in the href came from
-					//   a template.
-					//
-					// We have to wait for all templates to be expanded before we know the
-					// context (wikilink/category) this attr is showing up in. So, if this
-					// attr has been generated by a template/extension, keep around both the
-					// original as well as the stripped versions of the template-generated
-					// attr, and in the link handler, we will pick the right version.
+				if (!newK.match(/^mw:/)) {
 					updatedV = stripMetaTags( newA.v, this.options.wrapTemplates );
-					metaObjType = updatedV.metaObjType;
-					if (metaObjType) {
-						kv = new KV('mw:valAffected', [
-							metaObjType,
-							makeTplAffectedMeta("objectAttrVal", newK, updatedV)
-						]);
-						newAttrs.push( kv );
+					if (updatedV.hasGeneratedContent) {
+						if (!tmpDataMW.get(newK)) {
+							tmpDataMW.set(newK,  {
+								k: { "txt": newK },
+								v: { "html": newA.v }
+							});
+						}
 					}
-				} else if (!newK.match(/^mw:/)) {
-					updatedV = stripMetaTags( newA.v, this.options.wrapTemplates );
-					newA.v = updatedV.value;
-					metaObjType = updatedV.metaObjType;
-					if (metaObjType) {
-						producerObjType = metaObjType;
-						metaTokens.push( makeTplAffectedMeta("objectAttrVal", newK, updatedV) );
+
+					if (!newK.match(/^mw:/)) {
+						newA.v = updatedV.value;
 					}
 				}
 			} else if (newA.v !== a.v) {
@@ -291,8 +236,6 @@ AttributeExpander.prototype._returnAttributes = function ( token, cb, newAttrs )
 		}
 	}
 
-	var tokens = [];
-
 	// clone the token and update attrs
 	if (modified) {
 		// dont clone attribs since they will be replaced
@@ -300,37 +243,64 @@ AttributeExpander.prototype._returnAttributes = function ( token, cb, newAttrs )
 		token.attribs = newAttrs;
 		token.dataAttribs = Util.clone(token.dataAttribs);
 
-		// Update metatoken info
-		l = metaTokens.length;
-		if (l > 0) {
-			var tokenId = token.getAttribute( 'about' );
+		// If the token already has an about, it already has transclusion/extension
+		// wrapping. No need to record information about templated attributes in addition.
+		//
+		// FIXME: If there is a real use case for extension attributes getting
+		// template, this check can be relaxed to allow that.
+		// https://gerrit.wikimedia.org/r/#/c/65575 has some reference code that
+		// can be used then.
+		if ( !token.getAttribute( 'about' ) && tmpDataMW.size > 0) {
+			var manager = this.manager;
 
-			if ( !tokenId ) {
-				tokenId = this.manager.env.newAboutId();
-				token.addAttribute("about", tokenId);
-				var objType;
-				var producerObjTypeMatch = producerObjType.match(
-						"mw:(Transclusion|Param|Extension)"
+			// Flatten k-v pairs
+			var vals = [];
+			tmpDataMW.forEach(function(obj) {
+				vals = vals.concat([obj.k, obj.v]);
+			});
+
+			// Async-expand all token arrays to DOM
+			cb( { async: true } );
+			async.map(
+				vals,
+				function(k_or_v, cb1) {
+					// Build to DOM
+					if (k_or_v.html && k_or_v.html.constructor === Array) {
+						Util.processAttributeToDOM(
+							manager,
+							k_or_v.html,
+							function(dom) {
+								k_or_v.html = dom.body.innerHTML;
+								cb1(null, k_or_v);
+							}
 						);
-				if (producerObjTypeMatch) {
-					objType = producerObjTypeMatch[1];
-				} else {
-					objType = producerObjType.substring("mw:Includes/".length);
-				}
-				token.addSpaceSeparatedAttribute("typeof", "mw:ExpandedAttrs/" + objType);
-			}
+					} else {
+						cb1(null, k_or_v);
+					}
+				},
+				function(err, eVals) {
+					// Rebuild flattened k-v pairs
+					var expandedAttrs = [];
+					for (var i = 0; i < eVals.length; i += 2) {
+						expandedAttrs.push([eVals[i], eVals[i+1]]);
+					}
 
-			for (i = 0; i < l; i++) {
-				metaTokens[i].addAttribute("about", tokenId);
-			}
+					// Mark token as having expanded attrs
+					token.addAttribute("about", manager.env.newAboutId());
+					token.addSpaceSeparatedAttribute("typeof", "mw:ExpandedAttrs");
+					token.addAttribute("data-mw", JSON.stringify({
+						attribs: expandedAttrs
+					}));
+					cb( { tokens: [token] });
+				}
+			);
+
+			return;
 		}
-		tokens = metaTokens;
 		// console.warn("NEW TOK: " + JSON.stringify(token));
 	}
 
-	tokens.push(token);
-
-	cb( { tokens: tokens } );
+	cb( { tokens: [token] } );
 };
 
 if (typeof module === "object") {

js/lib/ext.core.LinkHandler.js
@@ -18,6 +18,7 @@ var PegTokenizer = require('./mediawiki.tokenizer.peg.js').PegTokenizer,
 	DU = require('./mediawiki.DOMUtils.js').DOMUtils;
 // define some constructor shortcuts
 var KV = defines.KV,
+    EOFTk = defines.EOFTk,
     TagTk = defines.TagTk,
     SelfclosingTagTk = defines.SelfclosingTagTk,
     EndTagTk = defines.EndTagTk,
@@ -242,6 +243,8 @@ function buildLinkAttrs(attrs, getLinkText, rdfaType, linkAttrs) {
 				rdfaType = rdfaType ? rdfaType + " " + v : v;
 			} else if (k.trim() === "about") {
 				about = v;
+			} else if (k.trim() === "data-mw") {
+				newAttrs.push(kv);
 			}
 		}
 	}
@@ -359,10 +362,11 @@ WikiLinkHandler.prototype.renderWikiLink = function (token, frame, cb, target) {
  * normally rendered in a box at the bottom of an article.
  */
 WikiLinkHandler.prototype.renderCategory = function (token, frame, cb, target) {
-
 	var tokens = [],
 		newTk = new SelfclosingTagTk('link'),
 		content = this.addLinkAttributesAndGetContent(newTk, token, target);
+
+	var env = this.manager.env;
 
 	// Change the rel to be mw:WikiLink / Category
 	Util.lookupKV( newTk.attribs, 'rel' ).v += '/Category';
@@ -379,30 +383,34 @@ WikiLinkHandler.prototype.renderCategory = function (token, frame, cb, target) {
 
 	tokens.push( newTk );
 
-	// Deal with sort keys generated via templates/extensions
-	var producerInfo = Util.lookupKV( token.attribs, 'mw:valAffected' );
-	if (producerInfo) {
-		// Ensure that the link has about set
-		var about = Util.lookup( newTk.attribs, 'about' );
-		if (!about) {
-			about = this.manager.env.newAboutId();
-			newTk.addAttribute("about", about);
-		}
+	if (content.length === 1) {
+		cb({tokens: tokens});
+	} else {
+		// Deal with sort keys that come from generated content (transclusions, etc.)
+		cb( { async: true } );
+		Util.processAttributeToDOM(
+			this.manager,
+			content,
+			function(dom) {
+				var sortKeyInfo = [ {"txt": "mw:sortKey"}, {"html": dom.body.innerHTML} ],
+					dataMW = newTk.getAttribute("data-mw");
+				if (dataMW) {
+					dataMW = JSON.parse(dataMW);
+					dataMW.attribs.push(sortKeyInfo);
+				} else {
+					dataMW = { attribs: [sortKeyInfo] };
+				}
 
-		// Update typeof
-		newTk.addSpaceSeparatedAttribute("typeof",
-				"mw:ExpandedAttrs/" + producerInfo.v[0].match(/mw:(.*)/)[1]);
+				// Mark token as having expanded attrs
+				newTk.addAttribute("about", env.newAboutId());
+				newTk.addSpaceSeparatedAttribute("typeof", "mw:ExpandedAttrs");
+				newTk.addAttribute("data-mw", JSON.stringify(dataMW));
 
-		// Update producer meta-token and add it to the token stream
-		var metaToken = producerInfo.v[1];
-		metaToken.addAttribute("about", about);
-		var propKV = Util.lookupKV(metaToken.attribs, "property");
-		propKV.v = propKV.v.replace(/mw:maybeContent/, 'mw:sortKey'); // keep it clean
-		tokens.push(metaToken);
+				cb( { tokens: tokens } );
+			}
+		);
 	}
-	cb({tokens: tokens});
 };
-
 
 /**
  * Render a language link. Those normally appear in the list of alternate
@@ -432,7 +440,7 @@ WikiLinkHandler.prototype.renderInterwikiLink = function (token, frame, cb, targ
 
 	var tokens = [],
 		newTk = new TagTk('a', [], token.dataAttribs),
-		content = this.addLinkAttributesAndGetContent(newTk, token, target);
+		content = this.addLinkAttributesAndGetContent(newTk, token, target, true);
 
 	// We set an absolute link to the article in the other wiki/language
 	var absHref = target.interwiki.url.replace( "$1", target.href );

js/lib/mediawiki.Util.js
@@ -844,6 +844,21 @@ var Util = {
 		pipeline.process(content, opts.tplArgs ? opts.tplArgs.cacheKey : undefined);
 	},
 
+	processAttributeToDOM: function(manager, content, cb) {
+		this.processContentInPipeline(
+			manager,
+			content.concat([new pd.EOFTk()]), {
+				pipelineType: "tokens/x-mediawiki/expanded",
+				pipelineOpts: {
+					inBlockToken: true,
+					noPre: true,
+					wrapTemplates: true
+				},
+				documentCB: cb
+			}
+		);
+	},
+
 	extractExtBody: function(extName, extTagSrc) {
 		var re = "<" + extName + "[^>]*/?>([\\s\\S]*)";
 		return extTagSrc.replace(new RegExp(re, "mi"), function() {

commit d3e5d034a7df82e055c052adfb739c6c242d6d5d

Bug 57707: Hotfix, don't push undefined onto wikitext array
This might have been broken in 283bfd55, but this patch just works around the
breakage for now by not pushing undefined onto the wikitext array. Proper
investigation and fix to be done.

Change-Id: Ic1706c8da69c0046b1acd39f1e8b3de55a3fee92

js/lib/ext.core.AttributeExpander.js
@@ -34,7 +34,7 @@ function stripMetaTags( tokens, wrapTemplates ) {
 		if ([TagTk, SelfclosingTagTk].indexOf(token.constructor) !== -1) {
 			// Strip all meta tags.
 			if (wrapTemplates) {
-				if (inInclude) {
+				if (inInclude && token.dataAttribs.src) {
 					wikitext.push(token.dataAttribs.src);
 				}
 
@@ -46,7 +46,9 @@ function stripMetaTags( tokens, wrapTemplates ) {
 						inTpl = typeMatch[1].match(/Transclusion|Param|Extension/);
 						inInclude = !inTpl;
 						hasGeneratedContent = true;
-						wikitext.push(token.dataAttribs.src);
+						if (token.dataAttribs.src) {
+							wikitext.push(token.dataAttribs.src);
+						}
 					} else {
 						inTpl = false;
 						inInclude = false;