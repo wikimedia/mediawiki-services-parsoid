eab5718a716d1c1952a7ec02fafc2900c4a424ba
Marc Ordinas i Llopis <mail@marcoil.org>
2013-12-30 20:13:36 +0100

    Add a script to manage a cluster of round-trip testing clients
    rtclient-cluster.js simply starts as many rt clients as cores present, by
    default, and restarts them if they die. To change the number of rtclients,
    use the -c argument.
    
    Change-Id: I52c5bd01168d3147e5854822f99f93a48ec21bdd

---
+++ tests/client/rtclient-cluster.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var cluster = require( 'cluster' ),
+	path = require( 'path' );
+
+var opts = require( 'optimist' )
+	.default( {
+		// By default, start one rtclient + api server per core.
+		c: require( 'os' ).cpus().length
+	} )
+	.alias( 'c', 'children' ).argv;
+
+if ( !module.parent ) {
+	var numClients = opts.c;
+
+	cluster.setupMaster( {
+		exec: path.join( __dirname, 'client.js' ),
+		args: opts._
+	} );
+
+	console.log( "rtclient-cluster initializing", numClients, "rtclients");
+	for ( var i = 0; i < numClients; i++ ) {
+		cluster.fork();
+	}
+
+	cluster.on( 'exit', function ( worker, code, signal ) {
+		if ( !worker.suicide ) {
+			var exitCode = worker.process.exitCode;
+			console.log( 'rtclient', worker.process.pid,
+				'died (' + exitCode + '), restarting.' );
+			cluster.fork();
+		}
+	} );
+
+	var shutdown_cluster = function () {
+		console.log( 'rtclient cluster shutting down, killing all rtclients' );
+		var workers = cluster.workers;
+		Object.keys( workers ).forEach( function ( id ) {
+			console.log( 'Killing rtclient', id );
+			workers[ id ].destroy();
+		} );
+		console.log( 'Done killing rtclients, exiting rtclient-cluster.' );
+		process.exit( 0 );
+	};
+
+	process.on( 'SIGINT', shutdown_cluster );
+	process.on( 'SIGTERM', shutdown_cluster );
+}
