wikimedia/mediawiki-services-parsoid

commit 2c30cbfa9eeb3ab8d6cbcd9a4eb59dff728b00ea

(Bug 59674) Emit non-piped links for edited redirect links
* Added a new parser test which passes.

Change-Id: Ifa35e99a44af6792bdff92e7a139cb3ce374326b

lib/mediawiki.WikitextSerializer.js
@@ -1388,12 +1388,11 @@ var getLinkRoundTripData = function( env, node, state ) {
 	} else if ( node.childNodes.length ) {
 		rtData.contentNode = node;
 	} else if ( /^mw:PageProp\/redirect$/.test( rtData.type ) ) {
+		rtData.isRedirect = true;
 		rtData.tail = '';
 		rtData.prefix = dp.src ||
 			( ( env.conf.wiki.mwAliases.redirect[0] || '#REDIRECT' ) + ' ' );
-		rtData.content.string = dp.content ||
-			(dp.sa && dp.sa.href) ||
-			(href || '').replace(/^(\.\.?\/)*/, '').replace(/_/g, ' ');
+		rtData.target.value = rtData.target.value.replace(/^(\.\.?\/)*/, '').replace(/_/g, ' ');
 	}
 
 	return rtData;
@@ -2032,7 +2031,9 @@ WSP.linkHandler = function(node, state, cb) {
 			// The string value of the content, if it is plain text.
 			var linkTarget;
 
-			if ( this._isSimpleWikiLink(env, dp, target, linkData) ) {
+			if ( linkData.isRedirect ) {
+				cb( linkData.prefix + '[[' + target.value + ']]', node );
+			} else if ( this._isSimpleWikiLink(env, dp, target, linkData) ) {
 				// Simple case
 				if (!target.modified && !linkData.contentModified) {
 					linkTarget = target.value;
@@ -2045,7 +2046,6 @@ WSP.linkHandler = function(node, state, cb) {
 				cb( linkData.prefix + '[[' + linkTarget + ']]' + linkData.tail, node );
 				return;
 			} else {
-
 				var usePipeTrick = this._usePipeTrick(env, dp, target, linkData);
 
 				// First get the content source

commit 07203b441d0d2f0801de83d162eef55172c82b25

Fix RT-testing regressions caused by 2c30cbfa
* #REDIRECT [[Main_Page]] was being RT-ed back in canonical
  form #REDIRECT [[Main Page]]. This showed up in RT testing
  where enwiki:Elizabeth_Truman and a bunch of other pages
  showing dirty diffs.

* This patch fixes that breakage.

* Added new tests to capture additional redirect variants.

Change-Id: I5d0020e774d8a2a92be11f1fcf9a08895bce8d65

lib/mediawiki.WikitextSerializer.js
@@ -1389,10 +1389,8 @@ var getLinkRoundTripData = function( env, node, state ) {
 		rtData.contentNode = node;
 	} else if ( /^mw:PageProp\/redirect$/.test( rtData.type ) ) {
 		rtData.isRedirect = true;
-		rtData.tail = '';
 		rtData.prefix = dp.src ||
 			( ( env.conf.wiki.mwAliases.redirect[0] || '#REDIRECT' ) + ' ' );
-		rtData.target.value = rtData.target.value.replace(/^(\.\.?\/)*/, '').replace(/_/g, ' ');
 	}
 
 	return rtData;
@@ -2032,7 +2030,13 @@ WSP.linkHandler = function(node, state, cb) {
 			var linkTarget;
 
 			if ( linkData.isRedirect ) {
-				cb( linkData.prefix + '[[' + target.value + ']]', node );
+				linkTarget = target.value;
+				if (target.modified || !target.fromsrc) {
+					linkTarget = linkTarget.replace(/^(\.\.?\/)*/, '').replace(/_/g, ' ');
+					linkTarget = escapeWikiLinkContentString(linkTarget,
+						state, linkData.contentNode);
+				}
+				cb( linkData.prefix + '[[' + linkTarget + ']]', node );
 			} else if ( this._isSimpleWikiLink(env, dp, target, linkData) ) {
 				// Simple case
 				if (!target.modified && !linkData.contentModified) {