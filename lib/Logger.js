wikimedia/mediawiki-services-parsoid

commit fcfb47b38e5f0df3debc79e65c6531397460613e

A few tweaks to the logging functionality.
* Addresses a few (not all) review comments from
  Maria's structured logging patch.

* Removed changeLogTypes -- setting a new logger in
  parse.js and parserTests.js where necessary.

* Updated default log types to "error" and "fatal".

* Tweaked the trace test generating regexp to add a (\/|$) check
  to all log types so that env.log("fatalities", ..) does not match
  the "fatal" logtype. All subtypes are always matched by default
  so an explicit "fatal/request" is not required in default logtypes.

* Fixed --trace output breakage by adding back checks from the
  previous patch into the default backend.

* Unrelated: arranged requires in alphabetical order in parserTests.js.

Change-Id: I0ae4d51f90dcf5e507bd509f303362c4974ae93a

lib/Logger.js
@@ -18,14 +18,15 @@ var LogData = require('./LogData.js').LogData,
  * @param {MWParserEnvironment} env
  * @param {boolean} dontRegisterDefaultBackend
  */
-var Logger = function(env, dontRegisterDefaultBackend) {
+var Logger = function(env, opts) {
+	if (!opts) { opts = {}; }
 	this._env = env;
 	this._logRequestQueue = [];
 	this._backends = new Map();
-	this._defaultLogLevels = ["error", "warning", "fatal", "fatal\/request"];
-	this._traceTestRegExp = this._traceTest();
+	this._defaultLogLevels = opts.logLevels || ["error", "fatal"];
+	this._traceTestRegExp = this._buildTraceTestRE();
 
-	if (!dontRegisterDefaultBackend) {
+	if (!opts.dontRegisterDefaultBackend) {
 		this.registerBackend(this._traceTestRegExp, this._defaultBackend);
 	}
 };
@@ -78,18 +79,6 @@ Logger.prototype.log = function (logType) {
 /**
  * @method
  *
- * Enables us to turn logging on and off for different logTypes,
- * by changing defaultLogTypes.
- * @param {String} logType
- */
-Logger.prototype.changeLogTypes = function (newLogLevels) {
-	this._defaultLogLevels = Array.prototype.slice.call(arguments);
-	this._traceTestRegExp = this._traceTest();
-};
-
-/**
- * @method
- *
  * Registers a backend by adding it to the collection of backends.
  * @param {RegExp} logType
  * @param {Function} backend Backend to send logging / tracing info to.
@@ -112,21 +101,20 @@ Logger.prototype.registerBackend = function (logType, backend) {
  * Generates regexp to test if logType is a valid logType.
  * If the logType is not valid, the logger remains silent.
  */
-Logger.prototype._traceTest = function() {
-	var traceTypes = "";
-	var debugOn = "";
-	var escapedTraceFlags;
+Logger.prototype._buildTraceTestRE = function() {
+	var fixFlag = function(flag) { return Util.escapeRegExp(flag) + "(\\/|$)"; };
+
+	var flagsArray = this._defaultLogLevels.map(fixFlag);
+	if (this._env.conf.parsoid.debug) {
+		flagsArray.push(fixFlag("debug"));
+	}
 
 	if (this._env.conf.parsoid.traceFlags !== null) {
-		escapedTraceFlags = this._env.conf.parsoid.traceFlags.map(Util.escapeRegExp);
-		traceTypes = "|trace\/(" + escapedTraceFlags.join("|") + ")";
+		var escapedTraceFlags = this._env.conf.parsoid.traceFlags.map(Util.escapeRegExp);
+		flagsArray.push("trace\/(" + escapedTraceFlags.join("|") + ")(\\/|$)");
 	}
 
-	if (this._env.conf.parsoid.debug) {
-		debugOn = "|debug";
-	}
-
-	return new RegExp(this._defaultLogLevels.join("|") + traceTypes + debugOn);
+	return new RegExp(flagsArray.join("|"));
 };
 
 /**
@@ -138,11 +126,19 @@ Logger.prototype._traceTest = function() {
  */
 Logger.prototype._defaultBackend = function(logData, cb) {
 	try {
-		console.warn(logData.fullMsg());
-		cb();
+		var logType = logData.logType;
+		var msg = logData.msg();
+		if (/^(error|warning)(\/|$)/.test(logType)) {
+			msg += "\n" + this.locationMsg();
+		}
+		if (/(^(error|fatal)|(^|\/)stacktrace)(\/|$)/.test(logType)) {
+			msg += "\n" + this.stack();
+		}
+		console.warn(msg);
 	} catch (e) {
-		cb();
 		return;
+	} finally {
+		cb();
 	}
 };
 

lib/LogData.js
@@ -18,7 +18,7 @@ var util = require('util');
 var LogData = function (env, logType, logObject) {
 	this.logType = logType;
 	this.logObject = logObject;
-	
+
 	this._env = env;
 	// Cache log information if previously constructed.
 	this._cache = {};
@@ -131,7 +131,9 @@ LogData.prototype._flatten = function(o, topLevel) {
 	var f, stack, msg, longMsg,
 	self = this;
 
-	if ( Array.isArray(o) && topLevel ) {
+	if ( typeof(o) === 'undefined' || o === null ) {
+		return { msg: '' };
+	} else if ( Array.isArray(o) && topLevel ) {
 		// flatten components, but no longer in a top-level context.
 		f = o.map(function(oo) { return self._flatten(oo); });
 		// join all the messages with spaces or newlines between them.
@@ -148,8 +150,6 @@ LogData.prototype._flatten = function(o, topLevel) {
 			msg: msg,
 			longMsg: longMsg
 		});
-	} else if ( typeof(o) === 'undefined' || o === null ) {
-		return { msg: '' };
 	} else if (o instanceof Error) {
 		f = { msg: o.message, stack: o.stack };
 		if ( o.code ) {

lib/mediawiki.parser.environment.js
@@ -21,8 +21,6 @@ var util = require('util');
  * @param {WikiConfig/null} wikiConfig
  */
 var MWParserEnvironment = function ( parsoidConfig, wikiConfig ) {
-	var self = this;
-
 	// page information
 	this.page = {
 		name: 'Main_Page',
@@ -75,8 +73,7 @@ var MWParserEnvironment = function ( parsoidConfig, wikiConfig ) {
 	// Outstanding page requests (for templates etc)
 	this.requestQueue = {};
 
-	this.logger = new Logger(self, false);
-	this.log = this.logger.log.bind(this.logger);
+	this.setLogger(new Logger(this));
 };
 
 // Cache for wiki configurations, shared between requests.
@@ -158,12 +155,10 @@ MWParserEnvironment.prototype.setPageSrcInfo = function ( src_or_metadata ) {
 	}
 };
 
-/**
- * @property {Object} conf
- * @property {WikiConfig} conf.wiki
- * @property {ParsoidConfig} conf.parsoid
- */
-
+MWParserEnvironment.prototype.setLogger = function (logger) {
+	this.logger = logger;
+	this.log = this.logger.log.bind(this.logger);
+};
 
 /**
  * @method

tests/parse.js
@@ -12,6 +12,7 @@ var ParserEnv = require('../lib/mediawiki.parser.environment.js').MWParserEnviro
 	TemplateRequest = require('../lib/mediawiki.ApiRequest.js').TemplateRequest,
 	Util = require('../lib/mediawiki.Util.js').Util,
 	DU = require('../lib/mediawiki.DOMUtils.js').DOMUtils,
+	Logger = require('../lib/Logger.js').Logger,
 	optimist = require('optimist'),
 	fs = require('fs');
 
@@ -124,8 +125,11 @@ var ParserEnv = require('../lib/mediawiki.parser.environment.js').MWParserEnviro
 
 	ParserEnv.getParserEnv( parsoidConfig, null, prefix, argv.page || null, null, function ( err, env ) {
 		if ( err !== null ) {
-			env.log("fatal", err);
+			console.error(err);
+			return;
 		}
+
+		env.setLogger(new Logger(env, { logLevels: ["fatal", "error", "warning"] }));
 
 		// fetch templates from enwiki by default.
 		if ( argv.wgScriptPath ) {

tests/parserTests.js
@@ -14,20 +14,21 @@
 
 require('../lib/core-upgrade.js');
 
-var fs = require('fs'),
-	path = require('path'),
+var apiServer = require( './apiServer.js' ),
+	async = require( 'async' ),
 	colors = require('colors'),
-	Util = require( '../lib/mediawiki.Util.js' ).Util,
-	DU = require('../lib/mediawiki.DOMUtils.js').DOMUtils,
 	childProc = require('child_process'),
 	fork = childProc.fork,
-	DOMUtils = require( '../lib/mediawiki.DOMUtils.js' ).DOMUtils,
-	async = require( 'async' ),
-	PEG = require('pegjs'),
-	Alea = require('alea'),
+	fs = require('fs'),
+	path = require('path'),
 	// Handle options/arguments with optimist module
 	optimist = require('optimist'),
-	apiServer = require( './apiServer.js' );
+	Alea = require('alea'),
+	DU = require('../lib/mediawiki.DOMUtils.js').DOMUtils,
+	DOMUtils = require( '../lib/mediawiki.DOMUtils.js' ).DOMUtils,
+	Logger = require('../lib/Logger.js').Logger,
+	PEG = require('pegjs'),
+	Util = require( '../lib/mediawiki.Util.js' ).Util;
 
 // Fetch up some of our wacky parser bits...
 
@@ -1508,8 +1509,8 @@ ParserTests.prototype.main = function ( options ) {
 		// to be basically empty, since the parserTests environment is very bare.
 		this.env = env;
 
-		if (booleanOption( options.quiet )) {
-			this.env.logger.changeLogTypes("fatal", "error");
+		if (!booleanOption( options.quiet )) {
+			env.setLogger(new Logger(env, { logLevels: ["fatal", "error", "warning"] }));
 		}
 
 		this.env.conf.parsoid.editMode = options.editMode;

commit 923f367752cf9e36428e65fc6dc1f8ced61e59a8

Fix default backend that I broke in fcfb47b3.
Change-Id: I3b7398f8fbfa739b1871156aad6031a05e801114

lib/Logger.js
@@ -129,13 +129,14 @@ Logger.prototype._defaultBackend = function(logData, cb) {
 		var logType = logData.logType;
 		var msg = logData.msg();
 		if (/^(error|warning)(\/|$)/.test(logType)) {
-			msg += "\n" + this.locationMsg();
+			msg += "\n" + logData.locationMsg();
 		}
 		if (/(^(error|fatal)|(^|\/)stacktrace)(\/|$)/.test(logType)) {
-			msg += "\n" + this.stack();
+			msg += "\n" + logData.stack();
 		}
 		console.warn(msg);
 	} catch (e) {
+		console.error("Error in defaultBackend: " + e);
 		return;
 	} finally {
 		cb();