wikimedia/mediawiki-services-parsoid

commit 54b4f37bbfbcabe28abae060bfba5c7b8acde55d

(Bug 64017) Template encapsulation bug fixes + cleanup.
getDOMRange
-----------
* After a template range was identified, range.start was being moved
  ahead past any meta-tags (since these meta tags would get deleted
  and data-mw would be set on other elements). However, this led to
  subtle bugs (in fostered content scenarios) where the new DOM range
  order didn't reflect the original source wikitext order.

* The real fix (which should have been there all along) is to leave
  the DOM range unaltered, but in the last step of the algorithm,
  move ahead to find the first non-meta node to add data-mw to.

* This patch makes both these fixes. In turn, this required:
  1. useless span-wrapping-eliding code (Ex: {{echo|foo}} scenarios)
     to get extracted into a helper and applied in the last step.
  2. meta-tag dsr info to be transferred to the first element of template
     content in nested-transclusion scenarios (even though this shouldn't
     have mattered). The presence of this info was being exploited
     by the strip-double-tds pass.

* For purposes of code clarity, reversed the polarity of setting
  range.flipped flag (see code comment).

findTopLevelNonOverlappingRanges
--------------------------------
* Generalized range overlapping code once more and got rid of a
  long-standing FIXME which had been ignored till now.

encapsulateTemplates
--------------------
* Handle the rare case where range.end might not be an ELEMENT.
* Some minor code cleanup for code readability reasons.
* Walk past meta-marker nodes to find the data-mw target node.
* Cleaned up the code and reorganized a bunch of variables into
  an encapInfo object to keep related information together.
* Added a FIXME about a possible bug for further investigation.

findWrappableTemplateRanges
---------------------------
* While debugging some selser failures and wt2html changes, noticed
  a FIXME that alerted to a different regexp in that function and

lib/dom.wrapTemplates.js
@@ -21,6 +21,90 @@ var DU = require('./mediawiki.DOMUtils.js').DOMUtils,
 	dumpDOM = require('./dom.dumper.js').dumpDOM,
 	Util = require('./mediawiki.Util.js').Util;
 
+function expandRangeToAvoidSpanWrapping(range, knownTextContent) {
+	// SSS FIXME: Later on, if safe, we could consider expanding the
+	// range unconditionally rather than only if a span is required.
+
+	var mightAddSpan = knownTextContent;
+	if (knownTextContent === undefined) {
+		var n = range.start;
+		if (DU.isTplMarkerMeta(n)) {
+			n = n.nextSibling;
+		}
+		mightAddSpan = DU.isText(n);
+	}
+
+	var expandable = false;
+	if (mightAddSpan) {
+		// See if we can expand the range to the parent node.
+		// Eliminates useless spanning of wikitext of the form: {{echo|foo}}
+		// where the the entire template content is contained in a paragraph.
+		var contentParent = range.start.parentNode;
+		expandable = true
+			&& DU.hasNodeName(contentParent, 'p')
+			&& !DU.isLiteralHTMLNode(contentParent)
+			&& contentParent.firstChild === range.startElem
+			&& contentParent.lastChild === range.endElem
+			&& contentParent === range.end.parentNode;
+
+		if (expandable) {
+			range.start = contentParent;
+			range.end = contentParent;
+		}
+	}
+
+	return expandable;
+}
+
+function updateDSRForFirstTplNode(source, target) {
+	var srcDP = DU.getDataParsoid( source );
+	var tgtDP = DU.getDataParsoid( target );
+
+	// Since TSRs on template content tokens are cleared by the
+	// template handler, all computed dsr values for template content
+	// is always inferred from top-level content values and is safe.
+	// So, do not overwrite a bigger end-dsr value.
+	if (srcDP.dsr && (tgtDP.dsr && tgtDP.dsr[1] > srcDP.dsr[1])) {
+		tgtDP.dsr[0] = srcDP.dsr[0];
+	} else {
+		tgtDP.dsr = Util.clone( srcDP.dsr );
+		tgtDP.src = srcDP.src;
+	}
+}
+
+function getRangeEndDSR(range) {
+	var endNode = range.end;
+	if (DU.isElt(endNode)) {
+		return (DU.getDataParsoid(endNode) || {}).dsr;
+	} else {
+		// In the rare scenario where the last element of a range is not an ELEMENT,
+		// extrapolate based on DSR of first leftmost sibling that is an ELEMENT.
+		// We don't try any harder than this for now.
+		var offset = 0;
+		var n = endNode.previousSibling;
+		while (n && !DU.isElt(n)) {
+			if (DU.isText(n)) {
+				offset += n.data.length;
+			} else {
+				offset += n.data.length + 7;
+			}
+			n = n.previousSibling;
+		}
+
+		var dsr = null;
+		if (n) {
+			dsr = (DU.getDataParsoid(n) || {}).dsr;
+		}
+
+		if (dsr && typeof(dsr[1]) === 'number') {
+			var len = DU.isText(endNode) ? endNode.data.length : endNode.data.length + 7;
+			dsr = [dsr[1] + offset, dsr[1] + offset + len];
+		}
+
+		return dsr;
+	}
+}
+
 /**
  * Find the common DOM ancestor of two DOM nodes
  */
@@ -60,7 +144,6 @@ function getDOMRange( env, doc, startElem, endMeta, endElem ) {
 			// widen the scope to include the full subtree
 			range.root = startElem;
 			range.start = startElem.firstChild;
-			// SSS FIXME: startElem.lastChild might not be an ELEMENT_NODE
 			range.end = startElem.lastChild;
 			break;
 		} else if (i > 0) {
@@ -73,19 +156,11 @@ function getDOMRange( env, doc, startElem, endMeta, endElem ) {
 		parentNode = elem.parentNode;
 	}
 
-	var updateDP = false;
-	var tcStart = range.start;
-
-	// Skip meta-tags
-	if (startElemIsMeta && tcStart === startElem) {
-		tcStart = tcStart.nextSibling;
-		range.start = tcStart;
-		updateDP = true;
-	}
-
 	// Ensure range.start is an element node since we want to
 	// add/update the data-parsoid attribute to it.
-	if (tcStart.nodeType === doc.COMMENT_NODE || tcStart.nodeType === doc.TEXT_NODE) {
+	var updateDP = false;
+	var tcStart = range.start;
+	if (!DU.isElt(tcStart)) {
 		var skipSpan = false,
 			tcStartParent = tcStart.parentNode;
 
@@ -124,22 +199,10 @@ function getDOMRange( env, doc, startElem, endMeta, endElem ) {
 				updateDP = false;
 			}
 		} else {
-			// See if we can go up one level.
-			//
-			// Eliminates useless spanning of wikitext of the form: {{echo|foo}}
-			// where the the entire template content is contained in a paragraph.
-			if (tcStartParent.firstChild === startElem &&
-				tcStartParent.lastChild === endElem &&
-				tcStartParent === range.end.parentNode)
-			{
-				if (DU.hasNodeName(tcStartParent, 'p') && !DU.isLiteralHTMLNode(tcStartParent)) {
-					tcStart = tcStartParent;
-					range.end = tcStartParent;
-					skipSpan = true;
-				}
-			}
-
 			updateDP = true;
+			if (expandRangeToAvoidSpanWrapping(range, true)) {
+				skipSpan = true;
+			}
 		}
 
 		if (!skipSpan) {
@@ -153,27 +216,20 @@ function getDOMRange( env, doc, startElem, endMeta, endElem ) {
 	}
 
 	if (updateDP) {
-		var done = false;
-
-		var tcDP = DU.getDataParsoid( tcStart );
-		var seDP = DU.getDataParsoid( startElem );
-
-		// Since TSRs on template content tokens are cleared by the
-		// template handler, all computed dsr values for template content
-		// is always inferred from top-level content values and is safe.
-		// So, do not overwrite a bigger end-dsr value.
-		if (seDP.dsr && (tcDP.dsr && tcDP.dsr[1] > seDP.dsr[1])) {
-			tcDP.dsr[0] = seDP.dsr[0];
-			done = true;
-		}
-
-		if (!done) {
-			tcDP.dsr = Util.clone( seDP.dsr );
-			tcDP.src = seDP.src;
-		}
+		updateDSRForFirstTplNode(startElem, tcStart);
+	} else if (tcStart === startElem && DU.isElt(tcStart.nextSibling)) {
+		// HACK!
+		// The strip-double-tds pass has a HACK that requires DSR and src
+		// information being set on this element node. So, this HACK here
+		// is supporting that HACK there.
+		//
+		// (The parser test for bug 50603 will fail without this fix)
+		updateDSRForFirstTplNode(startElem, tcStart.nextSibling);
 	}
 
-	if (DU.inSiblingOrder(range.end, range.start)) {
+	// Use the negative test since it doesn't mark the range as flipped
+	// if range.start === range.end
+	if (!DU.inSiblingOrder(range.start, range.end)) {
 		// In foster-parenting situations, the end-meta tag (and hence range.end)
 		// can show up before the range.start which would be the table itself.
 		// So, we record this info for later analysis.
@@ -253,6 +309,12 @@ function findTopLevelNonOverlappingRanges(document, env, docRoot, tplRanges) {
 			elt = nestingInfo[elt];
 		}
 		return false;
+	}
+
+	function rangesOverlap(prev, curr) {
+		var prevEnd   = !r.flipped ? prev.end : prev.start;
+		var currStart = !r.flipped ? curr.start : curr.end;
+		return DU.inSiblingOrder(currStart, prevEnd);
 	}
 
 	var i, r, n, e, data, ranges;
@@ -431,7 +493,7 @@ function findTopLevelNonOverlappingRanges(document, env, docRoot, tplRanges) {
 				}
 				recordTemplateInfo(compoundTpls, enclosingRangeId, r, argInfo);
 			}
-		} else if (prev && !r.flipped && DU.inSiblingOrder(r.start, prev.end)) {
+		} else if (prev && rangesOverlap(prev, r)) {
 			// In the common case, in overlapping scenarios, r.start is
 			// identical to prev.end. However, in fostered content scenarios,
 			// there can true overlap of the ranges.
@@ -440,9 +502,6 @@ function findTopLevelNonOverlappingRanges(document, env, docRoot, tplRanges) {
 			// Overlapping ranges.
 			// r is the regular kind
 			// Merge r with prev
-			//
-			// SSS FIXME: What if r is the flipped kind?
-			// Does that require special treatment?
 
 			startTagToStrip = r.startElem;
 			endTagToRemove = prev.endElem;
@@ -488,23 +547,24 @@ function findTopLevelNonOverlappingRanges(document, env, docRoot, tplRanges) {
 function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 	var i, numRanges = tplRanges.length;
 	for (i = 0; i < numRanges; i++) {
-		var span,
-			range = tplRanges[i],
-			n = !range.flipped ? range.start : range.end,
+		var range = tplRanges[i];
+
+		expandRangeToAvoidSpanWrapping(range);
+
+		var n = !range.flipped ? range.start : range.end,
 			e = !range.flipped ? range.end : range.start,
 			startElem = range.startElem,
 			about = startElem.getAttribute('about');
 
 		while (n) {
 			var next = n.nextSibling;
-
-			if ( n.nodeType === n.TEXT_NODE || n.nodeType === n.COMMENT_NODE ) {
+			if (!DU.isElt(n)) {
 				// Dont add span-wrappers in fosterable positions
 				//
 				// NOTE: there cannot be any non-IEW text in fosterable position
 				// since the HTML tree builder would already have fostered it out.
 				if (!DU.isFosterablePosition(n)) {
-					span = doc.createElement( 'span' );
+					var span = doc.createElement( 'span' );
 					span.setAttribute( 'about', about );
 					// attach the new span to the DOM
 					n.parentNode.insertBefore( span, n );
@@ -512,7 +572,7 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 					span.appendChild( n );
 					n = span;
 				}
-			} else if (DU.isElt(n)) {
+			} else {
 				n.setAttribute( 'about', about );
 			}
 
@@ -523,20 +583,37 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 			n = next;
 		}
 
-		// update type-of
-		var tcStart = range.start;
-		var tcEnd = range.end;
-		if (startElem !== tcStart) {
+		// SSS FIXME: is it a bug that tplArray can be null?
+		// To be separately investigated.
+		//
+		// Encap. info for the range
+		var encapInfo = {
+			valid: false,
+			target: range.start,
+			tplArray: tplArrays[range.id],
+			datamw: null,
+			dp: null
+		};
+
+		// Skip template-marker meta-tags
+		while (DU.isTplMarkerMeta(encapInfo.target)) {
+			encapInfo.target = encapInfo.target.nextSibling;
+		}
+
+		// Update type-of (always even if tpl-encap below will fail).
+		// This ensures that VE will still "edit-protect" this template
+		// and not allow its content to be edited directly.
+		if (startElem !== encapInfo.target) {
 			var t1 = startElem.getAttribute("typeof"),
-				t2 = tcStart.getAttribute("typeof");
-			tcStart.setAttribute("typeof", t2 ? t1 + " " + t2 : t1);
+				t2 = encapInfo.target.getAttribute("typeof");
+			encapInfo.target.setAttribute("typeof", t2 ? t1 + " " + t2 : t1);
 		}
 
 /*
 		console.log("startElem: " + startElem.outerHTML);
 		console.log("endElem: " + range.endElem.outerHTML);
-		console.log("tcStart: " + tcStart.outerHTML);
-		console.log("tcEnd: " + tcEnd.outerHTML);
+		console.log("range.start: " + range.start.outerHTML);
+		console.log("range.end: " + range.end.outerHTML);
 */
 
 		/* ----------------------------------------------------------------
@@ -546,34 +623,33 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 		 * 1. In the common case, dp2.dsr[1] will be > dp1.dsr[1]
 		 *    If so, new range = dp1.dsr[0], dp2.dsr[1]
 		 *
-		 * 2. But, foster parenting can complicate this when tcEnd is a table
-		 *    and tcStart has been fostered out of the table (tcEnd).
+		 * 2. But, foster parenting can complicate this when range.end is a table
+		 *    and range.start has been fostered out of the table (range.end).
 		 *    But, we need to verify this assumption.
 		 *
 		 *    2a. If dp2.dsr[0] is smaller than dp1.dsr[0], this is a
-		 *        confirmed case of tcStart being fostered out of tcEnd.
+		 *        confirmed case of range.start being fostered out of range.end.
 		 *
 		 *    2b. If dp2.dsr[0] is unknown, we rely on fostered flag on
-		 *        tcStart, if any.
+		 *        range.start, if any.
 		 * ---------------------------------------------------------------- */
-		var dp1 = DU.getDataParsoid( tcStart ),
-			dp2 = DU.getDataParsoid( tcEnd ),
-			done = false,
-			datamw;
+		var dp1 = Util.clone(DU.getDataParsoid(range.start)),
+			dp2DSR = getRangeEndDSR(range);
+
 		/*
 		console.warn("dp1: " + JSON.stringify(dp1));
-		console.warn("dp2: " + JSON.stringify(dp2));
+		console.warn("dp2: " + JSON.stringify(dp2DSR));
 		*/
 		if (dp1.dsr) {
-			if (dp2.dsr) {
+			if (dp2DSR) {
 				// Case 1. above
-				if (dp2.dsr[1] > dp1.dsr[1]) {
-					dp1.dsr[1] = dp2.dsr[1];
+				if (dp2DSR[1] > dp1.dsr[1]) {
+					dp1.dsr[1] = dp2DSR[1];
 				}
 
 				// Case 2. above
-				var endDsr = dp2.dsr[0];
-				if (DU.hasNodeName(tcEnd, 'table') &&
+				var endDsr = dp2DSR[0];
+				if (DU.hasNodeName(range.end, 'table') &&
 					endDsr !== null &&
 					(endDsr < dp1.dsr[0] || dp1.fostered))
 				{
@@ -581,69 +657,66 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 				}
 			}
 
-			// Check if now have a useable range on dp1
-			if (dp1.dsr[0] !== null && dp1.dsr[1] !== null) {
-				dp1.src = env.page.src.substring( dp1.dsr[0], dp1.dsr[1] );
-				done = true;
-			}
+			// encapsulation possible only if dp1.dsr is valid
+			encapInfo.valid = dp1.dsr[0] !== null && dp1.dsr[1] !== null;
 		}
 
-		if (done) {
-			var tplArray = tplArrays[range.id];
-			if (tplArray) {
-				// Add any leading wikitext
-				var firstTplInfo = tplArray[0];
-				if (firstTplInfo.dsr[0] > dp1.dsr[0]) {
-					tplArray = [{ wt: env.page.src.substring(dp1.dsr[0], firstTplInfo.dsr[0]) }].concat(tplArray);
-				}
-
-				// Add any trailing wikitext
-				var lastTplInfo = tplArray[tplArray.length-1];
-				if (lastTplInfo.dsr[1] < dp1.dsr[1]) {
-					tplArray.push({ wt: env.page.src.substring(lastTplInfo.dsr[1], dp1.dsr[1]) });
-				}
-
-				// Extract the key orders for the templates
-				var paramInfoArrays = [];
-				/* jshint loopfunc: true */ // yes, this function is in a loop
-				tplArray.forEach(function(a) {
-					if (a.paramInfos) {
-						paramInfoArrays.push(a.paramInfos);
-					}
-				});
-
-				// Map the array of { dsr: .. , args: .. } objects to just the args property
-				/* jshint loopfunc: true */ // yes, this function is in a loop
-				var infoIndex = 0;
-				tplArray = tplArray.map(function(a) {
-					if (a.wt) {
-						return a.wt;
-					} else {
-						// Remember the position of the transclusion relative
-						// to other transclusions. Should match the index of
-						// the corresponding private metadata in paramInfoArrays
-						// above.
-						if (a.args) { // XXX: not sure why args can be undefined here
-							a.args.i = infoIndex;
-						}
-						infoIndex++;
-						return {template: a.args};
-					}
-				});
-
-				// Output the data-mw obj.
-				datamw = { parts: tplArray };
-				tcStart.setAttribute("data-mw", JSON.stringify(datamw));
-				DU.getDataParsoid( tcStart ).pi = paramInfoArrays;
+		var tplArray = encapInfo.tplArray;
+		if (encapInfo.valid && tplArray) {
+			// Add any leading wikitext
+			var firstTplInfo = tplArray[0];
+			if (firstTplInfo.dsr[0] > dp1.dsr[0]) {
+				tplArray = [{ wt: env.page.src.substring(dp1.dsr[0], firstTplInfo.dsr[0]) }].concat(tplArray);
 			}
-		} else {
 
+			// Add any trailing wikitext
+			var lastTplInfo = tplArray[tplArray.length-1];
+			if (lastTplInfo.dsr[1] < dp1.dsr[1]) {
+				tplArray.push({ wt: env.page.src.substring(lastTplInfo.dsr[1], dp1.dsr[1]) });
+			}
+
+			// Extract the key orders for the templates
+			var paramInfoArrays = [];
+			/* jshint loopfunc: true */ // yes, this function is in a loop
+			tplArray.forEach(function(a) {
+				if (a.paramInfos) {
+					paramInfoArrays.push(a.paramInfos);
+				}
+			});
+
+			// Map the array of { dsr: .. , args: .. } objects to just the args property
+			/* jshint loopfunc: true */ // yes, this function is in a loop
+			var infoIndex = 0;
+			tplArray = tplArray.map(function(a) {
+				if (a.wt) {
+					return a.wt;
+				} else {
+					// Remember the position of the transclusion relative
+					// to other transclusions. Should match the index of
+					// the corresponding private metadata in paramInfoArrays
+					// above.
+					if (a.args) { // XXX: not sure why args can be undefined here
+						a.args.i = infoIndex;
+					}
+					infoIndex++;
+					return {template: a.args};
+				}
+			});
+
+			// Set up dsr[0], dsr[1], and data-mw on the target node
+			encapInfo.dp = DU.getDataParsoid( encapInfo.target );
+			encapInfo.datamw = { parts: tplArray };
+			encapInfo.target.setAttribute("data-mw", JSON.stringify(encapInfo.datamw));
+			encapInfo.dp.pi = paramInfoArrays;
+		} else if (!encapInfo.valid) {
 			var errors = [ "Do not have necessary info. to encapsulate Tpl: " + i ];
 			errors.push( "Start Elt : " + startElem.outerHTML );
 			errors.push( "End Elt   : " + range.endElem.innerHTML );
 			errors.push( "Start DSR : " + JSON.stringify(dp1 || {}) );
-			errors.push( "End   DSR : " + JSON.stringify(dp2 || {}) );
+			errors.push( "End   DSR : " + JSON.stringify(dp2DSR || []) );
 			env.log( "error", errors.join("\n"));
+		} else {
+			env.log( "error", "Missing data-mw for tpl: " + i);
 		}
 
 		// Make DSR range zero-width for fostered templates after
@@ -661,17 +734,32 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 		// 2. In both cases, should we mark these uneditable by adding
 		//    mw:Placeholder to the typeof?
 		if (dp1.fostered) {
-			datamw = DU.getDataMw(tcStart);
-			if (!datamw || !datamw.parts || datamw.parts.length === 1) {
+			encapInfo.datamw = DU.getDataMw(encapInfo.target);
+			if (!encapInfo.datamw || !encapInfo.datamw.parts || encapInfo.datamw.parts.length === 1) {
 				dp1.dsr[1] = dp1.dsr[0];
 			}
+		}
+
+		// Update DSR after fostering-related fixes are done.
+		if (encapInfo.valid) {
+			if (!encapInfo.dp) {
+				// This wouldn't have been initialized if tplArray was null
+				encapInfo.dp = DU.getDataParsoid( encapInfo.target );
+			}
+			// encapInfo.dp points to DU.getDataParsoid(encapInfo.target)
+			// and all updates below update properties in that object tree.
+			if (!encapInfo.dp.dsr) {
+				encapInfo.dp.dsr = dp1.dsr;
+			} else {
+				encapInfo.dp.dsr[0] = dp1.dsr[0];
+				encapInfo.dp.dsr[1] = dp1.dsr[1];
+			}
+			encapInfo.dp.src = env.page.src.substring( encapInfo.dp.dsr[0], encapInfo.dp.dsr[1] );
 		}
 
 		// Remove startElem (=range.startElem) if a meta.  If a meta,
 		// it is guaranteed to be a marker meta added to mark the start
 		// of the template.
-		// However, tcStart (= range.start), even if a meta, need not be
-		// a marker meta added for the template.
 		if (DU.hasNodeName(startElem, "meta") &&
 				/(?:^|\s)mw:(:?Transclusion|Param)(?=$|\s)/.test(startElem.getAttribute('typeof'))) {
 			DU.deleteNode(startElem);
@@ -709,7 +797,8 @@ function findWrappableTemplateRanges( doc, env, root, tpls ) {
 
 		if ( DU.isElt(elem) ) {
 			var type = elem.getAttribute( 'typeof' ),
-				// SSS FIXME: This regexp differs from that in isTplMetaType
+				// FIXME: This regexp is repeated in DOMUtils.isTplMetaType
+				// DRY it out, if possible.
 				metaMatch = type ? type.match( /(?:^|\s)(mw:(?:Transclusion|Param)(\/[^\s]+)?)(?=$|\s)/ ) : null;
 
 			// Ignore templates without tsr.

lib/mediawiki.DOMUtils.js
@@ -467,7 +467,7 @@ DOMUtils = DU = {
 	 * @param {string} nType
 	 */
 	isTplMetaType: function(nType)  {
-		return (/(?:^|\s)mw:Transclusion(\/[^\s]+)*(?=$|\s)/).test(nType);
+		return (/(?:^|\s)(mw:(?:Transclusion|Param)(\/[^\s]+)?)(?=$|\s)/).test(nType);
 	},
 
 	/**

commit a5a116266edd10663dd92ffdfdc324f3df3d1939

Fix crasher in template wrapping (introduced in 54b4f37bb).
Change-Id: Id640287cc1107d6491b850c863a1980dc111ef76

lib/dom.wrapTemplates.js
@@ -596,7 +596,10 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 		};
 
 		// Skip template-marker meta-tags
-		while (DU.isTplMarkerMeta(encapInfo.target)) {
+		// Also, skip past comments/text nodes found in fosterable positions
+		// which wouldn't have been span-wrapped in the while-loop above.
+		while (DU.isTplMarkerMeta(encapInfo.target) || !DU.isElt(encapInfo.target)) {
+			console.assert(DU.isElt(encapInfo.target) || DU.isFosterablePosition(encapInfo.target));
 			encapInfo.target = encapInfo.target.nextSibling;
 		}
 