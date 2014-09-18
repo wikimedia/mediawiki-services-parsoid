wikimedia/mediawiki-services-parsoid

commit 6e302233f41a1aa8646ed2a03a84d879081d3bf0

(Bug 70894) Fix bugs serializing modified wikilinks
* Fixed serialization of simple category links (strip leading "./" and
  normalize '_' to ' ').

* Fixed detection of isSimpleWikiLink case by normalizing hrefs
  (by replacing '_' to ' ' first).

* Found a missing /g/ flag on a regexp replace. A number
  of previously blacklisted tests change their output because
  of this. The new output is correct.

* Added a new parser test.

Change-Id: I61cf366c481591f21d92983b941bc87e4923cd15

lib/wts.LinkHandler.js
@@ -193,8 +193,8 @@ var isSimpleWikiLink = function(env, dp, target, linkData) {
 	var contentString = linkData.content.string,
 		canUseSimple = false;
 
-	// Would need to pipe for any non-string content
-	// Preserve unmodified or non-minimal piped links
+	// Would need to pipe for any non-string content.
+	// Preserve unmodified or non-minimal piped links.
 	if ( contentString !== undefined
 		&& ( target.modified
 			|| linkData.contentModified
@@ -203,7 +203,7 @@ var isSimpleWikiLink = function(env, dp, target, linkData) {
 		// Strip colon escapes from the original target as that is
 		// stripped when deriving the content string.
 		var strippedTargetValue = target.value.replace(/^:/, ''),
-			decodedTarget = Util.decodeURI(Util.decodeEntities(strippedTargetValue)),
+			decodedTarget = Util.decodeURI(Util.decodeEntities(strippedTargetValue)).replace(/_/g, ' '),
 			hrefHasProto = /^\w+:\/\//.test(linkData.href);
 
 		// See if the (normalized) content matches the
@@ -400,7 +400,8 @@ var linkHandler = function(node, state, cb) {
 					// in the LinkHandler
 					linkData.content.string = '';
 				} else { // No sort key, will serialize to simple link
-					linkData.content.string = target.value;
+					// Normalize the content string
+					linkData.content.string = target.value.replace(/^\.\//, '').replace(/_/g, ' ');
 				}
 
 				// Special-case handling for template-affected sort keys
@@ -490,7 +491,7 @@ var linkHandler = function(node, state, cb) {
 				if (target.modified || !target.fromsrc) {
 					linkTarget = linkTarget.replace(/^(\.\.?\/)*/, '');
 					if (!linkData.isInterwiki) {
-						linkTarget = linkTarget.replace('_', ' ');
+						linkTarget = linkTarget.replace(/_/g, ' ');
 					}
 					escapedRes = escapeWikiLinkContentString(linkTarget,
 						state, node);

commit 7d3da3df43c0e40964dc0236e2e63cde10282279

(Bug 70894) Fix regressions introduced by 6e302233 (found in RT-testing)
* While testing for 'simple-link' serialization, normalize "_" chars
  in both content-string and link-target strings.
* Fixes regressions found in testing.
* No changes to parser test results (TODO: Add new tests).

Change-Id: I172cb0c7fb86e7271a25d3d2043b31dfa23891ee

lib/wts.LinkHandler.js
@@ -203,8 +203,13 @@ var isSimpleWikiLink = function(env, dp, target, linkData) {
 		// Strip colon escapes from the original target as that is
 		// stripped when deriving the content string.
 		var strippedTargetValue = target.value.replace(/^:/, ''),
-			decodedTarget = Util.decodeURI(Util.decodeEntities(strippedTargetValue)).replace(/_/g, ' '),
+			decodedTarget = Util.decodeURI(Util.decodeEntities(strippedTargetValue)),
 			hrefHasProto = /^\w+:\/\//.test(linkData.href);
+
+		// Normalize content string and decoded target before comparison.
+		// Piped links don't come down this path => it is safe to normalize both.
+		contentString = contentString.replace(/_/g, ' ');
+		decodedTarget = decodedTarget.replace(/_/g, ' ');
 
 		// See if the (normalized) content matches the
 		// target, either shadowed or actual.
@@ -288,6 +293,7 @@ var linkHandler = function(node, state, cb) {
 	if ( linkData.type !== null && linkData.target.value !== null  ) {
 		// We have a type and target info
 
+		// SSS FIXME: Do we still need this or can we purge these old types now??
 		// Temporary backwards-compatibility for types
 		if (linkData.type === 'mw:WikiLink/Category') {
 			linkData.type = 'mw:PageProp/Category';