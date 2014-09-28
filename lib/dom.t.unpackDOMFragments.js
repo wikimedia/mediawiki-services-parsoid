wikimedia/mediawiki-services-parsoid

commit 0fe59757f0c8e8d4227d072360b34d232e2c1c41

(Bug 53109) Move data-mw away from manual json attr loading
 * This is both faster and consistent with how we're accessing other
   parsoid attributes. It's also a step towards not having this data in
   the html output.

 * Changes to parserTests and the blacklist are for attribute order.

 * Requires upgrading domino to 1.0.18
   https://github.com/fgnass/domino/pull/48

Change-Id: I1edbc260887d480adf04763b15043c374e27cceb

lib/dom.t.unpackDOMFragments.js
@@ -55,7 +55,7 @@ function fixUpMisnestedTagDSR(targetNode, fragment, env) {
 					dp.dsr = [currOffset, currOffset];
 				}
 				dp.misnested = true;
-				DU.setDataParsoid( node, dp );
+				DU.storeDataParsoid( node, dp );
 			} else if ( dp.tmp.wrapper ) {
 				// Unnecessary wrapper added above -- strip it.
 				var next = node.nextSibling;
@@ -75,7 +75,7 @@ function fixUpMisnestedTagDSR(targetNode, fragment, env) {
 			if ( DU.isElt( node ) ) {
 				var dp = DU.getDataParsoid( node );
 				if ( dp.dsr ) {
-					DU.setDataParsoid( node, dp );
+					DU.storeDataParsoid( node, dp );
 				}
 			}
 			return true;
@@ -196,7 +196,7 @@ function unpackDOMFragments(env, node) {
 
 			// Transfer mw:Transclusion typeof
 			if (/(?:^|\s)mw:Transclusion(?=$|\s)/.test(typeOf)) {
-				contentNode.setAttribute("data-mw", node.getAttribute("data-mw"));
+				DU.setDataMw(contentNode, Util.clone(DU.getDataMw(node)));
 				DU.addTypeOf(contentNode, "mw:Transclusion");
 			}
 

lib/mediawiki.DOMPostProcessor.js
@@ -95,9 +95,11 @@ if (testDom.body.getAttribute('somerandomstring') === '') {
 function prepareDOM( dpp, env, node ) {
 	if ( DU.isElt( node ) ) {
 
-		// Load data parsoid and remove it from the DOM.
+		// Load data-(parsoid|mw) and remove them from the DOM.
 		DU.loadDataParsoid( node );
 		node.removeAttribute( "data-parsoid" );
+		DU.loadDataMw( node );
+		node.removeAttribute( "data-mw" );
 
 		// Set title to display when present (last one wins).
 		if ( DU.hasNodeName(node, "meta") &&

lib/mediawiki.DOMUtils.js
@@ -243,13 +243,8 @@ DOMUtils = DU = {
 		return typeOfs.split(' ').indexOf(type) !== -1;
 	},
 
-	/**
-	 * Decode a JSON object into the data member of DOM nodes
-	 *
-	 * @param {Node} node
-	 * @param {string} name We'll use the data-[name] attribute of the passed-in node as the new value.
-	 * @param {Mixed} defaultVal What to use if there is no JSON attribute by that name.
-	 */
+	// Direct manipulation of the nodes for load and store.
+
 	loadDataAttrib: function(node, name, defaultVal) {
 		if ( !isElt(node) ) {
 			return;
@@ -261,15 +256,12 @@ DOMUtils = DU = {
 		return data[name];
 	},
 
-	/**
-	 * Save all node.data.* structures to data attributes
-	 */
 	saveDataAttribs: function(node) {
 		if ( !isElt(node) ) {
 			return;
 		}
 		var data = this.getNodeData( node );
-		for(var key in data) {
+		for (var key in data) {
 			if ( key.match( /^tmp_/ ) !== null ) {
 				continue;
 			}
@@ -283,15 +275,31 @@ DOMUtils = DU = {
 		}
 	},
 
-	/**
-	 * Decode data-parsoid into node.data.parsoid
-	 */
+	// Load and stores the data as JSON attributes on the nodes.
+	// These should only used when transferring between pipelines
+	// ie. when the serialized nodes will lose their .dataobject's
+
 	loadDataParsoid: function ( node ) {
 		var dp = this.loadDataAttrib( node, 'parsoid', {} );
 		if ( isElt( node ) && !dp.tmp ) {
 			dp.tmp = {};
 		}
 	},
+
+	loadDataMw: function ( node ) {
+		var mw = this.loadDataAttrib( node, 'mw', {} );
+	},
+
+	storeDataParsoid: function(node, dpObj) {
+		return this.setJSONAttribute( node, "data-parsoid", dpObj );
+	},
+
+	storeDataMw: function(node, dmObj) {
+		return this.setJSONAttribute( node, "data-mw", dmObj );
+	},
+
+	// The following getters and setters load from the .dataobject store,
+	// with the intention of eventually moving them off the nodes themselves.
 
 	getDataParsoid: function ( node ) {
 		var data = this.getNodeData( node );
@@ -304,19 +312,21 @@ DOMUtils = DU = {
 	getDataMw: function ( node ) {
 		var data = this.getNodeData( node );
 		if ( !data.mw ) {
-			this.loadDataAttrib( node, 'mw', {} );
+			this.loadDataMw( node );
 		}
 		return data.mw;
 	},
 
-	/**
-	 * Set the data-parsoid attribute on a node.
-	 *
-	 * @param {Object} dpObj The new value for data-parsoid
-	 * @returns {Node} `node`, with the attribute set on it
-	 */
 	setDataParsoid: function(node, dpObj) {
-		return this.setJSONAttribute( node, "data-parsoid", dpObj );
+		var data = this.getNodeData( node );
+		data.parsoid = dpObj;
+		return data.parsoid;
+	},
+
+	setDataMw: function(node, dmObj) {
+		var data = this.getNodeData( node );
+		data.mw = dmObj;
+		return data.mw;
 	},
 
 	getNodeData: function ( node ) {
@@ -1907,7 +1917,7 @@ DOMUtils = DU = {
 	// Generates a unique id with the following format:
 	//   mw<base64-encoded counter>
 	// but attempts to keep user defined ids.
-	storeDataParsoid: function ( node, dp ) {
+	stripDataParsoid: function ( node, dp ) {
 		var uid = node.id,
 			document = node.ownerDocument,
 			docDp = this.getDataParsoid( document );

lib/dom.cleanup.js
@@ -57,13 +57,18 @@ function cleanupAndSaveDataParsoid( env, node ) {
 
 			dp.tagId = undefined;
 
+			var validDM = !!Object.keys(DU.getDataMw(node)).length;
+			if ( !validDM ) {
+				// strip it
+				DU.setDataMw(node, undefined);
+			}
+
 			// Remove data-parsoid.src from templates and extensions that have
 			// valid data-mw and dsr.  This should reduce data-parsoid bloat.
 			//
 			// Transcluded nodes will not have dp.tsr set and dont need dp.src either
 			if (/(?:^|\s)mw:(Transclusion|Extension)(?=$|\s)/.test(node.getAttribute("typeof")) &&
-				(!dp.tsr ||
-				node.getAttribute("data-mw") && dp.dsr && dp.dsr[0] && dp.dsr[1]))
+				(!dp.tsr || validDM && dp.dsr && dp.dsr[0] && dp.dsr[1]))
 			{
 				dp.src = undefined;
 			}
@@ -87,7 +92,7 @@ function cleanupAndSaveDataParsoid( env, node ) {
 			}
 
 			if ( env.conf.parsoid.storeDataParsoid ) {
-				DU.storeDataParsoid( node, dp );
+				DU.stripDataParsoid( node, dp );
 			}
 		}
 		DU.saveDataAttribs( node );

lib/dom.linter.js
@@ -30,8 +30,8 @@ var DU = require('./mediawiki.DOMUtils.js').DOMUtils,
 
 function logTransclusions(env, c) {
 
-	if (DU.getDataMw(c)) {
-		var dmw = DU.getDataMw(c);
+	var dmw = DU.getDataMw(c);
+	if ( dmw ) {
 		var dsr = DU.getDataParsoid(c).dsr;
 		if (dmw.parts) {
 			var parts = dmw.parts, lintObj;

lib/dom.t.TableFixups.js
@@ -68,16 +68,12 @@ TableFixups.prototype.stripDoubleTDs = function (env, node) {
 			nextNodeDSR[0] = nodeDSR[0];
 		}
 
-		// Now update data-mw
-		// XXX: use data.mw instead, see
-		// https://bugzilla.wikimedia.org/show_bug.cgi?id=53109
-		var dataMW = DU.getJSONAttribute(nextNode, 'data-mw'),
+		var dataMW = DU.getDataMw(nextNode),
 			nodeSrc = DU.getWTSource(env, node);
 		if (!dataMW.parts) {
 			dataMW.parts = [];
 		}
 		dataMW.parts.unshift(nodeSrc);
-		DU.setJSONAttribute(nextNode, 'data-mw', dataMW);
 
 		// Delete the duplicated <td> node.
 		node.parentNode.removeChild(node);
@@ -240,7 +236,7 @@ TableFixups.prototype.reparseTemplatedAttributes = function (env, node) {
 				// Lift up the about group to our td node.
 				node.setAttribute('typeof', transclusionNode.getAttribute('typeof'));
 				node.setAttribute('about', transclusionNode.getAttribute('about'));
-				var dataMW = DU.getJSONAttribute(transclusionNode, 'data-mw'),
+				var dataMW = DU.getDataMw(transclusionNode),
 					parts = dataMW.parts,
 					tnDP = DU.getDataParsoid( transclusionNode );
 
@@ -259,9 +255,7 @@ TableFixups.prototype.reparseTemplatedAttributes = function (env, node) {
 				}
 
 				// Save the new data-mw on the td node
-				// XXX: use data.mw instead, see
-				// https://bugzilla.wikimedia.org/show_bug.cgi?id=53109
-				DU.setJSONAttribute(node, 'data-mw', {parts: parts});
+				DU.setDataMw(node, { parts: parts });
 				dp.pi = tnDP.pi;
 
 				// Remove the span wrapper

lib/dom.t.handleLinkNeighbours.js
@@ -138,9 +138,8 @@ function handleLinkNeighbours( env, node ) {
 			dp.prefix = prefix.src;
 			if ( DU.isFirstEncapsulationWrapperNode( node ) ) {
 				// only necessary if we're the first
-				dataMW = DU.getJSONAttribute( node, 'data-mw' );
+				dataMW = DU.getDataMw( node );
 				dataMW.parts.unshift( prefix.src );
-				DU.setJSONAttribute( node, 'data-mw', dataMW );
 			}
 			if ( dp.dsr ) {
 				dp.dsr[0] -= prefix.src.length;
@@ -164,9 +163,8 @@ function handleLinkNeighbours( env, node ) {
 				// template encapsulation will handle it
 				var wrapper = DU.findFirstEncapsulationWrapperNode( node );
 				if ( wrapper !== null ) {
-					dataMW = DU.getJSONAttribute( wrapper, 'data-mw' );
+					dataMW = DU.getDataMw( wrapper );
 					dataMW.parts.push( trail.src );
-					DU.setJSONAttribute( wrapper, 'data-mw', dataMW );
 				}
 			}
 			if ( dp.dsr ) {

lib/dom.wrapTemplates.js
@@ -711,7 +711,7 @@ function encapsulateTemplates( doc, env, tplRanges, tplArrays) {
 			// Set up dsr[0], dsr[1], and data-mw on the target node
 			encapInfo.dp = DU.getDataParsoid( encapInfo.target );
 			encapInfo.datamw = { parts: tplArray };
-			encapInfo.target.setAttribute("data-mw", JSON.stringify(encapInfo.datamw));
+			DU.setDataMw( encapInfo.target, encapInfo.datamw );
 			encapInfo.dp.pi = paramInfoArrays;
 		} else if (!encapInfo.valid) {
 			var errors = [ "Do not have necessary info. to encapsulate Tpl: " + i ];

commit 9e793ebe02d0159783e64d7783e34a623518b10c

Fix rt crashers
 * 0fe59757f0c8e8d4227d072360b34d232e2c1c41 introduced a bug where
   data-mw wasn't being migrated between docs in the domFragment
   unpacker.

 * Here we generalize that process a bit since we're doing it for
   data-parsoid already.

 * Ex. _rt/hewiki/רחל_בוימוול?oldid=15504362

Change-Id: Ic7827bfb83b9ea01406628e1c20f216bfb85ef64

lib/dom.t.unpackDOMFragments.js
@@ -41,42 +41,39 @@ function fixUpMisnestedTagDSR(targetNode, fragment, env) {
 	var resetDSR = false,
 		currOffset = 0,
 		dsrFixer = new DOMTraverser(env, true);
-	dsrFixer.addHandler(null, function(node) {
-		if (DU.isElt(node)) {
-			if (node.nodeName === 'A') {
+	var fixHandler = function(node) {
+		if ( DU.isElt(node) ) {
+			var dp = DU.getDataParsoid( node );
+			if ( node.nodeName === 'A' ) {
 				resetDSR = true;
 			}
-
-			var dp = DU.getDataParsoid( node );
-			if (resetDSR) {
+			if ( resetDSR ) {
 				if (dp.dsr && dp.dsr[0]) {
 					currOffset = dp.dsr[1] = dp.dsr[0];
 				} else {
 					dp.dsr = [currOffset, currOffset];
 				}
 				dp.misnested = true;
-				DU.storeDataParsoid( node, dp );
 			} else if ( dp.tmp.wrapper ) {
 				// Unnecessary wrapper added above -- strip it.
-				var next = node.nextSibling;
+				var next = node.firstChild || node.nextSibling;
 				DU.migrateChildren(node, node.parentNode, node);
 				DU.deleteNode(node);
 				return next;
 			}
+			DU.saveDataAttribs(node);
 		}
-
 		return true;
-	});
+	};
+	dsrFixer.addHandler(null, fixHandler);
 	dsrFixer.traverse(fragment.firstChild);
+	fixHandler(fragment);
 
 	// Since targetNode will get re-organized, save data.parsoid
 	var dsrSaver = new DOMTraverser(env, true),
 		saveHandler = function(node) {
 			if ( DU.isElt( node ) ) {
-				var dp = DU.getDataParsoid( node );
-				if ( dp.dsr ) {
-					DU.storeDataParsoid( node, dp );
-				}
+				DU.saveDataAttribs(node);
 			}
 			return true;
 		};

lib/mediawiki.DOMPostProcessor.js
@@ -96,10 +96,7 @@ function prepareDOM( dpp, env, node ) {
 	if ( DU.isElt( node ) ) {
 
 		// Load data-(parsoid|mw) and remove them from the DOM.
-		DU.loadDataParsoid( node );
-		node.removeAttribute( "data-parsoid" );
-		DU.loadDataMw( node );
-		node.removeAttribute( "data-mw" );
+		DU.loadDataAttribs( node );
 
 		// Set title to display when present (last one wins).
 		if ( DU.hasNodeName(node, "meta") &&

lib/mediawiki.DOMUtils.js
@@ -275,6 +275,16 @@ DOMUtils = DU = {
 		}
 	},
 
+	loadDataAttribs: function(node) {
+		if ( !isElt(node) ) {
+			return;
+		}
+		[ "Parsoid", "Mw" ].forEach(function(attr) {
+			DU["loadData" + attr](node);
+			node.removeAttribute( "data-" + attr );
+		});
+	},
+
 	// Load and stores the data as JSON attributes on the nodes.
 	// These should only used when transferring between pipelines
 	// ie. when the serialized nodes will lose their .dataobject's
@@ -1282,6 +1292,7 @@ DOMUtils = DU = {
 		// is optional and domino does not support it.
 		function importNode(destDoc, n) {
 			var newN = destDoc.importNode(n);
+			DU.loadDataAttribs(newN);
 			n = n.firstChild;
 			while (n) {
 				newN.appendChild(importNode(destDoc, n));