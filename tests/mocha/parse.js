a3adad16d0b313ee6edb32afe8f7ac685aa89ea7
C. Scott Ananian <cscott@cscott.net>
2014-11-07 13:02:04 -0500

    Properly escape question marks in page titles in <head>.
    The unescaped question mark in the <base href> caused OCG problems
    on the page
    [[:enwikibooks:A-level Physics (Advancing Physics)/What is a wave?/Worked Solutions]]
    and it turns out that the RDF isVersionOf property link had the same
    buglet.
    
    Add a mocha test to sanity-check our wikilinks and resource links.
    
    Change-Id: I0f1b92ba569d82832038ccbc5cd722d6d9cb1aaf

--- tests/mocha/parse.js
+++ tests/mocha/parse.js
@@ -7,18 +7,20 @@ require("prfun");
 
 var should = require("chai").should();
 
+var url = require('url');
+
 var MWParserEnvironment = require('../../lib/mediawiki.parser.environment.js' ).MWParserEnvironment,
 	Util = require('../../lib/mediawiki.Util.js').Util,
 	ParsoidConfig = require('../../lib/mediawiki.ParsoidConfig' ).ParsoidConfig;
 
-describe( 'mediawiki.Util', function() {
-	var parsoidConfig = new ParsoidConfig( null,	{ defaultWiki: 'enwiki' } );
+describe( 'ParserPipelineFactory', function() {
+	var parsoidConfig = new ParsoidConfig( null, { defaultWiki: 'enwiki' } );
 
 	describe( 'parse()', function() {
 
-		var parse = function(src, expansions) {
+		var parse = function(src, page_name, expansions) {
 			return new Promise(function(resolve, reject) {
-				MWParserEnvironment.getParserEnv( parsoidConfig, null, 'enwiki', 'Main_Page', null, function ( err, env ) {
+				MWParserEnvironment.getParserEnv( parsoidConfig, null, 'enwiki', page_name || 'Main_Page', null, function ( err, env ) {
 					if (err) { return reject(err); }
 					env.setPageSrcInfo(src);
 					var pipeline = env.pipelineFactory;
@@ -29,7 +31,7 @@ describe( 'mediawiki.Util', function() {
 			});
 		};
 
-		it('should create a sane document from an empty string', function() {
+		it('should create a sane document from a short string', function() {
 			return parse('foo').then(function(doc) {
 				doc.should.have.property('nodeName', '#document');
 				doc.outerHTML.startsWith('<!DOCTYPE html><html').should.equal(true);
@@ -49,6 +51,41 @@ describe( 'mediawiki.Util', function() {
 				body.firstElementChild.nodeName.should.equal('P');
 				var p = doc.body.firstElementChild;
 				p.innerHTML.should.equal('foo');
+			});
+		});
+
+		it('should handle page titles with embedded ?', function() {
+			return parse('[[Foo?/Bar]] [[File:Foo.jpg]]', 'A/B?/C').then(function(doc) {
+				var els;
+				els = doc.querySelectorAll('HEAD > BASE[href]');
+				els.length.should.equal(1);
+				var basehref = els[0].getAttribute('href');
+				// ensure base is a prototocol-relative url
+				basehref = basehref.replace(/^https?:/, '');
+
+				// check wikilink
+				els = doc.querySelectorAll('A[rel="mw:WikiLink"][href]');
+				els.length.should.equal(1);
+				var ahref1 = els[0].getAttribute('href');
+				url.resolve(basehref, ahref1).should.equal(
+					'//en.wikipedia.org/wiki/Foo%3F/Bar'
+				);
+
+				// check image link
+				els = doc.querySelectorAll('*[typeof="mw:Image"] > A[href]');
+				els.length.should.equal(1);
+				var ahref2 = els[0].getAttribute('href');
+				url.resolve(basehref, ahref2).should.equal(
+					'//en.wikipedia.org/wiki/File:Foo.jpg'
+				);
+
+				// check image resource
+				els = doc.querySelectorAll('*[typeof="mw:Image"] IMG[resource]');
+				els.length.should.equal(1);
+				var ahref3 = els[0].getAttribute('resource');
+				url.resolve(basehref, ahref3).should.equal(
+					'//en.wikipedia.org/wiki/File:Foo.jpg'
+				);
 			});
 		});
 	});
