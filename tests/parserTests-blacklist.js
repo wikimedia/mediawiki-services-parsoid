wikimedia/mediawiki-services-parsoid

commit 220374b36ac70f100333050cffdfc6de8d51876e

Context aware parsing of definition list colon
 * Ignore inside tags to prevent illegal overlapping.

 * Follows findColonNoLinks in the php parser.

 * Adds a new test to document this behaviour, which exposes a bug in
   parsing templates in definition lists. Serialization is introducing
   unnecessary nowikis, which looks like they should be fixed by
   https://gerrit.wikimedia.org/r/#/c/178871/ but probably aren't..

Bug: T71219
Change-Id: I2d8403eb732fdb022e8c87a990fd392a62e7f477

lib/ext.core.ListHandler.js
@@ -5,12 +5,13 @@
 "use strict";
 var Util = require('./mediawiki.Util.js').Util,
 	defines = require('./mediawiki.parser.defines.js');
+
 // define some constructor shortcuts
 var NlTk = defines.NlTk,
     TagTk = defines.TagTk,
     EndTagTk = defines.EndTagTk;
 
-function ListHandler ( manager ) {
+function ListHandler( manager ) {
 	this.manager = manager;
 	this.listFrames = [];
 	this.init();
@@ -42,7 +43,9 @@ function newListFrame() {
 		endtags  : [], // Stack of end tags
 		// Partial DOM building heuristic
 		// # of open block tags encountered within list context
-		numOpenBlockTags: 0
+		numOpenBlockTags: 0,
+		// # of open tags encountered within list context
+		numOpenTags: 0
 	};
 }
 
@@ -77,6 +80,14 @@ ListHandler.prototype.onAny = function ( token, frame, prevToken ) {
 		}
 
 		return { token: token };
+	}
+
+	// Keep track of open tags per list frame in order to prevent colons
+	// starting lists illegally. Php's findColonNoLinks.
+	if ( token.constructor === TagTk ) {
+		this.currListFrame.numOpenTags += 1;
+	} else if ( token.constructor === EndTagTk && this.currListFrame.numOpenTags > 0 ) {
+		this.currListFrame.numOpenTags -= 1;
 	}
 
 	if (token.constructor === EndTagTk) {
@@ -125,7 +136,7 @@ ListHandler.prototype.onAny = function ( token, frame, prevToken ) {
 	if (token.constructor === TagTk) {
 		if (token.name === 'table') {
 			this.listFrames.push(this.currListFrame);
-			this.currListFrame = null;
+			this.reset();
 		} else if (Util.isBlockTag(token.name)) {
 			this.currListFrame.numOpenBlockTags++;
 		}
@@ -175,9 +186,15 @@ ListHandler.prototype.closeLists = function(token) {
 	return tokens;
 };
 
-ListHandler.prototype.onListItem = function ( token, frame, prevToken ) {
-	if (token.constructor === TagTk){
-		if (!this.currListFrame) {
+ListHandler.prototype.onListItem = function( token, frame, prevToken ) {
+	if ( token.constructor === TagTk ){
+		if ( this.currListFrame ) {
+			// Ignoring colons inside tags to prevent illegal overlapping.
+			// Attempts to mimic findColonNoLinks in the php parser.
+			if ( token.bullets.last() === ":" && this.currListFrame.numOpenTags > 0 ) {
+				return { token: ":" };
+			}
+		} else {
 			this.currListFrame = newListFrame();
 		}
 		// convert listItem to list and list item tokens

commit 9f46b320074b95c39b0ba309cb00ea311dccc83d

Open tags only affect line when parsing definition list colon
 * Regression introduced in 220374b36ac70f100333050cffdfc6de8d51876e

 * Added tidy to the test so php doesn't generate broken html.

 * Omitted a parsoid section because this test should still be failing.
   It had been noted that "[the test] exposes a bug in parsing templates
   in definition lists." Particularly,

     ;{{echo|a:b}}

   We need to introduce an inList flag when parsing !atTopLevel.

Bug: T87708
Change-Id: I087e5707d4b88f8dfb4568eaf6d8c63b219e17ee

lib/ext.core.ListHandler.js
@@ -8,8 +8,8 @@ var Util = require('./mediawiki.Util.js').Util,
 
 // define some constructor shortcuts
 var NlTk = defines.NlTk,
-    TagTk = defines.TagTk,
-    EndTagTk = defines.EndTagTk;
+	TagTk = defines.TagTk,
+	EndTagTk = defines.EndTagTk;
 
 function ListHandler( manager ) {
 	this.manager = manager;
@@ -134,6 +134,10 @@ ListHandler.prototype.onAny = function ( token, frame, prevToken ) {
 	if ( token.constructor === NlTk ) {
 		this.currListFrame.atEOL = true;
 		this.currListFrame.nlTk = token;
+		// php's findColonNoLinks is run in doBlockLevels, which examines
+		// the text line-by-line. At nltk, any open tags will cease having
+		// an effect.
+		this.currListFrame.numOpenTags = 0;
 		return { };
 	}
 
@@ -277,6 +281,7 @@ ListHandler.prototype.doListItem = function ( bs, bn, token ) {
 	this.env.log("trace/list", this.manager.pipelineId, function() {
 		return "    bs: " + JSON.stringify(bs) + "; bn: " + JSON.stringify(bn);
 	});
+
 	if (prefix.length === bs.length && bn.length === bs.length) {
 		this.env.log("trace/list", this.manager.pipelineId, "    -> no nesting change");
 

tests/parserTests-blacklist.js
@@ -50,7 +50,7 @@ add("wt2html", "Templates: Don't strip whitespace from positional-param values",
 add("wt2html", "Templates: Other wikitext in parameter names (bug 67657)", "<p about=\"#mwt1\" typeof=\"mw:Transclusion\" data-parsoid='{\"dsr\":[0,18,0,0],\"pi\":[[{\"k\":\"&#39;&#39;1&#39;&#39;\",\"named\":true,\"spc\":[\"\",\"\",\"\",\"\"]}]]}' data-mw='{\"parts\":[{\"template\":{\"target\":{\"wt\":\"echo\",\"href\":\"./Template:Echo\"},\"params\":{\"&#39;&#39;1&#39;&#39;\":{\"wt\":\"foo\"}},\"i\":0}}]}'>foo</p>");
 add("wt2html", "4. Indent-Pre and extension tags", "<pre data-parsoid='{\"dsr\":[0,39,1,0]}'>a <span typeof=\"mw:Extension/gallery\" about=\"#mwt1\" data-parsoid='{\"stx\":\"html\",\"src\":\"&lt;gallery>\\nFile:foobar.jpg\\n&lt;/gallery>\",\"tagWidths\":[9,10],\"dsr\":[3,39,9,10]}'>&lt;gallery>\nFile:foobar.jpg\n&lt;/gallery></span></pre>");
 add("wt2html", "6. Pre-blocks should extend across lines with leading WS even when there is no wrappable content", "<pre data-parsoid='{\"dsr\":[0,26,1,0]}'>a\n\n <!-- continue -->\nb</pre>\n\n<pre data-parsoid='{\"dsr\":[28,30,1,0]}'>c</pre>\n \n<p data-parsoid='{\"dsr\":[33,34,0,0]}'>d</p>");
-add("wt2html", "Definition Lists: colons occurring in tags", "<dl data-parsoid='{\"dsr\":[0,93,0,0]}'><dt data-parsoid='{\"dsr\":[0,2,1,0]}'>a</dt><dd data-parsoid='{\"stx\":\"row\",\"dsr\":[2,4,1,0]}'>b</dd>\n<dt data-parsoid='{\"dsr\":[5,15,1,0]}'><b data-parsoid='{\"dsr\":[6,15,3,3]}'>a:b</b></dt>\n<dt data-parsoid='{\"dsr\":[16,27,1,0]}'><i data-parsoid='{\"stx\":\"html\",\"dsr\":[17,27,3,4]}'>a:b</i></dt>\n<dt data-parsoid='{\"dsr\":[28,45,1,0]}'><span data-parsoid='{\"stx\":\"html\",\"dsr\":[29,45,6,7]}'>a:b</span></dt>\n<dt data-parsoid='{\"dsr\":[46,61,1,0]}'><div data-parsoid='{\"stx\":\"html\",\"dsr\":[47,61,5,6]}'>a:b</div></dt>\n<dt data-parsoid='{\"dsr\":[62,75,1,0]}'><span about=\"#mwt1\" typeof=\"mw:Transclusion\" data-parsoid='{\"pi\":[[{\"k\":\"1\",\"spc\":[\"\",\"\",\"\",\"\"]}]],\"dsr\":[63,75,null,null]}' data-mw='{\"parts\":[{\"template\":{\"target\":{\"wt\":\"echo\",\"href\":\"./Template:Echo\"},\"params\":{\"1\":{\"wt\":\"a:b\"}},\"i\":0}}]}'>a:b</span></dt>\n<dt data-parsoid='{\"dsr\":[76,93,1,0]}'><i about=\"#mwt2\" typeof=\"mw:Transclusion\" data-parsoid='{\"dsr\":[77,93,null,null],\"pi\":[[{\"k\":\"1\",\"spc\":[\"\",\"\",\"\",\"\"]}]]}' data-mw='{\"parts\":[{\"template\":{\"target\":{\"wt\":\"echo\",\"href\":\"./Template:Echo\"},\"params\":{\"1\":{\"wt\":\"&#39;&#39;a:b&#39;&#39;\"}},\"i\":0}}]}'>a:b</i></dt></dl>");
+add("wt2html", "Definition Lists: colons occurring in tags", "<dl data-parsoid='{\"dsr\":[0,110,0,0]}'><dt data-parsoid='{\"dsr\":[0,2,1,0]}'>a</dt><dd data-parsoid='{\"stx\":\"row\",\"dsr\":[2,4,1,0]}'>b</dd>\n<dt data-parsoid='{\"dsr\":[5,15,1,0]}'><b data-parsoid='{\"dsr\":[6,15,3,3]}'>a:b</b></dt>\n<dt data-parsoid='{\"dsr\":[16,27,1,0]}'><i data-parsoid='{\"stx\":\"html\",\"dsr\":[17,27,3,4]}'>a:b</i></dt>\n<dt data-parsoid='{\"dsr\":[28,45,1,0]}'><span data-parsoid='{\"stx\":\"html\",\"dsr\":[29,45,6,7]}'>a:b</span></dt>\n<dt data-parsoid='{\"dsr\":[46,61,1,0]}'><div data-parsoid='{\"stx\":\"html\",\"dsr\":[47,61,5,6]}'>a:b</div></dt>\n<dt data-parsoid='{\"dsr\":[62,69,1,0]}'><div data-parsoid='{\"stx\":\"html\",\"autoInsertedEnd\":true,\"dsr\":[63,69,5,0]}'>a</div></dt>\n<dd data-parsoid='{\"dsr\":[70,78,1,0]}'>b</dd>\n<dt data-parsoid='{\"dsr\":[79,92,1,0]}'><span about=\"#mwt1\" typeof=\"mw:Transclusion\" data-parsoid='{\"pi\":[[{\"k\":\"1\",\"spc\":[\"\",\"\",\"\",\"\"]}]],\"dsr\":[80,92,null,null]}' data-mw='{\"parts\":[{\"template\":{\"target\":{\"wt\":\"echo\",\"href\":\"./Template:Echo\"},\"params\":{\"1\":{\"wt\":\"a:b\"}},\"i\":0}}]}'>a:b</span></dt>\n<dt data-parsoid='{\"dsr\":[93,110,1,0]}'><i about=\"#mwt2\" typeof=\"mw:Transclusion\" data-parsoid='{\"dsr\":[94,110,null,null],\"pi\":[[{\"k\":\"1\",\"spc\":[\"\",\"\",\"\",\"\"]}]]}' data-mw='{\"parts\":[{\"template\":{\"target\":{\"wt\":\"echo\",\"href\":\"./Template:Echo\"},\"params\":{\"1\":{\"wt\":\"&#39;&#39;a:b&#39;&#39;\"}},\"i\":0}}]}'>a:b</i></dt></dl>");
 add("wt2html", "Definition Lists: colons and tables 1", "<dl data-parsoid='{\"dsr\":[0,21,0,0]}'><dd data-parsoid='{\"dsr\":[0,10,1,0]}'><table data-parsoid='{\"dsr\":[1,10,2,2]}'>\n<tbody data-parsoid='{\"dsr\":[4,8,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[4,7,0,0]}'><td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[4,7,1,0]}'> x</td></tr>\n</tbody></table></dd>\n<dd data-parsoid='{\"dsr\":[11,21,1,0]}'><table data-parsoid='{\"dsr\":[12,21,2,2]}'>\n<tbody data-parsoid='{\"dsr\":[15,19,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[15,18,0,0]}'><td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[15,18,1,0]}'> y</td></tr>\n</tbody></table></dd></dl>");
 add("wt2html", "Bug 2702: Mismatched <i>, <b> and <a> tags are invalid", "<p data-parsoid='{\"dsr\":[0,204,0,0]}'><i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[0,29,2,0]}'><a rel=\"mw:ExtLink\" href=\"http://example.com\" data-parsoid='{\"targetOff\":22,\"contentOffsets\":[22,28],\"dsr\":[2,29,20,1]}'>text<i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[26,28,2,0]}'></i></a></i>\n<a rel=\"mw:ExtLink\" href=\"http://example.com\" data-parsoid='{\"targetOff\":50,\"contentOffsets\":[50,57],\"dsr\":[30,58,20,1]}'><b data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[50,57,3,0]}'>text</b></a><b data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[58,61,3,0]}'></b>\n<i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[62,106,2,0]}'>Something <a rel=\"mw:ExtLink\" href=\"http://example.com\" data-parsoid='{\"targetOff\":94,\"contentOffsets\":[94,105],\"dsr\":[74,106,20,1]}'>in italic<i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[103,105,2,0]}'></i></a></i>\n<i data-parsoid='{\"dsr\":[107,164,2,2]}'>Something <a rel=\"mw:ExtLink\" href=\"http://example.com\" data-parsoid='{\"targetOff\":139,\"contentOffsets\":[139,160],\"dsr\":[119,161,20,1]}'>mixed<b data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[144,160,3,0]}'><i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[147,160,2,0]}'>, even bold</i></b></a>'</i>\n<b data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[165,204,3,0]}'><i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[168,204,2,0]}'>Now <a rel=\"mw:ExtLink\" href=\"http://example.com\" data-parsoid='{\"targetOff\":194,\"contentOffsets\":[194,203],\"dsr\":[174,204,20,1]}'>both<b data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[198,203,3,0]}'><i data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[201,203,2,0]}'></i></b></a></i></b></p>");
 add("wt2html", "External link containing double-single-quotes in text embedded in italics (bug 4598 sanity check)", "<p data-parsoid='{\"dsr\":[0,60,0,0]}'><i data-parsoid='{\"dsr\":[0,60,2,2]}'>Some <a rel=\"mw:ExtLink\" href=\"http://example.com/\" data-parsoid='{\"targetOff\":28,\"contentOffsets\":[28,56],\"dsr\":[7,57,21,1]}'>pretty <i data-parsoid='{\"dsr\":[35,46,2,2]}'>italics</i> and stuff</a>!</i></p>");
@@ -419,6 +419,7 @@ add("wt2wt", "Templates: Indent-Pre: 1f: Wrapping should be based on expanded co
 add("wt2wt", "Templates: Handle comments in the target", "{{echo\n<!-- should be ignored -->\n|foo}}\n\n{{echo<!-- should be ignored -->\n|foo}}\n\n{{echo<!-- should be ignored -->|foo}}\n\n{{echo|foo}}");
 add("wt2wt", "4. Indent-Pre and extension tags", " a <gallery>\n File:foobar.jpg\n </gallery>\n");
 add("wt2wt", "Definition lists: self-closed tag", ";one<br />two : two-line fun");
+add("wt2wt", "Definition Lists: colons occurring in tags", ";a:b\n;'''a:b'''\n;<i>a:b</i>\n;<span>a:b</span>\n;<div>a:b</div>\n;<div>a\n:b\n;{{echo|a:b}}\n;{{echo|''a:b''}}");
 add("wt2wt", "BUG 289: \">\"-token in bracketed URL", "[http://www.example.com/ <hello> stuff]\n");
 add("wt2wt", "BUG 289: literal \">\"-token in bracketed URL", "[http://www.example.com/ <b>html</b> stuff]\n");
 add("wt2wt", "BUG 289: literal double quote in bracketed URL", "[http://www.example.com/ \"hello\" stuff]\n");
@@ -554,6 +555,7 @@ add("html2html", "HTML-pre: 3: other wikitext", "<pre data-parsoid='{\"dsr\":[0,
 add("html2html", "Definition list with wikilink containing colon", "<dl data-parsoid='{\"dsr\":[0,95,0,0]}'><dt data-parsoid='{\"dsr\":[0,24,1,0]}'> [/index.php?title=Help</dt><dd data-parsoid='{\"stx\":\"row\",\"dsr\":[24,60,1,0]}'>FAQ&amp;action=edit&amp;redlink=1 Help:FAQ]</dd>\n<dd data-parsoid='{\"dsr\":[61,95,1,0]}'> The least-read page on Wikipedia</dd></dl>\n");
 add("html2html", "Definition lists: colon in HTML attribute", "<dl data-parsoid='{\"dsr\":[0,12,0,0]}'><dt data-parsoid='{\"dsr\":[0,12,1,0]}'> <b data-parsoid='{\"dsr\":[2,12,3,3]}'>bold</b></dt></dl>\n");
 add("html2html", "Definition Lists: Nesting: Multi-level (Parsoid only)", "\n<dl data-parsoid='{\"dsr\":[1,116,0,0]}'><dt data-parsoid='{\"dsr\":[1,6,1,0]}'> t1 </dt>\n<dd data-parsoid='{\"dsr\":[7,17,1,0]}'> d1      </dd>\n<dt data-parsoid='{\"dsr\":[18,116,1,0]}'><dl data-parsoid='{\"dsr\":[19,116,0,0]}'><dt data-parsoid='{\"dsr\":[19,24,1,0]}'> t2 </dt>\n<dd data-parsoid='{\"dsr\":[25,62,1,0]}'> <span typeof=\"mw:Nowiki\" data-parsoid='{\"dsr\":[28,48,8,9]}'>:d2</span>              </dd>\n<dt data-parsoid='{\"dsr\":[63,116,2,0]}'><dl data-parsoid='{\"dsr\":[65,116,0,0]}'><dt data-parsoid='{\"dsr\":[65,70,1,0]}'> t3 </dt>\n<dd data-parsoid='{\"dsr\":[71,116,1,0]}'> <span typeof=\"mw:Nowiki\" data-parsoid='{\"dsr\":[75,96,8,9]}'>::d3</span>                    </dd></dl></dt></dl></dt></dl>\n");
+add("html2html", "Definition Lists: colons occurring in tags", "\n<dl data-parsoid='{\"dsr\":[1,73,0,0]}'><dt data-parsoid='{\"dsr\":[1,4,1,0]}'> a</dt>\n<dd data-parsoid='{\"dsr\":[5,8,1,0]}'> b</dd>\n<dt data-parsoid='{\"dsr\":[9,20,1,0]}'> <b data-parsoid='{\"dsr\":[11,20,3,3]}'>a:b</b></dt>\n<dt data-parsoid='{\"dsr\":[21,30,1,0]}'> <i data-parsoid='{\"dsr\":[23,30,2,2]}'>a:b</i></dt>\n<dt data-parsoid='{\"dsr\":[31,49,1,0]}'> <span data-parsoid='{\"stx\":\"html\",\"dsr\":[33,49,6,7]}'>a:b</span></dt>\n<dd data-parsoid='{\"dsr\":[50,65,1,0]}'><div data-parsoid='{\"stx\":\"html\",\"dsr\":[51,65,5,6]}'>a:b</div></dd>\n<dd data-parsoid='{\"dsr\":[66,73,1,0]}'><div data-parsoid='{\"stx\":\"html\",\"autoInsertedEnd\":true,\"dsr\":[67,73,5,0]}'>a</div></dd></dl>\n\n<dl data-parsoid='{\"dsr\":[75,78,0,0]}'><dd data-parsoid='{\"dsr\":[75,78,1,0]}'> b</dd></dl>\n\n\n<dl data-parsoid='{\"dsr\":[87,104,0,0]}'><dt data-parsoid='{\"dsr\":[87,90,1,0]}'> a</dt>\n<dd data-parsoid='{\"dsr\":[91,94,1,0]}'> b</dd>\n<dt data-parsoid='{\"dsr\":[95,104,1,0]}'> <i data-parsoid='{\"dsr\":[97,104,2,2]}'>a:b</i></dt></dl>\n");
 add("html2html", "Definition Lists: colons and tables 1", "<dl data-parsoid='{\"dsr\":[0,26,0,0]}'><dd data-parsoid='{\"dsr\":[0,12,1,0]}'> <table data-parsoid='{\"dsr\":[2,12,2,2]}'>\n\n<tbody data-parsoid='{\"dsr\":[6,10,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[6,9,0,0]}'><td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[6,9,1,0]}'> x</td></tr>\n</tbody></table></dd>\n\n<dd data-parsoid='{\"dsr\":[14,26,1,0]}'> <table data-parsoid='{\"dsr\":[16,26,2,2]}'>\n\n<tbody data-parsoid='{\"dsr\":[20,24,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[20,23,0,0]}'><td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[20,23,1,0]}'> y</td></tr>\n</tbody></table></dd></dl>\n");
 add("html2html", "Indented table markup mixed with indented pre content (proposed in bug 6200)", " <table data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[1,58,2,0]}'>\n \n <tbody data-parsoid='{\"dsr\":[7,58,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[7,58,0,0]}'><td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[7,58,1,0]}'>\n<pre data-parsoid='{\"dsr\":[9,58,1,0]}'>Text that should be rendered preformatted \n\n|}</pre></td></tr></tbody></table>\n");
 add("html2html", "Build table with wikilink", "<table class=\"wikitable\" data-parsoid='{\"dsr\":[0,216,20,2]}'>\n\n<tbody data-parsoid='{\"dsr\":[22,214,0,0]}'><tr data-parsoid='{\"autoInsertedEnd\":true,\"autoInsertedStart\":true,\"dsr\":[22,47,0,0]}'><th data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[22,31,1,0]}'> header </th>\n<th data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[32,47,1,0]}'> second header</th></tr>\n\n<tr style=\"color:red;\" data-parsoid='{\"startTagSrc\":\"|-\",\"autoInsertedEnd\":true,\"dsr\":[49,148,21,0]}'>\n<td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[71,106,1,0]}'> data <a rel=\"mw:WikiLink\" href=\"./Wiki/Main_Page\" title=\"Wiki/Main Page\" data-parsoid='{\"stx\":\"piped\",\"a\":{\"href\":\"./Wiki/Main_Page\"},\"sa\":{\"href\":\"wiki/Main Page\"},\"dsr\":[78,105,17,2]}'>linktext</a> </td>\n<td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[107,148,1,0]}'> second data <a rel=\"mw:WikiLink\" href=\"./Wiki/Main_Page\" title=\"Wiki/Main Page\" data-parsoid='{\"stx\":\"piped\",\"a\":{\"href\":\"./Wiki/Main_Page\"},\"sa\":{\"href\":\"wiki/Main Page\"},\"dsr\":[121,148,17,2]}'>linktext</a></td></tr>\n\n<tr data-parsoid='{\"startTagSrc\":\"|-\",\"autoInsertedEnd\":true,\"dsr\":[150,213,2,0]}'>\n<td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[153,160,1,0]}'> data </td>\n<td data-parsoid='{\"autoInsertedEnd\":true,\"dsr\":[161,213,1,0]}'> second data <a rel=\"mw:WikiLink\" href=\"./Wiki/Main_Page\" title=\"Wiki/Main Page\" data-parsoid='{\"stx\":\"piped\",\"a\":{\"href\":\"./Wiki/Main_Page\"},\"sa\":{\"href\":\"wiki/Main Page\"},\"dsr\":[175,213,17,2]}'>link|text with pipe</a></td></tr>\n</tbody></table>\n");
@@ -998,7 +1000,7 @@ add("html2wt", "Definition Lists: Mixed Lists: Test 9", "*; foo \n*: bar\n");
 add("html2wt", "Definition Lists: Mixed Lists: Test 10", "*#; foo \n*#: bar\n");
 add("html2wt", "Definition Lists: Mixed Lists: Test 11", "\n*#*#;*;; foo :bar\n*#*#; boo :baz\n");
 add("html2wt", "Definition Lists: Weird Ones: Test 1", "\n*#;*::;; foo<span> </span>: bar (who uses this?)\n");
-add("html2wt", "Definition Lists: colons occurring in tags", "; a\n: b\n; '''a:b'''\n; ''a:b''\n; <span>a:b</span>\n; <div>a:b</div>\n; a\n: b\n; ''a:b''\n");
+add("html2wt", "Definition Lists: colons occurring in tags", "\n; a\n: b\n; '''a:b'''\n; ''a:b''\n; <span>a:b</span>\n:<div>a:b</div>\n:<div>a\n\n: b\n\n</div>\n; a\n: b\n; ''a:b''\n");
 add("html2wt", "Definition Lists: colons and tables 1", ": {|\n\n| x\n|}\n\n: {|\n\n| y\n|}\n");
 add("html2wt", "Definition Lists: colons and tables 2", ": {|\n\n| x\n|}\n: {|\n\n| y\n|}\n");
 add("html2wt", "External links: Free with trailing punctuation", "http://example.com,\nhttp://example.com;\nhttp://example.com\\\nhttp://example.com.\nhttp://example.com:\nhttp://example.com!\nhttp://example.com?\nhttp://example.com)\nhttp://example.com/url_with_(brackets)\n(http://example.com/url_without_brackets)\nhttp://example.com/url_with_entity&nbsp;\nhttp://example.com/url_with_entity&nbsp;\nhttp://example.com/url_with_entity&nbsp;\nhttp://example.com/url_with_entity<nowiki/>&#x3C;\nhttp://example.com/url_with_entity&#x3C;\nhttp://example.com/url_with_entity&#x3C;\n");
@@ -1932,6 +1934,13 @@ add("selser", "Definition Lists: Weird Ones: Test 1 [[[2]]]", "*aaa153pk181fko6r
 add("selser", "Definition Lists: Weird Ones: Test 1 [[[[[[2]]]]]]", "*#: m79ersaiyeqaor\n;*::;; foo : bar (who uses this?)");
 add("selser", "Definition Lists: Weird Ones: Test 1 [[[[[[[2]]]]]]]", "*#;q93hogh7bi2ro1or\n*::;; foo : bar (who uses this?)");
 add("selser", "Definition Lists: Weird Ones: Test 1 [[[[2]]]]", "*# mug1j4xbiskprpb9\n#;*::;; foo : bar (who uses this?)");
+add("selser", "Definition Lists: colons occurring in tags [[0,4,2,1,0,[[4]],0,3,3,[[3]],0,0,2,0,0,3,2,3]]", ";a\n: a771iou6wnyojemi\n: q8kanpj3qa2oi529\n;'''a:b'''\n;<i>z4oyogdq6f5ka9k9</i>\n;<div></div>\n;<div>a\n: evxz2xnf15vu0udi\n:b</div>\n: 40w6y2kk7p6wdn29\n");
+add("selser", "Definition Lists: colons occurring in tags [2]", "qg79k8v4av6de7b9\n;a:b\n;'''a:b'''\n;<i>a:b</i>\n;<span>a:b</span>\n;<div>a:b</div>\n;<div>a\n:b</div>\n;{{echo|a:b}}\n;{{echo|''a:b''}}");
+add("selser", "Definition Lists: colons occurring in tags [[3,3,2,1,0,[[2]],2,[3],4,3,0,1,2,0,2,1,0,0]]", ": c5tz9nt5eiqqia4i\n;'''a:b'''\n;<i>sbswsuhjvx8vvx6ra:b</i>\n: m6vkrqb89e4b1emi\n;\n: qfrjrlas1800be29\n;<div>a\n: z5ny7xevao22zkt9\n:b</div>\n: 0829zan0mo14te29\n;{{echo|a:b}}\n;{{echo|''a:b''}}");
+add("selser", "Definition Lists: colons occurring in tags [[1,1,3,[3],0,[3],0,1,4,[1],0,3,3,0,4,0,0,2]]", ";a:b\n;\n;\n;<span>a:b</span>\n: h8hh4v34xh2maemi\n;<div data-foobar=\"u0847t68ar2akyb9\">a:b</div>\n:b</div>\n: sma0ums92l2z9f6r\n;{{echo|a:b}}\n: 8hiue1du4ujcq5mi\n;{{echo|''a:b''}}");
+add("selser", "Definition Lists: colons occurring in tags [[3,[4],0,[[2]],4,0,4,[4],0,[[2]],2,[[4]],2,0,0,0,0,4]]", ":5lkbigrdwdx11yvi\n;'''cqslr7dvsnd5z5mia:b'''\n: masspab3x9don7b9\n;<i>a:b</i>\n: oa29nopebnoy9zfr\n;98d7as2k961ug14i\n;<div>1bwhvaem3l0izfra:b</div>\n: vnpj9u2knmmzehfr\n;<div>zv7k43lu5scq5mi\n: 1f68p993haor\n:b</div>\n;{{echo|a:b}}\n: hxhtxsqncdi");
+add("selser", "Definition Lists: colons occurring in tags [[0,0,4,[2],3,[4],0,[[3]],4,[[3]],0,2,3,0,0,0,0,0]]", ";a:b\n: h3264ujrqrkq33di\n;h56kdmfdrrv34n29'''a:b'''\n;fxo6p8aww4p9zfr\n;<span></span>\n: h438k9e0o3aexw29\n;<div></div>\n: diwu40dj1dims4i\n;<div>a\n:b</div>\n;{{echo|a:b}}\n;{{echo|''a:b''}}");
+add("selser", "Definition Lists: colons occurring in tags [1]", ";a:b\n;'''a:b'''\n;<i>a:b</i>\n;<span>a:b</span>\n;<div>a:b</div>\n;<div>a\n:b</div>\n;{{echo|a:b}}\n;{{echo|''a:b''}}");
 add("selser", "BUG 289: \">\"-token in bracketed URL [2]", "ll0lxyd1rfgpsyvi\n\n[http://www.example.com/<hello> stuff]");
 add("selser", "BUG 289: \">\"-token in bracketed URL [1]", "[http://www.example.com/<hello> stuff]");
 add("selser", "BUG 289: \">\"-token in bracketed URL [[2]]", "kemi29jie4b1emi[http://www.example.com/<hello> stuff]");