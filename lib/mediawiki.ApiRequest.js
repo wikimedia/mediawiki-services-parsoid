wikimedia/mediawiki-services-parsoid

commit 923b64a5b7cef6c5a9efd4c36d23176da6992d55

Refactor request default options into ApiRequest.prototype.request().
Removes a lot of repetition.

The ParsoidCacheRequest is the only instance which is not actually
a MW API request.  In the future, we might split off a BaseRequest
underneath ApiRequest, and have ParsoidCacheRequest inherit from
BaseRequest instead of ApiRequest.  That doesn't seem worth doing
at the moment, though.

Change-Id: Ia86b601fa433e6507b6330e740a6296c4424894d

lib/mediawiki.ApiRequest.js
@@ -113,25 +113,40 @@ function ApiRequest(env, title) {
 util.inherits(ApiRequest, events.EventEmitter);
 
 ApiRequest.prototype.request = function( options, callback ) {
+	var env = this.env;
+	var proxy = env.conf.wiki.apiProxy;
 	// this is a good place to put debugging statements
 	// if you want to watch network requests.
 	//  console.log('ApiRequest', options);
 
 	// Forward the request id
 	if (!options.headers) { options.headers = {}; }
-	options.headers['X-Request-ID'] = this.env.reqId;
-	// When proxying, strip TLS and lie to the appserver to indicate
-	// unwrapping has just occurred. The appserver isn't listening on
-	// port 443 but a site setting may require a secure connection,
-	// which the header identifies.  (This is primarily for proxies
-	// used in WMF production, for which initMwApiMap sets the
-	// proxy_strip_https flag.)
-	if (options.proxy && options.proxy_strip_https && /^https[:]/.test(options.uri)) {
-		options.uri = options.uri.replace(/^https/, 'http');
-		options.headers['X-Forwarded-Proto'] = 'https';
-		// request will actually ignore any unused options, but play it
-		// safe by removing our custom 'proxy_strip_https' option.
-		delete options.proxy_strip_https;
+	options.headers['X-Request-ID'] = env.reqId;
+	// Set default options, forward cookie if set.
+	options.headers['User-Agent'] = env.conf.parsoid.userAgent;
+	options.headers.Connection = 'close';
+	options.strictSSL = env.conf.parsoid.strictSSL;
+	if (env.cookie) {
+		options.headers.Cookie = env.cookie;
+	}
+	// Proxy options should only be applied to MW API endpoints.
+	// Allow subclasses (ie, ParsoidCacheRequest) to manually set proxy
+	// to `null` or to a different proxy to override MW API proxy.
+	if (proxy && options.proxy === undefined) {
+		options.proxy = proxy.uri;
+		if (proxy.headers) {
+			Object.assign(options.headers, proxy.headers);
+		}
+		if (proxy.strip_https && options.proxy && /^https[:]/.test(options.uri)) {
+			// When proxying, strip TLS and lie to the appserver to indicate
+			// unwrapping has just occurred. The appserver isn't listening on
+			// port 443 but a site setting may require a secure connection,
+			// which the header identifies.  (This is primarily for proxies
+			// used in WMF production, for which initMwApiMap sets the
+			// proxy.strip_https flag.)
+			options.uri = options.uri.replace(/^https/, 'http');
+			options.headers['X-Forwarded-Proto'] = 'https';
+		}
 	}
 
 	return request( options, callback );
@@ -278,7 +293,6 @@ function TemplateRequest( env, title, oldid ) {
 	}
 
 	var uri = env.conf.wiki.apiURI;
-	var proxy = env.conf.wiki.apiProxy;
 
 	this.requestOptions = {
 		method: 'GET',
@@ -286,23 +300,7 @@ function TemplateRequest( env, title, oldid ) {
 		uri: uri,
 		qs: apiargs,
 		timeout: env.conf.parsoid.timeouts.mwApi.srcFetch,
-		proxy: proxy && proxy.uri,
-		proxy_strip_https: proxy && proxy.strip_https,
-		strictSSL: env.conf.parsoid.strictSSL,
-		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close'
-		}
 	};
-
-	if ( proxy && proxy.headers ) {
-		Object.assign( this.requestOptions.headers, proxy.headers );
-	}
-
-	if ( env.cookie ) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	// Start the request
 	this.request( this.requestOptions, this._requestCB.bind(this) );
@@ -439,7 +437,6 @@ function PreprocessorRequest( env, title, text ) {
 	}
 
 	var uri = env.conf.wiki.apiURI;
-	var proxy = env.conf.wiki.apiProxy;
 
 	this.requestOptions = {
 		// Use POST since we are passing a bit of source, and GET has a very
@@ -450,23 +447,7 @@ function PreprocessorRequest( env, title, text ) {
 		followRedirect: true,
 		uri: uri,
 		timeout: env.conf.parsoid.timeouts.mwApi.preprocessor,
-		proxy: proxy && proxy.uri,
-		proxy_strip_https: proxy && proxy.strip_https,
-		strictSSL: env.conf.parsoid.strictSSL,
-		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close'
-		}
 	};
-
-	if ( proxy && proxy.headers ) {
-		Object.assign( this.requestOptions.headers, proxy.headers );
-	}
-
-	if ( env.cookie ) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	// Start the request
 	this.request( this.requestOptions, this._requestCB.bind(this) );
@@ -574,23 +555,7 @@ function PHPParseRequest( env, name, text ) {
 		followRedirect: true,
 		uri: uri,
 		timeout: env.conf.parsoid.timeouts.mwApi.extParse,
-		proxy: proxy && proxy.uri,
-		proxy_strip_https: proxy && proxy.strip_https,
-		strictSSL: env.conf.parsoid.strictSSL,
-		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close'
-		}
 	};
-
-	if ( proxy && proxy.headers ) {
-		Object.assign( this.requestOptions.headers, proxy.headers );
-	}
-
-	if ( env.cookie ) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	// Start the request
 	this.request( this.requestOptions, this._requestCB.bind(this) );
@@ -709,18 +674,13 @@ function ParsoidCacheRequest(env, title, oldid, options) {
 		followRedirect: false,
 		uri: uri,
 		qs: apiargs,
-		strictSSL: env.conf.parsoid.strictSSL,
 		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close',
 			'x-parsoid-request': 'cache'
-		}
+		},
+		// Don't use MW API proxy.
+		// XXX add option for parsoid cache proxy?
+		proxy: null,
 	};
-
-	if (env.cookie) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	var timeouts = env.conf.parsoid.timeouts;
 	if (options.evenIfNotCached) {
@@ -800,23 +760,7 @@ var ConfigRequest = function(uri, env, proxy) {
 		uri: uri,
 		qs: apiargs,
 		timeout: env.conf.parsoid.timeouts.mwApi.configInfo,
-		proxy: proxy && proxy.uri,
-		proxy_strip_https: proxy && proxy.strip_https,
-		strictSSL: env.conf.parsoid.strictSSL,
-		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close',
-		},
 	};
-
-	if (proxy && proxy.headers) {
-		Object.assign(this.requestOptions.headers, proxy.headers);
-	}
-
-	if (env.cookie) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	this.request(this.requestOptions, this._requestCB.bind(this));
 };
@@ -909,22 +853,7 @@ function ImageInfoRequest( env, filename, dims ) {
 		uri: uri,
 		qs: apiArgs,
 		timeout: env.conf.parsoid.timeouts.mwApi.imgInfo,
-		proxy: proxy && proxy.uri,
-		proxy_strip_https: proxy && proxy.strip_https,
-		headers: {
-			'User-Agent': env.conf.parsoid.userAgent,
-			'Connection': 'close'
-		}
 	};
-
-	if ( proxy && proxy.headers ) {
-		Object.assign( this.requestOptions.headers, proxy.headers );
-	}
-
-	if ( env.cookie ) {
-		// Forward the cookie if set
-		this.requestOptions.headers.Cookie = env.cookie;
-	}
 
 	this.request( this.requestOptions, this._requestCB.bind( this ) );
 }

commit d2ef67cd619bbbed7c626ec5c88ff9aaf3b65282

Use the proxy passed to ConfigRequest
 * The default constructed wikiConfig *didn't* have the proxy set!

 * Broken in 923b64a5b7cef6c5a9efd4c36d23176da6992d55

Change-Id: Ibf571bc1531fb3ad77db7b3c78b8d5a18ebd25ae

lib/mediawiki.ApiRequest.js
@@ -107,6 +107,10 @@ function ApiRequest(env, title) {
 	this.title = title;
 	this.queueKey = title;
 	this.reqType = "Page Fetch";
+
+	// Proxy to the MW API. Set to null for subclasses going somewhere else.
+	// ie. ParsoidCacheRequest
+	this.proxy = env.conf.wiki.apiProxy;
 }
 
 // Inherit from EventEmitter
@@ -114,7 +118,7 @@ util.inherits(ApiRequest, events.EventEmitter);
 
 ApiRequest.prototype.request = function( options, callback ) {
 	var env = this.env;
-	var proxy = env.conf.wiki.apiProxy;
+	var proxy = this.proxy;
 	// this is a good place to put debugging statements
 	// if you want to watch network requests.
 	//  console.log('ApiRequest', options);
@@ -666,6 +670,9 @@ function ParsoidCacheRequest(env, title, oldid, options) {
 	this.queueKey = title + '?oldid=' + oldid;
 	this.reqType = "Parsoid cache request";
 
+	// Don't use MW API proxy.
+	this.proxy = null;
+
 	var apiargs = {
 		oldid: oldid
 	};
@@ -683,9 +690,6 @@ function ParsoidCacheRequest(env, title, oldid, options) {
 		headers: {
 			'x-parsoid-request': 'cache'
 		},
-		// Don't use MW API proxy.
-		// XXX add option for parsoid cache proxy?
-		proxy: null,
 	};
 
 	var timeouts = env.conf.parsoid.timeouts;
@@ -733,6 +737,10 @@ var ConfigRequest = function(uri, env, proxy) {
 	ApiRequest.call(this, env, null);
 	this.queueKey = uri;
 	this.reqType = "Config Request";
+
+	// Use the passed in proxy to the mw api. The default proxy set in
+	// the ApiRequest constructor might not be the right one.
+	this.proxy = proxy;
 
 	if (!uri) {
 		this.retries = env.conf.parsoid.retries.mwApi.configInfo;

lib/mediawiki.parser.environment.js
@@ -75,7 +75,11 @@ var MWParserEnvironment = function(parsoidConfig, wikiConfig, options) {
 
 	if ( !wikiConfig ) {
 		// Local things, per-wiki
-		wikiConfig = new WikiConfig( this, null, options.prefix );
+		wikiConfig = new WikiConfig(
+			this, null, options.prefix,
+			parsoidConfig.mwApiMap.get(options.prefix).uri,
+			this.getAPIProxy(options.prefix)
+		);
 	}
 
 	this.conf.wiki = wikiConfig;