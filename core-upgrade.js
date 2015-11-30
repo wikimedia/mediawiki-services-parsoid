792a2f49ec4c5f1988b0d92679fb6597f33a5f22
Arlo Breault <abreault@wikimedia.org>
2015-11-30 15:50:18 -0800

    Don't extend the prototype of native objects
     * We've enabled the `freeze` option in jshint but apparently it doesn't
       catch `Object.defineProperty` method of doing this.
    
     * Also, add core-upgrade.js to the files we're explicitly linting. Why
       does the package.json even have these declarations? Isn't that the
       point of having a .jshintignore file?
    
    Change-Id: Ib96bb9ec9f555d07da2b068ae21d5e68733d19b9

--- core-upgrade.js
+++ core-upgrade.js
@@ -1,8 +1,3 @@
-"use strict";
+'use strict';
 require('core-js/shim');
 require('prfun/smash'); // This mutates the global Promise object.
-if (!Array.prototype.last) {
-	Object.defineProperty(Array.prototype, 'last', {
-		value: function() { return this[this.length - 1]; },
-	});
-}
