wikimedia/mediawiki-services-parsoid

commit e7e1cf2dc8c5b7683b9207b268dea10d7dfd53f5

Remove wts buffering and move bullet emitting where it belongs
Change-Id: I9cf2a8d4127a831be6a0eb96135a472a19d0c7a7

lib/html2wt/DOMHandlers.js
@@ -256,13 +256,13 @@ function buildListHandler(firstChildNames) {
 		var dp = DU.getDataParsoid(node);
 		return dp && dp.autoInsertedStart && dp.autoInsertedEnd;
 	}
-
 	return {
 		forceSOL: true,
 		handle: Promise.method(function(node, state, wrapperUnmodified) {
 			// Disable single-line context here so that separators aren't
 			// suppressed between nested list elements.
 			state.singleLineContext.disable();
+
 			var firstChildElt = DU.firstNonSepChildNode(node);
 
 			// Skip builder-inserted wrappers
@@ -274,8 +274,6 @@ function buildListHandler(firstChildNames) {
 
 			if (!firstChildElt || !(firstChildElt.nodeName in firstChildNames)) {
 				state.emitChunk(getListBullets(state, node), node);
-			} else if (isTplListWithoutSharedPrefix(firstChildElt)) {
-				state.bufferChunk(getListBullets(state, node));
 			}
 
 			var liHandler = state.serializer.wteHandlers.liHandler
@@ -319,8 +317,6 @@ function buildDDHandler(stx) {
 			var chunk = (stx === 'row') ? ':' : getListBullets(state, node);
 			if (!DU.isList(firstChildElement)) {
 				state.emitChunk(chunk, node);
-			} else if (isTplListWithoutSharedPrefix(firstChildElement)) {
-				state.bufferChunk(chunk);
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
 					.bind(state.serializer.wteHandlers, node);
@@ -550,8 +546,6 @@ var tagHandlers = JSUtils.mapObject({
 			var firstChildElement = DU.firstNonSepChildNode(node);
 			if (!DU.isList(firstChildElement)) {
 				state.emitChunk(getListBullets(state, node), node);
-			} else if (isTplListWithoutSharedPrefix(firstChildElement)) {
-				state.bufferChunk(getListBullets(state, node));
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
 					.bind(state.serializer.wteHandlers, node);
@@ -586,8 +580,6 @@ var tagHandlers = JSUtils.mapObject({
 			var firstChildElement = DU.firstNonSepChildNode(node);
 			if (!DU.isList(firstChildElement)) {
 				state.emitChunk(getListBullets(state, node), node);
-			} else if (isTplListWithoutSharedPrefix(firstChildElement)) {
-				state.bufferChunk(getListBullets(state, node));
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
 					.bind(state.serializer.wteHandlers, node);
@@ -1295,6 +1287,19 @@ var tagHandlers = JSUtils.mapObject({
 	},
 });
 
+function handleListPrefix(node, state) {
+	var bullets = '';
+	if (DU.isListOrListItem(node) && !DU.previousNonSepSibling(node) &&
+			isTplListWithoutSharedPrefix(node) &&
+			// Nothing to do for definition list rows,
+			// since we're emitting for the parent node.
+			!(node.nodeName === 'DD' &&
+				DU.getDataParsoid(node).stx === 'row')) {
+		bullets = getListBullets(state, node.parentNode);
+	}
+	return bullets;
+}
+
 /**
  * Function returning `domHandler`s for nodes with encapsulated content.
  */
@@ -1369,10 +1374,9 @@ var _getEncapsulatedContentHandler = function(n) {
 				} else {
 					throw new Error("Should never reach here");
 				}
-
 				return p.then(function(s) {
 					state.singleLineContext.disable();
-					self.emitWikitext(s, node);
+					self.emitWikitext(handleListPrefix(node, state) + s, node);
 					state.singleLineContext.pop();
 					return DU.skipOverEncapsulatedContent(node);
 				});

lib/html2wt/SerializerState.js
@@ -77,18 +77,6 @@ var escapeLine = require('./ConstrainedText.js').escapeLine;
  *
  * singleLineContext
  *    Stack used to enforce single-line context
- *
- * buffer
- *    Buffered wikitext for scenarios where a single separator applies
- *    to multiple nodes and we don't want to emit the separator prematurely.
- *
- *    Ex: In list handlers, for nested lists, we implicitly buffer bullets by
- *    not emitting anything for parent lists. But, that implicit buffering
- *    is insufficient by itself for scenarios where templates are involved.
- *
- *    TODO: For followup patches, to make all buffering explicit instead of
- *    the implicit buffering currently in place in list and list item
- *    handlers.
  * ********************************************************************* */
 
 var initialState = {
@@ -107,7 +95,6 @@ var initialState = {
 	wteHandlerStack: [],
 	// XXX: replace with output buffering per line
 	currLine: null,
-	buffer: '',
 	out: '',
 	logPrefix: 'OUT:',
 };
@@ -286,16 +273,8 @@ SSP.pushSep = function(sep, node, debugPrefix) {
 		function() { return JSON.stringify(sep); });
 };
 
-// Buffer this chunk so that it can be emitted with the next emitChunk
-// along with any necessary separators before it.
-SSP.bufferChunk = function(res) {
-	this.buffer += res;
-};
-
 SSP.emitChunk = function(res, node) {
 	res = ConstrainedText.cast(res, node);
-	res.text = this.buffer + res.text;
-	this.buffer = '';
 
 	// Replace newlines if we're in a single-line context
 	if (this.singleLineContext.enforced()) {

commit 07494cf2e10d1e733def7b891774ce78295d0951

Only emit shared prefix bullets for the right parents
 * In the other cases (ie. when this child doesn't match expectations),
   we'll have already emitted bullets for the parent node, which is why
   we're seeing duplicates.

 * Fixes a regression introduced in e7e1cf2dc8c5b7683b9207b268dea10d7dfd53f5

 * Identified in rt https://gerrit.wikimedia.org/r/#/c/262723/

 * A test case was added for this edge.

 * Also, add checks for literal html nodes (discussed in review) and a
   test for that as well, for good measure.

Change-Id: Iae4dc35fdfd3058ae3bb12bc950f57e416398cb2

lib/html2wt/DOMHandlers.js
@@ -251,11 +251,12 @@ function isTplListWithoutSharedPrefix(node) {
 	}
 }
 
+function isBuilderInsertedElt(node) {
+	var dp = DU.getDataParsoid(node);
+	return dp && dp.autoInsertedStart && dp.autoInsertedEnd;
+}
+
 function buildListHandler(firstChildNames) {
-	function isBuilderInsertedElt(node) {
-		var dp = DU.getDataParsoid(node);
-		return dp && dp.autoInsertedStart && dp.autoInsertedEnd;
-	}
 	return {
 		forceSOL: true,
 		handle: Promise.method(function(node, state, wrapperUnmodified) {
@@ -272,7 +273,8 @@ function buildListHandler(firstChildNames) {
 				firstChildElt = DU.firstNonSepChildNode(firstChildElt);
 			}
 
-			if (!firstChildElt || !(firstChildElt.nodeName in firstChildNames)) {
+			if (!firstChildElt || !(firstChildElt.nodeName in firstChildNames) ||
+					DU.isLiteralHTMLNode(firstChildElt)) {
 				state.emitChunk(getListBullets(state, node), node);
 			}
 
@@ -315,7 +317,8 @@ function buildDDHandler(stx) {
 		handle: Promise.method(function(node, state, wrapperUnmodified) {
 			var firstChildElement = DU.firstNonSepChildNode(node);
 			var chunk = (stx === 'row') ? ':' : getListBullets(state, node);
-			if (!DU.isList(firstChildElement)) {
+			if (!DU.isList(firstChildElement) ||
+					DU.isLiteralHTMLNode(firstChildElement)) {
 				state.emitChunk(chunk, node);
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
@@ -544,7 +547,8 @@ var tagHandlers = JSUtils.mapObject({
 		forceSOL: true,
 		handle: Promise.method(function(node, state, wrapperUnmodified) {
 			var firstChildElement = DU.firstNonSepChildNode(node);
-			if (!DU.isList(firstChildElement)) {
+			if (!DU.isList(firstChildElement) ||
+					DU.isLiteralHTMLNode(firstChildElement)) {
 				state.emitChunk(getListBullets(state, node), node);
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
@@ -578,7 +582,8 @@ var tagHandlers = JSUtils.mapObject({
 		forceSOL: true,
 		handle: Promise.method(function(node, state, wrapperUnmodified) {
 			var firstChildElement = DU.firstNonSepChildNode(node);
-			if (!DU.isList(firstChildElement)) {
+			if (!DU.isList(firstChildElement) ||
+					DU.isLiteralHTMLNode(firstChildElement)) {
 				state.emitChunk(getListBullets(state, node), node);
 			}
 			var liHandler = state.serializer.wteHandlers.liHandler
@@ -1287,9 +1292,33 @@ var tagHandlers = JSUtils.mapObject({
 	},
 });
 
+var parentMap = {
+	LI: { UL: 1, OL: 1},
+	DT: { DL: 1 },
+	DD: { DL: 1 },
+};
+
+function parentBulletsHaveBeenEmitted(node) {
+	if (DU.isLiteralHTMLNode(node)) {
+		return true;
+	} else if (DU.isList(node)) {
+		return !DU.isListItem(node.parentNode);
+	} else {
+		console.assert(DU.isListItem(node));
+		var parentNode = node.parentNode;
+		// Skip builder-inserted wrappers
+		while (isBuilderInsertedElt(parentNode)) {
+			parentNode = parentNode.parentNode;
+		}
+		return !(parentNode.nodeName in parentMap[node.nodeName]);
+	}
+}
+
 function handleListPrefix(node, state) {
 	var bullets = '';
-	if (DU.isListOrListItem(node) && !DU.previousNonSepSibling(node) &&
+	if (DU.isListOrListItem(node) &&
+			!parentBulletsHaveBeenEmitted(node) &&
+			!DU.previousNonSepSibling(node) &&  // Maybe consider parentNode.
 			isTplListWithoutSharedPrefix(node) &&
 			// Nothing to do for definition list rows,
 			// since we're emitting for the parent node.