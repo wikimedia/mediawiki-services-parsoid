a391693660f8e5c4ebac3c7298410b7bf8d4bd73
Subramanya Sastry <ssastry@wikimedia.org>
2017-01-19 14:46:08 -0500

    Set relativeLinkPrefix before setting titlePageURI
    * Fixes the inconsistency reported in T155070 where internal cite
      links were missing the "./" prefix.
    
    * Since parser tests cannot easily verify this expectation (which is
      how we couldn't catch this in the first place), added a mocha test
      as a regression spec to ensure that internal cite links continue
      to use "./" prefixed urls.
    
    Change-Id: Id028ebcd496563fc77b85700780c572b3cc267f2

---
+++ tests/mocha/regression.specs.js
@@ -0,0 +1,35 @@
+'use strict';
+/*global describe, it*/
+
+require('../../core-upgrade.js');
+require("chai").should();
+var ParsoidConfig = require('../../lib/config/ParsoidConfig.js').ParsoidConfig;
+var helpers = require('./test.helpers.js');
+
+// FIXME: MWParserEnvironment.getParserEnv and switchToConfig both require
+// mwApiMap to be setup. This forces us to load WMF config. Fixing this
+// will require some changes to ParsoidConfig and MWParserEnvironment.
+var parsoidConfig = new ParsoidConfig(null, { loadWMF: true, defaultWiki: 'enwiki' });
+var parse = function(src, options) {
+	return helpers.parse(parsoidConfig, src, options).then(function(ret) {
+		return ret.doc;
+	});
+};
+
+// These are regression specs for when we fix bugs that cannot be easily
+// verified with the parser tests framework
+describe('Regression Specs', function() {
+
+	// Wikilinks use ./ prefixed urls. For reasons of consistency,
+	// we should use a similar format for internal cite urls.
+	// This spec ensures that we don't inadvertently break that requirement.
+	it('should use ./ prefixed urls for cite links', function() {
+		return parse('a [[Foo]] <ref>b</ref>').then(function(result) {
+			result.body.querySelector(".mw-ref a").getAttribute('href').
+				should.equal('./Main_Page#cite_note-1');
+			result.body.querySelector("#cite_note-1 a").getAttribute('href').
+				should.equal('./Main_Page#cite_ref-1');
+		});
+	});
+
+});
