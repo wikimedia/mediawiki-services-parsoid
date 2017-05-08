wikimedia/mediawiki-services-parsoid

commit f051b2620a276c4c9d1e43444f28d9a88a56ba6e

Munge link fragments and element ids as in the php parser
 * This revives patchset 1 from,
   https://gerrit.wikimedia.org/r/#/c/226032/1

Bug: T150112
Bug: T94949
Change-Id: I6e83e9a1fb13007ee44086d5c79cc513583130e0

lib/html2wt/LinkHandler.js
@@ -198,9 +198,11 @@ var getLinkRoundTripData = Promise.method(function(env, node, state) {
 					localPrefix += oldPrefix[1] + ':';
 				}
 
-				// should we preserve the old prefix?
-				if (oldPrefix && (
-						oldPrefix[1].toLowerCase() === interWikiMatch[0].toLowerCase() ||
+				if (target.fromsrc && !target.notmodified) {
+					/* jshint noempty: false */
+					// Leave the target alone!
+				} else if (oldPrefix &&  // Should we preserve the old prefix?
+						(oldPrefix[1].toLowerCase() === interWikiMatch[0].toLowerCase() ||
 						// Check if the old prefix mapped to the same URL as
 						// the new one. Use the old one if that's the case.
 						// Example: [[w:Foo]] vs. [[:en:Foo]]

lib/wt2html/tt/LinkHandler.js
@@ -554,7 +554,7 @@ WikiLinkHandler.prototype.renderLanguageLink = function(token, frame, cb, target
 	if (target.language.protorel !== undefined) {
 		absHref = absHref.replace(/^https?:/, '');
 	}
-	newTk.addNormalizedAttribute('href', absHref, target.hrefSrc);
+	newTk.addNormalizedAttribute('href', Util.sanitizeURI(absHref), target.hrefSrc);
 
 	// Change the rel to be mw:PageProp/Language
 	Util.lookupKV(newTk.attribs, 'rel').v = 'mw:PageProp/Language';
@@ -577,7 +577,7 @@ WikiLinkHandler.prototype.renderInterwikiLink = function(token, frame, cb, targe
 	if (target.interwiki.protorel !== undefined) {
 		absHref = absHref.replace(/^https?:/, '');
 	}
-	newTk.addNormalizedAttribute('href', absHref, target.hrefSrc);
+	newTk.addNormalizedAttribute('href', Util.sanitizeURI(absHref), target.hrefSrc);
 
 	// Change the rel to be mw:ExtLink
 	Util.lookupKV(newTk.attribs, 'rel').v = 'mw:ExtLink';

lib/utils/Util.js
@@ -11,6 +11,7 @@ var entities = require('entities');
 var Consts = require('../config/WikitextConstants.js').WikitextConstants;
 var JSUtils = require('./jsutils.js').JSUtils;
 var Promise = require('./promise.js');
+var Sanitizer;  // Circular definition
 
 var request = Promise.promisify(require('request'), true);
 var lastItem = JSUtils.lastItem;
@@ -1014,26 +1015,31 @@ var Util = {
 		});
 	},
 
+	// FIXME: Does this belong in the Sanitizer?
 	sanitizeTitleURI: function(title) {
+		if (!Sanitizer) {
+			Sanitizer = require('../wt2html/tt/Sanitizer.js').Sanitizer;
+		}
 		var bits = title.split('#');
 		var anchor = null;
-		var sanitize = function(s) {
-			return s.replace(/[%? \[\]#|<>]/g, function(m) {
-				return encodeURIComponent(m);
-			});
-		};
 		if (bits.length > 1) { // split at first '#'
 			anchor = title.substring(bits[0].length + 1);
 			title = bits[0];
 		}
-		title = sanitize(title);
+		title = title.replace(/[%? \[\]#|<>]/g, function(m) {
+			return encodeURIComponent(m);
+		});
 		if (anchor !== null) {
-			title += '#' + sanitize(anchor);
+			title += '#' + Sanitizer.escapeId(anchor, { noninitial: true });
 		}
 		return title;
 	},
 
+	// FIXME: Does this belong in the Sanitizer?
 	sanitizeURI: function(s) {
+		if (!Sanitizer) {
+			Sanitizer = require('../wt2html/tt/Sanitizer.js').Sanitizer;
+		}
 		var host = s.match(/^[a-zA-Z]+:\/\/[^\/]+(?:\/|$)/);
 		var path = s;
 		var anchor = null;
@@ -1056,7 +1062,7 @@ var Util = {
 		});
 		s = host + path;
 		if (anchor !== null) {
-			s += '#' + anchor;
+			s += '#' + Sanitizer.escapeId(anchor, { noninitial: true });
 		}
 		return s;
 	},

lib/wt2html/tt/Sanitizer.js
@@ -1042,9 +1042,9 @@ Sanitizer.prototype.sanitizeTagAttrs = function(token, attrs) {
 			v = this.checkCss(v);
 		}
 
-		// if (k === 'id') {
-		// 	v = Sanitizer.escapeId(v, { 'noninitial': true });
-		// }
+		if (k === 'id') {
+			v = Sanitizer.escapeId(v, { 'noninitial': true });
+		}
 
 		// RDFa and microdata properties allow URLs, URIs and/or CURIs.
 		// Check them for sanity

commit 42c900755ce8f74dbd8852433fc348786fd41d3f

Fix typo introduced in f051b262
Change-Id: Iec153aea6809c74fdbee87f622d3fb16af4f03d8

lib/html2wt/LinkHandler.js
@@ -199,7 +199,7 @@ var getLinkRoundTripData = Promise.method(function(env, node, state) {
 					localPrefix += oldPrefix[1] + ':';
 				}
 
-				if (target.fromsrc && !target.notmodified) {
+				if (target.fromsrc && !target.modified) {
 					// Leave the target alone!
 				} else if (oldPrefix &&  // Should we preserve the old prefix?
 						(oldPrefix[1].toLowerCase() === interWikiMatch[0].toLowerCase() ||