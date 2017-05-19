wikimedia/mediawiki-services-parsoid

commit d722f895c9670bf3782c909bb257e283e97b6d19

Remove dependence on native parser functions
 * The path in resolveTemplateTarget should not depend on the existence
   of the arbitrary parser functions that we've natively defined.

Change-Id: Id34b1172f8be42e5dc2fc7fb4f165f84714b5b80

lib/wt2html/tt/TemplateHandler.js
@@ -76,7 +76,6 @@ TemplateHandler.prototype.onTemplate = function(token, frame, cb) {
 	// to process all attributes to tokens, and force reprocessing of the token.
 	if (hasTemplateToken(token.attribs[0].k)) {
 		cb({ async: true });
-
 		var th = this;
 		this.ae.onToken(token, frame, function(ret) {
 			if (ret.tokens) {
@@ -267,10 +266,13 @@ TemplateHandler.prototype.encapTokens = function(state, tokens, extraDict) {
 TemplateHandler.prototype.processSpecialMagicWord = function(tplToken, resolvedTgt) {
 	var env = this.manager.env;
 
-	// special case for {{!}} magic word
+	// Special case for {{!}} magic word.  Note that this is only necessary
+	// because of the call from the TokenStreamPatcher.  Otherwise, ! is a
+	// variable like any other and can be dropped from this function.
+	// However, we keep both cases flowing through here for consistency.
 	if ((resolvedTgt && resolvedTgt.magicWordType === '!') || tplToken.attribs[0].k === "!") {
 		// If we're not at the top level, return a table cell. This will always
-		// be the case. Either {{!}} was tokenized as a td, or it is tokenized
+		// be the case. Either {{!}} was tokenized as a td, or it was tokenized
 		// as template but the recursive call to fetch its content returns a
 		// single | in an ambiguous context which will again be tokenized as td.
 		if (!this.atTopLevel) {
@@ -452,25 +454,18 @@ TemplateHandler.prototype.resolveTemplateTarget = function(state, targetToks) {
 
 	var pieces = target.split(':');
 	var prefix = pieces[0].trim();
+	var lowerPrefix = prefix.toLowerCase();
 	// The check for pieces.length > 1 is require to distinguish between
 	// {{lc:FOO}} and {{lc|FOO}}.  The latter is a template transclusion
 	// even though the target (=lc) matches a registered parser-function name.
 	var isPF = pieces.length > 1;
-	var lowerPrefix = prefix.toLowerCase();
-	var isSpecialMagicWord = (target === '!');
 
-	// Check if we have a parser function.
-	// Unalias to canonical form and look in config.functionHooks
-	var magicWordAlias = wiki.magicWords[prefix] || wiki.magicWords[lowerPrefix];
+	// Check if we have a parser function
+	var canonicalFunctionName =
+		wiki.functionHooks.get(prefix) || wiki.functionHooks.get(lowerPrefix) ||
+		wiki.variables.get(prefix) || wiki.variables.get(lowerPrefix);
 
-	// Retry by adding a ":" to prefix if necessary
-	if (!magicWordAlias && pieces.length > 1) {
-		magicWordAlias = wiki.magicWords[prefix + ':'] || wiki.magicWords[lowerPrefix + ':'];
-	}
-	var translatedPrefix = magicWordAlias || lowerPrefix || '';
-	if ((magicWordAlias && this.parserFunctions && this.parserFunctions['pf_' + magicWordAlias]) ||
-		(isPF && (translatedPrefix[0] === '#' || wiki.functionHooks.has(translatedPrefix)))) {
-
+	if (canonicalFunctionName !== undefined) {
 		// Extract toks that make up pfArg
 		var pfArgToks;
 		var firstTok = targetToks[0];
@@ -478,14 +473,12 @@ TemplateHandler.prototype.resolveTemplateTarget = function(state, targetToks) {
 			targetToks = targetToks.slice(1);
 			// Strip parser-func / magic-word prefix
 			firstTok = normalizeTarget(firstTok).replace(new RegExp("^" + prefix, 'i'), '');
-
 			// Strip ":" after skipping empty tokens, if any
 			if (isPF) {
 				while (firstTok === '') {
 					firstTok = targetToks[0];
 					targetToks = targetToks.slice(1);
 				}
-
 				console.assert(firstTok[0] === ':', 'Expecting : in parser function definiton');
 				firstTok = firstTok.slice(1);
 			}
@@ -499,13 +492,20 @@ TemplateHandler.prototype.resolveTemplateTarget = function(state, targetToks) {
 			pfArgToks = targetToks;
 		}
 
+		state.parserFunctionName = canonicalFunctionName;
 
-		state.parserFunctionName = translatedPrefix;
+		var magicWordType = null;
+		if (canonicalFunctionName === '!') {
+			magicWordType = '!';
+		} else if (Util.magicMasqs.has(canonicalFunctionName)) {
+			magicWordType = 'MASQ';
+		}
+
 		return {
 			isPF: true,
 			prefix: prefix,
-			magicWordType: Util.magicMasqs.has(lowerPrefix) ? 'MASQ' : null,
-			target: 'pf_' + translatedPrefix,
+			magicWordType: magicWordType,
+			target: 'pf_' + canonicalFunctionName,
 			pfArg: target.substr(prefix.length + 1),
 			pfArgToks: pfArgToks,
 		};
@@ -546,7 +546,7 @@ TemplateHandler.prototype.resolveTemplateTarget = function(state, targetToks) {
 
 	return {
 		isPF: false,
-		magicWordType: isSpecialMagicWord ? '!' : null,
+		magicWordType: null,
 		target: title.getPrefixedDBKey(),
 	};
 

lib/config/WikiConfig.js
@@ -283,6 +283,36 @@ function WikiConfig(parsoidConfig, resultConf, prefix) {
 	var replaceRegex = /\$1/;
 	var namedMagicOptions = [];
 
+	// Function hooks on this wiki
+	var functionHooks = new Set(resultConf.functionhooks || []);
+	this.functionHooks = new Map();
+
+	// Variables on this wiki
+	var variables = new Set(resultConf.variables || []);
+	this.variables = new Map();
+
+	// FIXME: Export this from CoreParserFunctions::register, maybe?
+	var noHashFunctions = new Set([
+		'ns', 'nse', 'urlencode', 'lcfirst', 'ucfirst', 'lc', 'uc',
+		'localurl', 'localurle', 'fullurl', 'fullurle', 'canonicalurl',
+		'canonicalurle', 'formatnum', 'grammar', 'gender', 'plural', 'bidi',
+		'numberofpages', 'numberofusers', 'numberofactiveusers',
+		'numberofarticles', 'numberoffiles', 'numberofadmins',
+		'numberingroup', 'numberofedits', 'language',
+		'padleft', 'padright', 'anchorencode', 'defaultsort', 'filepath',
+		'pagesincategory', 'pagesize', 'protectionlevel', 'protectionexpiry',
+		'namespacee', 'namespacenumber', 'talkspace', 'talkspacee',
+		'subjectspace', 'subjectspacee', 'pagename', 'pagenamee',
+		'fullpagename', 'fullpagenamee', 'rootpagename', 'rootpagenamee',
+		'basepagename', 'basepagenamee', 'subpagename', 'subpagenamee',
+		'talkpagename', 'talkpagenamee', 'subjectpagename',
+		'subjectpagenamee', 'pageid', 'revisionid', 'revisionday',
+		'revisionday2', 'revisionmonth', 'revisionmonth1', 'revisionyear',
+		'revisiontimestamp', 'revisionuser', 'cascadingsources',
+		// Special callbacks in core
+		'namespace', 'int', 'displaytitle', 'pagesinnamespace',
+	]);
+
 	// Canonical magic word names on this wiki, indexed by aliases.
 	this.magicWords = {};
 
@@ -324,6 +354,16 @@ function WikiConfig(parsoidConfig, resultConf, prefix) {
 			if (mw['case-sensitive'] !== '') {
 				alias = alias.toLowerCase();
 				this.mwAliases[mw.name].push(alias);
+			}
+			if (variables.has(mw.name)) {
+				this.variables.set(alias, mw.name);
+			}
+			// See Parser::setFunctionHook
+			if (functionHooks.has(mw.name)) {
+				var falias = alias;
+				if (falias.substr(-1) === ':') { falias = falias.slice(0, -1); }
+				if (!noHashFunctions.has(mw.name)) { falias = '#' + falias; }
+				this.functionHooks.set(falias, mw.name);
 			}
 			this.magicWords[alias] = mw.name;
 
@@ -569,9 +609,6 @@ function WikiConfig(parsoidConfig, resultConf, prefix) {
 			this.extContentModel.set(cm, ext.config.contentmodels[cm]);
 		}, this);
 	}, this);
-
-	// Function hooks on this wiki, indexed by their normalized form
-	this.functionHooks = new Set(resultConf.functionhooks || []);
 
 	// Somewhat annoyingly, although LanguageConversion is turned on by
 	// default for all WMF wikis (ie, $wgDisableLangConversion = false, as

lib/mw/ApiRequest.js
@@ -901,6 +901,7 @@ var ConfigRequest = function(uri, env, proxy) {
 		'protocols',
 		'specialpagealiases',
 		'defaultoptions',
+		'variables',
 	];
 	var apiargs = {
 		format: 'json',

lib/wt2html/tt/ParserFunctions.js
@@ -75,7 +75,7 @@ ParserFunctions.prototype.expandKV = function(kv, cb, defaultValue, type, trim) 
 };
 
 
-ParserFunctions.prototype['pf_#if'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_if = function(token, frame, cb, args) {
 	var target = args[0].k;
 	if (target.trim() !== '') {
 		this.expandKV(args[1], cb);
@@ -222,7 +222,7 @@ ParserFunctions.prototype._switchLookupFallback = function(frame, kvs, key, dict
 
 // TODO: Implement
 // http://www.mediawiki.org/wiki/Help:Extension:ParserFunctions#Grouping_results
-ParserFunctions.prototype['pf_#switch'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_switch = function(token, frame, cb, args) {
 	var target = args[0].k.trim();
 	this.env.log('debug', 'switch enter', target, token);
 	// create a dict from the remaining args
@@ -243,7 +243,7 @@ ParserFunctions.prototype['pf_#switch'] = function(token, frame, cb, args) {
 };
 
 // #ifeq
-ParserFunctions.prototype['pf_#ifeq'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_ifeq = function(token, frame, cb, args) {
 	if (args.length < 3) {
 		cb({});
 	} else {
@@ -260,7 +260,7 @@ ParserFunctions.prototype._ifeq_worker = function(cb, args, b) {
 	}
 };
 
-ParserFunctions.prototype['pf_#expr'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_expr = function(token, frame, cb, args) {
 	var res;
 	var target = args[0].k;
 	if (target) {
@@ -283,7 +283,7 @@ ParserFunctions.prototype['pf_#expr'] = function(token, frame, cb, args) {
 	cb({ tokens: [ res.toString() ] });
 };
 
-ParserFunctions.prototype['pf_#ifexpr'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_ifexpr = function(token, frame, cb, args) {
 	this.env.log('debug', '#ifexp: ', args);
 	var res = null;
 	var target = args[0].k;
@@ -304,7 +304,7 @@ ParserFunctions.prototype['pf_#ifexpr'] = function(token, frame, cb, args) {
 	}
 };
 
-ParserFunctions.prototype['pf_#iferror'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_iferror = function(token, frame, cb, args) {
 	var target = args[0].k;
 	if (target.indexOf('class="error"') >= 0) {
 		this.expandKV(args[1], cb);
@@ -406,7 +406,7 @@ ParserFunctions.prototype.pf_padright = function(token, frame, cb, params) {
 	}, 1, 3);
 };
 
-ParserFunctions.prototype['pf_#tag'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_tag = function(token, frame, cb, args) {
 	// Check http://www.mediawiki.org/wiki/Extension:TagParser for more info
 	// about the #tag parser function.
 	var target = args[0].k;
@@ -470,14 +470,14 @@ ParserFunctions.prototype.pf_localmonthnamegen = function(token, frame, cb, args
 //
 // First (very rough) approximation below based on
 // http://jacwright.com/projects/javascript/date_format/, MIT licensed.
-ParserFunctions.prototype['pf_#time'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_time = function(token, frame, cb, args) {
 	cb({ tokens: this._pf_time(args[0].k, args.slice(1)) });
 };
 
 ParserFunctions.prototype._pf_time_tokens = function(target, args) {
 	return { tokens: this._pf_time(target, args) };
 };
-ParserFunctions.prototype['pf_#timel'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_timel = function(token, frame, cb, args) {
 	cb({ tokens: this._pf_time(args[0].k, args.slice(1), 'local') });
 };
 
@@ -771,7 +771,7 @@ ParserFunctions.prototype.pf_urlencode = function(token, frame, cb, args) {
 // http://etherpad.wikimedia.org/ParserNotesExtensions and
 // http://www.mediawiki.org/wiki/Wikitext_parser/Environment.
 // There might be better solutions for some of these.
-ParserFunctions.prototype['pf_#ifexist'] = function(token, frame, cb, args) {
+ParserFunctions.prototype.pf_ifexist = function(token, frame, cb, args) {
 	this.expandKV(args[1], cb);
 };
 ParserFunctions.prototype.pf_pagesize = function(token, frame, cb, args) {

commit ebac189004d6edc4801719a5802766113bc84beb

Fix crasher introduced in d722f895
 * The old code would send, say, {{デフォルトソート:あきやま とくそう}}
   off for expansion, which would return
   {{DEFAULTSORT:あきやまとくそう}}.  This time around,
   resolveTemplateTarget would recognize that as a masquerading
   magicWordType and do the expected thing.

 * The new code determines beforehand that it's a masq, and so must
   provide the canonical name to be added as the page property.  If not,
   by the time we get to the tree builder, the token is expected to have
   typeof and crashes without it.

Change-Id: Ia2035f3eb26f2bb8a24d2bde5fe9f4a1c6c755d6

lib/wt2html/tt/TemplateHandler.js
@@ -503,7 +503,7 @@ TemplateHandler.prototype.resolveTemplateTarget = function(state, targetToks) {
 
 		return {
 			isPF: true,
-			prefix: prefix,
+			prefix: canonicalFunctionName,
 			magicWordType: magicWordType,
 			target: 'pf_' + canonicalFunctionName,
 			pfArg: target.substr(prefix.length + 1),