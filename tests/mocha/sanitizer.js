bc2ad25c3d121dcc02d34ecc5b24252bb158c1ae
Arlo Breault <abreault@wikimedia.org>
2017-07-12 12:25:29 -0400

    T73386: Allow attribute names to use any Unicode "Letter" or "Number"
     * Unicode characters (with the few enumerated exceptions) are valid in
       the attribute name.  They fall under "Anything else" in the parsing
       spec, https://www.w3.org/TR/html51/syntax.html#before-attribute-name-state
    
     * When node.js v4 is deprecated, we'll no longer need to transpile.
       See http://node.green/#ES2015-syntax-RegExp--y--and--u--flags
    
    Depends-On: If712841ba56c5d8f30bbbad500403446a165b07c
    Change-Id: Ifc5cda035c9f7d1bd0f23b0d3a71ffb7eac23bf3

--- tests/mocha/sanitizer.js
+++ tests/mocha/sanitizer.js
@@ -17,13 +17,21 @@ describe('Sanitizer', function() {
 		var sanitizer = new Sanitizer(fakeEnv);
 		var name = 'testelement';
 		sanitizer.attrWhiteListCache[name] = new Set([
-			'foo', 'עברית',
+			'foo', 'עברית', '६', '搭𨋢', 'ńgh',
 		]);
 		var token = new TagTk(name);
 		token.setAttribute('foo', 'bar');
+		token.setAttribute('bar', 'foo');
 		token.setAttribute('עברית', 'bar');
+		token.setAttribute('६', 'bar');
+		token.setAttribute('搭𨋢', 'bar');
+		token.setAttribute('ńgh', 'bar');
 		token = sanitizer.sanitizeToken(token);
 		token.getAttribute('foo').should.equal('bar');
-		should.equal(token.getAttribute('עברית'), null);
+		should.equal(token.getAttribute('bar'), null);
+		token.getAttribute('עברית').should.equal('bar');
+		token.getAttribute('६').should.equal('bar');
+		token.getAttribute('搭𨋢').should.equal('bar');
+		should.equal(token.getAttribute('ńgh'), null);
 	});
 });
