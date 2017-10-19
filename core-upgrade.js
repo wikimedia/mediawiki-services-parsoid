fd13a3176b99c8c6f480a749701e4951252132cc
Arlo Breault <abreault@wikimedia.org>
2017-10-19 15:46:17 -0400

    Limit core-js shimming to what we need
     * On node v4 it took us away from native JSON methods to add symbol
       support which we aren't using, resulting in an unnecessary performance
       penalty.
    
    Change-Id: Ife9b83e6cda15fd40ce1c4bdd51fe0842271b023

--- core-upgrade.js
+++ core-upgrade.js
@@ -1,3 +1,10 @@
 'use strict';
 
-require('core-js/shim');
+// Comments below annotate the highest version of node for which the polyfills
+// are necessary.  Remove when that version is no longer supported.
+
+// v4
+require('core-js/fn/array/includes');
+
+// v6
+require('core-js/fn/object/entries');
