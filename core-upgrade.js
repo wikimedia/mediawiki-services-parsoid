55c34478853019160869ca080b8028f9a383bfb4
Arlo Breault <abreault@wikimedia.org>
2017-10-20 11:17:41 -0400

    Follow up to fd13a317 for padStart
     * Only seems to be used with `--trace time`
    
    Change-Id: I83f8ffc73dcefe19ba858b3f63269eb0f554139d

--- core-upgrade.js
+++ core-upgrade.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// Comments below annotate the highest version of node for which the polyfills
-// are necessary.  Remove when that version is no longer supported.
+// Comments below annotate the highest lts version of node for which the
+// polyfills are necessary.  Remove when that version is no longer supported.
 
 // v4
 require('core-js/fn/array/includes');
 
 // v6
 require('core-js/fn/object/entries');
+require('core-js/fn/string/pad-start');
