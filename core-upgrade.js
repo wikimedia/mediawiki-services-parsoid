5da621468b910e07488e5ac3a230335e4c47f6a0
C. Scott Ananian <cananian@wikimedia.org>
2018-01-02 18:13:14 -0500

    Add node-pn package.
    This is used to promisify the standard node `fs` module, and is used
    in a number of follow-up commits.
    
    Change-Id: I732ae30a5295f6b1f833553c2293bec8b13ff1c9

--- core-upgrade.js
+++ core-upgrade.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Register prfun's Promises with node-pn
+var Promise = require('./lib/utils/promise.js');
+require('pn/_promise')(Promise); // This only needs to be done once.
+
 // Comments below annotate the highest lts version of node for which the
 // polyfills are necessary.  Remove when that version is no longer supported.
 
