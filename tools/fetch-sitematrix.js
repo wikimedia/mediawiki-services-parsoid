e3b54aa92c9579071eb33360b8332f01be19f92a
C. Scott Ananian <cananian@wikimedia.org>
2018-01-03 02:04:48 -0500

    Use Promise.async/yield in tools/fetch-sitematrix.js
    Also update the sitematrix.
    
    Depends-On: I732ae30a5295f6b1f833553c2293bec8b13ff1c9
    Change-Id: I61ae8d09a6a415ec030f95d3a4fc99d2448b676c

--- tools/fetch-sitematrix.js
+++ tools/fetch-sitematrix.js
@@ -9,22 +9,25 @@
 require('../core-upgrade.js');
 
 var Promise = require('../lib/utils/promise.js');
-var fs = require('fs');
+var Util = require('../lib/utils/Util.js').Util;
+
+var fs = require('pn/fs');
 var path = require('path');
 
-var writeFile = Promise.promisify(fs.writeFile, false, fs);
-var request = Promise.promisify(require('request'), true);
 var downloadUrl = 'https://en.wikipedia.org/w/api.php?action=sitematrix&format=json';
 var filename = path.join(__dirname, '/../lib/config/sitematrix.json');
 
-request({
-	url: downloadUrl,
-	json: true,
-}).spread(function(res, body) {
+Promise.async(function *() {
+	var resp = yield Util.retryingHTTPRequest(1, {
+		url: downloadUrl,
+		json: true,
+	});
+	var res = resp[0];
+	var body = resp[1];
 	if (res.statusCode !== 200) {
 		throw 'Error fetching sitematrix! Returned ' + res.statusCode;
 	}
-	return writeFile(filename, JSON.stringify(body, null, '\t'));
-}).then(function() {
+	yield fs.writeFile(filename, JSON.stringify(body, null, '\t'));
+	console.log('Wrote', filename);
 	console.log('Success!');
-}).done();
+})().done();
