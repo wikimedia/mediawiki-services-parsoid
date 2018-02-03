44a5d9842f3c8068194d2e0ca9941959a6327d7e
C. Scott Ananian <cananian@wikimedia.org>
2018-02-03 15:11:30 -0500

    Use Promise.async/yield in mocha test helpers.
    Change-Id: I180a4c77ef624008aacfd1c79fa097d1aed3856f

--- tests/mocha/test.helpers.js
+++ tests/mocha/test.helpers.js
@@ -2,56 +2,52 @@
 
 var DU = require('../../lib/utils/DOMUtils.js').DOMUtils;
 var MWParserEnvironment = require('../../lib/config/MWParserEnvironment.js').MWParserEnvironment;
+var Promise = require('../../lib/utils/promise.js');
 
-var parse = function(parsoidConfig, src, options) {
+var parse = Promise.async(function *(parsoidConfig, src, options) {
 	options = options || {};
-	return MWParserEnvironment.getParserEnv(parsoidConfig, {
+	var env = yield MWParserEnvironment.getParserEnv(parsoidConfig, {
 		prefix: options.prefix || 'enwiki',
 		pageName: options.pageName || 'Main_Page',
 		wrapSections: false,
-	}).then(function(env) {
-		if (options.tweakEnv) {
-			env = options.tweakEnv(env) || env;
-		}
-		env.setPageSrcInfo(src);
-		if (options.contentmodel) {
-			env.page.meta.revision.contentmodel = options.contentmodel;
-		}
-		return env.getContentHandler().toHTML(env)
-		.then(function(doc) {
-			// linter tests need the env object
-			return { env: env, doc: doc };
-		});
 	});
+	if (options.tweakEnv) {
+		env = options.tweakEnv(env) || env;
+	}
+	env.setPageSrcInfo(src);
+	if (options.contentmodel) {
+		env.page.meta.revision.contentmodel = options.contentmodel;
+	}
+	var doc = yield env.getContentHandler().toHTML(env);
+	// linter tests need the env object
+	return { env: env, doc: doc };
+});
 
-};
-
-var serialize = function(parsoidConfig, doc, pb, options) {
+var serialize = Promise.async(function *(parsoidConfig, doc, pb, options) {
 	options = options || {};
-	return MWParserEnvironment.getParserEnv(parsoidConfig, {
+	var env = yield MWParserEnvironment.getParserEnv(parsoidConfig, {
 		prefix: options.prefix || 'enwiki',
 		pageName: options.pageName || 'Main_Page',
-	}).then(function(env) {
-		if (options.tweakEnv) {
-			env = options.tweakEnv(env) || env;
-		}
-		if (!env.page.meta) {
-			env.page.meta = { revision: {} };
-		}
-		if (options.contentmodel) {
-			env.page.meta.revision.contentmodel = options.contentmodel;
-		}
-		pb = pb || DU.extractPageBundle(doc);
-		if (pb) {
-			DU.applyPageBundle(doc, pb);
-		}
-		if (options.useSelser) {
-			env.page.src = options.pageSrc;
-			env.page.dom = options.origDOM;
-		}
-		return env.getContentHandler().fromHTML(env, doc.body, options.useSelser);
 	});
-};
+	if (options.tweakEnv) {
+		env = options.tweakEnv(env) || env;
+	}
+	if (!env.page.meta) {
+		env.page.meta = { revision: {} };
+	}
+	if (options.contentmodel) {
+		env.page.meta.revision.contentmodel = options.contentmodel;
+	}
+	pb = pb || DU.extractPageBundle(doc);
+	if (pb) {
+		DU.applyPageBundle(doc, pb);
+	}
+	if (options.useSelser) {
+		env.page.src = options.pageSrc;
+		env.page.dom = options.origDOM;
+	}
+	return env.getContentHandler().fromHTML(env, doc.body, options.useSelser);
+});
 
 if (typeof module === 'object') {
 	module.exports = {
