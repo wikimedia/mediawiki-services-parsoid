wikimedia/mediawiki-services-parsoid

commit c8a81b793c00a2942c6027c86ad5d9db66260583

Replace `.bind(this)` and `var self = this;` with arrow functions.
Arrow functions don't redefine `this`; they are supported in node >= 4.

http://node.green/#ES2015-functions-arrow-functions

https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions

Change-Id: I03790eaa790b00194d137eef397371d63f242ac1

lib/wt2html/TokenTransformManager.js
@@ -118,14 +118,12 @@ TokenTransformManager.prototype._cmpTransformations = function(a, b) {
 // Use a new method to create this to prevent the closure
 // from holding onto more state than necessary.
 TokenTransformManager.prototype.timeTracer = function(transform, traceName) {
-	var env = this.env;
-	var self = this;
-	return function() {
+	return () => {
 		var s = Date.now();
 		var ret = transform.apply(null, arguments);
 		var t = Date.now() - s;
-		env.bumpTimeUse(traceName, t);
-		self.lastTokenTime = t;
+		this.env.bumpTimeUse(traceName, t);
+		this.lastTokenTime = t;
 		return ret;
 	};
 };
@@ -496,14 +494,13 @@ AccumChain.prototype = {
 		return { accum: newAccum, cb: maybeAsyncCB };
 	},
 	init: function(parentCB) {
-		var self = this;
 		// Local accum for synchronously returned fully processed tokens
 		// Make localAccum compatible with receiveToksFromSibling
 		var localAccum = [];
 		localAccum.receiveToksFromSibling = function() { return parentCB; };
-		localAccum.append = function(chunk) {
+		localAccum.append = (chunk) => {
 			// All tokens in firstAccum are fully processed
-			self.firstAccum.push.apply(self.firstAccum, chunk);
+			this.firstAccum.push.apply(this.firstAccum, chunk);
 		};
 
 		this.firstAccum = localAccum;

bin/parserTests.js
@@ -236,7 +236,6 @@ var staticRandomString = "ahseeyooxooZ8Oon0boh";
  * @return {Node} the altered body
  */
 ParserTests.prototype.applyChanges = function(item, body, changelist) {
-	var self = this;
 
 	// Seed the random-number generator based on the item title
 	var random = new Alea((item.seed || '') + (item.title || ''));
@@ -306,14 +305,14 @@ ParserTests.prototype.applyChanges = function(item, body, changelist) {
 		n.parentNode.insertBefore(newNode, n);
 	}
 
-	function removeNode(n) {
+	var removeNode = (n) => {
 		n.parentNode.removeChild(n);
-	}
+	};
 
-	function applyChangesInternal(node, changes) {
+	var applyChangesInternal = (node, changes) => {
 		if (!node) {
 			// FIXME: Generate change assignments dynamically
-			self.env.log("error", "no node in applyChangesInternal, ",
+			this.env.log("error", "no node in applyChangesInternal, ",
 					"HTML structure likely changed");
 			return;
 		}
@@ -339,7 +338,7 @@ ParserTests.prototype.applyChanges = function(item, body, changelist) {
 						if (DU.isElt(child)) {
 							child.setAttribute('data-foobar', randomString());
 						} else {
-							self.env.log("error", "Buggy changetree. changetype 1 (modify attribute) cannot be applied on text/comment nodes.");
+							this.env.log("error", "Buggy changetree. changetype 1 (modify attribute) cannot be applied on text/comment nodes.");
 						}
 						break;
 
@@ -362,7 +361,7 @@ ParserTests.prototype.applyChanges = function(item, body, changelist) {
 				}
 			}
 		}
-	}
+	};
 
 	if (this.env.conf.parsoid.dumpFlags &&
 		this.env.conf.parsoid.dumpFlags.has("dom:post-changes")) {
@@ -401,7 +400,6 @@ ParserTests.prototype.applyChanges = function(item, body, changelist) {
  * @return {Array} [return.changeTree] The list of changes
  */
 ParserTests.prototype.generateChanges = function(options, item, body) {
-	var self = this;
 	var random = new Alea((item.seed || '') + (item.title || ''));
 
 	/**
@@ -463,14 +461,14 @@ ParserTests.prototype.generateChanges = function(options, item, body) {
 			);
 	}
 
-	function hasChangeMarkers(list) {
+	var hasChangeMarkers = (list) => {
 		// If all recorded changes are 0, then nothing has been modified
 		return list.some(function(c) {
 			return Array.isArray(c) ? hasChangeMarkers(c) : (c > 0);
 		});
-	}
+	};
 
-	function genChangesInternal(node) {
+	var genChangesInternal = (node) => {
 		// Seed the random-number generator based on the item title
 		var changelist = [];
 		var children = node.childNodes;
@@ -480,7 +478,7 @@ ParserTests.prototype.generateChanges = function(options, item, body) {
 			var child = children[i];
 			var changeType = 0;
 
-			if (domSubtreeIsEditable(self.env, child)) {
+			if (domSubtreeIsEditable(this.env, child)) {
 				if (nodeIsUneditable(child) || random() < 0.5) {
 					// This call to random is a hack to preserve the current
 					// determined state of our blacklist entries after a
@@ -502,7 +500,7 @@ ParserTests.prototype.generateChanges = function(options, item, body) {
 		}
 
 		return hasChangeMarkers(changelist) ? changelist : 0;
-	}
+	};
 
 	var changeTree;
 	var numAttempts = 0;
@@ -511,7 +509,7 @@ ParserTests.prototype.generateChanges = function(options, item, body) {
 		changeTree = genChangesInternal(body);
 	} while (
 		numAttempts < 1000 &&
-		(changeTree.length === 0 || self.isDuplicateChangeTree(item.selserChangeTrees, changeTree))
+		(changeTree.length === 0 || this.isDuplicateChangeTree(item.selserChangeTrees, changeTree))
 	);
 
 	if (numAttempts === 1000) {
@@ -1206,7 +1204,7 @@ ParserTests.prototype.processCase = Promise.async(function *(i, options, earlyEx
 			var contents = shell[0];
 			contents += '// ### DO NOT REMOVE THIS LINE ### ';
 			contents += '(start of automatically-generated section)\n';
-			options.modes.forEach(function(mode) {
+			options.modes.forEach((mode) => {
 				contents += '\n// Blacklist for ' + mode + '\n';
 				this.stats.modes[mode].failList.forEach(function(fail) {
 					contents += 'add(' + JSON.stringify(mode) + ', ' +
@@ -1215,7 +1213,7 @@ ParserTests.prototype.processCase = Promise.async(function *(i, options, earlyEx
 					contents += ');\n';
 				});
 				contents += '\n';
-			}.bind(this));
+			});
 			contents += '// ### DO NOT REMOVE THIS LINE ### ';
 			contents += '(end of automatically-generated section)';
 			contents += shell[2];

lib/config/ParsoidConfig.js
@@ -408,7 +408,7 @@ ParsoidConfig.prototype.heapUsageSampleInterval = 5 * 60 * 1000;
  * Initialize the mwApiMap and friends.
  */
 ParsoidConfig.prototype.initMwApiMap = function() {
-	var insertInMaps = function(proxyURI, site) {
+	var insertInMaps = (proxyURI, site) => {
 		// Avoid overwriting those already set in localsettings setup.
 		if (!this.mwApiMap.has(site.dbname)) {
 			var apiConf = {
@@ -427,11 +427,10 @@ ParsoidConfig.prototype.initMwApiMap = function() {
 
 	// See getAPIProxy for the meaning of null / undefined in setMwApi.
 
-	var self = this;
-	Object.keys(sitematrix).forEach(function(key) {
+	Object.keys(sitematrix).forEach((key) => {
 		var val = sitematrix[key];
 		if (!Number.isNaN(Number(key))) {
-			val.site.forEach(insertInMaps.bind(self, undefined));
+			val.site.forEach(insertInMaps.bind(this, undefined));
 		} else if (key === "specials") {
 			val.forEach(function(site) {
 				// Don't use the default proxy for restricted sites.
@@ -445,7 +444,7 @@ ParsoidConfig.prototype.initMwApiMap = function() {
 					// Switch to `site.hasOwnProperty("nonglobal")` when
 					// https://gerrit.wikimedia.org/r/#/c/231086/ is deployed.
 					(site.dbname === 'labswiki' || site.dbname === 'labtestwiki');
-				insertInMaps.call(self, prv ? null : undefined, site);
+				insertInMaps.call(this, prv ? null : undefined, site);
 			});
 		}
 	});
@@ -668,8 +667,7 @@ ParsoidConfig.prototype.sanitizeConfig = function(obj, defaults) {
 	//
 	// Ex: parsoidConfig.timeouts = {}
 
-	var self = this;
-	Object.keys(defaults).forEach(function(key) {
+	Object.keys(defaults).forEach((key) => {
 		if (obj[key] === null || obj[key] === undefined || typeof obj[key] !== typeof defaults[key]) {
 			if (obj[key] !== undefined) {
 				console.warn("WARNING: For config property " + key + ", required a value of type: " + (typeof defaults[key]));
@@ -677,7 +675,7 @@ ParsoidConfig.prototype.sanitizeConfig = function(obj, defaults) {
 			}
 			obj[key] = Util.clone(defaults[key]);
 		} else if (typeof defaults[key] === 'object') {
-			self.sanitizeConfig(obj[key], defaults[key]);
+			this.sanitizeConfig(obj[key], defaults[key]);
 		}
 	});
 };

lib/config/WikiConfig.js
@@ -659,17 +659,17 @@ function WikiConfig(parsoidConfig, resultConf, prefix) {
 	// different.  Use the mechanism introduced in T153341 instead.
 	this.variants = new Map();
 	this.langConverterEnabled = new Set();
-	Object.keys(languagevariants).forEach(function(code) {
+	Object.keys(languagevariants).forEach((code) => {
 		if (general.langconversion !== undefined) {
 			this.langConverterEnabled.add(code);
 		}
-		Object.keys(languagevariants[code]).forEach(function(v) {
+		Object.keys(languagevariants[code]).forEach((v) => {
 			this.variants.set(v, {
 				base: code,
 				fallbacks: languagevariants[code][v].fallbacks,
 			});
-		}.bind(this));
-	}.bind(this));
+		});
+	});
 
 	// Match a wikitext line containing just whitespace, comments, and
 	// sol transparent links and behavior switches.
@@ -784,7 +784,7 @@ WikiConfig.prototype.getMagicWordMatcher = function(id) {
 WikiConfig.prototype.getMagicPatternMatcher = function(optionsMap) {
 	var aliases, regex;
 	var regexString = '';
-	var getInterpolatedMagicWord = function(text, useRegex, useMwList) {
+	var getInterpolatedMagicWord = (text, useRegex, useMwList) => {
 		useMwList = useMwList || this._interpolatedMagicWords;
 		useRegex = useRegex || this.namedMagicRegex;
 		var matches = text.match(useRegex);
@@ -807,11 +807,11 @@ WikiConfig.prototype.getMagicPatternMatcher = function(optionsMap) {
 			return { k: canonical, v: value, a: alias };
 		}
 		return null;
-	}.bind(this);
+	};
 
 	var mwlist = [];
 	var first = true;
-	optionsMap.forEach(function(value, option) {
+	optionsMap.forEach((value, option) => {
 		if (!first) {
 			regexString += '|';
 		}
@@ -822,7 +822,7 @@ WikiConfig.prototype.getMagicPatternMatcher = function(optionsMap) {
 			.replace(/((?:^|\|)(?:[^\$]|\$[^1])*)($|\|)/g, '$1$$1$2')
 			.replace(/\$1/g, '(.*)');
 		mwlist = mwlist.concat(aliases);
-	}.bind(this));
+	});
 	regex = new RegExp('^(?:' + regexString + ')$');
 
 	return function(text) {
@@ -956,14 +956,14 @@ WikiConfig.prototype.findValidProtocol = function(potentialLink) {
  */
 WikiConfig.prototype.detectFeatures = function(env) {
 	return ParamInfoRequest.promise(env)
-	.then(function(query) {
+	.then((query) => {
 		// Do we have the "videoinfo" prop?  Only relevant to legacy requests.
 		this.useVideoInfo = !env.conf.parsoid.useBatchAPI &&
 				Array.isArray(query.parameters) &&
 				query.parameters.some(function(o) {
 					return o && o.name === 'prop' && o.type.indexOf('videoinfo') > -1;
 				});
-	}.bind(this));
+	});
 };
 
 if (typeof module === 'object') {

lib/ext/Cite/index.js
@@ -643,9 +643,8 @@ References.prototype.insertReferencesIntoDOM = function(refsNode, refsData, nest
 References.prototype.insertMissingReferencesIntoDOM = function(refsData, node) {
 	var env = refsData.env;
 	var doc = node.ownerDocument;
-	var self = this;
 
-	refsData.refGroups.forEach(function(refsValue, refsGroup) {
+	refsData.refGroups.forEach((refsValue, refsGroup) => {
 		var frag = createReferences(env, null, env.newAboutId(), {
 			group: refsGroup,
 			responsive: null,
@@ -660,7 +659,7 @@ References.prototype.insertMissingReferencesIntoDOM = function(refsData, node) {
 		node.appendChild(doc.createTextNode("\n"));
 		node.appendChild(frag);
 
-		self.insertReferencesIntoDOM(frag, refsData, [""], true);
+		this.insertReferencesIntoDOM(frag, refsData, [""], true);
 	});
 };
 

lib/html2wt/SerializerState.js
@@ -206,22 +206,22 @@ SSP.serializeChildren = Promise.method(function(node, wtEscaper, firstChild) {
 	// SSS FIXME: Unsure if this is the right thing always
 	if (wtEscaper) { this.wteHandlerStack.push(wtEscaper); }
 
-	var serializeNextChild = Promise.method(function(child) {
+	var serializeNextChild = Promise.method((child) => {
 		if (child === null) { return; }
 		return this.serializer._serializeNode(child).then(function(next) {
 			if (next === node) { return; }  // Serialized all children
 			if (next === child) { next = next.nextSibling; }  // Advance
 			return serializeNextChild(next);
 		});
-	}).bind(this);
+	});
 
-	return serializeNextChild(firstChild || node.firstChild).then(function() {
+	return serializeNextChild(firstChild || node.firstChild).then(() => {
 		if (wtEscaper) { this.wteHandlerStack.pop(); }
 
 		// If we serialized children explicitly,
 		// we were obviously processing a modified node.
 		this.currNodeUnmodified = false;
-	}.bind(this));
+	});
 });
 
 /**
@@ -510,12 +510,12 @@ SSP._kickOffSerialize = function(node, wtEscaper) {
 	this.updateModificationFlags(node);
 	this.resetCurrLine(node.firstChild);
 	return this.serializeChildren(node, wtEscaper)
-	.then(function() {
+	.then(() => {
 		// Emit child-parent seps.
 		this.emitSepForNode(node);
 		// We've reached EOF, flush the remaining buffered text.
 		this.flushLine();
-	}.bind(this));
+	});
 };
 
 /**
@@ -546,7 +546,7 @@ SSP._serializeChildrenToString = Promise.method(function(node, wtEscaper, inStat
 	this[inState] = true;
 
 	return this._kickOffSerialize(node, wtEscaper)
-	.then(function() {
+	.then(() => {
 		// restore the state
 		var bits = this.out;
 		this.out = oldOut;
@@ -561,7 +561,7 @@ SSP._serializeChildrenToString = Promise.method(function(node, wtEscaper, inStat
 		this.currNodeUnmodified = oldCurrNodeUnmodified;
 		this.prevNode = oldPrevNode;
 		return bits;
-	}.bind(this));
+	});
 });
 
 SSP.serializeLinkChildrenToString = Promise.method(function(node, wtEscaper) {

lib/html2wt/WikitextSerializer.js
@@ -255,7 +255,7 @@ WSP._serializeAttributes = Promise.method(function(node, token, isWt) {
 	var attribs = token.attribs;
 
 	var out = [];
-	return Promise.reduce(attribs, function(_, kv) {
+	return Promise.reduce(attribs, (_, kv) => {
 		var k = kv.k;
 		var v, vInfo;
 
@@ -351,7 +351,7 @@ WSP._serializeAttributes = Promise.method(function(node, token, isWt) {
 		}
 
 		return;
-	}.bind(this), null).then(function() {
+	}, null).then(function() {
 		// SSS FIXME: It can be reasonably argued that we can permanently delete
 		// dangerous and unacceptable attributes in the interest of safety/security
 		// and the resultant dirty diffs should be acceptable.  But, this is
@@ -553,7 +553,6 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 		return buf + formatEnd;
 	}
 
-	var self = this;
 	var env = this.env;
 
 	// Per-parameter info from data-parsoid for pre-existing parameters
@@ -574,7 +573,7 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 	// 3. Format them according to formatParamName/formatParamValue
 
 	var kvMap = new Map();
-	return Promise.map(tplKeysFromDataMw, function(k) {
+	return Promise.map(tplKeysFromDataMw, (k) => {
 		var param = part.params[k];
 		var argInfo = dpArgInfoMap.get(k);
 		if (!argInfo) {
@@ -589,10 +588,10 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 		if (param.wt !== undefined) {
 			p = Promise.resolve(param.wt);
 		} else {
-			p = self.serializeHTML({ env: env }, param.html);
+			p = this.serializeHTML({ env: env }, param.html);
 		}
 
-		return p.then(function(value) {
+		return p.then((value) => {
 			console.assert(typeof value === 'string',
 				'For param: ' + k +
 				', wt property should be a string but got: ' + value);
@@ -618,21 +617,21 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 			// is present in 'param.key.wt'
 			kvMap.set(k, { serializeAsNamed: serializeAsNamed, name: name, value: value });
 		});
-	}).then(function() {
+	}).then(() => {
 		var argOrder = Array.from(kvMap.keys())
 			.sort(createParamComparator(dpArgInfo, tplData, kvMap.keys()));
 
 		var argIndex = 1;
 		var numericIndex = 1;
 
-		var numPositionalArgs = dpArgInfo.reduce(function(n, pi) {
+		var numPositionalArgs = dpArgInfo.reduce((n, pi) => {
 			return (part.params[pi.k] !== undefined && !pi.named) ? n + 1 : n;
 		}, 0);
 
-		return argOrder.reduce(function(argBuf, param) {
+		return argOrder.reduce((argBuf, param) => {
 			var kv = kvMap.get(param);
 			// Add nowiki escapes for the arg value, as required
-			var escapedValue = self.wteHandlers.escapeTplArgWT(kv.value, {
+			var escapedValue = this.wteHandlers.escapeTplArgWT(kv.value, {
 				serializeAsNamed: kv.serializeAsNamed || param !== numericIndex.toString(),
 				type: type,
 				argPositionalIndex: numericIndex,
@@ -651,7 +650,7 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 
 			return argBuf;
 		}, []);
-	}).then(function(argBuf) {
+	}).then((argBuf) => {
 		// If no explicit format is provided, default format is:
 		// - 'inline' for new args
 		// - whatever format is available from data-parsoid for old args
@@ -729,12 +728,11 @@ WSP.serializePart = Promise.method(function(node, type, part, tplData) {
 
 WSP.serializeFromParts = Promise.method(function(node, srcParts) {
 	var env = this.env;
-	var self = this;
 	var useTplData = DU.isNewElt(node) || DU.hasDiffMarkers(node, env);
-	return Promise.reduce(srcParts, function(buf, part) {
+	return Promise.reduce(srcParts, (buf, part) => {
 		var tplarg = part.templatearg;
 		if (tplarg) {
-			return self.serializePart(node, 'templatearg', tplarg, null)
+			return this.serializePart(node, 'templatearg', tplarg, null)
 			.then(function(str) {
 				return buf + str;
 			});
@@ -763,7 +761,7 @@ WSP.serializeFromParts = Promise.method(function(node, srcParts) {
 		}
 		var fetched = false;
 		var tplData = null;
-		return p.then(function(apiResp) {
+		return p.then((apiResp) => {
 			tplData = apiResp && apiResp[Object.keys(apiResp)[0]];
 			// If the template doesn't exist, or does but has no TemplateData,
 			// ignore it
@@ -771,8 +769,8 @@ WSP.serializeFromParts = Promise.method(function(node, srcParts) {
 				tplData = null;
 			}
 			fetched = true;
-			return self.serializePart(node, type, tpl, tplData);
-		}).catch(function(err) {
+			return this.serializePart(node, type, tpl, tplData);
+		}).catch((err) => {
 			if (fetched && tplData === null) {
 				// Retrying won't help here.
 				throw err;
@@ -780,11 +778,9 @@ WSP.serializeFromParts = Promise.method(function(node, srcParts) {
 				// No matter what error we encountered (fetching tpldata
 				// or using it), log the error, and use default serialization mode.
 				env.log('html2wt/tpldata/error', err);
-				return self.serializePart(node, type, tpl, null);
+				return this.serializePart(node, type, tpl, null);
 			}
-		}).then(function(str) {
-			return buf + str;
-		});
+		}).then(str => buf + str);
 	}, '');
 });
 
@@ -1182,7 +1178,7 @@ WSP._serializeNode = Promise.method(function(node) {
 			prev, this._getDOMHandler(prev),
 			node, domHandler);
 
-	return method.call(this, node, domHandler).then(function(nextNode) {
+	return method.call(this, node, domHandler).then((nextNode) => {
 		var next = DU.nextNonSepSibling(node) || node.parentNode;
 		this.updateSeparatorConstraints(
 				node, domHandler,
@@ -1197,7 +1193,7 @@ WSP._serializeNode = Promise.method(function(node) {
 			nextNode = node.nextSibling;
 		}
 		return nextNode;
-	}.bind(this));
+	});
 });
 
 // Regular expressions for testing if nowikis added around
@@ -1433,7 +1429,7 @@ WSP.serializeDOM = Promise.method(function(body, selserMode) {
 	}
 
 	return state._kickOffSerialize(body)
-	.then(function() {
+	.then(() => {
 		if (state.hasIndentPreNowikis) {
 			// FIXME: Perhaps this can be done on a per-line basis
 			// rather than do one post-pass on the entire document.
@@ -1448,13 +1444,12 @@ WSP.serializeDOM = Promise.method(function(body, selserMode) {
 			state.hasHeadingEscapes;
 
 		if (splitLines) {
-			var self = this;
-			state.out = state.out.split('\n').map(function(line) {
+			state.out = state.out.split('\n').map((line) => {
 				// Strip excess/useless nowikis
 				//
 				// FIXME: Perhaps this can be done on a per-line basis
 				// rather than do one post-pass on the entire document.
-				line = self._stripUnnecessaryQuoteNowikis(line);
+				line = this._stripUnnecessaryQuoteNowikis(line);
 
 				// Strip (useless) trailing <nowiki/>s
 				// Interim fix till we stop introducing them in the first place.
@@ -1465,7 +1460,7 @@ WSP.serializeDOM = Promise.method(function(body, selserMode) {
 				line = line.replace(/^([^=]*?)(?:<nowiki\s*\/>\s*)+$/, '$1');
 
 				// Get rid of spurious heading nowiki escapes
-				line = self._stripUnnecessaryHeadingNowikis(line);
+				line = this._stripUnnecessaryHeadingNowikis(line);
 				return line;
 			}).join('\n');
 		}
@@ -1477,7 +1472,7 @@ WSP.serializeDOM = Promise.method(function(body, selserMode) {
 		}
 
 		return state.out;
-	}.bind(this));
+	});
 });
 
 

lib/html2wt/escapeWikitext.js
@@ -731,15 +731,14 @@ WEHP.escapeWikiText = function(text, opts) {
 		// No more additional processing required. So, push/pop a null handler.
 		state.wteHandlerStack.push(null);
 
-		var self = this;
-		var ret = text.split(/\n/).map(function(line, i) {
+		var ret = text.split(/\n/).map((line, i) => {
 			if (i > 0) {
 				// Update state
 				state.onSOL = true;
 				state.currLine.text = '';
 				opts.inMultilineMode = true;
 			}
-			return self.escapeWikiText(line, opts);
+			return this.escapeWikiText(line, opts);
 		}).join('\n');
 
 		state.wteHandlerStack.pop();

commit f88403d50914c7259f9cf5d07fa3d50d198f929d

Fix regression in `--trace time`
Caused by c8a81b793c00a2942c6027c86ad5d9db66260583

Arrow functions don't have their own `this` or `arguments`.

Change-Id: I5c47cdcbd6be7dbbe1c7a9ac1558562c997676f7

lib/wt2html/TokenTransformManager.js
@@ -118,12 +118,13 @@ TokenTransformManager.prototype._cmpTransformations = function(a, b) {
 // Use a new method to create this to prevent the closure
 // from holding onto more state than necessary.
 TokenTransformManager.prototype.timeTracer = function(transform, traceName) {
-	return () => {
+	var self = this;
+	return function() {
 		var s = Date.now();
-		var ret = transform.apply(null, arguments);
+		var ret = transform.apply(this, arguments);
 		var t = Date.now() - s;
-		this.env.bumpTimeUse(traceName, t);
-		this.lastTokenTime = t;
+		self.env.bumpTimeUse(traceName, t);
+		self.lastTokenTime = t;
 		return ret;
 	};
 };