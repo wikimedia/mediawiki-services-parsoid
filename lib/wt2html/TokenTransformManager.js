wikimedia/mediawiki-services-parsoid

commit 96203dbe134caf3c0d575a6a86d1d6fc35425000

AttributeTransformManager#process now returns a Promise.
Related classes removed from jsduck configuration due to T156469.
Noted in T184573 to re-add it once jsduck is fixed/replaced.

Change-Id: Ibcf87e79764106632f643bea06c57cfb927c1a7c

lib/wt2html/TokenTransformManager.js
@@ -19,6 +19,7 @@
 var events = require('events');
 var util = require('util');
 var JSUtils = require('../utils/jsutils.js').JSUtils;
+var Promise = require('../utils/promise.js');
 var Util = require('../utils/Util.js').Util;
 var defines = require('./parser.defines.js');
 
@@ -1031,7 +1032,7 @@ SyncTokenTransformManager.prototype.onEndEvent = function() {
  * @constructor
  * @param {TokenTransformManager} manager
  * @param {Object} options
- * @param {Function} callback
+ * @param {Function} [callback] Optional callback
  * @param {Array} callback.attributes
  */
 function AttributeTransformManager(manager, options, callback) {
@@ -1039,7 +1040,6 @@ function AttributeTransformManager(manager, options, callback) {
 	this.options = options;
 	this.frame = this.manager.frame;
 	this.callback = callback;
-	this.outstanding = 1;
 	this.kvs = [];
 	// this.pipe = manager.getAttributePipeline( manager.args );
 }
@@ -1051,15 +1051,19 @@ AttributeTransformManager.prototype._toType = 'tokens/x-mediawiki/expanded';
  * Expand both key and values of all key/value pairs. Used for generic
  * (non-template) tokens in the AttributeExpander handler, which runs after
  * templates are already expanded.
+ *
+ * @return {Promise} resolved with expanded attribute array of KV when
+ *   expansion is complete
  */
-AttributeTransformManager.prototype.process = function(attributes) {
-	var n;
-
-	// transform each argument (key and value), and handle asynchronous returns
-	for (var i = 0, l = attributes.length; i < l; i++) {
-		var cur = attributes[i];
+AttributeTransformManager.prototype.process = Promise.async(function *(attributes) {
+	// Transform each argument (key and value), and handle asynchronous returns
+	// map-then-yield in order to let the individual attributes execute async
+	this.kvs.length = attributes.length; // Allocate result array
+	yield Promise.all(attributes.map(Promise.async(function *(cur, i) {
 		var k = cur.k;
 		var v = cur.v;
+		var tokens;
+		var n;
 
 		if (!v) {
 			cur.v = v = '';
@@ -1067,29 +1071,27 @@ AttributeTransformManager.prototype.process = function(attributes) {
 
 		// fast path for string-only attributes
 		if (k.constructor === String && v.constructor === String) {
-			this.kvs.push(cur);
-			continue;
+			this.kvs[i] = cur;
+			return;
 		}
 
-		var kv = new KV([], [], cur.srcOffsets);
-		this.kvs.push(kv);
+		var kv = this.kvs[i] = new KV([], [], cur.srcOffsets);
 
 		if (Array.isArray(v)) {
 			n = v.length;
 			if (n === 0 || (n === 1 && v[0].constructor === String)) {
 				kv.v = v;
 			} else {
-				// Assume that the return is async, will be decremented in callback
-				this.outstanding++;
-
 				// transform the value
-				this.frame.expand(v, {
+				tokens = yield this.frame.expand(v, {
 					wrapTemplates: this.options.wrapTemplates,
 					inTemplate: this.options.inTemplate,
 					type: this._toType,
-				}).then(function(i, tokens) {
-					this._returnAttributeValue(i, tokens);
-				}.bind(this, i)).done();
+				});
+				this.manager.env.log(
+					'debug', 'check _returnAttributeValue: ', i,  tokens
+				);
+				kv.v = Util.stripEOFTkfromTokens(tokens);
 			}
 		} else {
 			// NOTE: 'v' can be an unexpanded parser value
@@ -1102,54 +1104,25 @@ AttributeTransformManager.prototype.process = function(attributes) {
 			if (n === 0 || (n === 1 && k[0].constructor === String)) {
 				kv.k = k;
 			} else {
-				// Assume that the return is async, will be decremented in callback
-				this.outstanding++;
-
 				// transform the key
-				this.frame.expand(k, {
+				tokens = yield this.frame.expand(k, {
 					wrapTemplates: this.options.wrapTemplates,
 					inTemplate: this.options.inTemplate,
 					type: this._toType,
-				}).then(function(i, tokens) {
-					this._returnAttributeKey(i, tokens);
-				}.bind(this, i)).done();
+				});
+				this.manager.env.log(
+					'debug', 'check _returnAttributeKey: ', i,  tokens
+				);
+				kv.k = Util.stripEOFTkfromTokens(tokens);
 			}
 		} else {
 			kv.k = k;
 		}
-	}
-	this.outstanding--;
-	if (this.outstanding === 0) {
-		// synchronous, done
-		this.callback(this.kvs);
-	}
-};
+	}.bind(this))));
 
-/**
- * Callback for async argument value expansions
- * @private
- */
-AttributeTransformManager.prototype._returnAttributeValue = function(ref, tokens) {
-	this.manager.env.log('debug', 'check _returnAttributeValue: ', ref,  tokens);
-	this.kvs[ref].v = Util.stripEOFTkfromTokens(tokens);
-	this.outstanding--;
-	if (this.outstanding === 0) {
-		this.callback(this.kvs);
-	}
-};
-
-/**
- * Callback for async argument key expansions
- * @private
- */
-AttributeTransformManager.prototype._returnAttributeKey = function(ref, tokens) {
-	this.manager.env.log('debug', 'check _returnAttributeKey: ', ref,  tokens);
-	this.kvs[ref].k = Util.stripEOFTkfromTokens(tokens);
-	this.outstanding--;
-	if (this.outstanding === 0) {
-		this.callback(this.kvs);
-	}
-};
+	if (this.callback) { this.callback(this.kvs); }
+	return this.kvs;
+});
 
 
 /* ******************************* TokenAccumulator ************************* */

lib/wt2html/tt/TemplateHandler.js
@@ -65,7 +65,7 @@ TemplateHandler.prototype.init = function() {
  * processes the template.
  */
 TemplateHandler.prototype.onTemplate = function(token, frame, cb) {
-	var atm, toks;
+	var toks;
 
 	function hasTemplateToken(tokens) {
 		return Array.isArray(tokens) &&
@@ -221,13 +221,15 @@ TemplateHandler.prototype.onTemplate = function(token, frame, cb) {
 
 		// expand argument keys, with callback set to next processing step
 		// XXX: would likely be faster to do this in a tight loop here
-		atm = new AttributeTransformManager(
+		new AttributeTransformManager(
 					this.manager,
-					{ wrapTemplates: false, inTemplate: true },
-					this._expandTemplate.bind(this, state, frame, tgt,
-						accumReceiveToksFromSibling)
-				);
-		atm.process(token.attribs);
+					{ wrapTemplates: false, inTemplate: true }
+		).process(token.attribs).then(function(attribs) {
+			// could be inlined
+			this._expandTemplate(
+				state, frame, tgt, accumReceiveToksFromSibling, attribs
+			);
+		}.bind(this)).done();
 	}
 };
 

commit 0650195a08427d516731b35810e1c10d402ff8b0

Fix performance regression caused by 96203dbe134caf3c0d575a6a86d1d6fc35425000
On the Barack Obama wt2html benchmark, averaged over median-10-of-12 runs:
  Before 96203dbe: 15.299s
  After  96203dbe: 16.794s
  With this patch: 14.482s

Optimizations done:

* Factor out the _processOne function to avoid creating a function object
  every time. (Saved .118s)
* Avoid a yield when the attribute array is zero-length. (Saved .645s)
* Avoid serialization point between key and value expansion. (Saved .272s)
* Avoid a yield when no async expansion needs to be done. (Saved 1.277s)

Change-Id: I2b579ec1e9989b3f3196b5f909253fffe9f871de

lib/wt2html/TokenTransformManager.js
@@ -1038,6 +1038,7 @@ function AttributeTransformManager(manager, options) {
 	this.options = options;
 	this.frame = this.manager.frame;
 	this.kvs = [];
+	this._async = false;
 	// this.pipe = manager.getAttributePipeline( manager.args );
 }
 
@@ -1055,70 +1056,79 @@ AttributeTransformManager.prototype._toType = 'tokens/x-mediawiki/expanded';
 AttributeTransformManager.prototype.process = Promise.async(function *(attributes) {
 	// Transform each argument (key and value), and handle asynchronous returns
 	// map-then-yield in order to let the individual attributes execute async
+	// For performance reasons, avoid a yield if possible (common case where
+	// no async expansion is necessary).
 	this.kvs.length = attributes.length; // Allocate result array
-	yield Promise.all(attributes.map(Promise.async(function *(cur, i) {
-		var k = cur.k;
-		var v = cur.v;
-		var tokens;
-		var n;
-
-		if (!v) {
-			cur.v = v = '';
+	if (attributes.length) {
+		this._async = false;
+		var p = attributes.map(this._processOne, this);
+		if (this._async) {
+			yield Promise.all(p);
 		}
-
-		// fast path for string-only attributes
-		if (k.constructor === String && v.constructor === String) {
-			this.kvs[i] = cur;
-			return;
-		}
-
-		var kv = this.kvs[i] = new KV([], [], cur.srcOffsets);
-
-		if (Array.isArray(v)) {
-			n = v.length;
-			if (n === 0 || (n === 1 && v[0].constructor === String)) {
-				kv.v = v;
-			} else {
-				// transform the value
-				tokens = yield this.frame.expand(v, {
-					wrapTemplates: this.options.wrapTemplates,
-					inTemplate: this.options.inTemplate,
-					type: this._toType,
-				});
-				this.manager.env.log(
-					'debug', 'check _returnAttributeValue: ', i,  tokens
-				);
-				kv.v = Util.stripEOFTkfromTokens(tokens);
-			}
-		} else {
-			// NOTE: 'v' can be an unexpanded parser value
-			// This will be lazily expanded when it is needed.
-			kv.v = v;
-		}
-
-		if (Array.isArray(k)) {
-			n = k.length;
-			if (n === 0 || (n === 1 && k[0].constructor === String)) {
-				kv.k = k;
-			} else {
-				// transform the key
-				tokens = yield this.frame.expand(k, {
-					wrapTemplates: this.options.wrapTemplates,
-					inTemplate: this.options.inTemplate,
-					type: this._toType,
-				});
-				this.manager.env.log(
-					'debug', 'check _returnAttributeKey: ', i,  tokens
-				);
-				kv.k = Util.stripEOFTkfromTokens(tokens);
-			}
-		} else {
-			kv.k = k;
-		}
-	}.bind(this))));
-
+	}
 	return this.kvs;
 });
+
+/** @private */
+AttributeTransformManager.prototype._processOne = function(cur, i) {
+	var k = cur.k;
+	var v = cur.v;
+	var n;
+
+	if (!v) {
+		cur.v = v = '';
+	}
+
+	// fast path for string-only attributes
+	if (k.constructor === String && v.constructor === String) {
+		this.kvs[i] = cur;
+		return;
+	}
+
+	var kv = this.kvs[i] = new KV([], [], cur.srcOffsets);
+	var p;
+
+	if (Array.isArray(v)) {
+		n = v.length;
+		if (n === 0 || (n === 1 && v[0].constructor === String)) {
+			kv.v = v;
+		} else {
+			// transform the value
+			this._async = true;
+			p = this.frame.expand(v, {
+				wrapTemplates: this.options.wrapTemplates,
+				inTemplate: this.options.inTemplate,
+				type: this._toType,
+			}).then(
+				(tokens) => { kv.v = Util.stripEOFTkfromTokens(tokens); }
+			);
+		}
+	} else {
+		// NOTE: 'v' can be an unexpanded parser value
+		// This will be lazily expanded when it is needed.
+		kv.v = v;
+	}
+
+	if (Array.isArray(k)) {
+		n = k.length;
+		if (n === 0 || (n === 1 && k[0].constructor === String)) {
+			kv.k = k;
+		} else {
+			// transform the key
+			this._async = true;
+			p = Promise.join(p, this.frame.expand(k, {
+				wrapTemplates: this.options.wrapTemplates,
+				inTemplate: this.options.inTemplate,
+				type: this._toType,
+			}).then(
+				(tokens) => { kv.k = Util.stripEOFTkfromTokens(tokens); }
+			));
+		}
+	} else {
+		kv.k = k;
+	}
+	return p;
+};
 
 
 /* ******************************* TokenAccumulator ************************* */