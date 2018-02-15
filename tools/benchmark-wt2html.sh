75033904cb083966a72433914c8f298e3c03c261
C. Scott Ananian <cananian@wikimedia.org>
2018-02-15 13:24:32 -0500

    tools/benchmark-wt2html.sh: better option parsing, easier use from rebase
    Add basic option parsing to let you change various defaults.
    
    Add a "git style" argument to allow you to use git-rebase to track the
    benchmark performance over git history, in order to identify regressions.
    For example, to benchmark all revisions since 8a0ff6c1:
    
    $ git-rebase -x "tools/benchmark-wt2html.sh -g=8a0ff6c1 -o bench" 8a0ff6c1
    
    This will create files named `bench-NNN-HASH.times` after every commit.
    
    Change-Id: I38c176d62ecfbfc6f864b334305416b8f22a1dfc

--- tools/benchmark-wt2html.sh
+++ tools/benchmark-wt2html.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 REPEAT=10
-OUTPUT="$1.times"
-# page name should have spaces replaced with underscores
-PAGENAME=Barack_Obama
+GIT_STYLE=
+GIT_HASH=
+FORCE=
+OUTPUT="wt2html-time"
+PAGENAME="Barack Obama"
 
+# check dependencies
 if [ ! -x /usr/bin/time ]; then
     echo "You need to install GNU time (http://www.gnu.org/software/time)."
     echo "$ sudo apt-get install time"
@@ -15,7 +18,70 @@ if [ ! -x /usr/bin/bc ]; then
     exit 1
 fi
 
-if [ ! -f $(dirname $0)/../nocks/en.wikipedia.org/"$PAGENAME.js" ]; then
+# parse options
+while [[ "$#" -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -f|--force)
+            FORCE=1
+            ;;
+        -g|--git)
+            GIT_STYLE=1
+            ;;
+        -g=*|--git=*)
+            GIT_STYLE=1
+            GIT_HASH="${key#*=}"
+            ;;
+        -n|--repeat)
+            shift
+            REPEAT="$1"
+            ;;
+        -n=*|--repeat=*)
+            REPEAT="${key#*=}"
+            ;;
+        -o)
+            shift
+            OUTPUT="$1"
+            ;;
+        -o=*|--output=*)
+            OUTPUT="${key#*=}"
+            ;;
+        -p)
+            shift
+            PAGENAME="$1"
+            ;;
+        -p=*|--page=*)
+            PAGENAME="${key#*=}"
+            ;;
+        -*)
+            echo "Unknown option: $1" >&2
+            exit 1
+            ;;
+        *)
+            OUTPUT="$key"
+            ;;
+    esac
+    shift # Shift to get the next option
+done
+
+# page name should have spaces replaced with underscores
+PAGENAME=$(echo -n "$PAGENAME" | sed -e 's/ /_/g')
+
+# decorate with git commit info
+log=''
+if [ -n "$GIT_STYLE" ]; then
+    log="$(git log --oneline -n 1 --no-decorate)"
+    hash=$(echo "$log" | sed -e 's/ .*//')
+    # count commits since GIT_HASH
+    if [ -n "$GIT_HASH" ]; then
+        count=$(printf '%03d' $(git rev-list "$GIT_HASH"..."$hash" | wc -l))
+        OUTPUT="$OUTPUT-$count"
+    fi
+    OUTPUT="$OUTPUT-$hash"
+fi
+
+# Pre-record network traffic, if necessary.
+if [ -n "$FORCE" -o ! -f $(dirname $0)/../nocks/en.wikipedia.org/"$PAGENAME.js" ]; then
     echo "Recording parse network traffic."
     $(dirname $0)/../bin/parse.js --wt2html --domain en.wikipedia.org \
         --pageName $PAGENAME --record < /dev/null > /dev/null
@@ -23,13 +89,20 @@ fi
 
 # only re-run benchmark if output file doesn't exist.  otherwise, just
 # recompute average
-if [ ! -f "$OUTPUT" ]; then
-  for f in $(seq 1 $REPEAT) extra1 extra2; do
-    /usr/bin/time -f%U -o "$OUTPUT" -a \
-    $(dirname $0)/../bin/parse.js --wt2html --domain en.wikipedia.org \
+if [ -n "$FORCE" -o ! -f "$OUTPUT.times" ]; then
+    rm -f "$OUTPUT.times"
+    for f in $(seq 1 $REPEAT) extra1 extra2; do
+        /usr/bin/time -f%U -o "$OUTPUT.times" -a \
+        $(dirname $0)/../bin/parse.js --wt2html --domain en.wikipedia.org \
         --pageName $PAGENAME --replay < /dev/null > /dev/null
-  done
+    done
 fi
 
 # now summarize
-echo "scale=3;(" $(for f in $(sort -n "$OUTPUT" | head -n-1 | tail -$REPEAT) ; do echo -n $f + ; done) "0)/$REPEAT" | bc
+avg=$(echo "scale=3;(" $(for f in $(sort -n "$OUTPUT.times" | head -n-1 | tail -$REPEAT) ; do echo -n $f + ; done) "0)/$REPEAT" | bc)
+
+if [ -n "$GIT_STYLE" ]; then
+    echo $avg $log
+else
+    echo $avg
+fi
