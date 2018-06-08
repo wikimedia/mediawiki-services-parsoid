faffdbc103f28d27ac0c57d77499bc33f78de66c
Arlo Breault <abreault@wikimedia.org>
2018-06-08 11:31:22 -0400

    Stop shimming array includes
    node v4 is no longer supported
    
    Change-Id: I9ecbf850030eaf5d9c7978a73ee313c7947d08ba

--- core-upgrade.js
+++ core-upgrade.js
@@ -7,9 +7,6 @@ require('pn/_promise')(Promise); // This only needs to be done once.
 // Comments below annotate the highest lts version of node for which the
 // polyfills are necessary.  Remove when that version is no longer supported.
 
-// v4
-require('core-js/fn/array/includes');
-
 // v6
 require('core-js/fn/object/entries');
 require('core-js/fn/string/pad-start');
