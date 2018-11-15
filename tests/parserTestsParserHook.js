a0408f8aa27ee0675dee5467a43eecdb6362eb0d
Arlo Breault <abreault@wikimedia.org>
2018-11-15 15:49:00 -0500

    Convert parserTestsParserHook extensions to using toDOM instead of tokenHandler
    Change-Id: I356f5d7d3d1a0cdba7d52367f30761067e537bb1

--- tests/parserTestsParserHook.js
+++ tests/parserTestsParserHook.js
@@ -1,71 +1,59 @@
 'use strict';
 
-var ParsoidExtApi = module.parent.require('./extapi.js').versionCheck('^0.9.0');
-var PipelineUtils = ParsoidExtApi.PipelineUtils;
-var DU = ParsoidExtApi.DOMUtils;
-var Util = ParsoidExtApi.Util;
+const ParsoidExtApi = module.parent.require('./extapi.js').versionCheck('^0.9.0');
+const { DOMUtils: DU, returnDoc } = ParsoidExtApi;
 
 /**
- * See tests/parser/parserTestsParserHook.php in core.
+ * See tests/parser/ParserTestParserHook.php in core.
  */
 
-var myLittleHelper = function(env, extToken, argDict, html, cb) {
-	var tsr = extToken.dataAttribs.tsr;
-
-	if (!extToken.dataAttribs.tagWidths[1]) {
-		argDict.body = undefined;  // Serialize to self-closing.
-	}
-
-	var addWrapperAttrs = function(firstNode) {
-		firstNode.setAttribute('typeof', 'mw:Extension/' + argDict.name);
-		DU.setDataMw(firstNode, argDict);
-		DU.setDataParsoid(firstNode, {
-			tsr: Util.clone(tsr),
-			src: extToken.dataAttribs.src,
-		});
-	};
-
-	var body = DU.ppToDOM(html);
-	var tokens = PipelineUtils.buildDOMFragmentTokens(
-		env, extToken, body, addWrapperAttrs,
-		{ setDSR: true, isForeignContent: true }
-	);
-
-	cb({ tokens: tokens });
+const dumpHook = function(state, content, args) {
+	return returnDoc(state, DU.parseHTML('<pre />'));
 };
 
-var dumpHook = function(manager, pipelineOpts, extToken, cb) {
-	// All the interesting info is in data-mw.
-	var html = '<pre />';
-	var argDict = Util.getExtArgInfo(extToken).dict;
-	myLittleHelper(manager.env, extToken, argDict, html, cb);
+const staticTagHook = function(state, content, args) {
+	// FIXME: Choose a better DOM representation that doesn't mess with
+	// newline constraints.
+	return returnDoc(state, DU.parseHTML('<span />'));
 };
 
-// Async processing means this isn't guaranteed to be in the right order.
-// Plus, parserTests reuses the environment so state is bound to clash.
-var staticTagHook = function(manager, pipelineOpts, extToken, cb) {
-	var argDict = Util.getExtArgInfo(extToken).dict;
-	var html;
-	if (argDict.attrs.action === 'flush') {
-		html = '<p>' + this.state.buf + '</p>';
-		this.state.buf = '';  // Reset.
-	} else {
-		// FIXME: Choose a better DOM representation that doesn't mess with
-		// newline constraints.
-		html = '<span />';
-		this.state.buf += argDict.body.extsrc;
+const staticTagPostProcessor = function(node, obj) {
+	let child = node.firstChild;
+	while (child !== null) {
+		const nextChild = child.nextSibling;
+		if (DU.isElt(child)) {
+			const typeOf = child.getAttribute('typeOf');
+			if ((/(?:^|\s)mw:Extension\/statictag(?=$|\s)/).test(typeOf)) {
+				const dataMw = DU.getDataMw(child);
+				if (dataMw.attrs.action === 'flush') {
+					child.appendChild(child.ownerDocument.createTextNode(obj.buf));
+					obj.buf = '';
+				} else {
+					obj.buf += dataMw.body.extsrc;
+				}
+			} else if (child.hasChildNodes()) {
+				staticTagPostProcessor(child, obj);
+			}
+		}
+		child = nextChild;
 	}
-	myLittleHelper(manager.env, extToken, argDict, html, cb);
 };
 
 // Tag constructor
 module.exports = function() {
-	this.state = { buf: '' };  // Ughs
 	this.config = {
 		tags: [
-			{ name: 'tag', tokenHandler: dumpHook },
-			{ name: 'tåg', tokenHandler: dumpHook },
-			{ name: 'statictag', tokenHandler: staticTagHook.bind(this) },
+			{ name: 'tag', toDOM: dumpHook },
+			{ name: 'tåg', toDOM: dumpHook },
+			{ name: 'statictag', toDOM: staticTagHook },
 		],
+		domProcessors: {
+			wt2htmlPostProcessor: (body, env, options, atTopLevel) => {
+				if (atTopLevel) {
+					const obj = { buf: '' };
+					staticTagPostProcessor(body, obj);
+				}
+			},
+		},
 	};
 };
