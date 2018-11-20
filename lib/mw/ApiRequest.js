wikimedia/mediawiki-services-parsoid

commit 8fd4c9bbee79ab0fe30f86d6ba44dc73b11b62a1

Make the http connect timeout configurable
 * No longer do we set the global httpAgents, but instead only pass the
   ones we've constructed to the mw api requests.

 * Only construct the agents once we have a parsoid config so that we
   can make this knob configurable.

 * Set the http connect timeout to 10s while testing.

Change-Id: I212364070a58c72ae6fd35fb78b506cc58e8b3ff

lib/mw/ApiRequest.js
@@ -9,14 +9,8 @@ var semver = require('semver');
 
 var Promise = require('../utils/promise.js');
 
-// Max concurrency level for accessing the Mediawiki API
-var HTTP_MAX_SOCKETS = 15;
 
-// Connection timeout setting for the http agent
-var HTTP_CONNECT_TIMEOUT = 5 * 1000;  // 5 seconds
-
-
-function setupConnectionTimeout(protocol) {
+function setupConnectionTimeout(env, protocol) {
 	var http = require(protocol);
 	var Agent = http.Agent;
 
@@ -73,14 +67,11 @@ function setupConnectionTimeout(protocol) {
 		}
 	};
 
-	http.globalAgent = new ConnectTimeoutAgent({
-		connectTimeout: HTTP_CONNECT_TIMEOUT,
-		maxSockets: HTTP_MAX_SOCKETS,
+	return new ConnectTimeoutAgent({
+		connectTimeout: env.conf.parsoid.timeouts.mwApi.connect,
+		maxSockets: env.conf.parsoid.maxSockets,
 	});
 }
-setupConnectionTimeout('http');
-setupConnectionTimeout('https');
-
 
 var latestSerial = 0;
 
@@ -287,12 +278,22 @@ function ApiRequest(env, title) {
 // Inherit from EventEmitter
 util.inherits(ApiRequest, events.EventEmitter);
 
+var httpAgent = null;
+var httpsAgent = null;
+
 ApiRequest.prototype.request = function(options, callback) {
 	var env = this.env;
 	var proxy = this.proxy;
+
 	// this is a good place to put debugging statements
 	// if you want to watch network requests.
 	// console.log('ApiRequest', options);
+
+	if (httpAgent === null) {
+		httpAgent = setupConnectionTimeout(env, 'http');
+		httpsAgent = setupConnectionTimeout(env, 'https');
+	}
+	options.agent = /^https[:]/.test(options.uri) ? httpsAgent : httpAgent;
 
 	// Forward the request id
 	if (!options.headers) { options.headers = {}; }
@@ -307,12 +308,13 @@ ApiRequest.prototype.request = function(options, callback) {
 	// Proxy options should only be applied to MW API endpoints.
 	// Allow subclasses to manually set proxy to `null` or to a different
 	// proxy to override MW API proxy.
-	if (proxy && options.proxy === undefined) {
+	if (proxy && proxy.uri && options.proxy === undefined) {
 		options.proxy = proxy.uri;
+		options.agent = /^https[:]/.test(proxy.uri) ? httpsAgent : httpAgent;
 		if (proxy.headers) {
 			Object.assign(options.headers, proxy.headers);
 		}
-		if (proxy.strip_https && options.proxy && /^https[:]/.test(options.uri)) {
+		if (proxy.strip_https && /^https[:]/.test(options.uri)) {
 			// When proxying, strip TLS and lie to the appserver to indicate
 			// unwrapping has just occurred. The appserver isn't listening on
 			// port 443 but a site setting may require a secure connection,

bin/parserTests.js
@@ -1696,6 +1696,8 @@ ParserTests.prototype.main = function(options, popts) {
 		parsoidConfig.loggerSampling = [
 			[/^warning(\/|$)/, 100],
 		];
+
+		parsoidConfig.timeouts.mwApi.connect = 10000;
 	};
 
 	var parsoidConfig = new ParsoidConfig({ setup: setup }, options);

lib/config/ParsoidConfig.js
@@ -49,8 +49,13 @@ var CONFIG_DEFAULTS = Object.freeze({
 			imgInfo: 40 * 1000,
 			// action=query&meta=siteinfo
 			configInfo: 40 * 1000,
+			// Connection timeout setting for the http agent
+			connect: 5 * 1000,
 		},
 	},
+
+	// Max concurrency level for accessing the Mediawiki API
+	maxSockets: 15,
 
 	retries: {
 		mwApi: {
@@ -109,6 +114,7 @@ function ParsoidConfig(localSettings, options) {
 	this.timeouts = Util.clone(CONFIG_DEFAULTS.timeouts);
 	this.retries = Util.clone(CONFIG_DEFAULTS.retries);
 	this.limits = Util.clone(CONFIG_DEFAULTS.limits);
+	this.maxSockets = Util.clone(CONFIG_DEFAULTS.maxSockets);
 	this._uniq = 0;
 
 	if (localSettings && localSettings.setup) {

tests/mocha/api.js
@@ -13,6 +13,7 @@ var configPath = path.resolve(__dirname, './apitest.localsettings.js');
 var fakeConfig = {
 	setMwApi: function() {},
 	limits: { wt2html: {}, html2wt: {} },
+	timeouts: { mwApi: {} },
 };
 require(configPath).setup(fakeConfig);  // Set limits.
 

tests/mocha/apitest.localsettings.js
@@ -71,6 +71,7 @@ exports.setup = function(parsoidConfig) {
 		[/^info(\/|$)/, 100],
 	];
 
+	parsoidConfig.timeouts.mwApi.connect = 10000;
 	parsoidConfig.limits.wt2html.maxWikitextSize = 20000;
 	parsoidConfig.limits.html2wt.maxHTMLSize = 10000;
 

tests/mocha/templatedata.js
@@ -11,6 +11,7 @@ var configPath = path.resolve(__dirname, './apitest.localsettings.js');
 var fakeConfig = {
 	setMwApi: function() {},
 	limits: { wt2html: {}, html2wt: {} },
+	timeouts: { mwApi: {} },
 };
 require(configPath).setup(fakeConfig);  // Set limits
 

tests/mockAPI.js
@@ -12,6 +12,7 @@ var crypto = require('crypto');
 var fakeConfig = {
 	setMwApi: function() {},
 	limits: { wt2html: {}, html2wt: {} },
+	timeouts: { mwApi: {} },
 };
 require('./mocha/apitest.localsettings.js').setup(fakeConfig);
 

commit 6693ee4d1a8609f17030fed3dce571b657905fef

Fix tracing apirequest
Has this been broken since 8fd4c9b?

Change-Id: If0cfd71dc592c78b9f736054e75bd23586779084

lib/mw/ApiRequest.js
@@ -326,7 +326,10 @@ ApiRequest.prototype.request = function(requestOpts) {
 		}
 	}
 
-	this.trace("Starting HTTP request: ", options);
+	this.trace("Starting HTTP request: ", function() {
+		// Omit agent since it isn't exactly serializable
+		return Object.assign({}, options, { agent: undefined });
+	});
 	const startTime = this.traceTime ? JSUtils.startTime() : undefined;
 	return request(options,
 		(error, response, body) => this._requestCB(startTime, error, response, body));