wikimedia/mediawiki-services-parsoid

commit 7c6e86d321f2edea3c480ccabf3d493ed2de5d4c

Move addRedLinks to DOMPostProcessor; allow async processors.
The forthcoming LanguageConverter pass will also be hooked into
DOMPostProcessor.  This uses the 'yield' syntax (node 4 and up) to
allow asynchronous processing steps in DOMPostProcessor.

Change-Id: Id7d97a8c944c05177250adb31a22349f5db01943

lib/wt2html/DOMPostProcessor.js
@@ -11,6 +11,7 @@ var util = require('util');
 
 var DU = require('../utils/DOMUtils.js').DOMUtils;
 var DOMTraverser = require('../utils/DOMTraverser.js').DOMTraverser;
+var Promise = require('../utils/promise.js');
 
 // processors
 var requireProcessor = function(p) {
@@ -246,6 +247,13 @@ function DOMPostProcessor(env, options) {
 	domVisitor.addHandler(null, CleanUp.cleanupAndSaveDataParsoid);
 	addPP('stripMarkerMetas+cleanupAndSaveDP', domVisitor.traverse.bind(domVisitor));
 
+	// (Optional) red links
+	addPP('addRedLinks', function(rootNode, env, options, atTopLevel) {
+		if (atTopLevel && env.conf.parsoid.useBatchAPI) {
+			// Async; returns promise for completion.
+			return DU.addRedLinks(env, rootNode.ownerDocument);
+		}
+	});
 }
 
 // Inherit from EventEmitter
@@ -463,7 +471,7 @@ DOMPostProcessor.addMetaData = function(env, document) {
 	document.body.classList.add('mw-parser-output');
 };
 
-DOMPostProcessor.prototype.doPostProcess = function(document) {
+DOMPostProcessor.prototype.doPostProcess = Promise.async(function *(document) {
 	var env = this.env;
 
 	var psd = env.conf.parsoid;
@@ -507,7 +515,8 @@ DOMPostProcessor.prototype.doPostProcess = function(document) {
 				ppStart = Date.now();
 				env.log(logLevel, prefix + "; " + ppName + " start");
 			}
-			pp.proc(document.body, env, this.options, this.atTopLevel);
+			// Processors can return a Promise iff they need to be async.
+			yield pp.proc(document.body, env, this.options, this.atTopLevel);
 			if (tracePP) {
 				ppEnd = Date.now();
 				env.log(logLevel, prefix + "; " + ppName + " end; time = " + (ppEnd - ppStart));
@@ -536,7 +545,7 @@ DOMPostProcessor.prototype.doPostProcess = function(document) {
 	}
 
 	this.emit('document', document);
-};
+});
 
 /**
  * Register for the 'document' event, normally emitted from the HTML5 tree

lib/parse.js
@@ -33,11 +33,6 @@ _wt2html = function(obj, env, wt) {
 	}
 	var handler = env.getContentHandler(obj.contentmodel);
 	return handler.toHTML(env)
-	.tap(function(doc) {
-		if (env.conf.parsoid.useBatchAPI) {
-			return DU.addRedLinks(env, doc);
-		}
-	})
 	.then(function(doc) {
 		var out;
 		if (env.pageBundle) {

commit 8772af9616bab37914f80b24ed69c69896cd5654

Make sure errors in doPostProcess rethrow
Looks like this was introduced in 7c6e86d when it was promisified.

doPostProcess returns a promise since that commit but nothing was
handling it.  The `.done()` here takes care of that.

Change-Id: I4bcff92d147de8fc9c468814e3ca758ee6113567

lib/wt2html/DOMPostProcessor.js
@@ -693,7 +693,9 @@ DOMPostProcessor.prototype.doPostProcess = Promise.async(function *(document) {
  * builder.
  */
 DOMPostProcessor.prototype.addListenersOn = function(emitter) {
-	emitter.addListener('document', this.doPostProcess.bind(this));
+	emitter.addListener('document', (document) => {
+		this.doPostProcess(document).done();
+	});
 };
 
 if (typeof module === "object") {