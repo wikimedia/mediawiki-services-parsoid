wikimedia/mediawiki-services-parsoid

commit e0775d49b836738d8dd09200df7fc19257dc101c

Fix potential content version inconsistency in apiUtils.wt2htmlRes
Should only depend on the argument, not env.

Change-Id: Ia8ac75541c5cd58b870c2d7811d56328abab2710

lib/api/routes.js
@@ -376,7 +376,7 @@ module.exports = function routes(parsoidConfig, processLogger, parsoidOptions, p
 		if (opts.format === 'lint') {
 			apiUtils.jsonResponse(res, out.lint);
 		} else {
-			apiUtils.wt2htmlRes(env, res, out.html, out.pb, out.contentmodel, out.headers);
+			apiUtils.wt2htmlRes(res, out.html, out.pb, out.contentmodel, out.headers, env.outputContentVersion);
 		}
 		var html = out.html;
 		if (metrics) {

bin/roundtrip-test.js
@@ -559,7 +559,7 @@ var parsoidPost = Promise.async(function *(profile, options) {
 		if (options.oldid) {
 			uri += '/' + options.oldid;
 		}
-		httpOptions.headers.Accept = apiUtils.pagebundleContentType(null, options.contentVersion);
+		httpOptions.headers.Accept = apiUtils.pagebundleContentType(options.contentVersion);
 		// setting json here encodes the request *and* decodes the response.
 		httpOptions.json = true;
 	}

lib/api/apiUtils.js
@@ -237,41 +237,37 @@ apiUtils.wikitextContentType = function(env) {
 /**
  * Return the appropriate content-type string for Parsoid HTML.
  *
- * @param {MWParserEnvironment} env
- * @param {string} contentVersion
+ * @param {string} outputContentVersion
  */
-apiUtils.htmlContentType = function(env, contentVersion) {
-	return 'text/html; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/HTML/' + (contentVersion || env.outputContentVersion) + '"';
+apiUtils.htmlContentType = function(outputContentVersion) {
+	return 'text/html; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/HTML/' + outputContentVersion + '"';
 };
 
 /**
  * Return the appropriate content-type string for a Parsoid page bundle.
  *
- * @param {MWParserEnvironment} env
- * @param {string} contentVersion
+ * @param {string} outputContentVersion
  */
-apiUtils.pagebundleContentType = function(env, contentVersion) {
-	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/pagebundle/' + (contentVersion || env.outputContentVersion) + '"';
+apiUtils.pagebundleContentType = function(outputContentVersion) {
+	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/pagebundle/' + outputContentVersion + '"';
 };
 
 /**
  * Return the appropriate content-type string for a data-parsoid JSON blob.
  *
- * @param {MWParserEnvironment} env
- * @param {string} contentVersion
+ * @param {string} outputContentVersion
  */
-apiUtils.dataParsoidContentType = function(env, contentVersion) {
-	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/data-parsoid/' + (contentVersion || env.outputContentVersion) + '"';
+apiUtils.dataParsoidContentType = function(outputContentVersion) {
+	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/data-parsoid/' + outputContentVersion + '"';
 };
 
 /**
  * Return the appropriate content-type string for a data-mw JSON blob.
  *
- * @param {MWParserEnvironment} env
- * @param {string} contentVersion
+ * @param {string} outputContentVersion
  */
-apiUtils.dataMwContentType = function(env, contentVersion) {
-	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/data-mw/' + (contentVersion || env.outputContentVersion) + '"';
+apiUtils.dataMwContentType = function(outputContentVersion) {
+	return 'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/data-mw/' + outputContentVersion + '"';
 };
 
 /**
@@ -571,7 +567,7 @@ apiUtils.returnDowngrade = function(downgrade, metrics, env, revision, res, cont
 	var html = DU.toXML(res.locals.body_only ? doc.body : doc, {
 		innerXML: res.locals.body_only,
 	});
-	apiUtils.wt2htmlRes(env, res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc));
+	apiUtils.wt2htmlRes(res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc), env.outputContentVersion);
 };
 
 /**
@@ -591,7 +587,8 @@ apiUtils.updateRedLinks = Promise.async(function *(env, revision, res, contentmo
 	var html = DU.toXML(res.locals.body_only ? doc.body : doc, {
 		innerXML: res.locals.body_only,
 	});
-	apiUtils.wt2htmlRes(env, res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc), env.inputContentVersion);
+	// Since this an update, return the `inputContentVersion` as the `outputContentVersion`
+	apiUtils.wt2htmlRes(res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc), env.inputContentVersion);
 });
 
 apiUtils.languageConversion = function(env, revision, res, contentmodel, targetVariant, sourceVariant) {
@@ -635,7 +632,8 @@ apiUtils.languageConversion = function(env, revision, res, contentmodel, targetV
 		const html = DU.toXML(res.locals.body_only ? doc.body : doc, {
 			innerXML: res.locals.body_only,
 		});
-		apiUtils.wt2htmlRes(env, res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc), env.inputContentVersion);
+		// Since this an update, return the `inputContentVersion` as the `outputContentVersion`
+		apiUtils.wt2htmlRes(res, html, pb, contentmodel, DU.findHttpEquivHeaders(doc), env.inputContentVersion);
 	} else {
 		// Return 400 if you request LanguageConversion for a page which
 		// didn't set `Vary: Accept-Language`.
@@ -649,37 +647,38 @@ apiUtils.languageConversion = function(env, revision, res, contentmodel, targetV
 /**
  * Send an appropriate response with the right content types for wt2html.
  *
- * @param {MWParserEnvironment} env
  * @param {Object} res
  * @param {string} html
  * @param {Object} pb
  * @param {string} [contentmodel]
+ * @param {Object} headers
+ * @param {string} outputContentVersion
  */
-apiUtils.wt2htmlRes = function(env, res, html, pb, contentmodel, headers, contentVersion) {
+apiUtils.wt2htmlRes = function(res, html, pb, contentmodel, headers, outputContentVersion) {
 	if (pb) {
 		var response = {
 			contentmodel: contentmodel,
 			html: {
 				headers: Object.assign({
-					'content-type': apiUtils.htmlContentType(env, contentVersion),
+					'content-type': apiUtils.htmlContentType(outputContentVersion),
 				}, headers),
 				body: html,
 			},
 			'data-parsoid': {
-				headers: { 'content-type': apiUtils.dataParsoidContentType(env, contentVersion) },
+				headers: { 'content-type': apiUtils.dataParsoidContentType(outputContentVersion) },
 				body: pb.parsoid,
 			},
 		};
-		if (semver.satisfies(env.outputContentVersion, '^999.0.0')) {
+		if (semver.satisfies(outputContentVersion, '^999.0.0')) {
 			response['data-mw'] = {
-				headers: { 'content-type': apiUtils.dataMwContentType(env, contentVersion) },
+				headers: { 'content-type': apiUtils.dataMwContentType(outputContentVersion) },
 				body: pb.mw,
 			};
 		}
-		apiUtils.jsonResponse(res, response, undefined, apiUtils.pagebundleContentType(env, contentVersion));
+		apiUtils.jsonResponse(res, response, undefined, apiUtils.pagebundleContentType(outputContentVersion));
 	} else {
 		apiUtils.htmlResponse(res, html, undefined, Object.assign({
-			'content-type': apiUtils.htmlContentType(env, contentVersion),
+			'content-type': apiUtils.htmlContentType(outputContentVersion),
 		}, headers), true);
 	}
 };

commit 0b4982d8c1772fddc648f93f0d63dc52fa2a88bb

Fix unacceptable response
This was broken in e0775d4 when `env` was removed from the function
signature but not noticed because the method names were concatenated.

Noticed in https://logstash.wikimedia.org/app/kibana#/doc/logstash-*/logstash-2018.12.05/parsoid?id=AWd8l5Sci-uKhB2cRTVA&_g=h@44136fa

Change-Id: I4f3356f6607027daef69f6c5207ea196d6f76c26

lib/api/routes.js
@@ -220,7 +220,17 @@ module.exports = function routes(parsoidConfig, processLogger, parsoidOptions, p
 		// Validate and set the content version
 		if (!apiUtils.validateAndSetOutputContentVersion(res, acceptableTypes)) {
 			var text = env.availableVersions.reduce(function(prev, curr) {
-				return prev + apiUtils[opts.format + 'ContentType'](env, curr) + '\n';
+				switch (opts.format) {
+					case 'html':
+						prev += apiUtils.htmlContentType(curr);
+						break;
+					case 'pagebundle':
+						prev += apiUtils.pagebundleContentType(curr);
+						break;
+					default:
+						console.assert(false, `Unexpected format: ${opts.format}`);
+				}
+				return `${prev}\n`;
 			}, 'Not acceptable.\n');
 			return apiUtils.fatalRequest(env, text, 406);
 		}

tests/mocha/api.js
@@ -202,6 +202,14 @@ describe('Parsoid API', function() {
 			.set('Accept', 'text/html; profile="https://www.mediawiki.org/wiki/Specs/HTML/0.0.0"')
 			.send({ wikitext: '== h2 ==' })
 			.expect(406)
+			.expect(function(res) {
+				res.error.text.should.equal(
+					'Not acceptable.\n' +
+					'text/html; charset=utf-8; profile=&quot;https://www.mediawiki.org/wiki/Specs/HTML/2.0.0&quot;\n' +
+					'text/html; charset=utf-8; profile=&quot;https://www.mediawiki.org/wiki/Specs/HTML/1.8.0&quot;\n' +
+					'text/html; charset=utf-8; profile=&quot;https://www.mediawiki.org/wiki/Specs/HTML/999.0.0&quot;\n'
+				);
+			})
 			.end(done);
 		});
 
@@ -211,6 +219,14 @@ describe('Parsoid API', function() {
 			.set('Accept', 'application/json; profile="https://www.mediawiki.org/wiki/Specs/HTML/0.0.0"')
 			.send({ wikitext: '== h2 ==' })
 			.expect(406)
+			.expect(function(res) {
+				JSON.parse(res.error.text).error.should.equal(
+					'Not acceptable.\n' +
+					'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/pagebundle/2.0.0"\n' +
+					'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/pagebundle/1.8.0"\n' +
+					'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/pagebundle/999.0.0"\n'
+				);
+			})
 			.end(done);
 		});
 