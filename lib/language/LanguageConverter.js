wikimedia/mediawiki-libs-LangConv

Don't convert external link text which looks like a URL
Change-Id: I760670693a6c2053939da3e944ec156794745353

lib/LanguageConverter.js
@@ -221,14 +221,23 @@ class ConversionTraverser extends DOMTraverser {
 			// Don't convert title or children of interwiki links
 			return node.nextSibling;
 		} else if (rel === 'mw:ExtLink') {
-			if (DU.usesURLLinkSyntax(node)) {
+			// DU.usesURLLinkSyntax uses data-parsoid, but syntactic free
+			// links should also have class="external free"
+			if (DU.usesURLLinkSyntax(node) || node.classList.contains('free')) {
 				// Don't convert children of syntactic "free links"
 				return node.nextSibling;
 			}
-			// It appears that all other external link text is also
-			// marked to protect it from conversion (see
-			// Parser.php::replaceExternalLinks).  That seems suboptimal?
-			return node.nextSibling;
+			// Other external link text is protected from conversion iff
+			// (a) it doesn't starts/end with -{ ... }-
+			if (node.firstChild && DU.hasTypeOf(node.firstChild, 'mw:LanguageVariant')) {
+				return true;
+			}
+			// (b) it looks like a URL (protocol-relative links excluded)
+			const linkText = node.textContent; // XXX: this could be expensive
+			if (Util.isProtocolValid(linkText, env) &&
+				!linkText.startsWith('//')) {
+				return node.nextSibling;
+			}
 		}
 		return true;
 	}

wikimedia/mediawiki-services-parsoid

lib/language/LanguageConverter.js
@@ -223,14 +223,23 @@ class ConversionTraverser extends DOMTraverser {
 			// Don't convert title or children of interwiki links
 			return node.nextSibling;
 		} else if (rel === 'mw:ExtLink') {
-			if (WTUtils.usesURLLinkSyntax(node)) {
+			// WTUtils.usesURLLinkSyntax uses data-parsoid, but syntactic free
+			// links should also have class="external free"
+			if (WTUtils.usesURLLinkSyntax(node) || node.classList.contains('free')) {
 				// Don't convert children of syntactic "free links"
 				return node.nextSibling;
 			}
-			// It appears that all other external link text is also
-			// marked to protect it from conversion (see
-			// Parser.php::replaceExternalLinks).  That seems suboptimal?
-			return node.nextSibling;
+			// Other external link text is protected from conversion iff
+			// (a) it doesn't starts/end with -{ ... }-
+			if (node.firstChild && DOMDataUtils.hasTypeOf(node.firstChild, 'mw:LanguageVariant')) {
+				return true;
+			}
+			// (b) it looks like a URL (protocol-relative links excluded)
+			const linkText = node.textContent; // XXX: this could be expensive
+			if (Util.isProtocolValid(linkText, env) &&
+				!linkText.startsWith('//')) {
+				return node.nextSibling;
+			}
 		}
 		return true;
 	}