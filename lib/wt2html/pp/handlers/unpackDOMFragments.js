wikimedia/mediawiki-services-parsoid

commit b241ac60d01670a6ad2cdd0a40cf0a8e2d571a64

Extension content is the only `isForeignContent`
Moves span wrapping and annotation into the extension handler.  Having
control there will allow the nowiki extension omit these steps since its
content is not considered `isForeignContent`.

This also eliminated the need to discard unnecessary span wrappers.

`addAttrsCB` is removed as an option to `buildDOMFragmentTokens` since
it was only being called extensions.

Change-Id: I230d05fea44b206903102d2e97ff1e9c405dbd1f

lib/wt2html/pp/handlers/unpackDOMFragments.js
@@ -6,7 +6,6 @@ var DU = require('../../../utils/DOMUtils.js').DOMUtils;
 var Util = require('../../../utils/Util.js').Util;
 var DOMTraverser = require('../../../utils/DOMTraverser.js').DOMTraverser;
 
-
 function hasBadNesting(targetNode, fragment) {
 	// SSS FIXME: This is not entirely correct. This is only
 	// looking for nesting of identical tags. But, HTML tree building
@@ -222,10 +221,12 @@ function unpackDOMFragments(node, env) {
 
 	if (dp.tmp.isForeignContent) {
 		const about = node.getAttribute('about');
-		// Foreign Content = Transclusion and Extension content
+		// Foreign Content = Extension content
 		//
 		// Set about-id always to ensure the unwrapped node
 		// is recognized as encapsulated content as well.
+		//
+		// Foreign content is assumed to already have been span wrapped.
 		n = dummyNode.firstChild;
 		while (n) {
 			if (DU.isElt(n)) {
@@ -242,24 +243,7 @@ function unpackDOMFragments(node, env) {
 		// of those individual transclusions should get a new unique
 		// about id. Hence a need for an aboutIdMap and the need to
 		// walk the entire tree.
-
 		fixAbouts(env, dummyNode, new Map());
-
-		// Discard unnecessary span wrappers
-		n = dummyNode.firstChild;
-		while (n) {
-			var next = n.nextSibling;
-
-			// Preserve wrappers that have an about id
-			if (DU.isElt(n) && !n.getAttribute('about')) {
-				if (DU.getDataParsoid(n).tmp.wrapper) {
-					DU.migrateChildren(n, n.parentNode, n);
-					n.parentNode.removeChild(n);
-				}
-			}
-
-			n = next;
-		}
 	}
 
 	var nextNode = node.nextSibling;

lib/utils/PipelineUtils.js
@@ -472,34 +472,15 @@ var PipelineUtils = {
 	 * @param {Node} body
 	 *    The DOM that the token expanded to.
 	 *
-	 * @param {Function} addAttrsCB
-	 *    Callback that adds additional attributes to the generated tokens.
-	 *
 	 * @param {Object} opts
 	 *    Options to be passed onto the encapsulation code
 	 *    See encapsulateExpansionHTML's doc. for more info about these options.
 	 */
-	buildDOMFragmentTokens: function(env, token, body, addAttrsCB, opts) {
+	buildDOMFragmentTokens: function(env, token, body, opts) {
 		console.assert(DU.isBody(body), 'DOMFragment expected body node.');
-
-		var nodes;
-		if (body.hasChildNodes()) {
-			nodes = body.childNodes;
-		} else {
-			// RT extensions expanding to nothing.
-			nodes = [body.ownerDocument.createElement('link')];
-		}
-
-		// Wrap bare text nodes into spans
-		nodes = DU.addSpanWrappers(nodes);
-
-		if (addAttrsCB) {
-			addAttrsCB(nodes[0]);
-		}
-
 		// Get placeholder tokens to get our subdom through the token processing
 		// stages. These will be finally unwrapped on the DOM.
-		var expansion = PipelineUtils.makeExpansion(env, nodes);
+		var expansion = PipelineUtils.makeExpansion(env, body.childNodes);
 		return PipelineUtils.encapsulateExpansionHTML(env, token, expansion, opts);
 	},
 

lib/wt2html/tt/DOMFragmentBuilder.js
@@ -101,7 +101,7 @@ class DOMFragmentBuilder extends TokenHandler {
 
 	wrapDOMFragment(cb, scopeToken, pipelineOpts, dom) {
 		// Pass through pipeline options
-		const toks = PipelineUtils.buildDOMFragmentTokens(this.manager.env, scopeToken, dom.body, false, {
+		const toks = PipelineUtils.buildDOMFragmentTokens(this.manager.env, scopeToken, dom.body, {
 			pipelineOpts,
 		});
 

lib/wt2html/tt/ExtensionHandler.js
@@ -211,24 +211,6 @@ class ExtensionHandler extends TokenHandler {
 			nativeExt.modifyArgDict(env, argDict);
 		}
 
-		const addWrapperAttrs = function(firstNode) {
-			// Adds the wrapper attributes to the first element
-			firstNode.setAttribute('typeof', state.wrapperType);
-
-			// Set data-mw
-			DU.setDataMw(
-				firstNode,
-				Object.assign(state.wrapperDataMw || {}, argDict)
-			);
-
-			// Update data-parsoid
-			const dp = DU.getDataParsoid(firstNode);
-			dp.tsr = Util.clone(state.token.dataAttribs.tsr);
-			dp.src = state.token.dataAttribs.src;
-			dp.tmp.isHtmlExt = state.isHtmlExt;
-			DU.setDataParsoid(firstNode, dp);
-		};
-
 		const opts = Object.assign({
 			setDSR: true,
 			isForeignContent: true,
@@ -237,13 +219,36 @@ class ExtensionHandler extends TokenHandler {
 		// The default setting is to unwrap the content DOM fragment automatically.
 		}, nativeExt && nativeExt.fragmentOptions);
 
-		const toks = PipelineUtils.buildDOMFragmentTokens(
-			env,
-			state.token,
-			doc.body,
-			addWrapperAttrs,
-			opts
-		);
+		const body = doc.body;
+
+		if (opts.isForeignContent) {
+			if (!body.hasChildNodes()) {
+				// RT extensions expanding to nothing.
+				body.appendChild(body.ownerDocument.createElement('link'));
+			}
+
+			// Wrap the top-level nodes so that we have a firstNode element
+			// to annotate with the typeof and to apply about ids.
+			DU.addSpanWrappers(body.childNodes);
+
+			// Now get the firstNode
+			const firstNode = body.firstChild;
+
+			// Adds the wrapper attributes to the first element
+			firstNode.setAttribute('typeof', state.wrapperType);
+
+			// Set data-mw
+			DU.setDataMw(firstNode, Object.assign(state.wrapperDataMw || {}, argDict));
+
+			// Update data-parsoid
+			const dp = DU.getDataParsoid(firstNode);
+			dp.tsr = Util.clone(state.token.dataAttribs.tsr);
+			dp.src = state.token.dataAttribs.src;
+			dp.tmp.isHtmlExt = state.isHtmlExt;
+			DU.setDataParsoid(firstNode, dp);
+		}
+
+		const toks = PipelineUtils.buildDOMFragmentTokens(env, state.token, body, opts);
 
 		cb({ tokens: toks });
 	}

lib/wt2html/tt/LinkHandler.js
@@ -263,7 +263,7 @@ class WikiLinkHandler extends TokenHandler {
 			}
 
 			const body = DU.ppToDOM(html);
-			dft = PipelineUtils.buildDOMFragmentTokens(env, token, body, null, {
+			dft = PipelineUtils.buildDOMFragmentTokens(env, token, body, {
 				tsr: tsr,
 				pipelineOpts: { inlineContext: true },
 			});

commit 4eba44e58d95d2feb92700617eb729247fd48ba0

Account for span wrapping when fragment is first transclusion node
A regression introduced in b241ac6.

Fixes the crashers on
http://localhost:8000/zh.wikipedia.org/v3/page/html/%E9%8C%B8%E7%9A%84%E5%90%8C%E4%BD%8D%E7%B4%A0/52156747
http://localhost:8000/en.wikipedia.org/v3/page/html/User_talk%3ANetha_Hussain%2FArchive_4/872287243
and others.

An isolated case from the zhwiki one is,
```
[[Test|{{1x|[[Hmm|Something <sup>strange</sup>]]}}]]
```

This edge case of link-in-link exposes that it's possible to end up with
a document fragment at the first encapsulation position of a template
without having span wrapped the fragment contents.  At present, only link
content (ignoring the nowiki extension exception) omits span wrapping
the fragment contents since it's being placed in an anchor.  Normally,
transclusion annotation would end up on the anchor itself, instead of
the link contents, but because of the content model violation here, we
end up trying to place the annotation on the text node ("Something").

This is more than just edge case handling since in the future we may
drop the span wrapping in the nowiki extension or come up with some
other wikitext construct that uses dom fragment without wrapping.

Bug: T211941
Change-Id: Ic398a6082b5f012823afe2c32c4606c70df1c5da

lib/wt2html/pp/handlers/unpackDOMFragments.js
@@ -183,9 +183,20 @@ function unpackDOMFragments(node, env) {
 		DOMUtils.visitDOM(dummyNode, DOMDataUtils.loadDataAttribs);
 	}
 
-	var contentNode = dummyNode.firstChild;
+	let contentNode = dummyNode.firstChild;
 
 	if (/(?:^|\s)mw:Transclusion(?=$|\s)/.test(typeOf)) {
+		// Ensure our `firstChild` is an element to add annotation.  At present,
+		// we're unlikely to end up with translusion annotations on fragments
+		// where span wrapping hasn't occurred (ie. link contents, since that's
+		// placed on the anchor itself) but in the future, nowiki spans may be
+		// omitted or new uses for dom fragments found.  For now, the test case
+		// necessitating this is an edgy link-in-link scenario:
+		//   [[Test|{{1x|[[Hmm|Something <sup>strange</sup>]]}}]]
+		PipelineUtils.addSpanWrappers(dummyNode.childNodes);
+		// Reset `contentNode`, since the `firstChild` may have changed in
+		// span wrapping.
+		contentNode = dummyNode.firstChild;
 		// Transfer typeof, data-mw, and param info
 		// about attributes are transferred below.
 		DOMDataUtils.setDataMw(contentNode, Util.clone(DOMDataUtils.getDataMw(node)));
@@ -249,6 +260,8 @@ function unpackDOMFragments(node, env) {
 	// transclusion.
 	const about = node.getAttribute('about');
 	if (about !== null) {
+		// Span wrapping may not have happened for the transclusion above if
+		// the fragment is not the first encapsulation wrapper node.
 		PipelineUtils.addSpanWrappers(dummyNode.childNodes);
 		let n = dummyNode.firstChild;
 		while (n) {