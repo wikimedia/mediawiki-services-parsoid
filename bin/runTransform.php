db704eea853603433ec702cc50b21675093ff397
Subramanya Sastry <ssastry@wikimedia.org>
2019-01-09 13:54:43 -0600

    Add ability to splice a PHP transformer into the pipeline
    * I've now tested this with the QuoteTransformer and ParagraphWrapper and
      all wt2html tests pass currently with this hybrid pipeline.
    
      In the process,
      - fixed a few bugs in these transformers (separate patch submitted)
      - fixed JSON string -> token generation code (separate patch submitted)
      - fixed token -> JSON string output (separate patch submitted).
    
    * The pipeline factory looks for the presence of pipeline.yaml in the root
      directory, and if present, uses that config to replace JS transformers
      with PHP transformers.
    
    * A similar mechanism for DOM transformations is coming in a followup.
    
    * Once this is done, we could start running parser tests as more
      transformers get ported and would help catch a lot more bugs early.
    
    * TODO:
      - Potential performance improvements. Right now, every transformer
        in every pipeline forks a new process. If there is a chain of
        transformers that are all going to run in PHP, we might be able
        to run the entire chain of transformations in one shot.
    
    Change-Id: Iab8088d8552b9e873f0e3ccd9ff77f6539e82edc

---
+++ bin/runTransform.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../tests/MockEnv.php';
+
+use Parsoid\Tests\MockEnv;
+use Parsoid\Tokens\Token;
+use Parsoid\Utils\PHPUtils;
+use Parsoid\Wt2Html\TT\QuoteTransformer;
+use Parsoid\Wt2Html\TT\ParagraphWrapper;
+
+if ( PHP_SAPI !== 'cli' ) {
+	die( 'CLI only' );
+}
+
+if ( $argc < 2 ) {
+	throw new \Exception( "Provide the transformer name as the first arg." );
+}
+
+/**
+ * Read pipeline options + tokens from STDIN
+ */
+$input_file = 'php://stdin';
+$transformerName = $argv[1];
+$lines = explode( "\n", file_get_contents( $input_file ) );
+$optsString = array_shift( $lines );
+$pipelineOpts = PHPUtils::jsonDecode( $optsString );
+if ( !$pipelineOpts ) {
+	throw new \Exception( "Missing pipeline opts in first line of stdin" );
+}
+// fwrite(STDERR, $optsString . "\n");
+
+/**
+ * Decode the json-encoded strings to build tokens
+ */
+$tokens = [];
+foreach ( $lines as $line ) {
+	$tokens[] = Token::getToken( PHPUtils::jsonDecode( $line ) );
+}
+
+/**
+ * Build the requested transformer
+ */
+$transformer = null;
+$manager = (object)[];
+$manager->env = new MockEnv( [] );
+$manager->pipelineId = 0;
+$manager->options = [];
+switch ( $transformerName ) {
+	case "QuoteTransformer":
+		$transformer = new QuoteTransformer( $manager, $pipelineOpts );
+		break;
+	case "ParagraphWrapper":
+		$transformer = new ParagraphWrapper( $manager, $pipelineOpts );
+		break;
+	default:
+		throw new \Exception( "Unsupported!" );
+}
+
+/**
+ * Transform the input tokens to output tokens
+ */
+if ( !$transformer->disabled ) {
+	// fwrite(STDERR, "$transformerName running ...\n");
+	$tokens = $transformer->processTokensSync( $manager->env, $tokens, [] );
+}
+
+/**
+ * Serialize output tokens to JSON
+ */
+$output = "";
+foreach ( $tokens as $t ) {
+	$output .= PHPUtils::jsonEncode( $t );
+	$output .= "\n";
+}
+
+/**
+ * Write serialized tokens to STDOUT
+ */
+// fwrite(STDERR, "RET: $output\n");
+print $output;
