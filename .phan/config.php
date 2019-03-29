a032ce102192a2aaaea30f099e5273325533bd25
Subramanya Sastry <ssastry@wikimedia.org>
2019-03-29 18:18:11 -0500

    Exclude a few more files from phan
    * Files that will be moved to mediawiki core repository.
    * Grammar generated by wikipeg.
    
    Change-Id: I3a6be6b3e5f3a4926293b1d239789e97b6435d3c

--- .phan/config.php
+++ .phan/config.php
@@ -42,4 +42,11 @@ foreach ( $phpFiles as $f ) {
 	}
 }
 
+// Exclude files that are not going to be in this directory
+$cfg['exclude_file_list'][] = "src/Config/MediaWikiPageConfig.php";
+$cfg['exclude_file_list'][] = "src/Config/MediaWikiPageContent.php";
+$cfg['exclude_file_list'][] = "src/Config/MediaWikiSiteConfig.php";
+// Exclude peg-generated output
+$cfg['exclude_file_list'][] = "src/Wt2Html/Grammar.php";
+
 return $cfg;
