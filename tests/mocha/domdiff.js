70562f4a672b533784d78a43fde466021d6d0217
Subramanya Sastry <ssastry@wikimedia.org>
2019-04-08 12:46:47 -0500

    Update dom diff mocha tests
    * Use a common test format for tests and use a test runner function.
    * Add more specs for all tests to test for diff markers in attributes
      which the PHP port needs to be tested against.
    * Makes it "simpler' to add new tests (as long as one is familiar with
      the CSS selector formats).
    * The -f <search-string> works with mocha even with this format
      since the test runner function generates a new test for each entry
      in the test array.
    * Added two new tests that exercise some more code paths.
    
    Change-Id: If06fa10ed593e5c07fa36c00ca18cf5d0e7619c7

--- tests/mocha/domdiff.js
+++ tests/mocha/domdiff.js
@@ -7,8 +7,8 @@ require('../../core-upgrade.js');
 require('chai').should();
 
 const { DOMDiff } = require('../../lib/html2wt/DOMDiff.js');
-const { DiffUtils } = require('../../lib/html2wt/DiffUtils.js');
 const { DOMUtils } = require('../../lib/utils/DOMUtils.js');
+const { DOMDataUtils } = require('../../lib/utils/DOMDataUtils.js');
 const { TestUtils } = require('../../tests/TestUtils.js');
 
 const parseAndDiff = function(a, b) {
@@ -26,67 +26,122 @@ const parseAndDiff = function(a, b) {
 	return { body: newDOM, env: dummyEnv };
 };
 
+// FIXME: The subtree-changed marker seems to be applied inconsistently.
+// Check if that marker is still needed / used by serialization code and
+// update code accordingly. If possible, simplify / reduce the different
+// markers being used.
+const tests = [
+	{
+		desc: 'changing text in a node',
+		orig: '<p>a</p><p>b</p>',
+		edit: '<p>A</p><p>b</p>',
+		specs: [
+			{ selector: 'body > p:first-child', markers: [ 'children-changed', 'subtree-changed'] },
+			{ selector: 'body > p:first-child > meta:first-child', diff: 'deleted' },
+		]
+	},
+	{
+		desc: 'deleting a node',
+		orig: '<p>a</p><p>b</p>',
+		edit: '<p>a</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p + meta', diff: 'deleted' },
+		]
+	},
+	{
+		desc: 'reordering nodes',
+		orig: '<p>a</p><p>b</p>',
+		edit: '<p>b</p><p>a</p>',
+		specs: [
+			{ selector: 'body > p:nth-child(1)', markers: [ 'children-changed', 'subtree-changed'] },
+			{ selector: 'body > p:nth-child(1) > meta', diff: 'deleted' },
+			{ selector: 'body > p:nth-child(2)', markers: [ 'children-changed', 'subtree-changed'] },
+			{ selector: 'body > p:nth-child(2) > meta', diff: 'deleted' },
+		]
+	},
+	{
+		desc: 'adding multiple nodes',
+		orig: '<p>a</p>',
+		edit: '<p>x</p><p>a</p><p>y</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p:nth-child(1)', markers: [ 'children-changed', 'subtree-changed'] },
+			{ selector: 'body > p:nth-child(1) > meta', diff: 'deleted' },
+			{ selector: 'body > p:nth-child(2)', markers: [ 'inserted'] },
+			{ selector: 'body > p:nth-child(3)', markers: [ 'inserted'] },
+		]
+	},
+	{
+		desc: 'adding and deleting nodes',
+		orig: '<p>a</p><p>b</p><p>c</p>',
+		edit: '<p>x</p><p>b</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p:nth-child(1)', markers: [ 'children-changed', 'subtree-changed'] },
+			{ selector: 'body > p:nth-child(1) > meta', diff: 'deleted' },
+			{ selector: 'body > meta:nth-child(3)', diff: [ 'deleted'] },
+		]
+	},
+	{
+		desc: 'changing an attribute',
+		orig: '<p class="a">a</p><p class="b">b</p>',
+		edit: '<p class="X">a</p><p class="b">b</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p:nth-child(1)', markers: [ 'modified-wrapper'] },
+		]
+	},
+	{
+		desc: 'changing data-mw for a template',
+		orig: '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"a"}},"i":0}}]}\'>a</p>',
+		edit: '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"foo"}},"i":0}}]}\'>foo</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p:nth-child(1)', markers: [ 'modified-wrapper'] },
+		]
+	},
+	// The additional subtrees added to the template's content should simply be ignored
+	{
+		desc: 'adding additional DOM trees to templated content',
+		orig: '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"a"}},"i":0}}]}\'>a</p>',
+		edit: '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"foo\\n\\nbar\\n\\nbaz"}},"i":0}}]}\'>foo</p>' +
+			'<p about="#mwt1">bar</p><p about="#mwt1">baz</p>',
+		specs: [
+			{ selector: 'body', markers: [ 'children-changed'] },
+			{ selector: 'body > p:nth-child(1)', markers: [ 'modified-wrapper'] },
+		]
+	},
+];
+
 describe('DOMDiff', function() {
-	it('should find a diff when changing text in a node', function() {
-		const { body } = parseAndDiff(
-			'<p>a</p>\n<p>b</p>',
-			'<p>A</p>\n<p>b</p>'
-		);
-		const meta = body.firstChild.firstChild;
-		(DOMUtils.isDiffMarker(meta, 'deleted')).should.be.true;
-	});
-	it('should find a diff deleting a node', function() {
-		const { body } = parseAndDiff(
-			'<p>a</p>\n<p>b</p>',
-			'<p>a</p>'
-		);
-		const meta = body.firstChild.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-	});
-	it('should find a diff when reordering nodes', function() {
-		const { body } = parseAndDiff(
-			'<p>a</p>\n<p>b</p>',
-			'<p>b</p>\n<p>a</p>'
-		);
-		let meta = body.firstChild.firstChild;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-		meta = body.firstChild.nextSibling.nextSibling.firstChild;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-	});
-	it('should find a diff when adding multiple nodes', function() {
-		const { body } = parseAndDiff(
-			'<p>a</p>\n<p>b</p>',
-			'<p>p</p>\n<p>q</p>\n<p>a</p>\n<p>b</p>\n<p>r</p>\n<p>s</p>'
-		);
-		let meta = body.firstChild.firstChild;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-		meta = body.firstChild.nextSibling.nextSibling.firstChild;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-		meta = body.firstChild.nextSibling.nextSibling.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'inserted').should.be.true;
-		meta = meta.nextSibling.nextSibling.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'inserted').should.be.true;
-		meta = meta.nextSibling.nextSibling.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'inserted').should.be.true;
-		meta = meta.nextSibling.nextSibling.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'inserted').should.be.true;
-	});
-	it('should find a diff when adding and deleting nodes', function() {
-		const { body } = parseAndDiff(
-			'<p>a</p>\n<p>b</p>\n<p>c</p>',
-			'<p>p</p>\n<p>b</p>'
-		);
-		let meta = body.firstChild.firstChild;
-		(DOMUtils.isDiffMarker(meta, 'deleted')).should.be.true;
-		meta = body.firstChild.nextSibling.nextSibling.nextSibling;
-		DOMUtils.isDiffMarker(meta, 'deleted').should.be.true;
-	});
-	it('should find a diff when changing an attribute', function() {
-		const { body, env } = parseAndDiff(
-			"<p class='a'>a</p>\n<p class='b'>b</p>",
-			"<p class='aa'>a</p>\n<p class='b'>b</p>"
-		);
-		const { diff } = DiffUtils.getDiffMark(body.firstChild, env);
-		diff.includes('modified-wrapper').should.be.true;
+	tests.forEach(function(t) {
+		it(`should find diff correctly when ${t.desc}`, function() {
+			const { body } = parseAndDiff(t.orig, t.edit);
+			t.specs.forEach(function(spec) {
+				let node;
+				if (spec.selector === 'body') { // Hmm .. why is this?
+					node = body;
+				} else {
+					const nodes = body.querySelectorAll(spec.selector);
+					nodes.length.should.equal(1);
+					node = nodes[0];
+				}
+				if (spec.diff) {
+					DOMUtils.isDiffMarker(node, spec.diff).should.be.true;
+				} else if (spec.markers) {
+					// NOTE: Not using DiffUtils.getDiffMark because that
+					// tests for page id and we may not be mocking that
+					// precisely here. And, we need to revisit whether that
+					// page id comparison is still needed / useful.
+					const data = DOMDataUtils.getNodeData(node);
+					const markers = data['parsoid-diff'].diff;
+					markers.length.should.equal(spec.markers.length);
+					markers.forEach(function(m, j) {
+						m.should.equal(spec.markers[j]);
+					});
+				}
+			});
+		});
 	});
 });
