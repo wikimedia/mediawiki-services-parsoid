6f0a762090dd1fddc3e9faf01a453ce9b5de1d6c
Subramanya Sastry <ssastry@wikimedia.org>
2019-08-08 17:03:20 -0500

    RT testing: Get test ids from a file; 'PHP:' prefix triggers Parsoid/PHP
    * In order to control when new test runs are triggered, we are adding a
      getTestRunId method that will be used in the testreduce client config
      file for parsoid-rt-testing service on scandium.
    
      The first line of the file will be used as the test run id.
    
    * If the testrun id has a "PHP:" prefix, we will hit the Parsoid/PHP
      endpoints. If not, we will hit the Parsoid/JS endpoints. This URL is
      cached by the wrapper. So, you will need to restart the testing service
      (parsoid-rt-client on scandium) if you want to switch between versions.
    
    * Added a single-line parsoid.rt-test.ids file to seed this. This entry
      is the id of the latest test run on scandium. Since this doesn't have
      a "PHP:" prefix, we will continue to run rt tests with Parsoid/JS.
    
      However, note that till we update the testreduce client config on
      scandium (via a puppet patch) to use the getTestRunId method we added
      here, it will continue to use the git HEAD id to trigger new test runs
      whenever the git repository updates.
    
      Once the testreduce client config is updated, to trigger a new test run,
      simply add a new entry to the top of the file. You can continue to use
      the git sha of the Parsoid repo or use any other symbolic meaningful
      string to identify the test run.
    
    Bug: T230166
    Change-Id: Ic94cfd542b8fa1984c673386f2cee178ff0849b7

--- tests/testreduce/rtTestWrapper.js
+++ tests/testreduce/rtTestWrapper.js
@@ -1,14 +1,23 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+
 /* This is a wrapper around the roundtrip testing script */
-var serviceWrapper = require('../serviceWrapper.js');
-var rtTest = require('../../bin/roundtrip-test.js');
+const serviceWrapper = require('../serviceWrapper.js');
+const rtTest = require('../../bin/roundtrip-test.js');
 
 // If we've started a Parsoid server, cache the URL so that subsequent calls
 // don't need to start their own.
-var parsoidURL = null;
+let parsoidURL = null;
 
-var _run = function(test) {
+// Read ids from a file and return the first line of the file
+function getTestRunId(opts) {
+	const testRunIdFile = opts.testRunIdFile || path.resolve(__dirname, 'parsoid.rt-test.ids');
+	return fs.readFileSync(testRunIdFile, 'utf-8').split('\n')[0];
+}
+
+function _run(test) {
 	return rtTest.runTests(test.title, {
 		prefix: test.prefix,
 		rtTestMode: true,
@@ -16,10 +25,20 @@ var _run = function(test) {
 	}, rtTest.xmlFormat).then(function(result) {
 		return result.output;
 	});
-};
+}
 
-var runRoundTripTest = function(config, test) {
-	parsoidURL = parsoidURL || config.parsoidURL;
+function runRoundTripTest(config, test) {
+	if (!parsoidURL) {
+		// If the test run id starts with PHP: we'll
+		// run tests with Parsoid/PHP. If not, we'll
+		// run tests with Parsoid/JS.
+		const testRunId = getTestRunId({});
+		if (/^PHP:/.test(testRunId)) {
+			parsoidURL = config.parsoidPHPURL;
+		} else {
+			parsoidURL = config.parsoidURL;
+		}
+	}
 	if (parsoidURL) {
 		return _run(test);
 	} else {
@@ -33,8 +52,9 @@ var runRoundTripTest = function(config, test) {
 			return _run(test);
 		});
 	}
-};
+}
 
 if (typeof module === 'object') {
 	module.exports.runRoundTripTest = runRoundTripTest;
+	module.exports.getTestRunId = getTestRunId;
 }
