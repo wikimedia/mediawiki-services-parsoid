c05f6ab0f2f838d85aaa4ac683a6759ded7b2e3e
Gergő Tisza <tgr.huwiki@gmail.com>
2019-10-02 18:40:35 +0200

    Remove phpspec
    Bug: T234040
    Change-Id: Ic761f71cbbbe8a38fff961507451840c100ce2fe

---
+++ tests/phpunit/Parsoid/Utils/DOMUtilsTest.php
@@ -0,0 +1,243 @@
+<?php
+namespace spec\Parsoid\Utils;
+
+use DOMElement;
+use Parsoid\Html2Wt\DOMDiff;
+use Parsoid\Tests\MockEnv;
+use Parsoid\Utils\ContentUtils;
+use Parsoid\Utils\DOMCompat;
+use Parsoid\Utils\DOMDataUtils;
+use Parsoid\Utils\DOMUtils;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Based on tests/mocha/domdiff.js
+ * @coversDefaultClass \Parsoid\Utils\DOMUtils
+ */
+class DOMUtilsTest extends TestCase {
+
+	/**
+	 * @covers ::isDiffMarker
+	 */
+	public function testIsDiffMarker_changingTextInNode() {
+		$orig = '<p>a</p><p>b</p>';
+		$edit = '<p>A</p><p>b</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:first-child' ),
+			[ 'children-changed', 'subtree-changed' ]
+		);
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p:first-child > meta:first-child' ),
+			'deleted'
+		) );
+	}
+
+	/**
+	 * @covers ::isDiffMarker
+	 */
+	public function testIsDiffMarker_deletingNode() {
+		$orig = '<p>a</p><p>b</p>';
+		$edit = '<p>a</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p + meta' ),
+			'deleted'
+		) );
+	}
+
+	/**
+	 * @covers ::isDiffMarker
+	 */
+	public function testIsDiffMarker_reorderingNodes() {
+		$orig = '<p>a</p><p>b</p>';
+		$edit = '<p>b</p><p>a</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'children-changed', 'subtree-changed' ]
+		);
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p:nth-child(1) > meta' ),
+			'deleted'
+		) );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(2)' ),
+			[ 'children-changed', 'subtree-changed' ]
+		);
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p:nth-child(2) > meta' ),
+			'deleted'
+		) );
+	}
+
+	/**
+	 * @covers ::isDiffMarker
+	 */
+	public function testIsDiffMarker_addingMultipleNodes() {
+		$orig = '<p>a</p>';
+		$edit = '<p>x</p><p>a</p><p>y</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'children-changed', 'subtree-changed' ]
+		);
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p:nth-child(1) > meta' ),
+			'deleted'
+		) );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(2)' ),
+			[ 'inserted' ]
+		);
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(3)' ),
+			[ 'inserted' ]
+		);
+	}
+
+	/**
+	 * @covers ::isDiffMarker
+	 */
+	public function testIsDiffMarker_addingAndDeletingNodes() {
+		$orig = '<p>a</p><p>b</p><p>c</p>';
+		$edit = '<p>x</p><p>b</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'children-changed', 'subtree-changed' ]
+		);
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > p:nth-child(1) > meta' ),
+			'deleted'
+		) );
+
+		$this->assertTrue( DOMUtils::isDiffMarker(
+			$this->selectNode( $body, 'body > meta:nth-child(3)' ),
+			'deleted'
+		) );
+	}
+
+	/**
+	 * @coversNothing
+	 */
+	public function testSomething_changingAttribute() {
+		$orig = '<p class="a">a</p><p class="b">b</p>';
+		$edit = '<p class="X">a</p><p class="b">b</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'modified-wrapper' ]
+		);
+	}
+
+	/**
+	 * @coversNothing
+	 */
+	public function testSomething_changingDataMwForTemplate() {
+		$orig = '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":' .
+			'{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"a"}},"i":0}}]}\'>a</p>';
+		$edit = '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":' .
+			'{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"foo"}},"i":0}}]}\'>foo</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'modified-wrapper' ]
+		);
+	}
+
+	/**
+	 * @coversNothing
+	 * The additional subtrees added to the template's content should simply be ignored
+	 */
+	public function testSomething_addingAdditionalDomTreesToTemplatedContent() {
+		$orig = '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":' .
+			'{"target":{"wt":"1x","href":"./Template:1x"},"params":' .
+			'{"1":{"wt":"a"}},"i":0}}]}\'>a</p>';
+		$edit = '<p about="#mwt1" typeof="mw:Transclusion" data-mw=\'{"parts":[{"template":' .
+			'{"target":{"wt":"1x","href":"./Template:1x"},"params":' .
+			'{"1":{"wt":"foo\n\nbar\n\nbaz"}},"i":0}}]}\'>foo</p>' .
+			'<p about="#mwt1">bar</p><p about="#mwt1">baz</p>';
+
+		$body = $this->parseAndDiff( $orig, $edit );
+
+		$this->checkMarkers( $body, [ 'children-changed' ] );
+
+		$this->checkMarkers(
+			$this->selectNode( $body, 'body > p:nth-child(1)' ),
+			[ 'modified-wrapper' ]
+		);
+	}
+
+	/**
+	 * @param string $html1
+	 * @param string $html2
+	 * @return DOMElement
+	 */
+	private function parseAndDiff( string $html1, string $html2 ): DOMElement {
+		$mockEnv = new MockEnv( [] );
+		$body1 = ContentUtils::ppToDOM( $mockEnv, $html1 );
+		$body2 = ContentUtils::ppToDOM( $mockEnv, $html2 );
+
+		$domDiff = new DOMDiff( $mockEnv );
+		$domDiff->diff( $body1, $body2 );
+
+		return $body2;
+	}
+
+	private function selectNode( DOMElement $body, string $selector ): DOMElement {
+		$nodes = DOMCompat::querySelectorAll( $body, $selector );
+		if ( count( $nodes ) !== 1 ) {
+			$this->fail( 'It should be exactly one node for the selector' );
+		}
+		return $nodes[0];
+	}
+
+	private function checkMarkers( DOMElement $node, array $markers ): void {
+		$data = DOMDataUtils::getNodeData( $node );
+		$diff = $data->parsoid_diff->diff;
+		if ( count( $markers ) !== count( $diff ) ) {
+			var_dump( $markers );
+			var_dump( $diff );
+			$this->fail( 'Count of diff should be equal count of markers' );
+		}
+		foreach ( $markers as $key => $value ) {
+			if ( $diff[$key] !== $value ) {
+				$this->fail( 'Diff is not equal to the marker' );
+			}
+		}
+	}
+
+}
--- tests/phpunit/Parsoid/Wt2Html/XMLSerializerTest.php
+++ tests/phpunit/Parsoid/Wt2Html/XMLSerializerTest.php
@@ -2,16 +2,245 @@
 
 namespace Test\Parsoid\Wt2Html;
 
+use DOMDocument;
 use Parsoid\Wt2Html\XMLSerializer;
 use Wikimedia\TestingAccessWrapper;
 
 /**
  * Test the entity encoding logic (which the JS version did not have as it called
- * on the entities npm package). phpspec does not allow testing private methods so
- * this is done in PHPUnit.
+ * on the entities npm package).
  * @coversDefaultClass \Parsoid\Wt2Html\XMLSerializer
  */
 class XMLSerializerTest extends \PHPUnit\Framework\TestCase {
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize() {
+		$html = '<html><head><title>hi</title><body>'
+			. '<div id="123">ok<div id="234">nope</div></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></body></html>';
+		$expectedHtml = "<!DOCTYPE html>\n<html><head><title>hi</title></head><body>"
+			. '<div id="123">ok<div id="234">nope</div></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></body></html>';
+		$expectedInnerHtml = "<head><title>hi</title></head><body>"
+			. '<div id="123">ok<div id="234">nope</div></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></body>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedHtml, $ret['html'] );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedHtml, $ret['html'] );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'innerXML' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedInnerHtml, $ret['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_captureOffsets() {
+		$html = '<html><head><title>hi</title><body>'
+			. '<div id="123">ok<div id="234">nope</div></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'offsets', $ret );
+		$this->assertArrayHasKey( '123', $ret['offsets'] );
+		$this->assertSame( [ 0, 62 ], $ret['offsets']['123']['html'] );
+		$this->assertArrayNotHasKey( '234', $ret['offsets'] );
+		$this->assertArrayHasKey( '345', $ret['offsets'] );
+		$this->assertSame( [ 62, 85 ], $ret['offsets']['345']['html'] );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true, 'innerXML' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'offsets', $ret );
+		$this->assertArrayHasKey( '123', $ret['offsets'] );
+		$this->assertSame( [ 0, 62 ], $ret['offsets']['123']['html'] );
+		$this->assertArrayNotHasKey( '234', $ret['offsets'] );
+		$this->assertArrayHasKey( '345', $ret['offsets'] );
+		$this->assertSame( [ 62, 85 ], $ret['offsets']['345']['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_captureOffsets_template() {
+		$html = '<html><head><title>hi</title><body>'
+			. '<p about="#mwt1" typeof="mw:Transclusion" id="mwAQ">a</p>'
+			. '<p about="#mwt1" id="justhappenstobehere">b</p>'
+			. '<p id="mwAg">c</p>'
+			. '</body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'offsets', $ret );
+		$this->assertArrayHasKey( 'mwAQ', $ret['offsets'] );
+		$this->assertArrayHasKey( 'mwAg', $ret['offsets'] );
+		$this->assertArrayNotHasKey( 'justhappenstobehere', $ret['offsets'] );
+		$this->assertSame( [ 0, 104 ], $ret['offsets']['mwAQ']['html'] );
+		$this->assertSame( [ 104, 122 ], $ret['offsets']['mwAg']['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_captureOffsets_expandedAttrs() {
+		$html = '<html><head><title>hi</title><body>'
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+			. '<div style="color:red" about="#mwt2" typeof="mw:ExpandedAttrs" id="mwAQ" data-mw=\'{"attribs":[[{"txt":"style"},{"html":"&lt;span about=\"#mwt1\" typeof=\"mw:Transclusion\" data-parsoid=\"{&amp;quot;pi&amp;quot;:[[{&amp;quot;k&amp;quot;:&amp;quot;1&amp;quot;}]],&amp;quot;dsr&amp;quot;:[12,30,null,null]}\" data-mw=\"{&amp;quot;parts&amp;quot;:[{&amp;quot;template&amp;quot;:{&amp;quot;target&amp;quot;:{&amp;quot;wt&amp;quot;:&amp;quot;echo&amp;quot;,&amp;quot;href&amp;quot;:&amp;quot;./Template:Echo&amp;quot;},&amp;quot;params&amp;quot;:{&amp;quot;1&amp;quot;:{&amp;quot;wt&amp;quot;:&amp;quot;color:red&amp;quot;}},&amp;quot;i&amp;quot;:0}}]}\">color:red&lt;/span>"}]]}\'>boo</div>'
+			. '<p id="mwAg">next!</p>'
+			. '</body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'offsets', $ret );
+		$this->assertArrayHasKey( 'mwAQ', $ret['offsets'] );
+		$this->assertArrayHasKey( 'mwAg', $ret['offsets'] );
+		$this->assertSame( [ 0, 684 ], $ret['offsets']['mwAQ']['html'] );
+		$this->assertSame( [ 684, 706 ], $ret['offsets']['mwAg']['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_captureOffsets_extensionContentNestedInTemplates() {
+		// Mostly scooped from, echo "{{Demografia/Apricale}}" | node tests/parse --prefix itwiki --dp
+		$html = '<html><head><title>hi</title><body>'
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+			. '<p about="#mwt1" typeof="mw:Transclusion" id="mwAQ" data-mw=\'{"parts":[{"template":{"target":{"wt":"Demografia/Apricale","href":"./Template:Demografia/Apricale"},"params":{},"i":0}}]}\'><i>Abitanti censiti</i></p>'
+			// phpcs:ignore Generic.Files.LineLength.TooLong
+			. '<map name="timeline" id="timeline" typeof="mw:Extension/timeline" data-mw=\'{"name":"timeline","attrs":{},"body":{"extsrc":"yadayadayada"}}\' about="#mwt1"></map>'
+			. '</body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'captureOffsets' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'offsets', $ret );
+		$this->assertArrayHasKey( 'mwAQ', $ret['offsets'] );
+		$this->assertArrayNotHasKey( 'timeline', $ret['offsets'] );
+		$this->assertSame( [ 0, 372 ], $ret['offsets']['mwAQ']['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_uppercaseTagnames() {
+		$html = '<HTML><HeAD><Title>hi</title><body>'
+			. '<DIV ID="123">ok<div id="234">nope</DIV></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></Body></HTML>';
+		$expectedHtml = "<!DOCTYPE html>\n<html><head><title>hi</title></head><body>"
+			. '<div id="123">ok<div id="234">nope</div></div>'
+			. "\n\n" . '<!--comment--><div id="345">end</div></body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedHtml, $ret['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_quotes() {
+		$html = '<html><body>'
+			. '<div attr="&quot;&apos;&quot;"></div>'
+			. '<div attr=\'&quot;&apos;&quot;\'></div>'
+			. '<div attr="&apos;&quot;&apos;"></div>'
+			. '<div attr=\'&apos;&quot;&apos;\'></div>'
+			. '</body></html>';
+		$expectedNonSmart = "<!DOCTYPE html>\n<html><body>"
+			. '<div attr="&quot;\'&quot;"></div>'
+			. '<div attr="&quot;\'&quot;"></div>'
+			. '<div attr="\'&quot;\'"></div>'
+			. '<div attr="\'&quot;\'"></div>'
+			. '</body></html>';
+		$expectedSmart = "<!DOCTYPE html>\n<html><body>"
+			. '<div attr=\'"&apos;"\'></div>'
+			. '<div attr=\'"&apos;"\'></div>'
+			. '<div attr="\'&quot;\'"></div>'
+			. '<div attr="\'&quot;\'"></div>'
+			. '</body></html>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'smartQuote' => false ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedNonSmart, $ret['html'] );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'smartQuote' => true ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedSmart, $ret['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_emptyElements() {
+		// Must have a single root node, otherwise libxml messes up parsing in NOIMPLIED mode.
+		$html = '<div><span /><hr/></div>';
+		$expectedHtml = '<div><span></span><hr/></div>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html, LIBXML_HTML_NOIMPLIED );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'smartQuote' => false ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedHtml, $ret['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_rawContent() {
+		$html = '<script>x</script>';
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html, LIBXML_HTML_NOIMPLIED );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'smartQuote' => false ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $html, $ret['html'] );
+	}
+
+	/**
+	 * @covers ::serialize
+	 */
+	public function testSerialize_newlineStrippingElements() {
+		// Must have a single root node, otherwise libxml messes up parsing in NOIMPLIED mode.
+		// This test looks confusing because DOMDocument::loadHTML doesn't fully follow the spec;
+		// it should strip the first newline within a pre block.
+		$html = "<div><pre>\n</pre><div>\n</div></div>";
+		$expectedHtml = "<div><pre>\n\n</pre><div>\n</div></div>";
+		$doc = new DOMDocument();
+		$doc->loadHTML( $html, LIBXML_HTML_NOIMPLIED );
+
+		$ret = XMLSerializer::serialize( $doc, [ 'smartQuote' => false ] );
+		$this->assertInternalType( 'array', $ret );
+		$this->assertArrayHasKey( 'html', $ret );
+		$this->assertSame( $expectedHtml, $ret['html'] );
+	}
 
 	/**
 	 * @covers ::encodeHtmlEntities
