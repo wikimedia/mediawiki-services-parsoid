wikimedia/mediawiki-services-parsoid

commit 18a4fc7068ca383cbf6c8abbab20d987eb7fbf91

ExtensionHandler: Process extension metadata on DOM instead of HTML
* No need to generate a DOM node -> HTML string -> DOM again.
* As it turns out, the extracted method will be useful for processing
  extension metadata produced by native extension handlers as well.

Change-Id: I3bb6f89f6635e196a97b97f87a210b17eb811e80

src/Wt2Html/TT/ExtensionHandler.php
@@ -54,41 +54,37 @@ class ExtensionHandler extends TokenHandler {
 	}
 
 	/**
-	 * @param Token $token
-	 * @param array $ret
-	 * @return string
+	 * Process extension metadata and record it somewhere (Env state or the DOM)
+	 *
+	 * @param DOMDocument $doc
+	 * @param array $modules
+	 * @param array $modulestyles
+	 * @param ?array $categories
 	 */
-	private function mangleParserResponse( Token $token, array $ret ): string {
-		$env = $this->env;
-		$html = $ret['html'];
-
-		// Strip a paragraph wrapper, if any
-		$html = preg_replace( '#(^<p>)|(\n</p>$)#D', '', $html );
-
+	private function processExtMetadata(
+		DOMDocument $doc, array $modules, array $modulestyles, ?array $categories
+	): void {
 		// Add the modules to the page data
-		$env->addOutputProperty( 'modules', $ret['modules'] );
-		$env->addOutputProperty( 'modulestyles', $ret['modulestyles'] );
+		$this->env->addOutputProperty( 'modules', $modules );
+		$this->env->addOutputProperty( 'modulestyles', $modulestyles );
 
 		/*  - categories: (array) [ Category name => sortkey ] */
 		// Add the categories which were added by extensions directly into the
 		// page and not as in-text links
-		if ( $ret['categories'] ) {
-			foreach ( $ret['categories'] as $name => $sortkey ) {
-				$dummyDoc = $env->createDocument( '' );
-				$link = $dummyDoc->createElement( "link" );
-				$link->setAttribute( "rel", "mw:PageProp/Category" );
-				$href = $env->getSiteConfig()->relativeLinkPrefix() .
-					"Category:" . PHPUtils::encodeURIComponent( (string)$name );
-				if ( $sortkey ) {
-					$href .= "#" . PHPUtils::encodeURIComponent( $sortkey );
-				}
-				$link->setAttribute( "href", $href );
-
-				$html .= "\n" . DOMCompat::getOuterHTML( $link );
+		foreach ( ( $categories ?? [] ) as $name => $sortkey ) {
+			$link = $doc->createElement( "link" );
+			$link->setAttribute( "rel", "mw:PageProp/Category" );
+			$href = $this->env->getSiteConfig()->relativeLinkPrefix() .
+				"Category:" . PHPUtils::encodeURIComponent( (string)$name );
+			if ( $sortkey ) {
+				$href .= "#" . PHPUtils::encodeURIComponent( $sortkey );
 			}
-		}
+			$link->setAttribute( "href", $href );
 
-		return $html;
+			$body = DOMCompat::getBody( $doc );
+			$body->appendChild( $doc->createTextNode( '\n' ) );
+			$body->appendChild( $link );
+		}
 	}
 
 	/**
@@ -146,8 +142,9 @@ class ExtensionHandler extends TokenHandler {
 		} else {
 			$pageConfig = $env->getPageConfig();
 			$ret = $env->getDataAccess()->parseWikitext( $pageConfig, $token->getAttribute( 'source' ) );
-			$html = $this->mangleParserResponse( $token, $ret );
-			$doc = $env->createDocument( $html );
+			// Strip a paragraph wrapper, if any, before parsing HTML to DOM
+			$doc = $env->createDocument( preg_replace( '#(^<p>)|(\n</p>$)#D', '', $ret['html'] ) );
+			$this->processExtMetadata( $doc, $ret['modules'], $ret['modulestyles'], $ret['categories'] );
 			$toks = $this->onDocument( $nativeExt, $token, $doc );
 		}
 		return( [ 'tokens' => $toks ] );

commit bdc5681300e7b051755b9086b61e1fc14551e0b2

"\n", not '\n': Fix regression introduced by 18a4fc70
Change-Id: I99be5968ded3b7fd1c7f18eb55727ab9b82d0f8a

src/Wt2Html/TT/ExtensionHandler.php
@@ -82,7 +82,7 @@ class ExtensionHandler extends TokenHandler {
 			$link->setAttribute( "href", $href );
 
 			$body = DOMCompat::getBody( $doc );
-			$body->appendChild( $doc->createTextNode( '\n' ) );
+			$body->appendChild( $doc->createTextNode( "\n" ) );
 			$body->appendChild( $link );
 		}
 	}