wikimedia/mediawiki-services-parsoid

commit d710c327fc1049a6064179c83f2e2d97674b85a1

SerializerState: Bug fix + code refactoring in current line handling
currLine->text maintains the running emitted text for the currently
line and wasn't being updated whenever separators were emitted.

This matters for wikitext escaping code where a " " separator emitted
after a "|" for <td> and its content "-" (or "+") prevents the need
for a <nowiki>. It looks like this doesn't trigger for any of our
tests currently but a downstream patch that deals with trimmed
whitespace for table cells might be impacted.

Change-Id: I69ab044223bcf420c2d3a59fe1649f77e025950c

src/Html2Wt/SerializerState.php
@@ -441,12 +441,20 @@ class SerializerState {
 
 	/**
 	 * Accumulates chunks on the current line.
-	 * @param ConstrainedText $text
+	 * @param ConstrainedText $chunk
 	 * @param DOMNode $node
+	 * @param string $logPrefix
 	 */
-	private function pushToCurrLine( ConstrainedText $text, DOMNode $node ) {
+	private function pushToCurrLine( ConstrainedText $chunk, DOMNode $node, string $logPrefix ) {
+		// Emitting text that has not been escaped
+		$this->currLine->text .= $chunk->text;
+
 		// TODO $node is probably not needed since ConstrainedText already includes it
-		$this->currLine->chunks[] = $text;
+		$this->currLine->chunks[] = $chunk;
+
+		$this->serializer->trace( '--->', $logPrefix, function () use ( $chunk ) {
+			return PHPUtils::jsonEncode( $chunk->text );
+		} );
 	}
 
 	/**
@@ -463,17 +471,13 @@ class SerializerState {
 			$sep->text = preg_replace( '/\n/', ' ', $sep->text );
 		}
 
-		$this->pushToCurrLine( $sep, $node );
+		$this->pushToCurrLine( $sep, $node, $debugPrefix );
 
 		// Reset separator state
 		$this->resetSep();
 		$this->updateSep( $node );
 
 		$this->sepIntroducedSOL( $sep->text );
-
-		$this->serializer->trace( '--->', $debugPrefix, function () use ( $sep ) {
-			return PHPUtils::jsonEncode( $sep->text );
-		} );
 	}
 
 	/**
@@ -641,14 +645,8 @@ class SerializerState {
 			}
 		}
 
-		// Emitting text that has not been escaped
-		$this->currLine->text .= $res->text;
-
 		// Output res
-		$this->serializer->trace( '--->', $this->logPrefix, function () use ( $res ) {
-			return PHPUtils::jsonEncode( $res->text );
-		} );
-		$this->pushToCurrLine( $res, $node );
+		$this->pushToCurrLine( $res, $node, $this->logPrefix );
 
 		// Update sol flag. Test for newlines followed by optional includeonly or comments
 		if ( !$res->match( $this->solRegexp() ) ) {

commit 39af07a6855b5f6d115a59303346b48ae2dce6ff

Followup on d710c327: Ensure \n in separators reset current line
* d710c327 caused a regression in rt-testing which was reducible
  to the newly added test case.

* This patch carries forward the DRY-ing and refactoring further
  which fixes this long-standing inconsistency in resetting state.

Change-Id: If3dce5042a44f517e0bea303646525c9317ff1c8

src/Html2Wt/DOMHandlers/DOMHandler.php
@@ -444,7 +444,7 @@ class DOMHandler {
 		// FIXME: Should this also check for tabs and plain space
 		// chars interspersed with newlines?
 		if ( preg_match( '/^\n+$/D', $dp->src ?? '' ) ) {
-			$state->appendSep( $dp->src );
+			$state->appendSep( $dp->src, $node );
 		} else {
 			$state->serializer->emitWikitext( $dp->src, $node );
 		}

src/Html2Wt/DOMHandlers/PreHandler.php
@@ -63,7 +63,7 @@ class PreHandler extends DOMHandler {
 		$state->emitChunk( $content, $node );
 
 		// Preserve separator source
-		$state->appendSep( $trailingNL[0] ?? '' );
+		$state->appendSep( $trailingNL[0] ?? '', $node );
 		return $node->nextSibling;
 	}
 

src/Html2Wt/DOMHandlers/TDHandler.php
@@ -68,7 +68,7 @@ class TDHandler extends DOMHandler {
 		if ( $nextUsesRowSyntax && !preg_match( '/\s$/D', $state->currLine->text ) ) {
 			$trailingSpace = $this->getTrailingSpace( $state, $node, '' );
 			if ( $trailingSpace ) {
-				$state->appendSep( $trailingSpace );
+				$state->appendSep( $trailingSpace, $node );
 			}
 		}
 

src/Html2Wt/DOMHandlers/THHandler.php
@@ -69,7 +69,7 @@ class THHandler extends DOMHandler {
 		if ( $nextUsesRowSyntax && !preg_match( '/\s$/D', $state->currLine->text ) ) {
 			$trailingSpace = $this->getTrailingSpace( $state, $node, '' );
 			if ( $trailingSpace ) {
-				$state->appendSep( $trailingSpace );
+				$state->appendSep( $trailingSpace, $node );
 			}
 		}
 		return $node->nextSibling;

src/Html2Wt/SerializerState.php
@@ -339,10 +339,11 @@ class SerializerState {
 	/**
 	 * Appends the seperator source and updates the SOL state if necessary.
 	 * @param string $src
+	 * @param DOMNode $node
 	 */
-	public function appendSep( string $src ): void {
+	public function appendSep( string $src, DOMNode $node ): void {
 		$this->sep->src = ( $this->sep->src ?: '' ) . $src;
-		$this->sepIntroducedSOL( $src );
+		$this->sepIntroducedSOL( $src, $node );
 	}
 
 	/**
@@ -417,11 +418,13 @@ class SerializerState {
 	/**
 	 * Separators put us in SOL state.
 	 * @param string $sep
+	 * @param DOMNode $node
 	 */
-	private function sepIntroducedSOL( string $sep ): void {
+	private function sepIntroducedSOL( string $sep, DOMNode $node ): void {
 		// Don't get tripped by newlines in comments!  Be wary of nowikis added
 		// by makeSepIndentPreSafe on the last line.
-		if ( substr( preg_replace( Utils::COMMENT_REGEXP, '', $sep ), -1 ) === "\n" ) {
+		$nonCommentSep = preg_replace( Utils::COMMENT_REGEXP, '', $sep );
+		if ( substr( $nonCommentSep, -1 ) === "\n" ) {
 			// Since we are stashing away newlines for emitting
 			// before the next element, we are in SOL state wrt
 			// the content of that next element.
@@ -436,6 +439,12 @@ class SerializerState {
 			// HTML to avoid these problems. To be figured out later when
 			// it is a real issue.
 			$this->onSOL = true;
+		}
+
+		if ( preg_match( '/\n/', $nonCommentSep ) ) {
+			// process escapes in our full line
+			$this->flushLine();
+			$this->resetCurrLine( $node );
 		}
 	}
 
@@ -470,12 +479,11 @@ class SerializerState {
 		}
 
 		$this->pushToCurrLine( $sep, $debugPrefix );
+		$this->sepIntroducedSOL( $sep->text, $node );
 
 		// Reset separator state
 		$this->resetSep();
 		$this->updateSep( $node );
-
-		$this->sepIntroducedSOL( $sep->text );
 	}
 
 	/**
@@ -568,14 +576,13 @@ class SerializerState {
 		// Emit separator first
 		if ( $res->noSep ) {
 			/* skip separators for internal tokens from SelSer */
+			if ( $this->onSOL ) {
+				// process escapes in our full line
+				$this->flushLine();
+				$this->resetCurrLine( $node );
+			}
 		} else {
 			$this->emitSepForNode( $node );
-		}
-
-		if ( $this->onSOL ) {
-			// process escapes in our full line
-			$this->flushLine();
-			$this->resetCurrLine( $node );
 		}
 
 		// Escape 'res' if necessary

src/Html2Wt/WikitextSerializer.php
@@ -1085,7 +1085,7 @@ class WikitextSerializer {
 		if ( !$state->inIndentPre ) {
 			// Strip leading newlines and other whitespace
 			if ( preg_match( self::$separatorREs['sepPrefixWithNlsRE'], $res, $match ) ) {
-				$state->appendSep( $match[0] );
+				$state->appendSep( $match[0], $node );
 				$res = substr( $res, strlen( $match[0] ) );
 			}
 		}
@@ -1106,7 +1106,7 @@ class WikitextSerializer {
 		// Move trailing newlines into the next separator
 		if ( $newSepMatch ) {
 			if ( !$state->sep->src ) {
-				$state->appendSep( $newSepMatch[0] );
+				$state->appendSep( $newSepMatch[0], $node );
 			} else {
 				/* SSS FIXME: what are we doing with the stripped NLs?? */
 			}
@@ -1344,7 +1344,7 @@ class WikitextSerializer {
 					// but that does not seem useful
 					&& preg_match( self::$separatorREs['pureSepRE'], $text )
 				) {
-					$state->appendSep( $text );
+					$state->appendSep( $text, $node );
 					return $node->nextSibling;
 				}
 				if ( $state->selserMode ) {
@@ -1364,7 +1364,7 @@ class WikitextSerializer {
 				break;
 			case XML_COMMENT_NODE:
 				// Merge this into separators
-				$state->appendSep( WTSUtils::commentWT( $node->nodeValue ) );
+				$state->appendSep( WTSUtils::commentWT( $node->nodeValue ), $node );
 				return $node->nextSibling;
 			default:
 				// PORT-FIXME the JS code used node.outerHTML here; probably a bug?

tests/phpunit/Parsoid/Html2Wt/SerializerStateTest.php
@@ -85,8 +85,9 @@ class SerializerStateTest extends TestCase {
 	 */
 	public function testAppendSep() {
 		$state = $this->getState();
-		$state->appendSep( 'foo' );
-		$state->appendSep( 'bar' );
+		$node = $this->getNode(); // some dummy node to satisfy appendSep
+		$state->appendSep( 'foo', $node );
+		$state->appendSep( 'bar', $node );
 		$this->assertSame( 'foobar', $state->sep->src );
 	}
 