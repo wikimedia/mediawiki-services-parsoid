486f49eef0123a2dee70618d24825d1748c2abf7
Petr Pchelko <ppchelko@wikimedia.org>
2020-11-05 14:26:41 -0700

    Don't access MWServices in onRegistration hook
    Accessing MWServices here forces onMediaWikiServices hook
    to run twice breaking other extensions.
    
    Bug: T267146
    Change-Id: I93d52e7f5afb949bfd1c1fd064477f01c9df8d2e

--- extension/src/RegistrationHookHandler.php
+++ extension/src/RegistrationHookHandler.php
@@ -2,16 +2,15 @@
 
 namespace MWParsoid;
 
-use MediaWiki\MediaWikiServices;
-
 class RegistrationHookHandler {
 	public static function onRegistration() {
 		global $wgRestAPIAdditionalRouteFiles;
 		// phpcs:ignore MediaWiki.NamingConventions.ValidGlobalName.allowedPrefix
 		global $IP;
-
-		$config = MediaWikiServices::getInstance()->getMainConfig();
-		if ( $config->get( 'ParsoidEnableREST' ) ) {
+		// Use globals instead of Config.
+		// Accessing it so early blows up unrelated extensions (T267146)
+		global $wgParsoidEnableREST;
+		if ( $wgParsoidEnableREST ) {
 			$wgRestAPIAdditionalRouteFiles[] = wfRelativePath(
 				__DIR__ . '/../restRoutes.json', $IP
 			);
