e5862895806d915f7c56f75b9a95a7180b8b46ca
C. Scott Ananian <cananian@wikimedia.org>
2020-11-25 00:46:19 -0500

    Replace references to FakeConverter
    In 61e0908fa2915d73243686c4013f0af244fbc7f2 the new method
    LanguageConverter::hasVariants() was added to eliminate the need to test
    whether the LanguageConverter instanceof FakeConverter.
    
    Change-Id: Ic9777a68ac649f86cd2ee5c4f134385e700a3504

--- extension/src/Config/SiteConfig.php
+++ extension/src/Config/SiteConfig.php
@@ -23,7 +23,6 @@ namespace MWParsoid\Config;
 
 use Config;
 use ExtensionRegistry;
-use FakeConverter;
 use Language;
 use LanguageConverter;
 use Liuggio\StatsdClient\Factory\StatsdDataFactoryInterface;
@@ -396,7 +395,7 @@ class SiteConfig extends ISiteConfig {
 		try {
 			return !$this->config->get( 'DisableLangConversion' ) &&
 				in_array( $lang, LanguageConverter::$languagesWithVariants, true ) &&
-				!Language::factory( $lang )->getConverter() instanceof FakeConverter;
+				!Language::factory( $lang )->getConverter()->hasVariants();
 		} catch ( \MWException $ex ) {
 			// Probably a syntactically invalid language code
 			return false;
@@ -435,9 +434,7 @@ class SiteConfig extends ISiteConfig {
 
 			foreach ( $langNames as $langCode ) {
 				$lang = Language::factory( $langCode );
-				if ( $lang->getConverter() instanceof FakeConverter ) {
-					// Only languages which do not return instances of
-					// FakeConverter implement language conversion.
+				if ( $lang->getConverter()->hasVariants() ) {
 					continue;
 				}
 
