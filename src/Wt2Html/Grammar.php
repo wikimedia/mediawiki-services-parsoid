736b06070de5d23642292136edae91fc7aeb2b87
Arlo Breault <abreault@wikimedia.org>
2021-03-11 15:51:00 -0500

    No need to protect opening angle bracket in extension tag
    Tests for this were added in 03ed2f3
    
    Change-Id: Ib11fc92c6198766e60fec4c1cc768e48659ccca6

--- src/Wt2Html/Grammar.php
+++ src/Wt2Html/Grammar.php
@@ -229,7 +229,7 @@ class Grammar extends \WikiPEG\PEGParserBase {
   					//
   					// Expand `extSrc` as long as there is a <tagName> found in the
   					// extension source body.
-  					$startTagRE = '~<' . preg_quote( $tagName, '~' ) . '[^/<>]*>~i';
+  					$startTagRE = '~<' . preg_quote( $tagName, '~' ) . '[^/>]*>~i';
   					$s = substr( $extSrc, $dp->tsr->end - $dp->tsr->start );
   					while ( strlen( $s ) ) {
   						if ( !preg_match( $startTagRE, $s ) ) {
