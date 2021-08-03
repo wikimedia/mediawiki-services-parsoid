17a1312140d781ad55e130f0f5a2dc9821d0313d
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2021-08-03 14:07:03 +0200

    Add a comment in Grammar.php to indicate how to generate it
    Use the option added in I074794b2e9ae56 to add a comment when the file
    is generated. The Grammar.php file is also regenerated in this patch
    with the new comment.
    
    Change-Id: Ie1ee89ca90be3f16cf7538b3236030c743dfd5bc

--- bin/inspectTokenizer.js
+++ bin/inspectTokenizer.js
@@ -53,6 +53,12 @@ yargs.options({
 		'boolean': true,
 		'default': false,
 	},
+
+	'header-comment-file': {
+		description: 'File containing the comment to add in the header of the generated file',
+		'boolean': false,
+		'default': __dirname + '/grammarheader-script.txt'
+	}
 });
 
 yargs.help();
@@ -68,7 +74,8 @@ function getOutputStream(opts) {
 function generateSource(opts) {
 	var file = getOutputStream(opts);
 	var tokenizer = new PegTokenizer();
-	var pegOpts = { trace: opts.trace };
+	var headerComment = fs.readFileSync(opts['header-comment-file']).toString();
+	var pegOpts = { trace: opts.trace, headerComment: headerComment };
 	var source = tokenizer.compileTokenizer(tokenizer.parseTokenizer(pegOpts), pegOpts);
 	file.write(source, 'utf8');
 }
