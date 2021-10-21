wikimedia/mediawiki-services-parsoid

commit e067007c459a412a9e508702be78582cd1d47a52

One document to rule them all
The description in T179082 suggests that by using one document for the
entire parse, we'd probably see some performance gains from not having
to import nodes when we get to the top level pipeline and we'd avoid the
validation errors from 19a9c3c.

However, the spec seems to suggest creating a new document when parsing
an HTML fragment,
https://html.spec.whatwg.org/#html-fragment-parsing-algorithm

And, indeed, domino implements it that way,
https://github.com/fgnass/domino/blob/12a5f67136a0ac10e3fa1649b8787ba3b309e9a7/lib/htmlelts.js#L84-L96

So, the request in T217705 may be a little misguided.

What then is this patch good for?  In T221790 the ask is that
sub-pipelines produce DocumentFragment which make for cleaner interfaces
and less confusion when migrating children.

The general outline here is that a document is created when the
environment is constructed that gives us the 1-1 correspondence.
Sub-pipelines do create their own documents for the purpose of tree
building, as in the fragment parsing algorithm, but are then immediately
imported to DocumentFragments to be used for the rest of the
post-processing passes.

Bug: T221790
Bug: T179082
Bug: T217705
Change-Id: Idf856d4e071d742ca38486c8ab402e39b3c8949f

src/Utils/ContentUtils.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace Wikimedia\Parsoid\Utils;
 
 use DOMDocument;
+use DOMDocumentFragment;
 use DOMElement;
 use DOMNode;
 use Wikimedia\Assert\Assert;
@@ -47,20 +48,22 @@ class ContentUtils {
 	 * @param Env $env
 	 * @param string $html
 	 * @param array $options
-	 * @return DOMElement
+	 * @return DOMElement|DOMDocumentFragment
 	 */
 	public static function ppToDOM(
 		Env $env, string $html, array $options = []
-	): DOMElement {
+	): DOMNode {
 		$options += [
 			'node' => null,
 			'reinsertFosterableContent' => null,
+			'toFragment' => false,
 		];
 		$node = $options['node'];
-		if ( $node === null ) {
+		if ( $options['toFragment'] ) {
+			$node = DOMUtils::parseHTMLToFragment( $env->topLevelDoc, $html );
+		} elseif ( $node === null ) {
 			$node = DOMCompat::getBody( $env->createDocument( $html ) );
 		} else {
-			DOMUtils::assertElt( $node );
 			DOMCompat::setInnerHTML( $node, $html );
 		}
 
@@ -245,10 +248,8 @@ class ContentUtils {
 			if ( DOMUtils::matchTypeOf( $node, '#^mw:DOMFragment(/|$)#D' ) ) {
 				$dp = DOMDataUtils::getDataParsoid( $node );
 				if ( $dp->html ?? null ) {
-					$nodes = $env->getDOMFragment( $dp->html );
-					foreach ( $nodes as $n ) {
-						DOMPostOrder::traverse( $n, $convertNode );
-					}
+					$domFragment = $env->getDOMFragment( $dp->html );
+					DOMPostOrder::traverse( $domFragment, $convertNode );
 				}
 			}
 		};

src/Config/Env.php
@@ -4,7 +4,12 @@ declare( strict_types = 1 );
 namespace Wikimedia\Parsoid\Config;
 
 use DOMDocument;
-use DOMNode;
+use DOMDocumentFragment;
+use RemexHtml\DOM\DOMBuilder;
+use RemexHtml\Tokenizer\PlainAttributes;
+use RemexHtml\Tokenizer\Tokenizer;
+use RemexHtml\TreeBuilder\Dispatcher;
+use RemexHtml\TreeBuilder\TreeBuilder;
 use Wikimedia\Parsoid\Core\ContentModelHandler;
 use Wikimedia\Parsoid\Core\ResourceLimitExceededException;
 use Wikimedia\Parsoid\Logger\ParsoidLogger;
@@ -98,7 +103,7 @@ class Env {
 
 	/**
 	 * Maps fragment id to the fragment forest (array of DOMNodes).
-	 * @var array<string,DOMNode[]>
+	 * @var array<string,DOMDocumentFragment>
 	 */
 	private $fragmentMap = [];
 
@@ -219,6 +224,15 @@ class Env {
 	 */
 	public $extensionCache = [];
 
+	/** @var DOMDocument */
+	public $topLevelDoc;
+
+	/** @var Dispatcher */
+	private $dispatcher;
+
+	/** @var DataBag */
+	private $bag;
+
 	/**
 	 * @param SiteConfig $siteConfig
 	 * @param PageConfig $pageConfig
@@ -290,6 +304,8 @@ class Env {
 			'dumpFlags' => $this->dumpFlags,
 			'traceFlags' => $this->traceFlags
 		] );
+
+		$this->initDocumentDispatcher();
 	}
 
 	/**
@@ -670,6 +686,63 @@ class Env {
 	}
 
 	/**
+	 * When an environment is constructed, we initialize a document (and
+	 * dispatcher to it) to be used throughout the parse.
+	 */
+	private function initDocumentDispatcher() {
+		$this->bag = new DataBag();
+		list(
+			$this->topLevelDoc,
+			$this->dispatcher
+		) = $this->createDocumentDispatcher();
+	}
+
+	/**
+	 * @param bool $atTopLevel
+	 * @return array
+	 */
+	public function fetchDocumentDispatcher( bool $atTopLevel ): array {
+		if ( $atTopLevel ) {
+			return [ $this->topLevelDoc, $this->dispatcher ];
+		} else {
+			return $this->createDocumentDispatcher();
+		}
+	}
+
+	/**
+	 * Returns a document and dispatcher to it.
+	 *
+	 * The dispatcher tracks the insertion mode and relays token events to
+	 * specific handlers.
+	 *
+	 * Since we do our own tokenizing, the dispatcher is needed as our
+	 * entrypoint to tree building.
+	 *
+	 * @return array
+	 */
+	private function createDocumentDispatcher(): array {
+		// The options to DOMBuilder should be kept in sync with its other
+		// uses, so grep for it before changing
+		$domBuilder = new DOMBuilder( [ 'suppressHtmlNamespace' => true ] );
+		$dispatcher = new Dispatcher( new TreeBuilder( $domBuilder ) );
+
+		// PORT-FIXME: Necessary to setEnableCdataCallback
+		$tokenizer = new Tokenizer( $dispatcher, '', [ 'ignoreErrors' => true ] );
+
+		$dispatcher->startDocument( $tokenizer, null, null );
+		$dispatcher->doctype( 'html', '', '', false, 0, 0 );
+		$dispatcher->startTag( 'body', new PlainAttributes(), false, 0, 0 );
+
+		$doc = $domBuilder->getFragment();
+		'@phan-var DOMDocument $doc'; // @var DOMDocument $doc
+
+		// Special case where we can't call `$env->createDocument()`
+		$this->referenceDataObject( $doc, $this->bag );
+
+		return [ $doc, $dispatcher ];
+	}
+
+	/**
 	 * @param string $html
 	 * @param bool $validateXMLNames
 	 * @return DOMDocument
@@ -721,7 +794,7 @@ class Env {
 	}
 
 	/**
-	 * @return array<string,DOMNode[]>
+	 * @return array<string,DOMDocumentFragment>
 	 */
 	public function getDOMFragmentMap(): array {
 		return $this->fragmentMap;
@@ -729,18 +802,20 @@ class Env {
 
 	/**
 	 * @param string $id Fragment id
-	 * @return DOMNode[]
+	 * @return DOMDocumentFragment
 	 */
-	public function getDOMFragment( string $id ): array {
+	public function getDOMFragment( string $id ): DOMDocumentFragment {
 		return $this->fragmentMap[$id];
 	}
 
 	/**
 	 * @param string $id Fragment id
-	 * @param DOMNode[] $forest DOM forest (contiguous array of DOM trees)
+	 * @param DOMDocumentFragment $forest DOM forest
 	 *   to store against the fragment id
 	 */
-	public function setDOMFragment( string $id, array $forest ): void {
+	public function setDOMFragment(
+		string $id, DOMDocumentFragment $forest
+	): void {
 		$this->fragmentMap[$id] = $forest;
 	}
 

src/Ext/Cite/Ref.php
@@ -3,6 +3,7 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Ext\Cite;
 
+use DOMDocumentFragment;
 use DOMElement;
 use DOMNode;
 use Exception;
@@ -19,7 +20,9 @@ use Wikimedia\Parsoid\Utils\DOMCompat;
 class Ref extends ExtensionTagHandler {
 
 	/** @inheritDoc */
-	public function sourceToDom( ParsoidExtensionAPI $extApi, string $txt, array $extArgs ) {
+	public function sourceToDom(
+		ParsoidExtensionAPI $extApi, string $txt, array $extArgs
+	): ?DOMDocumentFragment {
 		// Drop nested refs entirely, unless we've explicitly allowed them
 		$parentExtTag = $extApi->parentExtTag();
 		if ( $parentExtTag === 'ref' && empty( $extApi->parentExtTagOpts()['allowNestedRef'] ) ) {

src/Ext/Cite/RefGroup.php
@@ -97,11 +97,10 @@ class RefGroup {
 			]
 		);
 		if ( $refContentId ) {
-			$content = $extApi->getContentDOM( $refContentId );
-			// The data-mw and data-parsoid attributes aren't needed on the ref content
-			// in the references section. The content wrapper will remain in the original
-			// site where the <ref> tag showed up and will retain data-parsoid & data-mw.
-			ParsoidExtensionAPI::migrateChildrenBetweenDocs( $content, $reftextSpan, false );
+			// `sup` is the wrapper created by Ref::sourceToDom()'s call to
+			// `extApi->extTagToDOM()`.  Only its contents are relevant.
+			$sup = $extApi->getContentDOM( $refContentId )->firstChild;
+			DOMUtils::migrateChildren( $sup, $reftextSpan );
 		}
 		$li->appendChild( $reftextSpan );
 

src/Ext/Cite/RefProcessor.php
@@ -3,7 +3,7 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Ext\Cite;
 
-use DOMElement;
+use DOMNode;
 use Wikimedia\Parsoid\Ext\DOMProcessor;
 use Wikimedia\Parsoid\Ext\ParsoidExtensionAPI;
 
@@ -16,12 +16,12 @@ class RefProcessor extends DOMProcessor {
 	 * @inheritDoc
 	 */
 	public function wtPostprocess(
-		ParsoidExtensionAPI $extApi, DOMElement $body, array $options, bool $atTopLevel
+		ParsoidExtensionAPI $extApi, DOMNode $node, array $options, bool $atTopLevel
 	): void {
 		if ( $atTopLevel ) {
 			$refsData = new ReferencesData();
-			References::processRefs( $extApi, $refsData, $body );
-			References::insertMissingReferencesIntoDOM( $extApi, $refsData, $body );
+			References::processRefs( $extApi, $refsData, $node );
+			References::insertMissingReferencesIntoDOM( $extApi, $refsData, $node );
 		}
 	}
 

src/Ext/Cite/References.php
@@ -3,7 +3,7 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Ext\Cite;
 
-use DOMDocument;
+use DOMDocumentFragment;
 use DOMElement;
 use DOMNode;
 use stdClass;
@@ -39,24 +39,23 @@ class References extends ExtensionTagHandler {
 
 	/**
 	 * @param ParsoidExtensionAPI $extApi
-	 * @param DOMDocument $doc
-	 * @param ?DOMNode $body
+	 * @param DOMDocumentFragment $domFragment
 	 * @param array $refsOpts
 	 * @param ?callable $modifyDp
 	 * @param bool $autoGenerated
 	 * @return DOMElement
 	 */
 	private static function createReferences(
-		ParsoidExtensionAPI $extApi, DOMDocument $doc, ?DOMNode $body,
+		ParsoidExtensionAPI $extApi, DOMDocumentFragment $domFragment,
 		array $refsOpts, ?callable $modifyDp, bool $autoGenerated = false
 	): DOMElement {
+		$doc = $domFragment->ownerDocument;
+
 		$ol = $doc->createElement( 'ol' );
 		DOMCompat::getClassList( $ol )->add( 'mw-references' );
 		DOMCompat::getClassList( $ol )->add( 'references' );
 
-		if ( $body ) {
-			DOMUtils::migrateChildren( $body, $ol );
-		}
+		DOMUtils::migrateChildren( $domFragment, $ol );
 
 		// Support the `responsive` parameter
 		$rrOpts = $extApi->getSiteConfig()->responsiveReferences();
@@ -128,7 +127,8 @@ class References extends ExtensionTagHandler {
 		$tplDmw = $isTplWrapper ? DOMDataUtils::getDataMw( $node ) : null;
 
 		// This is the <sup> that's the meat of the sealed fragment
-		$c = $extApi->getContentDOM( $contentId );
+		$c = $extApi->getContentDOM( $contentId )->firstChild;
+		DOMUtils::assertElt( $c );
 		$cDp = DOMDataUtils::getDataParsoid( $c );
 		$refDmw = DOMDataUtils::getDataMw( $c );
 		if ( empty( $cDp->empty ) && self::hasRef( $c ) ) { // nested ref-in-ref
@@ -183,20 +183,18 @@ class References extends ExtensionTagHandler {
 					$validFollow = true;
 					$ref = $group->indexByName[$followName];
 
-					$span = $c->ownerDocument->createElement( 'span' );
+					$span = $doc->createElement( 'span' );
 					DOMUtils::addTypeOf( $span, 'mw:Cite/Follow' );
 					$span->setAttribute( 'about', $about );
 					$span->appendChild(
-						$c->ownerDocument->createTextNode( ' ' )
+						$doc->createTextNode( ' ' )
 					);
 					DOMUtils::migrateChildren( $c, $span );
 					$c->appendChild( $span );
 
 					if ( $ref->contentId ) {
-						$refContent = $extApi->getContentDOM( $ref->contentId );
-						ParsoidExtensionAPI::migrateChildrenBetweenDocs(
-							$c, $refContent, false
-						);
+						$refContent = $extApi->getContentDOM( $ref->contentId )->firstChild;
+						DOMUtils::migrateChildren( $c, $refContent );
 					} else {
 						// Otherwise, we have a follow that comes after a named
 						// ref without content so use the follow fragment as
@@ -434,12 +432,11 @@ class References extends ExtensionTagHandler {
 		ParsoidExtensionAPI $extApi, ReferencesData $refsData, DOMNode $node
 	): void {
 		$doc = $node->ownerDocument;
-
 		foreach ( $refsData->getRefGroups() as $groupName => $refsGroup ) {
+			$domFragment = $doc->createDocumentFragment();
 			$frag = self::createReferences(
 				$extApi,
-				$doc,
-				null,
+				$domFragment,
 				[
 					// Force string cast here since in the foreach above, $groupName
 					// is an array key. In that context, number-like strings are
@@ -476,18 +473,18 @@ class References extends ExtensionTagHandler {
 	private static function processEmbeddedRefs(
 		ParsoidExtensionAPI $extApi, ReferencesData $refsData, string $str
 	): string {
-		$domBody = DOMCompat::getBody( $extApi->htmlToDom( $str ) );
-		self::processRefs( $extApi, $refsData, $domBody );
-		return $extApi->domToHtml( $domBody, true, true );
+		$domFragment = $extApi->htmlToDom( $str );
+		self::processRefs( $extApi, $refsData, $domFragment );
+		return $extApi->domToHtml( $domFragment, true, true );
 	}
 
 	/**
 	 * @param ParsoidExtensionAPI $extApi
 	 * @param ReferencesData $refsData
-	 * @param DOMElement $node
+	 * @param DOMNode $node
 	 */
 	public static function processRefs(
-		ParsoidExtensionAPI $extApi, ReferencesData $refsData, DOMElement $node
+		ParsoidExtensionAPI $extApi, ReferencesData $refsData, DOMNode $node
 	): void {
 		$child = $node->firstChild;
 		while ( $child !== null ) {
@@ -569,8 +566,8 @@ class References extends ExtensionTagHandler {
 	/** @inheritDoc */
 	public function sourceToDom(
 		ParsoidExtensionAPI $extApi, string $txt, array $extArgs
-	): DOMDocument {
-		$doc = $extApi->extTagToDOM(
+	): DOMDocumentFragment {
+		$domFragment = $extApi->extTagToDOM(
 			$extArgs,
 			'',
 			$txt,
@@ -585,12 +582,9 @@ class References extends ExtensionTagHandler {
 			'responsive' => null,
 		];
 
-		$docBody = DOMCompat::getBody( $doc );
-
 		$frag = self::createReferences(
 			$extApi,
-			$doc,
-			$docBody,
+			$domFragment,
 			$refsOpts,
 			function ( $dp ) use ( $extApi ) {
 				$dp->src = $extApi->extTag->getSource();
@@ -601,8 +595,8 @@ class References extends ExtensionTagHandler {
 				}
 			}
 		);
-		DOMCompat::getBody( $doc )->appendChild( $frag );
-		return $doc;
+		$domFragment->appendChild( $frag );
+		return $domFragment;
 	}
 
 	/** @inheritDoc */

src/Ext/Cite/ReferencesData.php
@@ -69,9 +69,8 @@ class ReferencesData {
 			$ref = $group->indexByName[$refName];
 			if ( $ref->contentId && !$ref->hasMultiples ) {
 				$ref->hasMultiples = true;
-				// Use the non-pp version here since we've already stored attribs
-				// before putting them in the map.
-				$ref->cachedHtml = $extApi->getContentHTML( $ref->contentId );
+				$c = $extApi->getContentDOM( $ref->contentId )->firstChild;
+				$ref->cachedHtml = $extApi->domToHtml( $c, true, false );
 			}
 			$ref->nodes[] = $linkBack;
 		} else {

src/Ext/DOMProcessor.php
@@ -3,7 +3,9 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Ext;
 
+use DOMDocumentFragment;
 use DOMElement;
+use DOMNode;
 
 /**
  * A Parsoid extension module may contain one or more DOMProcessors,
@@ -16,14 +18,14 @@ abstract class DOMProcessor {
 	 * Post-process DOM in the wt2html direction.
 	 *
 	 * @param ParsoidExtensionAPI $extApi
-	 * @param DOMElement $root The root of the tree to process
+	 * @param DOMDocumentFragment|DOMElement $root The root of the tree to process
 	 * @param array $options
 	 * @param bool $atTopLevel Is this processor invoked on the top level page?
 	 *   If false, this is being invoked in a sub-pipeline (ex: extensions)
 	 */
 	public function wtPostprocess(
 		ParsoidExtensionAPI $extApi,
-		DOMElement $root,
+		DOMNode $root,
 		array $options,
 		bool $atTopLevel
 	): void {

commit 8de3251a92397cb3c3d91e435f8e40760d3afea5

Unbreak ContentUtils::dumpDOM for DocumentFragments
* Broken since e067007c.
* Found while trying to debug T293308 where trying to dump
  DOM before/after a DOM pass crashed later passes because
  the data attributes would end up in wrong state.
* Also refactor and DRY out the code a bit.

Change-Id: I32c0d0edf81ae6f12dbca1554eff01e94fe443dd

src/Utils/ContentUtils.php
@@ -367,34 +367,57 @@ class ContentUtils {
 	}
 
 	/**
+	 * @param Node $node
+	 * @param array $options
+	 * @return string
+	 */
+	private static function dumpNode( Node $node, array $options ): string {
+		if ( $node instanceof Element ) {
+			// cloneNode doesn't clone data => walk DOM to clone it
+			$clonedNode = $node->cloneNode( true );
+			self::cloneData( $node, $clonedNode, $options );
+			$node = $clonedNode;
+		}
+
+		return self::ppToXML( $node, $options );
+	}
+
+	/**
+	 * @param string[] $buf
+	 * @param Node $rootNode
+	 * @param array &$options
+	 */
+	private static function dumpFragment( array $buf, Node $rootNode, array &$options ): void {
+		if ( $rootNode instanceof DocumentFragment ) {
+			foreach ( $rootNode->childNodes as $child ) {
+				$buf[] = self::dumpNode( $child, $options );
+			}
+		} else {
+			$buf[] = self::dumpNode( $rootNode, $options );
+		}
+
+		self::emit( $buf, $options );
+	}
+
+	/**
 	 * Dump the DOM with attributes.
 	 *
 	 * @param Node $rootNode
 	 * @param string $title
-	 * @param array &$options
+	 * @param array $options
 	 */
 	public static function dumpDOM(
-		Node $rootNode, string $title, array &$options = []
+		Node $rootNode, string $title = '', array $options = []
 	): void {
 		if ( !empty( $options['storeDiffMark'] ) || !empty( $options['dumpFragmentMap'] ) ) {
 			Assert::invariant( isset( $options['env'] ), "env should be set" );
-		}
-
-		if ( $rootNode instanceof Element ) {
-			// cloneNode doesn't clone data => walk DOM to clone it
-			$clonedRoot = $rootNode->cloneNode( true );
-			self::cloneData( $rootNode, $clonedRoot, $options );
-		} else {
-			$clonedRoot = $rootNode;
 		}
 
 		$buf = [];
 		if ( empty( $options['quiet'] ) ) {
 			$buf[] = '----- ' . $title . ' -----';
 		}
-
-		$buf[] = self::ppToXML( $clonedRoot, $options );
-		self::emit( $buf, $options );
+		self::dumpFragment( $buf, $rootNode, $options );
 
 		// Dump cached fragments
 		if ( !empty( $options['dumpFragmentMap'] ) ) {
@@ -402,13 +425,9 @@ class ContentUtils {
 				$buf = [];
 				$buf[] = str_repeat( '=', 15 );
 				$buf[] = 'FRAGMENT ' . $k;
-				$buf[] = '';
 				self::emit( $buf, $options );
 
-				$newOpts = $options;
-				$newOpts['dumpFragmentMap'] = false;
-				$newOpts['quiet'] = true;
-				self::dumpDOM( is_array( $fragment ) ? $fragment[0] : $fragment, '', $newOpts );
+				self::dumpFragment( [], is_array( $fragment ) ? $fragment[0] : $fragment, $options );
 			}
 		}
 