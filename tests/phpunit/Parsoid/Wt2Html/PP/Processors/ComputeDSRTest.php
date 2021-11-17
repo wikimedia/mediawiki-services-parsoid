606be1f01073bde1cbba7df86fe2a181a916b0ef
Tim Starling <tstarling@wikimedia.org>
2021-11-17 12:50:31 +1100

    ComputeDSRTest: Reformat and clean up
    * The assertions all had their parameters around the wrong way. The
      expected value is meant to go first.
    * Instead of grouping test cases by having separate tests that run
      identical code, use named data sets, which can be used with --filter
      to select the group of interest.
    * Instead of passing an array with named parameters to the test, use
      separate formal parameters, since that is simpler, it eliminates an
      indent level in the providers, and it has better type checking.
      Associative arrays can still be used for readability in the provider,
      the names are just ignored.
    * Fix non-standard array indenting style, which was confusing PHPStorm.
    
    Change-Id: Icebbd422e25ffea4704589dbbe2021b61a8a82a3

--- tests/phpunit/Parsoid/Wt2Html/PP/Processors/ComputeDSRTest.php
+++ tests/phpunit/Parsoid/Wt2Html/PP/Processors/ComputeDSRTest.php
@@ -3,9 +3,9 @@
 namespace Test\Parsoid\Wt2Html\PP\Processors;
 
 use PHPUnit\Framework\TestCase;
+use Wikimedia\Parsoid\Core\DomSourceRange;
 use Wikimedia\Parsoid\DOM\Element;
 use Wikimedia\Parsoid\Mocks\MockDataAccess;
-use Wikimedia\Parsoid\Mocks\MockEnv;
 use Wikimedia\Parsoid\Mocks\MockPageConfig;
 use Wikimedia\Parsoid\Mocks\MockPageContent;
 use Wikimedia\Parsoid\Mocks\MockSiteConfig;
@@ -26,44 +26,24 @@ class ComputeDSRTest extends TestCase {
 	 */
 	public function validateSpec( string $wt, Element $doc, array $spec ): void {
 		$elts = DOMCompat::querySelectorAll( $doc, $spec['selector'] );
-		$this->assertEquals( count( $elts ), 1 );
+		$this->assertCount( 1, $elts,
+				"Cannot match selector \"{$spec['selector']}\" in doc " .
+				ContentUtils::toXML( $doc ) );
 
 		$dp = DOMDataUtils::getDataParsoid( $elts[0] );
 		$dsr = $dp->dsr;
-		$this->assertEquals( !empty( $dsr ), true );
-		$this->assertEquals( is_object( $dsr ), true );
+		$this->assertTrue( !empty( $dsr ) );
+		$this->assertInstanceOf( DomSourceRange::class, $dsr );
 
-		$this->assertEquals( $dsr->substr( $wt ), $spec['dsrContent'][0],
+		$this->assertEquals( $spec['dsrContent'][0], $dsr->substr( $wt ),
 			"dsr all actual mismatch: \n\"" .
 			$dsr->substr( $wt ) . "\"\ndoes not match expected:\n\"" . $spec['dsrContent'][0] . "\"" );
-		$this->assertEquals( $dsr->openSubstr( $wt ), $spec['dsrContent'][1],
+		$this->assertEquals( $spec['dsrContent'][1], $dsr->openSubstr( $wt ),
 			"dsr open actual mismatch: \n\"" .
 			$dsr->openSubstr( $wt ) . "\"\ndoes not match expected:\n\"" . $spec['dsrContent'][1] . "\"" );
-		$this->assertEquals( $dsr->closeSubstr( $wt ), $spec['dsrContent'][2],
+		$this->assertEquals( $spec['dsrContent'][2], $dsr->closeSubstr( $wt ),
 			"dsr close actual mismatch: \n\"" .
 			$dsr->closeSubstr( $wt ) . "\"\ndoes not match expected:\n\"" . $spec['dsrContent'][2] . "\"" );
-	}
-
-	/**
-	 * @param array $test
-	 */
-	public function runDSRTest( array $test ): void {
-		$siteConfig = new MockSiteConfig( [] );
-		$dataAccess = new MockDataAccess( [] );
-		$parsoid = new Parsoid( $siteConfig, $dataAccess );
-
-		$content = new MockPageContent( [ 'main' => $test['wt'] ] );
-		$pageConfig = new MockPageConfig( [], $content );
-		$html = $parsoid->wikitext2html( $pageConfig, [ "wrapSections" => false ] );
-
-		$mockEnv = new MockEnv( [] );
-		$doc = ContentUtils::createAndLoadDocument( $html );
-
-		$wt = $test['wt'];
-		$specs = $test['specs'];
-		foreach ( $specs as $spec ) {
-			$this->validateSpec( $wt, DOMCompat::getBody( $doc ), $spec );
-		}
 	}
 
 	/**
@@ -75,209 +55,135 @@ class ComputeDSRTest extends TestCase {
 	 *               to the wikitext substring between dsr[0]..dsr[1]. The second and
 	 *               third elements are the opening/closing wikitext tag for that node.
 	 * @covers ::computeNodeDSR
-	 * @dataProvider provideParaComputeDSR
-	 * @param array $testInfo
+	 * @dataProvider provideComputeDSR
+	 * @param string $wt
+	 * @param array $specs
 	 */
-	public function testParaComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
+	public function testComputeDSR( string $wt, array $specs ): void {
+		$siteConfig = new MockSiteConfig( [] );
+		$dataAccess = new MockDataAccess( [] );
+		$parsoid = new Parsoid( $siteConfig, $dataAccess );
+
+		$content = new MockPageContent( [ 'main' => $wt ] );
+		$pageConfig = new MockPageConfig( [], $content );
+		$html = $parsoid->wikitext2html( $pageConfig, [ "wrapSections" => false ] );
+
+		$doc = ContentUtils::createAndLoadDocument( $html );
+
+		foreach ( $specs as $spec ) {
+			$this->validateSpec( $wt, DOMCompat::getBody( $doc ), $spec );
+		}
 	}
 
 	/**
 	 * @return array
 	 */
-	public function provideParaComputeDSR(): array {
+	public function provideComputeDSR(): array {
 		return [
-			[
-				[ 'wt' => 'a',    // disable first test so we can get to the juicy failure immediately.
-					'specs' => [
-						[ 'selector' => 'body > p', 'dsrContent' => [ 'a', '', '' ] ]
+			// Paragraph wrapping
+			'para 1' => [
+				'wt' => 'a',    // disable first test so we can get to the juicy failure immediately.
+				'specs' => [
+					[ 'selector' => 'body > p', 'dsrContent' => [ 'a', '', '' ] ]
+				]
+			],
+			'para 2' => [
+				'wt' => "a\n\nb",
+				'specs' => [
+					[ 'selector' => 'body > p:nth-child(1)', 'dsrContent' => [ 'a', '', '' ] ],
+					[ 'selector' => 'body > p:nth-child(2)', 'dsrContent' => [ 'b', '', '' ] ]
+				]
+			],
+
+			// Lists
+			'list 1' => [
+				'wt' => "*a\n*b",
+				'specs' => [
+					[ 'selector' => 'ul', 'dsrContent' => [ "*a\n*b", '', '' ] ],
+					[ 'selector' => 'ul > li:nth-child(1)', 'dsrContent' => [ '*a', '*', '' ] ],
+					[ 'selector' => 'ul > li:nth-child(2)', 'dsrContent' => [ '*b', '*', '' ] ]
+				]
+			],
+			'list 2' => [
+				'wt' => "*a\n**b\n***c\n*d",
+				'specs' => [
+					[ 'selector' => 'body > ul', 'dsrContent' => [ "*a\n**b\n***c\n*d", '', '' ] ],
+					[ 'selector' => 'body > ul > li:first-child', 'dsrContent' => [ "*a\n**b\n***c", '*', '' ] ],
+					[ 'selector' => 'body > ul > li:first-child > ul', 'dsrContent' => [ "**b\n***c", '', '' ] ],
+					[
+						'selector' => 'body > ul > li:first-child > ul > li:first-child',
+						'dsrContent' => [ "**b\n***c", '**', '' ]
+					],
+					[
+						'selector' => 'body > ul > li:first-child > ul > li:first-child > ul > li',
+						'dsrContent' => [ '***c', '***', '' ]
+					],
+					[ 'selector' => 'body > ul > li:nth-child(2)', 'dsrContent' => [ '*d', '*', '' ] ]
+				]
+			],
+
+			// Headings
+			'heading 1' => [
+				'wt' => "=A=\n==B==\n===C===\n====D====",
+				'specs' => [
+					[ 'selector' => 'body > h1', 'dsrContent' => [ '=A=', '=', '=' ] ],
+					[ 'selector' => 'body > h2', 'dsrContent' => [ '==B==', '==', '==' ] ],
+					[ 'selector' => 'body > h3', 'dsrContent' => [ '===C===', '===', '===' ] ],
+					[ 'selector' => 'body > h4', 'dsrContent' => [ '====D====', '====', '====' ] ]
+				]
+			],
+			'heading 2' => [
+				'wt' => "=A New Use for the = Sign=\n==The == Operator==",
+				'specs' => [
+					[
+						'selector' => 'body > h1',
+						'dsrContent' => [ '=A New Use for the = Sign=', '=', '=' ]
+					],
+					[ 'selector' => 'body > h2', 'dsrContent' => [ '==The == Operator==', '==', '==' ] ]
+				]
+			],
+
+			// Quotes
+			'quotes 1' => [
+				'wt' => "''a''\n'''b'''",
+				'specs' => [
+					[ 'selector' => 'p > i', 'dsrContent' => [ "''a''", "''", "''" ] ],
+					[ 'selector' => 'p > b', 'dsrContent' => [ "'''b'''", "'''", "'''" ] ]
+				]
+			],
+
+			// Tables
+			'tables 1' => [
+				'wt' => "{|\n|-\n|A\n|}",
+				'specs' => [
+					[ 'selector' => 'body > table', 'dsrContent' => [ "{|\n|-\n|A\n|}", '{|', '|}' ] ],
+					[ 'selector' => 'body > table > tbody > tr', 'dsrContent' => [ "|-\n|A", '|-', '' ] ],
+					[ 'selector' => 'body > table > tbody > tr > td', 'dsrContent' => [ '|A', '|', '' ] ]
+				]
+			],
+
+			// Pre
+			'pre 1' => [
+				'wt' => " Preformatted text ",
+				'specs' => [
+					[ 'selector' => 'body > pre', 'dsrContent' => [ " Preformatted text ", ' ', '' ] ]
+				]
+			],
+
+			// Elements
+			'elt 1' => [
+				'wt' => "<small>'''bold'''</small>",
+				'specs' => [
+					[
+						'selector' => 'body > p',
+						'dsrContent' => [ "<small>'''bold'''</small>", '', '' ]
+					],
+					[
+						'selector' => 'body > p > small',
+						'dsrContent' => [ "<small>'''bold'''</small>", '<small>', '</small>' ]
 					]
 				]
 			],
-			[
-				[ 'wt' => "a\n\nb",
-					'specs' => [
-						[ 'selector' => 'body > p:nth-child(1)', 'dsrContent' => [ 'a', '', '' ] ],
-						[ 'selector' => 'body > p:nth-child(2)', 'dsrContent' => [ 'b', '', '' ] ]
-					]
-				]
-			]
 		];
 	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider provideListComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testListComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function provideListComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => "*a\n*b",
-					'specs' => [
-						[ 'selector' => 'ul', 'dsrContent' => [ "*a\n*b", '', '' ] ],
-						[ 'selector' => 'ul > li:nth-child(1)', 'dsrContent' => [ '*a', '*', '' ] ],
-						[ 'selector' => 'ul > li:nth-child(2)', 'dsrContent' => [ '*b', '*', '' ] ]
-					]
-				]
-			],
-			[
-				[ 'wt' => "*a\n**b\n***c\n*d",
-					'specs' => [
-						[ 'selector' => 'body > ul', 'dsrContent' => [ "*a\n**b\n***c\n*d", '', '' ] ],
-						[ 'selector' => 'body > ul > li:first-child', 'dsrContent' => [ "*a\n**b\n***c", '*', '' ] ],
-						[ 'selector' => 'body > ul > li:first-child > ul', 'dsrContent' => [ "**b\n***c", '', '' ] ],
-						[ 'selector' => 'body > ul > li:first-child > ul > li:first-child',
-							'dsrContent' => [ "**b\n***c", '**', '' ] ],
-						[ 'selector' => 'body > ul > li:first-child > ul > li:first-child > ul > li',
-							'dsrContent' => [ '***c', '***', '' ] ],
-						[ 'selector' => 'body > ul > li:nth-child(2)', 'dsrContent' => [ '*d', '*', '' ] ]
-					]
-				]
-			]
-		];
-	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider provideHeadingComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testHeadingComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	public function provideHeadingComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => "=A=\n==B==\n===C===\n====D====",
-					'specs' => [
-						[ 'selector' => 'body > h1', 'dsrContent' => [ '=A=', '=', '=' ] ],
-						[ 'selector' => 'body > h2', 'dsrContent' => [ '==B==', '==', '==' ] ],
-						[ 'selector' => 'body > h3', 'dsrContent' => [ '===C===', '===', '===' ] ],
-						[ 'selector' => 'body > h4', 'dsrContent' => [ '====D====', '====', '====' ] ]
-					]
-				]
-			],
-			[
-				[ 'wt' => "=A New Use for the = Sign=\n==The == Operator==",
-					'specs' => [
-						[ 'selector' => 'body > h1',
-							'dsrContent' => [ '=A New Use for the = Sign=', '=', '=' ] ],
-						[ 'selector' => 'body > h2', 'dsrContent' => [ '==The == Operator==', '==', '==' ] ]
-					]
-				]
-			]
-		];
-	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider provideQuotesComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testQuotesComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function provideQuotesComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => "''a''\n'''b'''",
-					'specs' => [
-						[ 'selector' => 'p > i', 'dsrContent' => [ "''a''", "''", "''" ] ],
-						[ 'selector' => 'p > b', 'dsrContent' => [ "'''b'''", "'''", "'''" ] ]
-					]
-				]
-			]
-		];
-	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider provideTableComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testTableComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function provideTableComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => "{|\n|-\n|A\n|}",
-					'specs' => [
-						[ 'selector' => 'body > table', 'dsrContent' => [ "{|\n|-\n|A\n|}", '{|', '|}' ] ],
-						[ 'selector' => 'body > table > tbody > tr', 'dsrContent' => [ "|-\n|A", '|-', '' ] ],
-						[ 'selector' => 'body > table > tbody > tr > td', 'dsrContent' => [ '|A', '|', '' ] ]
-					]
-				]
-			]
-		];
-	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider providePreComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testPreComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function providePreComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => " Preformatted text ",
-					'specs' => [
-						[ 'selector' => 'body > pre', 'dsrContent' => [ " Preformatted text ", ' ', '' ] ]
-					]
-				]
-			]
-		];
-	}
-
-	/**
-	 * @covers ::computeNodeDSR
-	 * @dataProvider provideEltComputeDSR
-	 * @param array $testInfo
-	 */
-	public function testEltComputeDSR( array $testInfo ): void {
-		$this->runDSRTest( $testInfo );
-	}
-
-	/**
-	 * @return array
-	 */
-	public function provideEltComputeDSR(): array {
-		return [
-			[
-				[ 'wt' => "<small>'''bold'''</small>",
-					'specs' => [
-						[ 'selector' => 'body > p',
-							'dsrContent' => [ "<small>'''bold'''</small>", '', '' ] ],
-						[ 'selector' => 'body > p > small',
-							'dsrContent' => [ "<small>'''bold'''</small>", '<small>', '</small>' ] ]
-					]
-				]
-			]
-		];
-	}
-
 }
