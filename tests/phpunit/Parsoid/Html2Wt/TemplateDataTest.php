wikimedia/mediawiki-services-parsoid

commit 6658cad9b34681c060397b05d538c7726a9e29eb

Port WikitextSerializer
Change-Id: I98af12bc4a6761625dc85254ed0eddd235fe2205

src/placeholder/Html2Wt/WikitextSerializer.php
@@ -1,155 +1,0 @@
-<?php
-declare( strict_types = 1 );
-
-namespace Parsoid\Html2Wt;
-
-use DOMElement;
-use DOMNode;
-use LogicException;
-use Parsoid\Config\Env;
-use Parsoid\Tokens\Token;
-
-/**
- * Wikitext to HTML serializer.
- * Serializes a chunk of tokens or an HTML DOM to MediaWiki's wikitext flavor.
- */
-class WikitextSerializer {
-
-	/** @var Env */
-	public $env;
-
-	/**
-	 * @note Porting note: this replaces WikitextSerializer.wteHandlers.escapeWikiText
-	 * @param SerializerState $state
-	 * @param string $text
-	 * @param array $opts
-	 *   - node: (DOMNode)
-	 *   - isLastChild: (bool)
-	 * @return string
-	 */
-	public function escapeWikiText( SerializerState $state, string $text, array $opts ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * Emit non-separator wikitext that does not need to be escaped.
-	 * @param string $src
-	 * @param DOMNode $node
-	 */
-	public function emitWikitext( string $src, DOMNode $node ): void {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * Internal worker. Recursively serialize a DOM subtree.
-	 * @param DOMNode $child
-	 * @return DOMNode|null
-	 * @private
-	 */
-	public function serializeNode( DOMNode $child ): ?DOMNode {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @param bool $wrapperUnmodified
-	 * @return string
-	 */
-	public function serializeHTMLTag( DOMNode $node, bool $wrapperUnmodified ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @param bool $wrapperUnmodified
-	 * @return string
-	 */
-	public function serializeHTMLEndTag( DOMNode $node, bool $wrapperUnmodified ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @param Token $token
-	 * @return string
-	 */
-	public function serializeAttributes( DOMNode $node, Token $token ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param SerializerState $state
-	 * @param DOMNode $node
-	 * @param array $parts
-	 * @return string
-	 */
-	public function serializeFromParts( SerializerState $state, DOMNode $node, array $parts ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @param string $name
-	 * @return array
-	 */
-	public function serializedAttrVal( DOMNode $node, string $name ): array {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * Emit a separator based on the collected (and merged) constraints
-	 * and existing separator text. Called when new output is triggered.
-	 * @param DOMNode $node
-	 * @return string
-	 */
-	public function buildSep( DOMNode $node ): string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMElement $node
-	 */
-	public function linkHandler( DOMElement $node ): void {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMElement $node
-	 */
-	public function figureHandler( DOMElement $node ): void {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @param SerializerState $state
-	 * @return string|null
-	 */
-	public function defaultExtensionHandler( DOMNode $node, SerializerState $state ): ?string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMNode $node
-	 * @return string|null
-	 */
-	public function languageVariantHandler( DOMNode $node ): ?string {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @param DOMElement $node
-	 */
-	public function handleLIHackIfApplicable( DOMElement $node ): void {
-		throw new LogicException( 'Not ported yet' );
-	}
-
-	/**
-	 * @note Porting note: this replaces the pattern $serializer->env->log( $serializer->logType, ... )
-	 * @param mixed ...$args
-	 * @deprecated Use PSR-3 logging instead
-	 */
-	public function trace( ...$args ) {
-	}
-
-}

.phpcs.xml
@@ -9,6 +9,7 @@
 	<exclude-pattern>src/Wt2Html/Grammar.php</exclude-pattern>
 	<rule ref="./vendor/mediawiki/mediawiki-codesniffer/MediaWiki">
 		<exclude name="MediaWiki.WhiteSpace.SpaceBeforeSingleLineComment.NewLineComment" />
+		<exclude name="MediaWiki.WhiteSpace.SpaceBeforeSingleLineComment.SingleSpaceBeforeSingleLineComment" />
 		<exclude name="MediaWiki.PHP70Features" />
 		<exclude name="MediaWiki.PHP71Features" />
 	</rule>

src/Config/PageConfig.php
@@ -3,10 +3,26 @@ declare( strict_types = 1 );
 
 namespace Parsoid\Config;
 
+use DOMDocument;
+
 /**
  * Page-level configuration interface for Parsoid
  */
 abstract class PageConfig {
+
+	/**
+	 * Content type of the page (when parsing HTML).
+	 * @var string
+	 */
+	public $dpContentType;
+
+	/**
+	 * The owner document of the page. Used to transfer context between WikitextSerializer and
+	 * some extensions, see WikitextSerializer::serializeDOM.
+	 * PORT-FIXME this should not be here.
+	 * @var DOMDocument|null
+	 */
+	public $editedDoc;
 
 	/**
 	 * Whether the page has a lintable content model

src/Html2Wt/DOMHandlers/TDHandler.php
@@ -32,7 +32,7 @@ class TDHandler extends DOMHandler {
 		// T149209: Special case to deal with scenarios
 		// where the previous sibling put us in a SOL state
 		// (or will put in a SOL state when the separator is emitted)
-		if ( $state->onSOL || ( $state->sep->constraints->min ?? 0 ) > 0 ) {
+		if ( $state->onSOL || ( $state->sep->constraints['min'] ?? 0 ) > 0 ) {
 			$startTagSrc = preg_replace( '/\|\|/', '|', $startTagSrc, 1 );
 			$startTagSrc = preg_replace( '/{{!}}{{!}}/', '{{!}}', $startTagSrc, 1 );
 		}

src/Html2Wt/DOMHandlers/THHandler.php
@@ -32,7 +32,7 @@ class THHandler extends DOMHandler {
 		// T149209: Special case to deal with scenarios
 		// where the previous sibling put us in a SOL state
 		// (or will put in a SOL state when the separator is emitted)
-		if ( $state->onSOL || ( $state->sep->constraints->min ?? 0 ) > 0 ) {
+		if ( $state->onSOL || ( $state->sep->constraints['min'] ?? 0 ) > 0 ) {
 			// You can use both "!!" and "||" for same-row headings (ugh!)
 			$startTagSrc = preg_replace( '/!!/', '!', $startTagSrc, 1 );
 			$startTagSrc = preg_replace( '/\|\|/', '!', $startTagSrc, 1 );

src/Html2Wt/SerializerState.php
@@ -130,6 +130,12 @@ class SerializerState {
 	public $inAttribute = false;
 
 	/**
+	 * PORT-FIXME document
+	 * @var bool
+	 */
+	public $inModifiedContent;
+
+	/**
 	 * Did we introduce nowikis for indent-pre protection?
 	 * If yes, we might run a post-pass to strip useless ones.
 	 * @var bool
@@ -510,10 +516,10 @@ class SerializerState {
 
 	/**
 	 * Pushes the chunk to the current line.
-	 * @param string $res
+	 * @param ConstrainedText|string $res
 	 * @param DOMNode $node
 	 */
-	public function emitChunk( string $res, DOMNode $node ): void {
+	public function emitChunk( $res, DOMNode $node ): void {
 		$res = ConstrainedText::cast( $res, $node );
 
 		// Replace newlines if we're in a single-line context

tests/phpunit/Parsoid/Html2Wt/DOMNormalizerTest.php
@@ -32,8 +32,7 @@ class DOMNormalizerTest extends TestCase {
 			'scrubWikitext' => true
 		];
 		$mockEnv = new MockEnv( $opts );
-		$mockSerializer = new WikitextSerializer;
-		$mockSerializer->env = $mockEnv;
+		$mockSerializer = new WikitextSerializer( [ 'env' => $mockEnv ] );
 		$mockState = new SerializerState( $mockSerializer, [
 			'selserMode' => false,
 			'rtTestMode' => false,

commit 3c0cc090ac1e145a31d8acbe83ec96d06b1feb67

Fix using sort order from data-mw when serializing template params
The bug was introduced in 6658cad while porting.

The output of the test prior to this patch, on php 7.4 is,

{{Meh|f1=foo|f10=foo|f16=foo|f15=foo|f14=foo|f13=foo|f12=foo|f11=foo
|f9=foo|f2=foo|f8=foo|f7=foo|f6=foo|f5=foo|f4=foo|f3=foo|f17=foo}}

The docs for usort say, "If two members compare as equal, they retain
their original order. Prior to PHP 8.0.0, their relative order in the
sorted array was undefined."

Without the fix, the comparator returns 0 (equal) for all members.  The
bug was only apparent on php versions prior to 8 and when the number of
elements in the array was greater than 16, which I guess triggers some
change in the backing store and where the undefined behaviour came from.

Bug: T304730
Change-Id: I72033e4a7cd931fc2bc5628460b790ced14aede0

src/Html2Wt/WikitextSerializer.php
@@ -584,9 +584,10 @@ class WikitextSerializer {
 		array $dpArgInfo, ?array $tplData, array $dataMwKeys
 	): Closure {
 		// Record order of parameters in new data-mw
-		$newOrder = array_map( static function ( $key, $i ) {
-			return [ $key, [ 'order' => $i ] ];
-		}, $dataMwKeys, array_keys( $dataMwKeys ) );
+		$newOrder = [];
+		foreach ( $dataMwKeys as $i => $key ) {
+			$newOrder[$key] = [ 'order' => $i ];
+		}
 		// Record order of parameters in templatedata (if present)
 		$tplDataOrder = [];
 		$aliasMap = [];

tests/phpunit/Parsoid/Html2Wt/TemplateDataTest.php
@@ -102,6 +102,7 @@ class TemplateDataTest extends TestCase {
 					'edited' => "{{InlineTplNoParamOrder|f1=BAR|f2=foo}}"
 				]
 			],
+
 			// 5. block-tpl (but written in inline format originally); no param order
 			[
 				'name' => 'Enforce block format',
@@ -399,6 +400,40 @@ class TemplateDataTest extends TestCase {
 					'edited' => "x{{BlockTplNoParamOrder}}y" // normalized
 				]
 			],
+
+			// 23. Sort arguments as in data-mw - T304730
+			[
+				'name' => 'Sort arguments as in data-mw',
+				'html' => '<span about="#mwt1" typeof="mw:Transclusion" data-mw=\'' .
+					'{"parts":[{"template":{"target":{"wt":"Meh", "href":"./Template:Meh"},"params":{' .
+						'"f1":{"wt":"foo"},' .
+						'"f2":{"wt":"foo"},' .
+						'"f3":{"wt":"foo"},' .
+						'"f4":{"wt":"foo"},' .
+						'"f5":{"wt":"foo"},' .
+						'"f6":{"wt":"foo"},' .
+						'"f7":{"wt":"foo"},' .
+						'"f8":{"wt":"foo"},' .
+						'"f9":{"wt":"foo"},' .
+						'"f10":{"wt":"foo"},' .
+						'"f11":{"wt":"foo"},' .
+						'"f12":{"wt":"foo"},' .
+						'"f13":{"wt":"foo"},' .
+						'"f14":{"wt":"foo"},' .
+						'"f15":{"wt":"foo"},' .
+						'"f16":{"wt":"foo"},' .
+						'"f17":{"wt":"foo"}' .
+					'},"i":0}}]}' . "'" . '>foo</span>',
+				'wt' => [
+					'no_selser' => "{{Meh|f1=foo|f2=foo|f3=foo|f4=foo|f5=foo|f6=foo|f7=foo|f8=foo|" .
+						"f9=foo|f10=foo|f11=foo|f12=foo|f13=foo|f14=foo|f15=foo|f16=foo|f17=foo}}",
+					'new_content' => "{{Meh|f1=foo|f2=foo|f3=foo|f4=foo|f5=foo|f6=foo|f7=foo|f8=foo|" .
+						"f9=foo|f10=foo|f11=foo|f12=foo|f13=foo|f14=foo|f15=foo|f16=foo|f17=foo}}",
+					'edited' => "{{Meh|f1=BAR|f2=foo|f3=foo|f4=foo|f5=foo|f6=foo|f7=foo|f8=foo|" .
+						"f9=foo|f10=foo|f11=foo|f12=foo|f13=foo|f14=foo|f15=foo|f16=foo|f17=foo}}"
+				]
+			],
+
 		];
 	}
 