4f75e4bbf3d91fb275cc9edecfe3117273ee3282
Subramanya Sastry <ssastry@wikimedia.org>
2022-10-08 08:33:57 -0500

    Update regression-testing.php to php 7.4
    php7.2-fpm no longer exists
    
    Change-Id: I16502d279bfd4f39573ffc33cbcb109807ae00fe

--- tools/regression-testing.php
+++ tools/regression-testing.php
@@ -178,7 +178,7 @@ class RegressionTesting extends \Wikimedia\Parsoid\Tools\Maintenance {
 	 */
 	public function runTest( $commit ): void {
 		$cdDir = self::cmd( 'cd /srv/parsoid-testing' );
-		$restartPHP = self::cmd( 'sudo systemctl restart php7.2-fpm.service' );
+		$restartPHP = self::cmd( 'sudo systemctl restart php7.4-fpm.service' );
 		$resultPath = "/tmp/results.$commit.json";
 		$testScript = self::cmd(
 			$cdDir, '&&',
