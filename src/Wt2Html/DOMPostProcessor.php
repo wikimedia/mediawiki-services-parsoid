67f1e355e23dc66e01c07c7432deabcb613aba07
Umherirrender <umherirrender_de.wp@web.de>
2022-12-01 19:31:21 +0100

    build: Updating mediawiki/mediawiki-phan-config to 0.12.0
    Be more specific on some ...$args to help phan understand that never
    zero arguments are passed, which would be possible on ...arguments
    
    Bug: T397362
    Change-Id: Ieb9898986209ff86bb890662df771ed853dddbda
    (cherry picked from commit af0174759d92344ed6a385705254788dda9518d2)

--- src/Wt2Html/DOMPostProcessor.php
+++ src/Wt2Html/DOMPostProcessor.php
@@ -145,16 +145,16 @@ class DOMPostProcessor extends PipelineStage {
 				foreach ( $p['handlers'] as $h ) {
 					$t->addHandler( $h['nodeName'], $h['action'] );
 				}
-				$p['proc'] = function ( ...$args ) use ( $t ) {
-					return $t->run( $this->env, ...$args );
+				$p['proc'] = function ( Node $workNode, array $options, bool $atTopLevel ) use ( $t ) {
+					return $t->run( $this->env, $workNode, $options, $atTopLevel );
 				};
 			} else {
 				$classNameOrSpec = $p['Processor'];
 				if ( empty( $p['isExtPP'] ) ) {
 					// Internal processor w/ ::run() method, class name given
 					$c = new $classNameOrSpec();
-					$p['proc'] = function ( ...$args ) use ( $c ) {
-						return $c->run( $this->env, ...$args );
+					$p['proc'] = function ( Node $workNode, array $options, bool $atTopLevel ) use ( $c ) {
+						return $c->run( $this->env, $workNode, $options, $atTopLevel );
 					};
 				} else {
 					// Extension post processor, object factory spec given
@@ -163,8 +163,8 @@ class DOMPostProcessor extends PipelineStage {
 						'allowClassName' => true,
 						'assertClass' => ExtDOMProcessor::class,
 					] );
-					$p['proc'] = function ( ...$args ) use ( $c ) {
-						return $c->wtPostprocess( $this->extApi, ...$args );
+					$p['proc'] = function ( Node $workNode, array $options, bool $atTopLevel ) use ( $c ) {
+						return $c->wtPostprocess( $this->extApi, $workNode, $options, $atTopLevel );
 					};
 				}
 			}
