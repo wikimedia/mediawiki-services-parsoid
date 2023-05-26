0dc439dd46b5db02bd515d642caa15f9e081270d
Arlo Breault <abreault@wikimedia.org>
2023-05-26 12:51:51 -0400

    Bump Parsoid HTML version to 2.8.0
    The output is the same as 2.7.1 but RESTBase doesn't respect patch
    versions and we'd like to be able to request the latest version for
    VisualEditor.
    
    Change-Id: Iecdd5dc318c6589fa34c2be39dce8bca6f243369

--- bin/roundtrip-test.js
+++ bin/roundtrip-test.js
@@ -23,7 +23,7 @@ var Diff = require('../lib/utils/Diff.js').Diff;
 var JSUtils = require('../lib/utils/jsutils.js').JSUtils;
 var MockEnv = require('../tests/MockEnv.js').MockEnv;
 
-var defaultContentVersion = '2.7.1';
+var defaultContentVersion = '2.8.0';
 
 var MAX_RETRIES = 10;
 
