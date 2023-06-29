df44c835ae4107194e50c034a5a14c5b0b311541
Subramanya Sastry <ssastry@wikimedia.org>
2023-06-29 17:08:19 -0500

    Extract useparsoid processing into a static method for use in exts
    This common code will be useful in the FlaggedRevs extension
    and avoids code duplication.
    
    Change-Id: I3daaa57bc7dca883eb060143fc514e2a32e1e61a

--- extension/src/ArticleParserOptionsHandler.php
+++ extension/src/ArticleParserOptionsHandler.php
@@ -5,20 +5,20 @@ namespace MWParsoid;
 use Article;
 use MediaWiki\Page\Hook\ArticleParserOptionsHook;
 use ParserOptions;
+use WebRequest;
 
 class ArticleParserOptionsHandler implements ArticleParserOptionsHook {
+
 	/**
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ArticleParserOptions
-	 * @param Article $article
+	 * Check if 'useparsoid=1' is passed in as a query param, and if so,
+	 * set the useParsoid option in ParserOptions.
+	 *
+	 * @param WebRequest $request
 	 * @param ParserOptions $popts
-	 * @return bool|void
 	 */
-	public function onArticleParserOptions(
-		Article $article, ParserOptions $popts
-	) {
-		// T335157: Enable Parsoid Read Views for articles as an experimental
-		// feature; this is primarily used for internal testing at this time.
-		$request = $article->getContext()->getRequest();
+	public static function processUseParsoidQueryParam(
+		WebRequest $request, ParserOptions $popts
+	): void {
 		$queryEnable = $request->getRawVal( 'useparsoid' );
 		if (
 			$queryEnable &&
@@ -27,6 +27,18 @@ class ArticleParserOptionsHandler implements ArticleParserOptionsHook {
 		) {
 			$popts->setUseParsoid();
 		}
+	}
+
+	/**
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ArticleParserOptions
+	 * @param Article $article
+	 * @param ParserOptions $popts
+	 * @return bool|void
+	 */
+	public function onArticleParserOptions( Article $article, ParserOptions $popts ) {
+		// T335157: Enable Parsoid Read Views for articles as an experimental
+		// feature; this is primarily used for internal testing at this time.
+		self::processUseParsoidQueryParam( $article->getContext()->getRequest(), $popts );
 		return true;
 	}
 }
