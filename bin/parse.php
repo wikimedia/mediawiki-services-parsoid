wikimedia/mediawiki-services-parsoid

commit 0fed58bb276680feba1478d428075ac915160639

Use LinkTarget in Parsoid Config APIs
Depends on the new methods first being implemented in mediawiki-core,
and core extending Parsoid's LinkTarget.

Added some caching to Title::getPrefixedText() and Title::getPrefixedDBKey()
to ensure we don't lose any performance.

Depends-On: I30e2df89e5060ce11f1d6c181a2ba4866fecf99b
Depends-On: I9adc349b230834827b002f412dcc697b44773abe
Change-Id: Id3c3ff34a0c796ed0956cb9fb73247056e68667b

bin/parse.php
@@ -337,11 +337,11 @@ class Parse extends \Wikimedia\Parsoid\Tools\Maintenance {
 	private function setupMockConfig( array $configOpts ) {
 		$siteConfig = new MockSiteConfig( $configOpts );
 		$siteConfig->registerExtensionModule( DummyAnnotation::class );
-		$dataAccess = new MockDataAccess( $configOpts );
+		$dataAccess = new MockDataAccess( $siteConfig, $configOpts );
 		$pageContent = new MockPageContent( [ 'main' =>
 			$configOpts['pageContent'] ?? '' ] );
 		$this->siteConfig = $siteConfig;
-		$this->pageConfig = new MockPageConfig( $configOpts, $pageContent );
+		$this->pageConfig = new MockPageConfig( $siteConfig, $configOpts, $pageContent );
 		$this->metadata = new StubMetadataCollector();
 		$this->parsoid = new Parsoid( $siteConfig, $dataAccess );
 	}

src/Config/Api/DataAccess.php
@@ -4,12 +4,16 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Config\Api;
 
+use Wikimedia\Parsoid\Config\Api\SiteConfig as ApiSiteConfig;
 use Wikimedia\Parsoid\Config\DataAccess as IDataAccess;
 use Wikimedia\Parsoid\Config\PageConfig;
 use Wikimedia\Parsoid\Config\PageContent;
+use Wikimedia\Parsoid\Config\SiteConfig as ISiteConfig;
 use Wikimedia\Parsoid\Core\ContentMetadataCollector;
+use Wikimedia\Parsoid\Core\LinkTarget;
 use Wikimedia\Parsoid\Mocks\MockPageContent;
 use Wikimedia\Parsoid\Utils\PHPUtils;
+use Wikimedia\Parsoid\Utils\Title;
 
 /**
  * DataAccess via MediaWiki's Action API
@@ -46,10 +50,7 @@ class DataAccess extends IDataAccess {
 	 */
 	private $cache = [];
 
-	/**
-	 * @var ?SiteConfig
-	 */
-	private $siteConfig = null;
+	private ISiteConfig $siteConfig;
 
 	/**
 	 * Get from cache
@@ -90,10 +91,10 @@ class DataAccess extends IDataAccess {
 
 	/**
 	 * @param ApiHelper $api
-	 * @param ?SiteConfig $siteConfig
+	 * @param ISiteConfig $siteConfig
 	 * @param array $opts
 	 */
-	public function __construct( ApiHelper $api, ?SiteConfig $siteConfig, array $opts ) {
+	public function __construct( ApiHelper $api, ISiteConfig $siteConfig, array $opts ) {
 		$this->api = $api;
 		$this->siteConfig = $siteConfig;
 		$this->stripProto = $opts['stripProto'] ?? true;
@@ -106,12 +107,13 @@ class DataAccess extends IDataAccess {
 		}
 
 		$ret = [];
+		$pageConfigTitle = $this->toPrefixedText( $pageConfig->getLinkTarget() );
 		foreach ( array_chunk( $titles, 50 ) as $batch ) {
 			$data = $this->api->makeRequest( [
 				'action' => 'query',
 				'prop' => 'info',
 				'inprop' => 'linkclasses',
-				'inlinkcontext' => $pageConfig->getTitle(),
+				'inlinkcontext' => $pageConfigTitle,
 				'titles' => implode( '|', $batch ),
 			] )['query'];
 			$norm = [];
@@ -154,8 +156,9 @@ class DataAccess extends IDataAccess {
 
 	/** @inheritDoc */
 	public function getFileInfo( PageConfig $pageConfig, array $files ): array {
+		$pageConfigTitle = $this->toPrefixedText( $pageConfig->getLinkTarget() );
 		$sc = $this->siteConfig;
-		if ( $sc && $sc->hasVideoInfo() ) {
+		if ( $sc instanceof ApiSiteConfig && $sc->hasVideoInfo() ) {
 			$prefix = "vi";
 			$propName = "videoinfo";
 		} else {
@@ -168,7 +171,7 @@ class DataAccess extends IDataAccess {
 			'formatversion' => 2,
 			'rawcontinue' => 1,
 			'prop' => $propName,
-			"{$prefix}badfilecontexttitle" => $pageConfig->getTitle(),
+			"{$prefix}badfilecontexttitle" => $pageConfigTitle,
 			"{$prefix}prop" => implode( '|', [
 				'mediatype', 'mime', 'size', 'url', 'badfile'
 			] )
@@ -182,7 +185,7 @@ class DataAccess extends IDataAccess {
 			$name = $file[0];
 			$dims = $file[1];
 
-			$imgNS = $sc ? $sc->namespaceName( $sc->canonicalNamespaceId( "File" ) ) : "File";
+			$imgNS = $sc->namespaceName( $sc->canonicalNamespaceId( 'file' ) );
 			$apiArgs['titles'] = "$imgNS:$name";
 			$needsWidth = isset( $dims['page'] ) || isset( $dims['lang'] );
 			if ( isset( $dims['width'] ) ) {
@@ -267,12 +270,13 @@ class DataAccess extends IDataAccess {
 
 	/** @inheritDoc */
 	public function doPst( PageConfig $pageConfig, string $wikitext ): string {
-		$key = implode( ':', [ 'pst', md5( $pageConfig->getTitle() ) , md5( $wikitext ) ] );
+		$pageConfigTitle = $this->toPrefixedText( $pageConfig->getLinkTarget() );
+		$key = implode( ':', [ 'pst', md5( $pageConfigTitle ) , md5( $wikitext ) ] );
 		$ret = $this->getCache( $key );
 		if ( $ret === null ) {
 			$data = $this->api->makeRequest( [
 				'action' => 'parse',
-				'title' => $pageConfig->getTitle(),
+				'title' => $pageConfigTitle,
 				'text' => $wikitext,
 				'contentmodel' => 'wikitext',
 				'onlypst' => 1,
@@ -326,12 +330,13 @@ class DataAccess extends IDataAccess {
 		string $wikitext
 	): string {
 		$revid = $pageConfig->getRevisionId();
-		$key = implode( ':', [ 'parse', md5( $pageConfig->getTitle() ), md5( $wikitext ), $revid ] );
+		$pageConfigTitle = $this->toPrefixedText( $pageConfig->getLinkTarget() );
+		$key = implode( ':', [ 'parse', md5( $pageConfigTitle ), md5( $wikitext ), $revid ] );
 		$data = $this->getCache( $key );
 		if ( $data === null ) {
 			$params = [
 				'action' => 'parse',
-				'title' => $pageConfig->getTitle(),
+				'title' => $pageConfigTitle,
 				'text' => $wikitext,
 				'contentmodel' => 'wikitext',
 				'prop' => 'text|modules|jsconfigvars|categories|properties|externallinks',
@@ -356,12 +361,13 @@ class DataAccess extends IDataAccess {
 		string $wikitext
 	): string {
 		$revid = $pageConfig->getRevisionId();
-		$key = implode( ':', [ 'preprocess', md5( $pageConfig->getTitle() ), md5( $wikitext ), $revid ] );
+		$pageConfigTitle = $this->toPrefixedText( $pageConfig->getLinkTarget() );
+		$key = implode( ':', [ 'preprocess', md5( $pageConfigTitle ), md5( $wikitext ), $revid ] );
 		$data = $this->getCache( $key );
 		if ( $data === null ) {
 			$params = [
 				'action' => 'expandtemplates',
-				'title' => $pageConfig->getTitle(),
+				'title' => $pageConfigTitle,
 				'text' => $wikitext,
 				'prop' => 'wikitext|modules|jsconfigvars|categories|properties',
 				'showstrategykeys' => 1,
@@ -380,8 +386,11 @@ class DataAccess extends IDataAccess {
 
 	/** @inheritDoc */
 	public function fetchTemplateSource(
-		PageConfig $pageConfig, string $title
+		PageConfig $pageConfig, $title
 	): ?PageContent {
+		if ( !is_string( $title ) ) {
+			$title = $this->toPrefixedText( $title );
+		}
 		$key = implode( ':', [ 'content', md5( $title ) ] );
 		$ret = $this->getCache( $key );
 		if ( $ret === null ) {
@@ -408,7 +417,10 @@ class DataAccess extends IDataAccess {
 	}
 
 	/** @inheritDoc */
-	public function fetchTemplateData( PageConfig $pageConfig, string $title ): ?array {
+	public function fetchTemplateData( PageConfig $pageConfig, $title ): ?array {
+		if ( !is_string( $title ) ) {
+			$title = $this->toPrefixedText( $title );
+		}
 		$key = implode( ':', [ 'templatedata', md5( $title ) ] );
 		$ret = $this->getCache( $key );
 		if ( $ret === null ) {
@@ -429,5 +441,17 @@ class DataAccess extends IDataAccess {
 		foreach ( $lints as $l ) {
 			error_log( PHPUtils::jsonEncode( $l ) );
 		}
+	}
+
+	/**
+	 * Helper to turn a LinkTarget object into the "prefixed text" title form
+	 * expected by the MediaWiki action API.
+	 * @param LinkTarget $linkTarget
+	 * @return string The title, as prefixed text
+	 */
+	private function toPrefixedText( LinkTarget $linkTarget ): string {
+		return Title::newFromLinkTarget(
+			$linkTarget, $this->siteConfig
+		)->getPrefixedText();
 	}
 }

src/Config/Api/Env.php
@@ -25,8 +25,8 @@ class Env extends IEnv {
 	public function __construct( array $opts ) {
 		$api = new ApiHelper( $opts );
 
-		$pageConfig = new PageConfig( $api, $opts );
 		$siteConfig = new SiteConfig( $api, $opts );
+		$pageConfig = new PageConfig( $api, $siteConfig, $opts );
 		$dataAccess = new DataAccess( $api, $siteConfig, $opts );
 		$metadata = new StubMetadataCollector( $siteConfig->getLogger() );
 		parent::__construct( $siteConfig, $pageConfig, $dataAccess, $metadata, $opts );

src/Config/Api/PageConfig.php
@@ -8,7 +8,10 @@ use Wikimedia\Assert\Assert;
 use Wikimedia\Bcp47Code\Bcp47Code;
 use Wikimedia\Parsoid\Config\PageConfig as IPageConfig;
 use Wikimedia\Parsoid\Config\PageContent;
+use Wikimedia\Parsoid\Config\SiteConfig as ISiteConfig;
+use Wikimedia\Parsoid\Core\LinkTarget;
 use Wikimedia\Parsoid\Mocks\MockPageContent;
+use Wikimedia\Parsoid\Utils\Title;
 use Wikimedia\Parsoid\Utils\Utils;
 
 /**
@@ -21,7 +24,10 @@ class PageConfig extends IPageConfig {
 	/** @var ?ApiHelper */
 	private $api;
 
-	/** @var string */
+	/** @var ISiteConfig */
+	private ISiteConfig $siteConfig;
+
+	/** @var Title */
 	private $title;
 
 	/** @var string|null */
@@ -44,13 +50,19 @@ class PageConfig extends IPageConfig {
 
 	/**
 	 * @param ?ApiHelper $api (only needed if $opts doesn't provide page info)
+	 * @param ISiteConfig $siteConfig
 	 * @param array $opts
 	 */
-	public function __construct( ?ApiHelper $api, array $opts ) {
+	public function __construct( ?ApiHelper $api, ISiteConfig $siteConfig, array $opts ) {
+		parent::__construct();
 		$this->api = $api;
+		$this->siteConfig = $siteConfig;
 
 		if ( !isset( $opts['title'] ) ) {
 			throw new \InvalidArgumentException( '$opts[\'title\'] must be set' );
+		}
+		if ( !( $opts['title'] instanceof Title ) ) {
+			throw new \InvalidArgumentException( '$opts[\'title\'] must be a Title' );
 		}
 		$this->title = $opts['title'];
 		$this->revid = $opts['revid'] ?? null;
@@ -82,8 +94,8 @@ class PageConfig extends IPageConfig {
 	 */
 	private function mockPageContent( array $opts ) {
 		$this->page = [
-			'title' => $this->title,
-			'ns' => $opts['pagens'] ?? 0,
+			'title' => $this->title->getPrefixedText(),
+			'ns' => $this->title->getNamespace(),
 			'pageid' => -1,
 			'pagelanguage' => $opts['pageLanguage'] ?? 'en',
 			'pagelanguagedir' => $opts['pageLanguageDir'] ?? 'ltr',
@@ -110,7 +122,7 @@ class PageConfig extends IPageConfig {
 		if ( !empty( $this->revid ) ) {
 			$params['revids'] = $this->revid;
 		} else {
-			$params['titles'] = $this->title;
+			$params['titles'] = $this->title->getPrefixedDBKey();
 			$params['rvlimit'] = 1;
 		}
 
@@ -146,9 +158,11 @@ class PageConfig extends IPageConfig {
 	}
 
 	/** @inheritDoc */
-	public function getNs(): int {
+	public function getLinkTarget(): LinkTarget {
 		$this->loadData();
-		return $this->page['ns'];
+		return Title::newFromText(
+			$this->page['title'], $this->siteConfig, $this->page['ns']
+		);
 	}
 
 	/** @inheritDoc */

src/Config/Api/SiteConfig.php
@@ -13,6 +13,7 @@ use Wikimedia\Parsoid\DOM\Document;
 use Wikimedia\Parsoid\Mocks\MockMetrics;
 use Wikimedia\Parsoid\Utils\ConfigUtils;
 use Wikimedia\Parsoid\Utils\PHPUtils;
+use Wikimedia\Parsoid\Utils\Title;
 use Wikimedia\Parsoid\Utils\UrlUtils;
 use Wikimedia\Parsoid\Utils\Utils;
 
@@ -491,6 +492,11 @@ class SiteConfig extends ISiteConfig {
 	public function mainpage(): string {
 		$this->loadSiteData();
 		return $this->siteData['mainpage'];
+	}
+
+	public function mainPageLinkTarget(): Title {
+		$this->loadSiteData();
+		return Title::newFromText( $this->siteData['mainpage'], $this );
 	}
 
 	/** @inheritDoc */

src/Config/DataAccess.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace Wikimedia\Parsoid\Config;
 
 use Wikimedia\Parsoid\Core\ContentMetadataCollector;
+use Wikimedia\Parsoid\Core\LinkTarget;
 
 /**
  * MediaWiki data access abstract class for Parsoid
@@ -128,11 +129,11 @@ abstract class DataAccess {
 	 * @todo TemplateRequest also returns a bunch of other data, but seems to never use it except for
 	 *   TemplateRequest.setPageSrcInfo() which is replaced by PageConfig.
 	 * @param PageConfig $pageConfig
-	 * @param string $title Title of the page to fetch
+	 * @param string|LinkTarget $title Title of the page to fetch
 	 * @return PageContent|null
 	 */
 	abstract public function fetchTemplateSource(
-		PageConfig $pageConfig, string $title
+		PageConfig $pageConfig, $title
 	): ?PageContent;
 
 	/**
@@ -141,10 +142,10 @@ abstract class DataAccess {
 	 * This replaces TemplateDataRequest
 	 *
 	 * @param PageConfig $pageConfig
-	 * @param string $title
+	 * @param string|LinkTarget $title
 	 * @return array|null
 	 */
-	abstract public function fetchTemplateData( PageConfig $pageConfig, string $title ): ?array;
+	abstract public function fetchTemplateData( PageConfig $pageConfig, $title ): ?array;
 
 	/**
 	 * Log linter data.

src/Config/Env.php
@@ -585,21 +585,24 @@ class Env {
 		$str = trim( $str );
 
 		$pageConfig = $this->getPageConfig();
+		$title = Title::newFromLinkTarget(
+			$pageConfig->getLinkTarget(), $this->siteConfig
+		);
 
 		// Resolve lonely fragments (important if the current page is a subpage,
 		// otherwise the relative link will be wrong)
 		if ( $str !== '' && $str[0] === '#' ) {
-			return $pageConfig->getTitle() . $str;
+			return $title->getPrefixedText() . $str;
 		}
 
 		// Default return value
 		$titleKey = $str;
-		if ( $this->getSiteConfig()->namespaceHasSubpages( $pageConfig->getNs() ) ) {
+		if ( $this->getSiteConfig()->namespaceHasSubpages( $title->getNamespace() ) ) {
 			// Resolve subpages
 			$reNormalize = false;
 			if ( preg_match( '!^(?:\.\./)+!', $str, $relUp ) ) {
 				$levels = strlen( $relUp[0] ) / 3;  // Levels are indicated by '../'.
-				$titleBits = explode( '/', $pageConfig->getTitle() );
+				$titleBits = explode( '/', $title->getPrefixedText() );
 				if ( $titleBits[0] === '' ) {
 					// FIXME: Punt on subpages of titles starting with "/" for now
 					return $origName;
@@ -616,7 +619,7 @@ class Env {
 				$reNormalize = true;
 			} elseif ( $str !== '' && $str[0] === '/' ) {
 				// Resolve absolute subpage links
-				$str = $pageConfig->getTitle() . $str;
+				$str = $title->getPrefixedText() . $str;
 				$reNormalize = true;
 			}
 
@@ -680,7 +683,7 @@ class Env {
 	private function makeTitle( string $text, ?int $defaultNs = null, bool $noExceptions = false ): ?Title {
 		try {
 			if ( preg_match( '!^(?:[#/]|\.\./)!', $text ) ) {
-				$defaultNs = $this->getPageConfig()->getNs();
+				$defaultNs = $this->getPageConfig()->getLinkTarget()->getNamespace();
 			}
 			$text = $this->resolveTitle( $text );
 			return Title::newFromText( $text, $this->getSiteConfig(), $defaultNs );

src/Config/PageConfig.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 namespace Wikimedia\Parsoid\Config;
 
 use Wikimedia\Bcp47Code\Bcp47Code;
+use Wikimedia\Parsoid\Core\LinkTarget;
 
 /**
  * Page-level configuration interface for Parsoid
@@ -45,18 +46,43 @@ abstract class PageConfig {
 	/**
 	 * The page's title, as a string.
 	 * @return string With namespace, spaces not underscores
+	 * @deprecated Use ::getLinkTarget() instead; Title::getPrefixedText() will
+	 *  return the title in the same format as before.
 	 */
 	abstract public function getTitle(): string;
+
+	// At least one of ::getTitleObj() and ::getLinkTarget() must be defined
+
+	/**
+	 * The page's title, as a LinkTarget.
+	 * @return LinkTarget
+	 * @deprecated Renamed to ::getLinkTarget()
+	 */
+	public function getTitleObj(): LinkTarget {
+		return $this->getLinkTarget();
+	}
+
+	/**
+	 * The page's title, as a LinkTarget.
+	 * @return LinkTarget
+	 */
+	public function getLinkTarget(): LinkTarget {
+		// @phan-suppress-next-line PhanDeprecatedFunction
+		return $this->getTitleObj();
+	}
 
 	/**
 	 * The page's namespace ID
 	 * @return int
+	 * @deprecated Use ::getLinkTarget()->getNamespace() instead
 	 */
-	abstract public function getNs(): int;
+	public function getNs(): int {
+		return $this->getLinkTarget()->getNamespace();
+	}
 
 	/**
 	 * The page's ID, if any
-	 * @return int 0 if the page doesn't exist
+	 * @return int 0 if the page doesn't (yet?) exist
 	 */
 	abstract public function getPageId(): int;
 

commit 2c56ab00a167475b9c0d3d418a0498d036aae3ac

Unbreak bin/parse.php (broken in 0fed58bb)
* CI didn't catch this because bin/toolcheck.php.sh calls
  bin/parse.php with the --mock argument.

Change-Id: I0e58a3228efb90bbcd1417398b177a5e3fafd5f5

bin/parse.php
@@ -323,8 +323,8 @@ class Parse extends \Wikimedia\Parsoid\Tools\Maintenance {
 		}
 		$dataAccess = new DataAccess( $api, $siteConfig, $configOpts );
 		$this->siteConfig = $siteConfig;
-		$this->pageConfig = new PageConfig( $api, $configOpts + [
-			'title' => $siteConfig->mainpage(),
+		$this->pageConfig = new PageConfig( $api, $siteConfig, $configOpts + [
+			'title' => $siteConfig->mainPageLinkTarget(),
 			'loadData' => true,
 		] );
 		$this->metadata = new StubMetadataCollector();