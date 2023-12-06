8240502a67fd5590548660ed9f3afe22230db922
Subramanya Sastry <ssastry@wikimedia.org>
2023-12-05 23:28:46 -0600

    TableFixups: Fix a subtle regex bug
    * This bug was exposed by the followup patch where I tweaked
      the parser test as in this patch to eliminate a spurious
      wt2wt failure. But, that test tweak caused the test to fail
      spectacularly in both wt2html and wt2wt modes.
    
    * The regexp to determine the cell splitting point used a regexp
      which would sometimes swallow too much content and miss a
      cell-splitting opportunity.
    
    * While fixing the regexps, also DRY-ed out the code a bit.
    
    * Augmented the test to handle more edge cases to prevent future
      regressions.
    
    Change-Id: I119e8c6e480f8bd1cf13d4b77008f7711b0cbb73

--- src/Wt2Html/PP/Handlers/TableFixups.php
+++ src/Wt2Html/PP/Handlers/TableFixups.php
@@ -690,14 +690,17 @@ class TableFixups {
 				// FIXME: This skips over scenarios like <div>foo||bar</div>.
 				$cellName = DOMCompat::nodeName( $cell );
 				$hasSpanWrapper = !( $child instanceof Text );
-				$match = null;
+				$match = $match1 = $match2 = null;
 
+				// Find the first match of ||
+				preg_match( '/^((?:[^|]*(?:\|[^|])?)*)\|\|([^|].*)?$/D', $child->textContent, $match1 );
 				if ( $isTd ) {
-					preg_match( '/^(.*?[^|])?\|\|([^|].*)?$/D', $child->textContent, $match );
-				} else { /* cellName === 'th' */
-					// Find the first match of || or !!
-					preg_match( '/^(.*?[^|])?\|\|([^|].*)?$/D', $child->textContent, $match1 );
-					preg_match( '/^(.*?[^!])?\!\!([^!].*)?$/D', $child->textContent, $match2 );
+					$match = $match1;
+				} else {
+					// Find the first match !!
+					preg_match( '/^((?:[^!]*(?:\![^!])?)*)\!\!([^!].*)?$/D', $child->textContent, $match2 );
+
+					// Pick the shortest match
 					if ( $match1 && $match2 ) {
 						$match = strlen( $match1[1] ?? '' ) < strlen( $match2[1] ?? '' )
 							? $match1
