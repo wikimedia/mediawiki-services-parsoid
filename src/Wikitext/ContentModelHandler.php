9682ab3abeed302415dc8854b8dabda192af54f3
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2024-04-11 13:42:20 +0200

    Process indicators as first nodes of templates
    Now that we're not clobbering the data-mw anymore on template handling,
    we can remove the guard on "mw:Transclusion" so that we actually add
    indicators to the page.
    
    Bug: T359483
    Change-Id: I8651c9c5ebdea516123ad528c14ab318eac0b540

--- src/Wikitext/ContentModelHandler.php
+++ src/Wikitext/ContentModelHandler.php
@@ -15,7 +15,6 @@ use Wikimedia\Parsoid\Html2Wt\WikitextSerializer;
 use Wikimedia\Parsoid\Utils\ContentUtils;
 use Wikimedia\Parsoid\Utils\DOMCompat;
 use Wikimedia\Parsoid\Utils\DOMDataUtils;
-use Wikimedia\Parsoid\Utils\DOMUtils;
 use Wikimedia\Parsoid\Utils\Timing;
 
 class ContentModelHandler extends IContentModelHandler {
@@ -136,12 +135,10 @@ class ContentModelHandler extends IContentModelHandler {
 		// Given that, the for-loop below implements "last-one-wins" semantics
 		// for indicators that use the same name key.
 		foreach ( $indicators as $meta ) {
-			if ( !DOMUtils::hasTypeOf( $meta, "mw:Transclusion" ) ) {
-				// Since the DOM is in "stored" state, we have to reparse data-mw here.
-				$dmw = DOMDataUtils::getJSONAttribute( $meta, 'data-mw', null );
-				$name = $dmw->attrs->name;
-				$iData[$name] = $dmw->html;
-			}
+			// Since the DOM is in "stored" state, we have to reparse data-mw here.
+			$dmw = DOMDataUtils::getJSONAttribute( $meta, 'data-mw', null );
+			$name = $dmw->attrs->name;
+			$iData[$name] = $dmw->html;
 		}
 
 		// set indicator metadata for unique keys
