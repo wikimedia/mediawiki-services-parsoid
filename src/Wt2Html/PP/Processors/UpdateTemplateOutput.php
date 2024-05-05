e5b1209a7b39266bb67dc0b86dba1fe2508779cc
Subramanya Sastry <ssastry@wikimedia.org>
2024-05-05 14:30:19 +0300

    Start plugging in selective parsing logic
    Change-Id: Ie001ee9da6d1aed3648bbf96a9e18c1ad7295dd8

--- src/Wt2Html/PP/Processors/UpdateTemplateOutput.php
+++ src/Wt2Html/PP/Processors/UpdateTemplateOutput.php
@@ -5,8 +5,12 @@ namespace Wikimedia\Parsoid\Wt2Html\PP\Processors;
 
 use Wikimedia\Parsoid\Config\Env;
 use Wikimedia\Parsoid\DOM\DocumentFragment;
+use Wikimedia\Parsoid\Utils\DOMCompat;
+use Wikimedia\Parsoid\Utils\DOMDataUtils;
+use Wikimedia\Parsoid\Utils\DOMUtils;
 use Wikimedia\Parsoid\DOM\Element;
 use Wikimedia\Parsoid\DOM\Node;
+use Wikimedia\Parsoid\Utils\PipelineUtils;
 use Wikimedia\Parsoid\Wt2Html\Wt2HtmlDOMProcessor;
 
 class UpdateTemplateOutput implements Wt2HtmlDOMProcessor {
@@ -17,5 +21,31 @@ class UpdateTemplateOutput implements Wt2HtmlDOMProcessor {
 		Env $env, Node $root, array $options = [], bool $atTopLevel = false
 	): void {
 		'@phan-var Element|DocumentFragment $root';  // @var Element|DocumentFragment $root
+
+		$selparData = $options['selparData'] ?? null;
+		if ( !$selparData ) {
+			error_log("Missing selpar data");
+			return;
+		}
+
+
+		// Hardcode for english
+		$tplTitle = "./Template:" . $selparData->templateTitle;
+		$tplNodes = DOMCompat::querySelectorAll( $root, '[typeof~="mw:Transclusion"]');
+		foreach ( $tplNodes as $tplNode ) {
+			$dataMw = DOMDataUtils::getDataMW( $tplNode );
+			if ( $dataMw->parts[0]->template->target->href === $tplTitle ) {
+				// we found it!
+				$dp = DOMDataUtils::getDataParsoid( $tplNode );
+				$wt = $dp->dsr->substr( $selparData->revText );
+				$opts = [
+					'pipelineType' => 'wikitext-to-dom',
+					'sol' => false,
+					'pipelineOpts' => []
+	 			];
+				$frag  = PipelineUtils::processContentInPipeline( $env, $options['frame'], $wt, $opts );
+				error_log("Frag: " . DOMUtils::getFragmentInnerHTML( $frag ) );
+			}
+		}
 	}
 }
