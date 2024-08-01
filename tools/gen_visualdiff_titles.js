97916b3971db5a46326603d5b7322762a5117f04
C. Scott Ananian <cananian@wikimedia.org>
2024-08-01 01:19:32 -0400

    Improve tools/gen_visualdiff_titles.js
    The script now handles the output format of the "top views" tool
    as well as quarry output.  See
    https://www.mediawiki.org/wiki/Parsing/Visual_Diff_Testing#Generating_new_title_lists
    for more usage information.
    
    Change-Id: I9e056be48c09e3d453accc266f3e5c0c415c415e

--- tools/gen_visualdiff_titles.js
+++ tools/gen_visualdiff_titles.js
@@ -1,8 +1,11 @@
 /* ----------------------------------------------------------------------------------------
  * - Run this query in quarry:
- *   select page_title,page_namespace from page where page_is_redirect=0 and mod(page_namespace,2) = 1;
+ *   Main namespace titles:
+ *     select page_title,page_namespace from page where page_is_redirect=0 and page_namespace = 0;
+ *   Talk pages (for discussion tools):
+ *     select page_title,page_namespace from page where page_is_redirect=0 and mod(page_namespace,2) = 1;
  * - Download those results as a json file in $FILE
- * - Run this as node tools/gen.titles.js $WIKIPREFIX $FILE $COUNT (however many entries you want)
+ * - Run this as node tools/gen_visualdiff_titles.js $WIKIPREFIX $FILE $COUNT (however many entries you want)
  *   and dump the output in a sql file.
  * - Import to the visual diff server and update the test database.
  * - Run visual diff tests and profit!
@@ -29,25 +32,31 @@ const numEntries = Number(process.argv[4]);
 const data =
 	JSON.parse(
 		require('fs').readFileSync(file, "utf8")
-	)
-	.rows
+	);
+const rows = (Array.isArray(data) ? data : data.rows)
 	.sort(function(a,b) {
-		return 0.5 - Math.random();
+		if (Array.isArray(a) && Array.isArray(b)) {
+			return 0.5 - Math.random();
+		}
+		return b.views - a.views;
 	})
 	.map(function(e) {
-		const nsId = Number(e[1]);
-		let ns;
-		if (nsId % 2 === 0) {
-			ns = nsMap[String(nsId)];
-			ns = ns ? ns + ":" : "";
-		} else {
-			ns = nsMap[String(nsId - 1)];
-			ns = ns + (ns ? "_" : "") + "Talk:";
+		if (Array.isArray(e)) {
+			const nsId = Number(e[1]);
+			let ns;
+			if (nsId % 2 === 0) {
+				ns = nsMap[String(nsId)];
+				ns = ns ? ns + ":" : "";
+			} else {
+				ns = nsMap[String(nsId - 1)];
+				ns = ns + (ns ? "_" : "") + "Talk:";
+			}
+			return ns + e[0];
 		}
-		return ns + e[0];
+		return e.article;
 	})
 	.slice(0,Number(numEntries))
 	.map(function(e) {
 		return `INSERT IGNORE INTO pages(prefix, title) VALUES("${ wikiPrefix }", "${ e.replace(/"/g, '\\"') }");`;
 	});
-console.log(data.join("\n"));
+console.log(rows.join("\n"));
