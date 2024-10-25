ed4ebeebc0d40a741c06b67c2a81f656d17aa440
Arlo Breault <abreault@wikimedia.org>
2024-10-25 18:26:12 -0400

    More selective update testing
    A few more cases and refactors a bit to allow for testing of different
    variations of selective updates.
    
    Bug: T363421
    Follows-Up: I9ad7e80fd884574981fe37a5269acb61bbc39457
    Change-Id: Idf80b50d53a0a2d718445855bd5cfdc479f9937a

--- tests/phpunit/Parsoid/SelectiveUpdateTest.php
+++ tests/phpunit/Parsoid/SelectiveUpdateTest.php
@@ -20,17 +20,22 @@ class SelectiveUpdateTest extends \PHPUnit\Framework\TestCase {
 	 * @dataProvider provideSelectiveUpdate
 	 */
 	public function testSelectiveUpdate(
-		$mode, $editedtemplatetitle, $revText, $revHTML, $updatedHTML, $parserOpts
+		string $mode, array $updateData, string $expectedHTML
 	) {
-		$revData = new SelectiveUpdateData( $revText, $revHTML, $mode );
-		$revData->templateTitle = $editedtemplatetitle;
+		$revData = new SelectiveUpdateData(
+			$updateData['revText'], $updateData['revHTML'], $mode
+		);
+
+		if ( $mode === 'template' ) {
+			$revData->templateTitle = $updateData['editedtemplatetitle'];
+		}
 
 		$opts = [];
 		$siteConfig = new MockSiteConfig( $opts );
 
 		$dataAccess = $this->createMock( MockDataAccess::class );
 		$dataAccess
-			->expects( $this->once() )
+			->expects( $this->exactly( $updateData['preprocessCalls'] ?? 0 ) )
 			->method( 'preprocessWikitext' )
 			->willReturnCallback( static function (
 				MockPageConfig $pageConfig, ContentMetadataCollector $metadata,
@@ -41,11 +46,16 @@ class SelectiveUpdateTest extends \PHPUnit\Framework\TestCase {
 			} );
 
 		$parsoid = new Parsoid( $siteConfig, $dataAccess );
-		$pageContent = new MockPageContent( [ 'main' => $revText ] );
+		$pageContent = new MockPageContent( [ 'main' => $revData->revText ] );
 		$pageConfig = new MockPageConfig( $siteConfig, $opts, $pageContent );
 
+		$parserOpts = [
+			'body_only' => true,
+			'wrapSections' => false,
+		];
+
 		$out = $parsoid->wikitext2html( $pageConfig, $parserOpts, $header, null, $revData );
-		$this->assertEquals( $updatedHTML, $out );
+		$this->assertEquals( $expectedHTML, $out );
 	}
 
 	public function provideSelectiveUpdate() {
@@ -53,14 +63,33 @@ class SelectiveUpdateTest extends \PHPUnit\Framework\TestCase {
 		return [
 			[
 				'template',
-				'1x',
-				'testing {{1x|123}} does not update {{2x|456}}',
-				'<p data-parsoid=\'{"dsr":[0,45,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123</span> does not update <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[35,45,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span></p>',
-				'<p data-parsoid=\'{"dsr":[0,45,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123123</span> does not update <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[35,45,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span></p>',
 				[
-					'body_only' => true,
-					'wrapSections' => false,
+					'editedtemplatetitle' => '1x',
+					'revText' => '{{2x|456}} does not update',
+					'preprocessCalls' => 0,
+					'revHTML' => '<p data-parsoid=\'{"dsr":[0,26,0,0]}\'><span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[0,10,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span> does not update</p>',
 				],
+				'<p data-parsoid=\'{"dsr":[0,26,0,0]}\'><span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[0,10,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span> does not update</p>',
+			],
+			[
+				'template',
+				[
+					'editedtemplatetitle' => '1x',
+					'revText' => 'testing {{1x|123}} does not update {{2x|456}}',
+					'preprocessCalls' => 1,
+					'revHTML' => '<p data-parsoid=\'{"dsr":[0,45,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123</span> does not update <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[35,45,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span></p>',
+				],
+				'<p data-parsoid=\'{"dsr":[0,45,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123123</span> does not update <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[35,45,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"2x","href":"./Template:2x"},"params":{"1":{"wt":"456"}},"i":0}}]}\'>456456</span></p>',
+			],
+			[
+				'template',
+				[
+					'editedtemplatetitle' => '1x',
+					'preprocessCalls' => 2,
+					'revText' => 'testing {{1x|123}} and {{1x|hiho}}',
+					'revHTML' => '<p data-parsoid=\'{"dsr":[0,34,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123</span> and <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[23,34,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"hiho"}},"i":0}}]}\'>hiho</span></p>',
+				],
+				'<p data-parsoid=\'{"dsr":[0,34,0,0]}\'>testing <span about="#mwt1" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[8,18,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"123"}},"i":0}}]}\'>123123</span> and <span about="#mwt2" typeof="mw:Transclusion" data-parsoid=\'{"pi":[[{"k":"1"}]],"dsr":[23,34,null,null]}\' data-mw=\'{"parts":[{"template":{"target":{"wt":"1x","href":"./Template:1x"},"params":{"1":{"wt":"hiho"}},"i":0}}]}\'>hihohiho</span></p>',
 			],
 		];
 		// phpcs:enable Generic.Files.LineLength.TooLong
