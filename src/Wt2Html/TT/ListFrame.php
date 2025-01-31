wikimedia/mediawiki-services-parsoid

commit ef86d93bc8848e89692b1972255bfb869df5ffc2

ListHandler: Handle edge case in dt-dd lists
* Generate listitem tokens for all ":" characters in
  dtdd lists rather than just the "first" one. It is not
  possble to detect the first one reliably in the PEG tokenizer
  because tags added during transforms (ex: quote tags) can
  disable the listitem. By generating listitem tokens for all
  ":" characters, after the first ":" listitem is processed as
  a <dd> list item, the later ones on the same line can be
  converted to ":" characters.

* Fixes the test case from the bug report.

* Fixes a previously failing dldt test which is equivalent
  to the failing test in the bug report!

* Updated known failures to reflect the change.

Bug: T363599
Change-Id: I92f8001f379a8d172ea2e150d86c06219b951afe

src/Wt2Html/TT/ListFrame.php
@@ -19,18 +19,16 @@ class ListFrame {
 	 * NlTk that triggered atEOL
 	 */
 	public ?NlTk $nlTk = null;
-	/** @var array */
-	public $solTokens = [];
+	public array $solTokens = [];
 	/**
 	 * Bullet stack, previous element's listStyle
-	 * @var array
 	 */
-	public $bstack = [];
+	public array $bstack = [];
 	/**
 	 * Stack of end tags
 	 * @var array<EndTagTk>
 	 */
-	public $endtags = [];
+	public array $endtags = [];
 	/**
 	 * Partial DOM building heuristic:
 	 * Number of open block tags encountered within list context.
@@ -40,4 +38,11 @@ class ListFrame {
 	 * Number of open tags encountered within list context.
 	 */
 	public int $numOpenTags = 0;
+
+	/**
+	 * Did we generate a <dd> already on this line?
+	 * Used to convert extra : listitems to ":" instead of extra <dl>s.
+	 * Gets reset on encountering a NlTk or a ; listitem.
+	 */
+	public bool $haveDD = false;
 }

src/Wt2Html/TT/ListHandler.php
@@ -169,6 +169,7 @@ class ListHandler extends TokenHandler {
 		if ( $token instanceof NlTk ) {
 			$this->currListFrame->atEOL = true;
 			$this->currListFrame->nlTk = $token;
+			$this->currListFrame->haveDD = false;
 			// php's findColonNoLinks is run in doBlockLevels, which examines
 			// the text line-by-line. At nltk, any open tags will cease having
 			// an effect.
@@ -255,7 +256,7 @@ class ListHandler extends TokenHandler {
 				// Ignoring colons inside tags to prevent illegal overlapping.
 				// Attempts to mimic findColonNoLinks in the php parser.
 				if ( PHPUtils::lastItem( $bullets ) === ':'
-					&& $this->currListFrame->numOpenTags > 0
+					&& ( $this->currListFrame->haveDD || $this->currListFrame->numOpenTags > 0 )
 				) {
 					$this->env->log( 'trace/list', $this->pipelineId,
 						'ANY:', static function () use ( $token ) { return PHPUtils::jsonEncode( $token );
@@ -304,6 +305,12 @@ class ListHandler extends TokenHandler {
 	private function pushList( array $container, DataParsoid $dp1, DataParsoid $dp2 ): array {
 		$this->currListFrame->endtags[] = new EndTagTk( $container['list'] );
 		$this->currListFrame->endtags[] = new EndTagTk( $container['item'] );
+
+		if ( $container['item'] === 'dd' ) {
+			$this->currListFrame->haveDD = true;
+		} elseif ( $container['item'] === 'dt' ) {
+			$this->currListFrame->haveDD = false; // reset
+		}
 
 		return [
 			new TagTk( $container['list'], [], $dp1 ),
@@ -429,6 +436,11 @@ class ListHandler extends TokenHandler {
 				$tokens = array_merge( $this->currListFrame->solTokens, $tokens );
 				$newName = self::$bullet_chars_map[$bn[$prefixLen]]['item'];
 				$endTag = array_pop( $this->currListFrame->endtags );
+				if ( $newName === 'dd' ) {
+					$this->currListFrame->haveDD = true;
+				} elseif ( $newName === 'dt' ) {
+					$this->currListFrame->haveDD = false; // reset
+				}
 				$this->currListFrame->endtags[] = new EndTagTk( $newName );
 
 				$newTag = null;

src/Wt2Html/Grammar.pegphp
@@ -1910,13 +1910,18 @@ hacky_dl_uses =
 	return TokenizerUtils::flattenIfArray( [ $li, $tbl, $line ?: [] ] );
 }
 
+dtdd_colon = c:inlineline_break_on_colon? cpos:(":" { return $this->endOffset(); })
+{
+	return [ $c, $cpos ];
+}
+
 dtdd =
 	bullets:(!(";" !list_char) lc:list_char { return $lc; })*
 	";"
-	c:inlineline_break_on_colon?
-	cpos:(":" { return $this->endOffset(); })
+	colons:dtdd_colon*
 	d:inlineline?
 	&eolf {
+		$bulletToks = [];
 		// Leave bullets as an array -- list handler expects this
 		// TSR: +1 for the leading ";"
 		$numBullets = count( $bullets ) + 1;
@@ -1926,17 +1931,26 @@ dtdd =
 		$li1Bullets[] = ';';
 		$dp = new DataParsoid;
 		$dp->tsr = $tsr;
-		$li1 = new TagTk( 'listItem', [ new KV( 'bullets', $li1Bullets, $tsr->expandTsrV() ) ], $dp );
-		// TSR: -1 for the intermediate ":"
-		$li2Bullets = $bullets;
-		$li2Bullets[] = ':';
-		$tsr2 = new SourceRange( $cpos - 1, $cpos );
-		$dp2 = new DataParsoid;
-		$dp2->tsr = $tsr2;
-		$dp2->stx = 'row';
-		$li2 = new TagTk( 'listItem', [ new KV( 'bullets', $li2Bullets, $tsr2->expandTsrV() ) ], $dp2 );
+		$bulletToks[] = new TagTk( 'listItem', [ new KV( 'bullets', $li1Bullets, $tsr->expandTsrV() ) ], $dp );
+		foreach ( $colons as $colon) {
+			if ( $colon[0] ) { // can be null because of "?" in dtdd_colon
+				$bulletToks[] = $colon[0];
+			}
+			$cpos = $colon[1];
+			// TSR: -1 for the intermediate ":"
+			$li2Bullets = $bullets;
+			$li2Bullets[] = ':';
+			$tsr2 = new SourceRange( $cpos - 1, $cpos );
+			$dp2 = new DataParsoid;
+			$dp2->tsr = $tsr2;
+			$dp2->stx = 'row';
+			$bulletToks[] = new TagTk( 'listItem', [ new KV( 'bullets', $li2Bullets, $tsr2->expandTsrV() ) ], $dp2 );
+		}
 
-		return array_merge( [ $li1 ], $c ?: [], [ $li2 ], $d ?: [] );
+		if ( $d ) {
+			$bulletToks = array_merge( $bulletToks, $d );
+		}
+		return $bulletToks;
 	}
 
 list_char = [*#:;]

commit 66674217c825cb3830de153d221807d52b90a21c

ListHandler: Fix dl-list breakage from ef86d93bc
* I overreached in that commit - the PEG grammar changes were
  sufficient. The changes in this patch don't break the tests
  added in that patch nor the test case referenced in T363599.
* This fixes a regression seen in production from recent changes
  in 3ec682e7.
* The new test result change is "correct" (in that the original
  output itself differed from legacy -- not sure why I/we added
  these tests in the first place).

Change-Id: If905fb28f175c77c51616e9d018bca60064812c8

src/Wt2Html/TT/ListFrame.php
@@ -38,11 +38,4 @@ class ListFrame {
 	 * Number of open tags encountered within list context.
 	 */
 	public int $numOpenTags = 0;
-
-	/**
-	 * Did we generate a <dd> already on this line?
-	 * Used to convert extra : listitems to ":" instead of extra <dl>s.
-	 * Gets reset on encountering a NlTk or a ; listitem.
-	 */
-	public bool $haveDD = false;
 }

src/Wt2Html/TT/ListHandler.php
@@ -169,7 +169,6 @@ class ListHandler extends TokenHandler {
 		if ( $token instanceof NlTk ) {
 			$this->currListFrame->atEOL = true;
 			$this->currListFrame->nlTk = $token;
-			$this->currListFrame->haveDD = false;
 			// php's findColonNoLinks is run in doBlockLevels, which examines
 			// the text line-by-line. At nltk, any open tags will cease having
 			// an effect.
@@ -256,7 +255,7 @@ class ListHandler extends TokenHandler {
 				// Ignoring colons inside tags to prevent illegal overlapping.
 				// Attempts to mimic findColonNoLinks in the php parser.
 				if ( PHPUtils::lastItem( $bullets ) === ':'
-					&& ( $this->currListFrame->haveDD || $this->currListFrame->numOpenTags > 0 )
+					&& ( $this->currListFrame->numOpenTags > 0 )
 				) {
 					$this->env->log( 'trace/list', $this->pipelineId,
 						'ANY:', static function () use ( $token ) { return PHPUtils::jsonEncode( $token );
@@ -305,12 +304,6 @@ class ListHandler extends TokenHandler {
 	private function pushList( array $container, DataParsoid $dp1, DataParsoid $dp2 ): array {
 		$this->currListFrame->endtags[] = new EndTagTk( $container['list'] );
 		$this->currListFrame->endtags[] = new EndTagTk( $container['item'] );
-
-		if ( $container['item'] === 'dd' ) {
-			$this->currListFrame->haveDD = true;
-		} elseif ( $container['item'] === 'dt' ) {
-			$this->currListFrame->haveDD = false; // reset
-		}
 
 		return [
 			new TagTk( $container['list'], [], $dp1 ),
@@ -436,11 +429,6 @@ class ListHandler extends TokenHandler {
 				$tokens = array_merge( $this->currListFrame->solTokens, $tokens );
 				$newName = self::$bullet_chars_map[$bn[$prefixLen]]['item'];
 				$endTag = array_pop( $this->currListFrame->endtags );
-				if ( $newName === 'dd' ) {
-					$this->currListFrame->haveDD = true;
-				} elseif ( $newName === 'dt' ) {
-					$this->currListFrame->haveDD = false; // reset
-				}
 				$this->currListFrame->endtags[] = new EndTagTk( $newName );
 
 				$newTag = null;