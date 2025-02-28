a3da3f1ef1c921ea9daae484431439bf1fa22555
Arlo Breault <abreault@wikimedia.org>
2025-02-28 13:45:25 -0500

    Remove pipeTrick from dp
    Hasn't been set since 07e8ced2.
    
    Change-Id: I912074cbbc26bab76045343b4d16f9921ce72481

--- src/NodeData/DataParsoid.php
+++ src/NodeData/DataParsoid.php
@@ -183,11 +183,6 @@ use Wikimedia\Parsoid\Utils\Utils;
  * Porting note: this can be '0', handle emptiness checks with care
  * @property string|null $prefix
  *
- * True if the link was a pipetrick (`[[Foo|]]`).
- * @note This will likely be removed soon since this should not show up in saved wikitext since
- * this is a pre-save transformation trick.
- * @property bool|null $pipeTrick
- *
  * Did the link use interwiki syntax?
  * Probably redundant with the rel=mw:WikiLink/Interwiki
  * @property bool|null $isIW
