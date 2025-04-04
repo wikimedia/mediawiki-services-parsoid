6d1f96a6e9b4c625f0533692906d2a877d63572e
C. Scott Ananian <cananian@wikimedia.org>
2025-04-04 10:18:04 -0400

    Improve documentation for Ext\ExtensionModule
    In particular, document that actually extending ExtensionModule
    should only be done internally to Parsoid (or for "Parsoid standalone
    modules", if such a thing ever exists); an extension should use its
    extension.json to specify the module configuration.
    
    Change-Id: Ic1d5b307ed0f1c251778a8027f40189529798352

--- src/Ext/ExtensionModule.php
+++ src/Ext/ExtensionModule.php
@@ -10,15 +10,24 @@ namespace Wikimedia\Parsoid\Ext;
  * registered object.  The only method required is `getConfig`.
  *
  * An ExtensionModule can be created on-demand from configuration data
- * specified in extension.json; see SiteConfig::registerExtensionModule().
+ * specified in extension.json; see SiteConfig::registerExtensionModule()
+ * and
+ * https://www.mediawiki.org/wiki/Manual:Extension.json/Schema#ParsoidModules
+ *
+ * Implementing an ExtensionModule should only be done by Parsoid-internal
+ * extensions.  If you are implementing a Parsoid module in an extension
+ * and have an `extension.json`, you should use that to specify your
+ * module configuration.
  */
 interface ExtensionModule {
 
 	/**
 	 * Return information about this extension module.
-	 * FIXME: Add more expected fields or create a class for this
-	 * FIXME: The 'name' is expected to be the same as the name defined
-	 * at the top level of extension.json.
+	 *
+	 * The structure of the return value is enforced by
+	 * `moduleconfig.schema.json`, in this directory.
+	 *
+	 * @see https://www.mediawiki.org/wiki/Parsoid/Internals/Module_Configuration_Schema
 	 * @return array{name:string}
 	 */
 	public function getConfig(): array;
