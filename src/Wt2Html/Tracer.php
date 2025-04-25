456d4e1e8ee29fa92ca5b5876a2dc2949741e2bc
Arlo Breault <abreault@wikimedia.org>
2025-04-25 16:36:41 -0400

    Bump wikipeg to 5.0.0 and remove redundant code in Tracer
    Depends-On: I5752ce18a84c11c56ad00531e9f2a3a2679d6f8e
    Change-Id: Idc197f2452d309f332cd52a4f50b02a9d1fccbcb

--- src/Wt2Html/Tracer.php
+++ src/Wt2Html/Tracer.php
@@ -3,15 +3,9 @@ declare( strict_types = 1 );
 
 namespace Wikimedia\Parsoid\Wt2Html;
 
-use InvalidArgumentException;
-use Wikimedia\WikiPEG\Tracer as ITracer;
+use Wikimedia\WikiPEG\DefaultTracer;
 
-/**
- * FIXME: DefaultTracer should make some things protected
- */
-class Tracer implements ITracer {
-	/** @var int */
-	private $indentLevel = 0;
+class Tracer extends DefaultTracer {
 	private string $text;
 
 	public function __construct( string $text ) {
@@ -21,32 +15,7 @@ class Tracer implements ITracer {
 	/**
 	 * @param array $event
 	 */
-	public function trace( $event ): void {
-		switch ( $event['type'] ) {
-			case 'rule.enter':
-				$this->log( $event );
-				$this->indentLevel++;
-				break;
-
-			case 'rule.match':
-				$this->indentLevel--;
-				$this->log( $event );
-				break;
-
-			case 'rule.fail':
-				$this->indentLevel--;
-				$this->log( $event );
-				break;
-
-			default:
-				throw new InvalidArgumentException( "Invalid event type {$event['type']}" );
-		}
-	}
-
-	/**
-	 * @param array $event
-	 */
-	private function log( $event ) {
+	protected function log( $event ) {
 		$offset = $event['location']->start->offset;
 		print str_pad(
 			'' . $event['location'],
@@ -57,40 +26,5 @@ class Tracer implements ITracer {
 			. str_repeat( ' ', $this->indentLevel ) . $event['rule']
 			. $this->formatArgs( $event['args'] ?? null )
 			. "\n";
-	}
-
-	/**
-	 * @param ?array $argMap
-	 * @return string
-	 */
-	private function formatArgs( $argMap ) {
-		if ( !$argMap ) {
-			return '';
-		}
-
-		$argParts = [];
-		foreach ( $argMap as $argName => $argValue ) {
-			if ( $argName === '$silence' ) {
-				continue;
-			}
-			if ( $argName === '$boolParams' ) {
-				$argParts[] = '0x' . base_convert( (string)$argValue, 10, 16 );
-			} else {
-				$displayName = str_replace( '$param_', '', $argName );
-				if ( $displayName[0] === '&' ) {
-					$displayName = substr( $displayName, 1 );
-					$ref = '&';
-				} else {
-					$ref = '';
-				}
-				$argParts[] = "$displayName=$ref" .
-						   json_encode( $argValue, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-			}
-		}
-		if ( $argParts ) {
-			return '<' . implode( ', ', $argParts ) . '>';
-		} else {
-			return '';
-		}
 	}
 }
