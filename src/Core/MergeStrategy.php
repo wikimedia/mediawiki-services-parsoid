5ad99a5d73e9fd40dd0c0ad90d75587165267eb7
C. Scott Ananian <cananian@wikimedia.org>
2025-09-02 15:25:44 -0400

    Add MergeStrategy enumeration
    This will be used by ContentMetadataCollector and ParserOutput in core,
    in ::appendExtensionData() and ::appendJsConfigVar().
    
    Bug: T403621
    Change-Id: I4c93f8490429660c60820b800a5dc30df1f6f3dd

---
+++ src/Core/MergeStrategy.php
@@ -0,0 +1,26 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Wikimedia\Parsoid\Core;
+
+/**
+ * Merge strategies to use for ContentMetadataCollectors.
+ *
+ * Strategies should be order-independent, so that portions of the
+ * final metadata can be generated and combined in any order.
+ */
+enum MergeStrategy: string {
+
+	/**
+	 * "Union" merge strategy means that values are strings, stored as
+	 * a set, and exposed as a PHP associative array mapping from
+	 * values to `true`.
+	 */
+	case UNION = 'union';
+
+	/**
+	 * "Counter" merge strategy means that values are integers and
+	 * are summed to make the final ParserOutput.
+	 */
+	case COUNTER = 'counter';
+}
