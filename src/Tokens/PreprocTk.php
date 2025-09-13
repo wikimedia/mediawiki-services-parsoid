5c5870650a31b72bbb1a775cbec8b681d888bb6b
C. Scott Ananian <cananian@wikimedia.org>
2025-09-13 18:23:28 -0400

    Add PreprocTk::splitContentsBy()
    This method will allow us to easily split parser functions and
    transclusion arguments when they are represented in PreprocTk form
    (or the KV which is the 'contents' array of a PreprocTk token).
    This patch only adds the new helper and test cases for it.
    It is used in Ibf3fc177713ec487bbace32a4b14b5a103358b5e and
    I1c78a6d67e63133bdd338d64e62ca2b19374762a.
    
    Bug: T394834
    Change-Id: I7c13a830d596755829b3abc1357f9e9dbd410ef2

--- src/Tokens/PreprocTk.php
+++ src/Tokens/PreprocTk.php
@@ -130,6 +130,103 @@ class PreprocTk extends Token {
 	}
 
 	/**
+	 * Split a contents KV by a string or a function.
+	 * @param string|list<string>|callable(string,int):array $sep Either a
+	 *   separator string, or a list of separator alternatives, or else a
+	 *   function compatible with `preg_split` with the
+	 *   PREG_SPLIT_DELIM_CAPTURE flag; that is, returns a list of
+	 *   strings with the "splitter" string in odd indexes.
+	 * @param KV $contents A contents KV, containing a `list<string|PreprocTk>`
+	 * @param int $limit A split limit, or -1 for "no limit"
+	 *   This is the *number of splits* permitted; the return array length
+	 *   will be `1 + 2*$limit`.
+	 * @return list<KV> a list of contents KVs, with the "splitter"
+	 *   strings in odd indexes.
+	 */
+	public static function splitContentsBy(
+		string|array|callable $sep,
+		KV $contents,
+		int $limit = -1
+	): array {
+		if ( is_string( $sep ) ) {
+			$sep = [ $sep ];
+		}
+		if ( is_array( $sep ) ) {
+			$sep = array_map( static fn ( $s )=>preg_quote( $s, '/' ), $sep );
+			// Create a splitter function from the given string.
+			$patt = '/(' . implode( '|', $sep ) . ')/';
+			// PHP's accounting of split limit is "number of elements in the
+			// result array, not counting the delimiters we capture", with
+			// the result that it appears off-by-one compared to our definition
+			$func = static fn ( $item, $limit ) =>
+				preg_split( $patt, $item, 1 + $limit, PREG_SPLIT_DELIM_CAPTURE )
+				?: [ $item ];
+		} else {
+			$func = $sep;
+		}
+		$result = [];
+		$start = $contents->srcOffsets->value->start;
+		$source = $contents->srcOffsets->value->source;
+		// @phan-suppress-next-line PhanTypeSuspiciousNonTraversableForeach
+		foreach ( $contents->v as $item ) {
+			if ( $item instanceof PreprocTk ) {
+				$sr = $item->dataParsoid->tsr;
+				$newItems = [
+					self::newContentsKV( [ $item ], $sr )
+				];
+				$start = $sr->end;
+				$source = $sr->source;
+			} else {
+				$splitResult = [ $item ];
+				if ( $limit === -1 || $limit > 0 ) {
+					$splitResult = $func( $item, $limit );
+				}
+				// Adjust limit
+				if ( $limit > 0 ) {
+					$limit -= ( count( $splitResult ) - 1 ) / 2;
+				}
+				// Convert all the strings to KVs.
+				$newItems = [];
+				foreach ( $splitResult as $piece ) {
+					$sr = new SourceRange( $start, $start + strlen( $piece ), $source );
+					$newItems[] = self::newContentsKV( [ $piece ], $sr );
+					$start = $sr->end;
+				}
+			}
+			// If necessary, merge first of new items with last of result
+			if ( $result ) {
+				[ $last, $first ] = [ array_pop( $result ), $newItems[0] ];
+				$sr = new SourceRange(
+					$last->srcOffsets->value->start,
+					$first->srcOffsets->value->end,
+					$last->srcOffsets->value->source
+				);
+				// To avoid O(N^2) append, mutate last KV in-place.
+				// (All KVs in $result were created inside this function.)
+				// @phan-suppress-next-line PhanTypeSuspiciousNonTraversableForeach
+				foreach ( $first->v as $v ) {
+					// Don't add needless array entries for ''
+					if ( $v !== '' ) {
+						if ( $last->v === [ '' ] ) {
+							$last->v = [ $v ];
+						} else {
+							$last->v[] = $v;
+						}
+					}
+				}
+				$last->srcOffsets = $sr->expandTsrV();
+				$newItems[0] = $last;
+			}
+			array_push( $result, ...$newItems );
+		}
+		// Ensure there's at least one element in the result
+		if ( count( $result ) === 0 ) {
+			$result[] = self::newContentsKV( [], $contents->srcOffsets->value );
+		}
+		return $result;
+	}
+
+	/**
 	 * Pretty-print a PreprocTk token.
 	 * @param bool $pretty if true (default) the output will be on multiple
 	 *  lines.  If false, the string result should be identical to the
---
+++ tests/phpunit/Parsoid/Tokens/PreprocTkTest.php
@@ -0,0 +1,149 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Test\Parsoid\Tokens;
+
+use Wikimedia\Parsoid\Mocks\MockEnv;
+use Wikimedia\Parsoid\Tokens\PreprocTk;
+use Wikimedia\Parsoid\Utils\PipelineUtils;
+use Wikimedia\Parsoid\Wt2Html\PegTokenizer;
+
+/**
+ * Test the methods in PreprocTk.
+ * @coversDefaultClass \Wikimedia\Parsoid\Tokens\PreprocTk
+ */
+class PreprocTkTest extends \PHPUnit\Framework\TestCase {
+	/**
+	 * @dataProvider provideSplitContentsBy
+	 * @covers ::splitContentsBy
+	 */
+	public function testSplitContentsBy(
+		string $input, $splitter, int $limit, array $expected
+	) {
+		$env = new MockEnv( [] );
+		$pt = new PegTokenizer( $env );
+		$pieces = $pt->tokenizeAs( $input, "preproc_pieces", false );
+		$kv = $pieces[0]->getContentsKV();
+		$source = $kv->srcOffsets->value->source;
+		$result = PreprocTk::splitContentsBy( $splitter, $kv, $limit );
+		foreach ( $result as $p ) {
+			$this->assertSame( $source, $p->srcOffsets->key->source );
+			$this->assertSame( $source, $p->srcOffsets->value->source );
+		}
+		$fmtOffsets = static fn ( $kvsr ) =>
+			"[" . $kvsr->value->start . "," . $kvsr->value->end . "] ";
+		$format = static fn ( $kv ) =>
+			$fmtOffsets( $kv->srcOffsets ) . PreprocTk::printContents( $kv );
+		$this->assertEquals( $expected, array_map( $format, $result ) );
+	}
+
+	public function provideSplitContentsBy() {
+		yield "Split empty string" => [
+			'{{}}',
+			'|',
+			-1,
+			[
+				// Always at least one item in the result
+				'[2,2] ',
+			],
+		];
+		yield "Split parser function on colon" => [
+			'{{#foo:bar|bat}}',
+			':',
+			1,
+			[
+				'[2,6] "#foo"',
+				'[6,7] ":"',
+				'[7,14] "bar|bat"'
+			],
+		];
+		yield "Split parser function on vertical bar" => [
+			'{{#foo:bar|bat|baz=back|limited}}',
+			'|',
+			2,
+			[
+				'[2,10] "#foo:bar"',
+				'[10,11] "|"',
+				'[11,14] "bat"',
+				'[14,15] "|"',
+				'[15,31] "baz=back|limited"',
+			],
+		];
+		yield "Split on whitespace with complex input" => [
+			'[[  File:Foo {{{1| }}}<pre x="y z"> ignore whitespace </pre>]]',
+			static fn ( $s, $l ) => preg_split( '/(\s+)/', $s, -1, PREG_SPLIT_DELIM_CAPTURE ),
+			-1,
+			[
+				'[2,2] ""',
+				'[2,4] "  "',
+				'[4,12] "File:Foo"',
+				'[12,13] " "',
+				<<<END
+				[13,60] {{{
+				  "1| "
+				}}}
+				<pre x="y z">
+				  " ignore whitespace "
+				</pre>
+				END,
+			],
+		];
+		yield "Split parser function on vertical bar w/ ext tag" => [
+			'{{#foo:bar|ext=<pre>foo|bar</pre>|pf={{pf|1|2}}}}',
+			'|',
+			-1,
+			[
+				'[2,10] "#foo:bar"',
+				'[10,11] "|"',
+				<<<END
+				[11,33] "ext="
+				<pre>
+				  "foo|bar"
+				</pre>
+				END,
+				'[33,34] "|"',
+				<<<END
+				[34,47] "pf="
+				{{
+				  "pf|1|2"
+				}}
+				END,
+			],
+		];
+		yield "Split template on vertical bar with heading" => [
+			// (headings have precedence!)
+			"{{1x|\n==Foo | Bar == <!-- x|y -->\t\nBat}}",
+			'|',
+			-1,
+			[
+				'[2,4] "1x"',
+				'[4,5] "|"',
+				<<<END
+				[5,38] "\\n"
+				==
+				  "Foo | Bar "
+				==
+				|" "
+				|<!--
+				|  " x|y "
+				|-->
+				|"\\t"
+				"\\n"
+				"Bat"
+				END,
+			],
+		];
+		yield "Split around a parsoid fragment marker" => [
+			"{{ Foo " . PipelineUtils::PARSOID_FRAGMENT_PREFIX . "123}} bar}}",
+			':',
+			-1,
+			[
+				<<<END
+				[2,36] " Foo "
+				<Parsoid Fragment 123>
+				" bar"
+				END,
+			],
+		];
+	}
+}
