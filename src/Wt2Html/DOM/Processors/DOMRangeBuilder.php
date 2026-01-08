2b263b49a1cb118925da2ab50a30f9c40ce2179d
Arlo Breault <abreault@wikimedia.org>
2026-01-07 20:59:11 -0500

    Stop using null as an array offset
    Fixes a warning on php@8.5:
    
    Deprecated: Using null as an array offset is deprecated, use an empty
    string instead in
    ./parsoid/src/Wt2Html/DOM/Processors/DOMRangeBuilder.php on line 335
    
    Change-Id: I5c91a261bcc0a829b8b4718e0160991f935cd2af
    (cherry picked from commit e1b9b1d5c8735aff5a235550d7b72816972f267c)

--- src/Wt2Html/DOM/Processors/DOMRangeBuilder.php
+++ src/Wt2Html/DOM/Processors/DOMRangeBuilder.php
@@ -346,7 +346,7 @@ class DOMRangeBuilder {
 	}
 
 	private static function findToplevelEnclosingRange(
-		array $nestingInfo, ?string $startId
+		array $nestingInfo, string $startId
 	): ?string {
 		// Walk up the implicit nesting tree to find the
 		// top-level range within which rId is nested.
@@ -360,7 +360,7 @@ class DOMRangeBuilder {
 			$visited[$rId] = true;
 			$rId = $nestingInfo[$rId];
 		}
-		return $rId;
+		return $rId === $startId ? null : $rId;
 	}
 
 	/**
@@ -601,8 +601,7 @@ class DOMRangeBuilder {
 			} );
 
 			$enclosingRangeId = self::findToplevelEnclosingRange(
-				$subsumedRanges,
-				$subsumedRanges[$r->id] ?? null
+				$subsumedRanges, $r->id
 			);
 			if ( $enclosingRangeId ) {
 				$this->env->log( "trace/{$this->traceType}/merge", '--nested in ', $enclosingRangeId, '--' );
