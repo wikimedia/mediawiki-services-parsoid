d79c17f03af7423c7c2dcc73d25a6f63a4b805e2
Otto Sleger <osleger@wikimedia.org>
2026-07-07 16:31:39 +0200

    Consts.php: Register math tag as inline element
    MathML uses math tag as capsule for the result, but parsoid takes it
    as a block tag, so it is always excluded from p-wraping. This should make
    the behaviour correct.
    
    Bug: T429711
    Change-Id: Ia65ae561fa3a8bb44760b223df989d13a8703194

--- src/Wikitext/Consts.php
+++ src/Wikitext/Consts.php
@@ -215,7 +215,7 @@ class Consts {
 				'a', 'abbr', 'acronym', 'applet', 'audio', 'b', 'basefont', 'bdi', 'bdo',
 				'big', 'br', 'button', 'cite', 'code', 'data', 'del', 'dfn', 'em',
 				'font', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label',
-				'legend', 'map', 'mark', 'object', 'param', 'q', 'rb', 'rbc', 'rp',
+				'legend', 'map', 'mark', 'math', 'object', 'param', 'q', 'rb', 'rbc', 'rp',
 				'rt', 'rtc', 'ruby', 's', 'samp', 'select', 'small', 'source', 'span',
 				'strike', 'strong', 'sub', 'sup', 'textarea', 'time', 'track', 'tt', 'u',
 				'var', 'video', 'wbr'
