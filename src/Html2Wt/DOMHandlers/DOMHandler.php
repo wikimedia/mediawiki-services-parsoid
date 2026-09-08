e86ac5a7d713eb82703b4ed7b9b1ccfb3445f2eb
Subramanya Sastry <ssastry@wikimedia.org>
2026-09-08 12:45:36 -0500

    html2wt: Get rid of non-actionable logging + use sensible fallbacks
    * This log is from the original Parsoid/JS days and was used to
      catch bugs and problems with clients.
    
    * At this point, this log has outlived its usefulness.
    
    * We should audit the codebase for any other such logs.
    
    Bug: T389801
    Change-Id: Idfa53ae4d55a19c6a2b9fa464a54dfe0b85b99c8

--- src/Html2Wt/DOMHandlers/DOMHandler.php
+++ src/Html2Wt/DOMHandlers/DOMHandler.php
@@ -4,7 +4,6 @@ declare( strict_types = 1 );
 namespace Wikimedia\Parsoid\Html2Wt\DOMHandlers;
 
 use LogicException;
-use Wikimedia\Parsoid\Core\DOMCompat;
 use Wikimedia\Parsoid\DOM\DocumentFragment;
 use Wikimedia\Parsoid\DOM\Element;
 use Wikimedia\Parsoid\DOM\Node;
@@ -226,10 +225,9 @@ class DOMHandler {
 							$res = $parentTypes[DOMUtils::nodeName( $parentNode )] . $res;
 						}
 					} else {
-						$state->getEnv()->log( 'error/html2wt', 'Input DOM is not well-formed.',
-							"Top-level <li> found that is not nested in <ol>/<ul>\n LI-node:",
-							DOMCompat::getOuterHTML( $node )
-						);
+						// Top-level <li> found that is not nested in <ol>/<ul>\n LI-node
+						// Default to UL
+						$res = $parentTypes['ul'] . $res;
 					}
 				} elseif ( !WTUtils::isLiteralHTMLNode( $node ) ) {
 					$res = $listTypes[$nodeName] . $res;
